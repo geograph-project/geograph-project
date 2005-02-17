1c5d19fae9972a26fb6dcd677e0686340e03aa2a
Paul Dixon <444004+lordelph@users.noreply.github.com>
2005-02-17 11:33:56 +0000

    basic working gridbuilder

--- public_html/admin/gridbuilder.php
+++ public_html/admin/gridbuilder.php
@@ -22,13 +22,45 @@
  */
 
 require_once('geograph/global.inc.php');
+require_once('geograph/gridshader.class.php');
 init_session();
 
 $USER->mustHavePerm("admin");
 
-
-
 $smarty = new GeographPage;
+
+
+//gather inputs
+$shader_image=isset($_POST['shader_image'])?$_POST['shader_image']:'admin/gb.png';
+$shader_x=isset($_POST['shader_x'])?$_POST['shader_x']:(54 + 206);
+$shader_y=isset($_POST['shader_y'])?$_POST['shader_y']:7;
+
+//do some processing?
+if (isset($_POST['shader']))
+{
+	//this takes a long time, so we output a header first of all
+	$smarty->display('_std_begin.tpl');
+	echo "<h3><a href=\"gridbuilder.php\">&lt;&lt;</a> Shading...</h3>";
+	flush();
+	set_time_limit(3600*24);
+	
+	//create shader and set it going!
+	$imgfile=$_SERVER['DOCUMENT_ROOT'].'/'.$shader_image;
+	$shader=new GridShader;
+
+	$shader->process($imgfile, $shader_x, $shader_y);
+	
+
+	//close output and exit (we don't want to output a page twice)
+
+	$smarty->display('_std_end.tpl');
+	exit;
+}
+
+
+$smarty->assign('shader_image', $shader_image);
+$smarty->assign('shader_x', $shader_x);
+$smarty->assign('shader_y', $shader_y);
 $smarty->display('gridbuilder.tpl');
 
 	
