f6b6eef782e2ad0a9e9379550bedc1abd603cbda
lordelph <lordelph@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2005-02-17 11:42:23 +0000

    new

---
+++ trunk/libs/geograph/gridshader.class.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id$
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 Paul Dixon (paul@elphin.com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+
+/**
+* Provides the GridShader class
+*
+* @package Geograph
+* @author Paul Dixon <paul@elphin.com>
+* @version $Revision$
+*/
+
+
+/**
+* Grid shader class
+*
+* Can process a PNG image of 1km pixels and create/update database grid squares
+* @package Geograph
+*/
+class GridShader
+{
+	var $db=null;
+	
+	/**
+	* internal function returns array of grid references by looking for
+	* suitable gridsquares in the gridprefix table.
+	*/
+	function _getGridRefs($x, $y)
+	{
+		$refs=array();
+		
+		//initialise refs with some dummy references - we guarantee to return something!
+		$dummy_ref=sprintf("#%d#%d", $x,$y);
+		$refs[1]=$dummy_ref;
+		$refs[2]=$dummy_ref;
+		
+		//find all grid boxes in which the coordinate falls
+		$sql="select reference_index,prefix,origin_x,origin_y from gridprefix where ".
+			"$x between origin_x and (origin_x+width-1) and ".
+			"$y between origin_y and (origin_y+height-1)";
+		
+		$recordSet = &$this->db->Execute($sql);
+		while (!$recordSet->EOF) 
+		{
+			$idx=$recordSet->fields[0];
+			$refs[$idx]=sprintf("%s%02d%02d", 
+				$recordSet->fields[1],
+				$x-$recordSet->fields[2],
+				$y-$recordSet->fields[3]);
+			
+			$recordSet->MoveNext();
+		}
+		$recordSet->Close(); 
+		
+		return $refs;
+	}
+	
+	/**
+	* adds or updates squares
+	*/
+	function process($imgfile, $x_offset, $y_offset)
+	{
+		if (file_exists($imgfile))
+		{
+			$img=imagecreatefrompng($imgfile);
+			if ($img)
+			{
+				$this->db = NewADOConnection($GLOBALS['DSN']);
+				if (!$this->db) die('Database connection failed');   
+						
+				
+				$imgw=imagesx($img);
+				$imgh=imagesy($img);
+
+				$this->_trace("Image is {$imgw}km x {$imgh}km");
+
+				$created=0;
+				$updated=0;
+				$untouched=0;
+				$skipped=0;
+				
+				$lastpercent=-1;
+				for ($imgy=0; $imgy<$imgh; $imgy++)
+				{
+					//output some progress
+					$percent=round(($imgy*100)/$imgh);
+					$percent=round($percent/5)*5;
+					if ($percent!=$lastpercent)
+					{
+						$this->_trace("{$percent}% completed...");
+						$lastpercent=$percent;
+					}
+					
+					for ($imgx=0; $imgx<$imgw; $imgx++)
+					{
+						//get colour of pixel 
+						//255=white, 0% land
+						//000=black, 100% land
+						$col=imagecolorat ($img, $imgx, $imgy);
+						$percent_land=round(((255-$col)*100)/255);
+						
+						//now lets figure out the internal grid ref
+						$gridx=$x_offset + $imgx;
+						$gridy=$y_offset + ($imgh-$imgy-1);
+						
+						//$this->_trace("img($imgx,$imgy) = grid($gridx,$gridy) $percent_land%");
+						
+						//ok, that's everything we need - can we obtain an existing grid square
+						$square = $this->db->GetRow("select grid_id,percent_land from gridsquare where x='$gridx' and y='$gridy'");	
+						
+						
+						if (is_array($square) && count($square))
+						{
+							if ($square['percent_land']!=$percent_land)
+							{
+								$sql="update gridsquare set percent_land='$percent_land' where gridsquare_id={$square['gridsquare_id']}";
+								$this->db->Execute($sql);
+								$updated++;
+							}
+							else
+							{
+								$untouched++;
+							}
+						}
+						else
+						{
+							//we only create squares for land
+							if ($percent_land>0)
+							{
+								//we don't have a gridsquare for this coordinate, so we must create one with a dummy
+								//grid reference
+								$gridrefs=$this->_getGridRefs($gridx,$gridy);
+
+								$sql="insert into gridsquare (grid_reference1,grid_reference2,x,y,percent_land) ".
+									"values('{$gridrefs[1]}','{$gridrefs[2]}',$gridx,$gridy,$percent_land)";
+								$this->db->Execute($sql);
+
+								$created++;
+							}
+							else
+							{
+								$skipped++;
+							}
+						}
+						
+			
+					}
+				
+				}
+
+				$this->_trace("$created new squares created");
+				$this->_trace("$updated squares updated with new land percentage");
+				$this->_trace("$untouched squares examined but left untouched");
+				$this->_trace("$skipped squares were all water and not created");
+
+				imagedestroy($img);
+			}
+			else
+			{
+				$this->_err("$imgfile is not a valid PNG"); 
+			}
+		}
+		else
+		{
+				$this->_err("$imgfile doesn't exist"); 
+		}
+	}
+	function _trace($msg)
+	{
+		echo "$msg<br/>";
+		flush();
+	}	
+	function _err($msg)
+	{
+		echo "<p><b>Error:</b> $msg</p>";
+		flush();
+	}
+	
+}
+
+?>
---
+++ trunk/libs/geograph/mapmaker.class.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id$
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 Paul Dixon (paul@elphin.com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+
+/**
+* Provides the MapMaker class
+*
+* @package Geograph
+* @author Paul Dixon <paul@elphin.com>
+* @version $Revision$
+*/
+
+
+/**
+* Map Maker
+*
+* Makes basic maps from grid square data
+* @package Geograph
+*/
+class MapMaker
+{
+	var $db=null;
+	
+	/**
+	* adds or updates squares
+	*/
+	function build($x1, $y1, $x2, $y2)
+	{
+		$this->db = NewADOConnection($GLOBALS['DSN']);
+		if (!$this->db) die('Database connection failed');   
+	
+	
+		//ensure coords are in right order
+		$left=min($x1, $x2);
+		$right=max($x1, $x2);
+		$top=max($y1, $y2);
+		$bottom=min($y1, $y2);
+	
+		//figure out filename
+		$filename="map_{$left}_{$top}_{$right}_{$bottom}.png";
+	
+		//figure out dimensions
+		$width=$right-$left;
+		$height=$top-$bottom;
+		
+		//create new image of appropriate size
+		$img=imagecreate($width,$height);
+		
+		$blue=imagecolorallocate ($img, 0,0,200);
+		imagefill($img,0,0,$blue);
+		
+		#get greens to use for percentages
+		$land=array();
+		for ($p=0; $p<=100; $p++)
+		{
+			//how much green?
+			$g=(200*$p)/100;
+			
+			//how much blue?
+			$b=200-$g;
+			
+			$land[$p]=imagecolorallocate($img, 0,$g,$b);
+		
+		}
+		
+		
+		//now plot all squares in the desired area
+		$sql="select x,y,percent_land from gridsquare where ".
+			"(x between $left and $right) and ".
+			"(y between $bottom and $top) and ".
+			"percent_land > 0";
+			
+		$recordSet = &$this->db->Execute($sql);
+		while (!$recordSet->EOF) 
+		{
+			$gridx=$recordSet->fields[0];
+			$gridy=$recordSet->fields[1];
+			
+			$imgx=$gridx-$left;
+			$imgy=$height-($gridy-$bottom);
+			
+			imagesetpixel($img, $imgx, $imgy, $land[$recordSet->fields[2]]);
+			
+			$recordSet->MoveNext();
+		}
+		$recordSet->Close(); 
+		
+		
+		//resize to half size
+		$scale=3.25;
+		$resized = imagecreatetruecolor($width*$scale,$height*$scale);
+		imagecopyresampled($resized, $img, 0, 0, 0, 0, 
+			$width*$scale,$height*$scale, $width, $height);
+
+		
+		
+		imagepng($resized, $_SERVER['DOCUMENT_ROOT'].'/maps/'.$filename);
+		imagedestroy($img);
+		imagedestroy($resized);
+	
+		return "/maps/$filename";
+	}
+
+	function _trace($msg)
+	{
+		echo "$msg<br/>";
+		flush();
+	}	
+	function _err($msg)
+	{
+		echo "<p><b>Error:</b> $msg</p>";
+		flush();
+	}
+	
+}
+
+?>
