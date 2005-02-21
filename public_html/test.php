2b89883679d68171d3fa90f701c0a9468e50d124
Paul Dixon <444004+lordelph@users.noreply.github.com>
2005-02-21 16:07:39 +0000

    added a lot more tests, still not comprehensive enough

--- public_html/test.php
+++ public_html/test.php
@@ -26,16 +26,65 @@
  * This file should test the local environment to ensure the geograph
  * application can run successfully
  */
-
 $ok=true;
 
-echo "<h1>Geograph System Test...</h1>";
-//do some tests
-if (!extension_loaded('gd'))
+function fail($msg)
+{
+	global $ok;
+	$ok=false;
+	echo "<li>$msg</li>";
+}
+
+function check_include($file)
 {
 	$ok=false;
-	echo "<li>PHP GD extension not available - REQUIRED</li>";
+	$f=@fopen ($file, 'r', true);	
+	if ($f)
+	{
+		fclose($f);
+		$ok=true;
+	}
+	return $ok;
 }
+
+echo "<h1>Geograph System Test...</h1>";
+
+//////////////////////////////////////////////////////////////////
+// general php configuration
+
+if (!extension_loaded('gd'))
+	fail('PHP GD extension not available - REQUIRED');
+
+if (!extension_loaded('exif'))
+	fail('PHP EXIF extension not available - REQUIRED');
+
+//////////////////////////////////////////////////////////////////
+// include files
+
+$inc=realpath($_SERVER['DOCUMENT_ROOT'].'/../libs');
+if (check_include('geograph/global.inc.php'))
+{
+	//include path is ok - let see if it contains the other stuff we need
+	if (!check_include('conf/'.$_SERVER['HTTP_HOST'].'.conf.php'))
+		fail('conf/'.$_SERVER['HTTP_HOST'].'conf.php not found - copy and adapt the www.example.com.conf.php file');
+	if (!check_include('adodb/adodb.inc.php'))
+		fail("ADOdb not found in $inc/adodb - download and install it there");
+	if (!check_include('smarty/libs/Smarty.class.php'))
+		fail("Smarty not found in $inc/smarty - download and install it there");
+}
+else
+{
+	fail($inc.' should be on include path');
+}
+
+//////////////////////////////////////////////////////////////////
+// directory permissions
+
+if (!is_writable($_SERVER['DOCUMENT_ROOT'].'/maps'))
+	fail('public_html/maps not writable - REQUIRED');
+
+if (!is_writable($_SERVER['DOCUMENT_ROOT'].'/photos'))
+	fail('public_html/photos not writable - REQUIRED');
 
 //show some diagnostics if not ok...
 if (!$ok)
