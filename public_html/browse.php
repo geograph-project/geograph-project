7ef0a93b8e5a50483ad59db14c1fb66c2a7f8452
Paul Dixon <444004+lordelph@users.noreply.github.com>
2005-02-21 16:06:32 +0000

    got basic browsing working

--- public_html/browse.php
+++ public_html/browse.php
@@ -22,12 +22,91 @@
  */
 
 require_once('geograph/global.inc.php');
+require_once('geograph/gridimage.class.php');
+require_once('geograph/gridsquare.class.php');
+//require_once('geograph/gridbrowser.class.php');
 init_session();
 
 
 
 
 $smarty = new GeographPage;
+//$browser=new GridBrowser;
+$square=new GridSquare;
+
+$smarty->assign('prefixes', $square->getGridPrefixes());
+$smarty->assign('kmlist', $square->getKMList());
+
+//we can be passed a gridreference as gridsquare/northings/eastings 
+//or just gridref. So lets initialise our grid square
+$grid_given=false;
+$grid_ok=false;
+if (isset($_GET['gridsquare']))
+{
+	$grid_given=true;
+	$grid_ok=$square->setGridPos($_GET['gridsquare'], $_GET['eastings'], $_GET['northings']);
+}
+
+if (isset($_GET['gridref']))
+{
+	$grid_given=true;
+	$grid_ok=$square->setGridRef($_GET['gridref']);
+}
+
+
+//process grid reference
+if ($grid_given)
+{
+	$square->rememberInSession();
+	
+
+	//preserve inputs in smarty
+	$smarty->assign('gridsquare', $square->gridsquare);
+	$smarty->assign('eastings', $square->eastings);
+	$smarty->assign('northings', $square->northings);
+	$smarty->assign('gridref', $square->gridref);
+	
+
+	//now we see if the grid reference is actually available...
+	if ($grid_ok)
+	{
+		//store details the browser manager has figured out
+		$smarty->assign('showresult', 1);
+		$smarty->assign('imagecount', $square->imagecount);
+		
+		//is this just a closest match?
+		if (is_object($square->nearest))
+		{
+			$smarty->assign('nearest_distance', $square->nearest->distance);
+			$smarty->assign('nearest_gridref', $square->nearest->gridref);
+		
+		}
+		
+		//otherwise, lets gether the info we need to display some thumbs
+		if ($square->imagecount)
+		{
+			$images=$square->getImages();
+			$smarty->assign_by_ref('images', $images);
+		}
+	
+	}
+	else
+	{
+		$smarty->assign('errormsg', $square->errormsg);
+		
+		
+		
+	}
+}
+else
+{
+	//no square specifed - populate with remembered values
+	$smarty->assign('gridsquare', $_SESSION['gridsquare']);
+	$smarty->assign('eastings', $_SESSION['eastings']);
+	$smarty->assign('northings', $_SESSION['northings']);
+	
+}
+
 $smarty->display('browse.tpl');
 
 	
