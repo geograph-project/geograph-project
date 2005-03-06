74009148ce17f6635bcc449406f21174093882ac
Paul Dixon <444004+lordelph@users.noreply.github.com>
2005-03-06 11:11:43 +0000

    added list of recent photos

--- public_html/index.php
+++ public_html/index.php
@@ -22,10 +22,21 @@
  */
 
 require_once('geograph/global.inc.php');
+require_once('geograph/gridimage.class.php');
+require_once('geograph/gridsquare.class.php');
+require_once('geograph/imagelist.class.php');
+
 init_session();
 
 
 $smarty = new GeographPage;
+
+//lets find some recent photos
+$images=new ImageList(array('pending', 'accepted'), 'submitted desc', 5);
+$images->assignSmarty(&$smarty, 'recent');
+
+
+
 $smarty->display("homepage.tpl");
 
 	
