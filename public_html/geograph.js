497a76630a8a5973976d7f7da50f31508006c301
Paul Dixon <444004+lordelph@users.noreply.github.com>
2005-03-10 16:12:19 +0000

    new

---
+++ public_html/geograph.js
@@ -0,0 +1,11 @@
+function popupOSMap(gridref)
+{
+        var wWidth = 740;
+        var wHeight = 520;
+        var wLeft = Math.round(0.5 * (screen.availWidth - wWidth));
+        var wTop = Math.round(0.5 * (screen.availHeight - wHeight)) - 20;
+        
+        var newWin = window.open('http://getamap.ordnancesurvey.co.uk/getamap/frames.htm?mapAction=gaz&gazName=g&gazString='+gridref, 
+		'gam',
+		'left='+wLeft+',screenX='+wLeft+',top='+wTop+',screenY='+wTop+',width='+wWidth+',height='+wHeight+',status,scrolling=no');
+}
