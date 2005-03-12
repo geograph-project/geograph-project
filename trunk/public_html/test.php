4b7f7fbb50fe28fb17c9f1ad273fc0a3067c87dc
barryhunter <barryhunter@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2005-03-12 00:10:57 +0000

    just testing commit...

--- trunk/public_html/test.php
+++ trunk/public_html/test.php
@@ -96,5 +96,5 @@ else
 {
 	echo "<li>Server is correctly configured to run Geograph!</li>";
 }
-//just adding this comment to test if can now commit... (Barry)
+//just adding this comment to test if can now commit... (Barry) another commit try!
 ?>
