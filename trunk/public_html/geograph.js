501980bf33d5cd2d6a484d3a5f02f5d7e9ebac00
barryhunter <barryhunter@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2005-03-13 16:04:08 +0000

    small mod to allow linking to getamap Without a location preselected

--- trunk/public_html/geograph.js
+++ trunk/public_html/geograph.js
@@ -4,8 +4,13 @@ function popupOSMap(gridref)
         var wHeight = 520;
         var wLeft = Math.round(0.5 * (screen.availWidth - wWidth));
         var wTop = Math.round(0.5 * (screen.availHeight - wHeight)) - 20;
-        
+        if (gridref.length > 0) {
         var newWin = window.open('http://getamap.ordnancesurvey.co.uk/getamap/frames.htm?mapAction=gaz&gazName=g&gazString='+gridref, 
 		'gam',
 		'left='+wLeft+',screenX='+wLeft+',top='+wTop+',screenY='+wTop+',width='+wWidth+',height='+wHeight+',status,scrolling=no');
+	} else {
+	var newWin = window.open('http://getamap.ordnancesurvey.co.uk/getamap/frames.htm', 
+		'gam',
+		'left='+wLeft+',screenX='+wLeft+',top='+wTop+',screenY='+wTop+',width='+wWidth+',height='+wHeight+',status,scrolling=no');
+	}
 }
