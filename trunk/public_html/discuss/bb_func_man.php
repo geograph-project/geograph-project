9783baa09cdaa9180e52fe162853f406f0a7198e
lordelph <lordelph@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2005-03-19 12:57:48 +0000

    removed extra styles from manual made script pull in a much reduced "user manual" rather than full miniBB manual

--- trunk/public_html/discuss/bb_func_man.php
+++ trunk/public_html/discuss/bb_func_man.php
@@ -12,65 +12,19 @@ $tplTmp=explode('<!--MANUAL-->',$tplTmp);
 $title.=$l_menu[4]; 
 $l_meta.=<<<out
 
-<style type="text/css">
-<!--
-P{
-font-family: Verdana,Arial,Helvetica,sans-serif;
-color: #000000;
-text-decoration: none;
-font-size: 11px;
-}
-
-
-SMALL{
-font-family: Verdana,Arial,Helvetica,sans-serif;
-color: #000000;
-text-decoration: none;
-font-size: 10px;
-}
-
-PRE{
-font-family: Helvetica,sans-serif;
-color: #000000;
-text-decoration: none;
-font-size: 12px;
-}
-
-H1{
-font-family: Verdana,Arial,Helvetica,sans-serif;
-color: #000000;
-text-decoration: none;
-font-size: 15px;
-}
-
-LI{
-font-family: Verdana,Arial,Helvetica,sans-serif;
-color: #000000;
-text-decoration: none;
-font-size: 11px;
-margin-top: 0px;
-margin-bottom: 0px;
-margin-right: 0px;
-margin-left: 15px;
-}
-
-UL{
-font-family: Verdana,Arial,Helvetica,sans-serif;
-color: #000000;
-text-decoration: none;
-font-size: 11px;
-margin-top: 0px;
-margin-bottom: 0px;
-margin-right: 15px;
-margin-left: 15px;
-}
-//-->
-</style>
 out;
 echo load_header();
 echo $tplTmp[0];
-if(file_exists($pathToFiles.'templates/manual_'.$lang.'.html')) include($pathToFiles.'templates/manual_'.$lang.'.html');
-elseif(file_exists($pathToFiles.'templates/manual_eng.html')) include($pathToFiles.'templates/manual_eng.html');
+
+$manbase="usermanual";
+$langpage=$pathToFiles.'templates/'.$manbase.'_'.$lang.'.html';
+$engpage=$pathToFiles.'templates/'.$manbase.'_eng.html';
+
+if(file_exists($manpage)) 
+	include($manpage);
+elseif(file_exists($engpage)) 
+	include($engpage);
+
 echo $tplTmp[1];
 
 ?>
