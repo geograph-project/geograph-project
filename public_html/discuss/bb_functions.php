0baf9af61809f10f3b171d100cfb8a4f1ab982c0
Paul Dixon <444004+lordelph@users.noreply.github.com>
2005-03-21 21:27:52 +0000

    made unsubscribe link clearer

--- public_html/discuss/bb_functions.php
+++ public_html/discuss/bb_functions.php
@@ -183,7 +183,7 @@ $true3=($true3a or $true3b);
 if ($true0 and $true1 and $true2 and $true3) {
 $checkEmail="<input type=\"checkbox\" id=\"CheckSendMail\" name=\"CheckSendMail\"><label for=\"CheckSendMail\">{$GLOBALS['l_emailNotify']}</label>";
 }
-elseif($isInDb) $checkEmail="<!--U--><a href=\"{$GLOBALS['indexphp']}action=unsubscribe&topic={$GLOBALS['topic']}&usrid={$GLOBALS['user_id']}\">{$GLOBALS['l_unsubscribe']}</a>";
+elseif($isInDb) $checkEmail="<!--U-->{$GLOBALS['l_unsubscribeinfo']}<a title=\"{$GLOBALS['l_unsubscribe']}\" href=\"{$GLOBALS['indexphp']}action=unsubscribe&amp;topic={$GLOBALS['topic']}&amp;usrid={$GLOBALS['user_id']}\">{$GLOBALS['l_unsubscribe']}</a>";
 }
 return $checkEmail;
 }
