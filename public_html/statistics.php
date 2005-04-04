db6a2d39505fd8bcb6db81da7b9d3d441ba8eac2
barryhunter <1413156+barryhunter@users.noreply.github.com>
2005-04-04 09:15:57 +0000

    fleshed out the page

--- public_html/statistics.php
+++ public_html/statistics.php
@@ -4,7 +4,7 @@
  * $Id$
  * 
  * GeoGraph geographic photo archive project
- * This file copyright (C) 2005 Paul Dixon (paul@elphin.com)
+ * This file copyright (C) 2005 Barry Hunter (geo@barryhunter.co.uk)
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -24,10 +24,132 @@
 require_once('geograph/global.inc.php');
 init_session();
 
-
-
-
 $smarty = new GeographPage;
+
+$by = $_GET['by'];
+$ri = intval($_GET['ri']);
+$u = intval($_GET['u']);
+$order = $_GET['order'];
+
+$template='statistics.tpl';
+$cacheid=$by.'_'.$ri.'_'.$u.'_'.$order;
+
+if (!$smarty->is_cached($template, $cacheid))
+{
+	$db=NewADOConnection($GLOBALS['DSN']);
+	if (!$db) die('Database connection failed');  
+	#$db->debug = true;
+	$smarty->assign('references',array(1 => 'Great Britain',2 => 'Ireland'));
+	
+
+	$bys = array('type' => 'Image Type','class' => 'Category','gridsq' => 'Grid Square');
+
+	$smarty->assign_by_ref('bys',$bys);
+	
+	$smarty->assign('by', $by);
+	
+	
+	if ($by) {
+		$smarty->assign('title', $bys[$by]);
+		if (!$ri)
+			$ri = 1;
+		$smarty->assign('ri', $ri);
+		$letterlength = 3 - $ri; #should this be auto-realised by selecting a item from gridprefix?
+		
+		if ($by == 'type') {
+			$sql_group = $sql_fieldname = 'moderation_status';
+		} else if ($by == 'class') {
+			$sql_group = $sql_fieldname = 'imageclass';
+		} else if ($by == 'gridsq') {
+			$sql_group = $sql_fieldname = "SUBSTRING(grid_reference,1,$letterlength)";
+		}
+		
+		$link .= "by=$by&amp;ri=$ri";
+		
+		if ($u) {
+			$user_crit = " and user_id = $u";
+			$link = "&amp;u=$u";
+			$smarty->assign_by_ref('u', $u);
+		}
+		$smarty->assign_by_ref('link', $link);
+		
+		
+		
+		if (strpos($order,'2') !== FALSE) {
+			$sql_dir = " DESC";
+		} else {
+		    $no .= "2";
+		}
+		$smarty->assign_by_ref('no', $no);
+		
+		if (strpos($order,'c') !== FALSE) {
+			$sql_order = "ORDER BY c $sql_dir";
+		} else {
+			$sql_order = "ORDER BY field $sql_dir";
+		}
+		
+$sql = "select 
+$sql_fieldname as field,
+count(distinct(gridimage_id)) as c 
+from gridimage inner join gridsquare using (gridsquare_id) 
+where reference_index = $ri $user_crit
+$andwhere
+group by $sql_group 
+$sql_order";
+		
+		$breakdown=$db->GetAll($sql);
+		
+		foreach($breakdown as $idx=>$entry) {
+			$total += $breakdown[$idx]['c'];
+		}
+		$totalperc = 100 /$total;
+		
+		foreach($breakdown as $idx=>$entry)
+		{
+			$breakdown[$idx]['per'] = sprintf("%.2f",$breakdown[$idx]['c'] * $totalperc);
+			if (!$breakdown[$idx]['field'])
+				$breakdown[$idx]['field'] = "<i>-unspecified-</i>";
+		}
+		
+		if ($by == 'type') {
+			$friendly = array('rejected' => 'Rejected', 'pending' => 'Pending', 'accepted' => 'Supplemental', 'geograph' => 'Geograph');
+			foreach($breakdown as $idx=>$entry) {
+				$breakdown[$idx]['field'] = $friendly[$breakdown[$idx]['field']];
+			}
+		}
+		
+		$smarty->assign_by_ref('total', $total);
+		$smarty->assign_by_ref('breakdown', $breakdown);
+	} else {
+
+		
+
+		$smarty->assign('users_submitted',  $db->GetOne("select count(distinct user_id) from gridimage"));
+		$smarty->assign('users_total',  $db->GetOne("select count(*) from user where rights>0"));
+		$smarty->assign('users_thisweek',  $db->GetOne("select count(*) from user where rights>0 and (unix_timestamp(now())-unix_timestamp(signup_date))<604800"));
+
+		$smarty->assign("images_ftf",  $db->GetOne("select count(*) from gridimage where ftf = 1"));
+
+
+		foreach (array(1,2) as $ri) {
+			$letterlength = 3 - $ri; #should this be auto-realised by selecting a item from gridprefix?
+			
+			$smarty->assign("images_total_$ri",  $db->GetOne("select count(*) from gridimage inner join gridsquare using (gridsquare_id) where reference_index = $ri"));
+			$smarty->assign("images_thisweek_$ri",  $db->GetOne("select count(*) from gridimage inner join gridsquare using (gridsquare_id) where reference_index = $ri and (unix_timestamp(now())-unix_timestamp(submitted))<604800"));
+			
+			$smarty->assign("squares_total_$ri",  $db->CacheGetOne(100*24*3600,"select count(*) from gridsquare where reference_index = $ri and percent_land > 0"));
+			$smarty->assign("squares_submitted_$ri",  $db->GetOne("select count(*) from gridsquare where reference_index = $ri and imagecount > 0"));
+
+			$smarty->assign("geographs_submitted_$ri",  $db->GetOne("select count(*) from gridsquare where reference_index = $ri and has_geographs > 0"));
+
+			$smarty->assign("grid_total_$ri",  $db->CacheGetOne(100*24*3600,"select count(*) from gridprefix where reference_index = $ri and landcount > 0"));
+			$smarty->assign("grid_submitted_$ri",  $db->GetOne("select count(distinct substring(grid_reference,1,$letterlength)) from gridimage inner join gridsquare using (gridsquare_id) where reference_index = $ri"));
+		}
+	}
+	$smarty->assign("gentime",date("D, d M Y H:i:s"));
+}
+
+
 $smarty->display('statistics.tpl');
 
 	
--- public_html/templates/basic/statistics.tpl
+++ public_html/templates/basic/statistics.tpl
@@ -1,8 +1,114 @@
+{assign var="page_title" value="Statistics"}
 {include file="_std_begin.tpl"}
 
-    <h2>Statistics</h2>
- 
-    To be implemented - come back soon!
+{if !$by}
+    <h2>Overview Statistics for Geograph British Isles</h2>
+
+{dynamic}
+<ul>
+<li><b>{$users_submitted}/{$users_total}</b> of Registered Users have Submitted Images</li>
+<li><b>{$users_thisweek}</b> new users in past 7 days</li>
+<li>A total of <b>{$images_ftf}</b> Geograph Points have been awarded</p>
+</ul>
+
+<table border="1" cellpadding="4" cellspacing="0" class="statstable">
+<thead><tr>
+	<th>&nbsp;</th>
+	<th>Great Britain</th>
+	<th>Ireland</th>
+	<th><i>Total</i></th>
+</tr></thead>
+<tbody><tr>
+	<th>Images Submitted</th>
+	<td><b>{$images_total_1}</b></td>
+	<td><b>{$images_total_2}</b></td>
+	<td>{$images_total_1+$images_total_2}</td>
+</tr>
+<tr>
+	<th>&nbsp;[in past 7 days]</th>
+	<td><b>{$images_thisweek_1}</b></td>
+	<td><b>{$images_thisweek_2}</b></td>
+	<td>{$images_thisweek_1+$images_thisweek_2}</td>
+</tr>
+<tr>
+	<th>1km<sup>2</sup> Grid Squares</th>
+	<td><b>{$squares_submitted_1}</b><br/>/{$squares_total_1}</td>
+	<td><b>{$squares_submitted_2}</b><br/>/{$squares_total_2}</td>
+	<td valign="top">{$squares_submitted_1+$squares_submitted_2}<br/>/{$squares_total_1+$squares_total_2}</td>
+</tr>
+<tr>
+	<th>&nbsp;[with geograph(s)]</th>
+	<td><b>{$geographs_submitted_1}</b></td>
+	<td><b>{$geographs_submitted_2}</b></td>
+	<td>{$geographs_submitted_1+$geographs_submitted_2}</td>
+</tr>
+<tr>
+	<th>100km<sup>2</sup> Grid Squares</th>
+	<td><b>{$grid_submitted_1}</b><br/>/{$grid_total_1}</td>
+	<td><b>{$grid_submitted_2}</b><br/>/{$grid_total_2}</td>
+	<td>{$grid_submitted_1+$grid_submitted_2}<br/>/{$grid_total_1+$grid_total_2}</td>
+</tr></tbody>
+</table>
+{/dynamic}
     
+    <h3>More Statistics</h3>
     
+{/if}
+    <form method="get" action="{$script_name}">
+    <p>View breakdown of images by 
+    <select name="by">
+    	{html_options options=$bys selected=$by}
+    </select> for <select name="ri">
+    	{html_options options=$references selected=$ri}
+    </select> 
+    
+    {if $user->registered}
+	<select name="u">
+		{if $u && $u != $user->user_id}
+			<option value="{$u}">Just for Selected User</option>
+		{/if}
+		<option value="{$user->user_id}">Just for {$user->realname}</option>
+		<option value="" {if !$u} selected{/if}>For Everyone</option>
+	</select>
+    {else}
+	{if $u}
+	<select name="u">
+		<option value="{$u}">Just for Selected User</option>
+		<option value="">For Everyone</option>
+	</select>
+	{/if}
+    {/if}
+    <input type="submit" value="Go"></p></form>
+    
+
+    
+{if $by}
+	<p><small>Click a column header to change the sort order.</small></p>
+{dynamic}
+	<table class="report">
+	<thead><tr>
+	<td><a href="{$script_name}?{$link}&amp;order={$no}">{$title}</a></td>
+	<td><a href="{$script_name}?{$link}&amp;order=c{$no}">Number</a></td>
+	<td>Percentage</td></tr></thead>
+	<tbody>
+
+	{foreach from=$breakdown item=line}
+	<tr><td>{$line.field}</td>
+	<td align=right>{$line.c}</td>
+	<td align=right>{$line.per}%</td></tr>
+	{/foreach}
+	
+	<tr class="totalrow"><td>&nbsp;</td>
+	<th align=right>{$total}</th>
+	<th align=right>100%</th></tr>
+	</tbody>
+	</table>
+{/dynamic}
+{else} 
+    <p>Here's a graph of photo submissions since we began...<br/>
+    <img src="/img/submission_graph.png" width="480" height="161"/>
+    </p> 
+{/if}
+	<p align="center"><i>This page was last updated {$gentime}</i>.</p>
+
 {include file="_std_end.tpl"}
