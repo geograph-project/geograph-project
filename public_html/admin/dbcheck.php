a6bf579c959a0a89aa76fc30e56a3d9925cd6e2d
Paul Dixon <444004+lordelph@users.noreply.github.com>
2005-04-06 13:43:30 +0000

    new

---
+++ public_html/admin/dbcheck.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id$
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 Paul Dixon (paul@elphin.com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once('geograph/global.inc.php');
+require_once('geograph/gridshader.class.php');
+init_session();
+
+$USER->mustHavePerm("admin");
+
+$smarty = new GeographPage;
+$smarty->debugging=false;
+
+
+
+//do some processing?
+if (isset($_POST['check']))
+{
+	//this takes a long time, so we output a header first of all
+	$smarty->display('_std_begin.tpl');
+	echo "<h2><a title=\"Admin home page\" href=\"/admin/index.php\">Admin</a> :Performing Database Check...</h2>";
+	flush();
+	set_time_limit(3600*24);
+	
+	$db = NewADOConnection($GLOBALS['DSN']);
+	if (!$db) die('Database connection failed'); 
+	
+	echo "<h3>Database tables</h3><table class=\"report\">";
+	echo "<thead><tr><td>Table</td><td>Status</td></tr></thead><tbody>";
+	$tables=$db->MetaTables();
+	foreach ($tables as $table)
+	{
+		$result=$db->GetRow("check table $table");
+		echo "<tr><td>$table</td><td>{$result[2]}:{$result[3]}</td></tr>\n";
+		flush();
+		
+	}
+	echo "</tbody></table>\n";
+	flush();
+	
+	echo "<h3>Gridsquare Integrity Check</h3>\n";
+	echo "<p>Here we check how many images we have for a gridsquare and see if it tallies ".
+		"with the cached count in gridsquare.imagecount. ".
+		"We also check if the gridsquare.has_geographs value is sane too.</p>";
+	echo "<p>This can take some time....<span id=\"completed\"></span></p>\n";
+	echo "<script language=\"javascript\">completed=document.getElementById('completed');</script>\n";
+	echo "<ul>";
+	
+	flush();
+	
+	$total=$db->GetOne("select count(*) from gridsquare");
+	$last_percent=0;
+	
+	$count=0;
+	$recordSet = &$db->Execute("select * from gridsquare");
+	while (!$recordSet->EOF) 
+	{
+		$count++;
+		$percent=round(($count*100)/$total);
+		if ($percent!=$last_percent)
+		{
+			$last_percent=$percent;
+			echo "<script language=\"javascript\">completed.innerHTML='$percent% completed';</script>\n";
+			flush();
+		}
+		
+		$gridsquare_id=$recordSet->fields['gridsquare_id'];
+		$grid_reference=$recordSet->fields['grid_reference'];
+		$cached_count=$recordSet->fields['imagecount'];
+		$has_geographs=$recordSet->fields['has_geographs'];
+		
+		$realcount=$db->GetOne("select count(*) from gridimage where ".
+			"gridsquare_id=$gridsquare_id and moderation_status<>'rejected'");
+		$geographcount=$db->GetOne("select count(*) from gridimage where ".
+					"gridsquare_id=$gridsquare_id and moderation_status='geograph'");
+		$real_has_geographs=($geographcount>0)?1:0;
+		
+		if ($cached_count!=$realcount)
+		{
+			echo "<li>gridsquare $gridsquare_id (<a href=\"/gridref/$grid_reference\">{$grid_reference}</a>) ".
+				"has imagecount $cached_count, should be $realcount ";
+			if (isset($_POST['fix']))
+			{
+				$db->Execute("update gridsquare set imagecount=$realcount where gridsquare_id=$gridsquare_id");
+				echo "[fixed]";
+			}
+			
+			echo "</li>";
+			
+			
+			flush();
+		}
+		
+		if ($real_has_geographs!=$has_geographs)
+		{
+			echo "<li>gridsquare $gridsquare_id (<a href=\"/gridref/$grid_reference\">{$grid_reference}</a>) ".
+				"has has_geographs $has_geographs, should be $real_has_geographs";
+			if (isset($_POST['fix']))
+			{
+				$db->Execute("update gridsquare set has_geographs=$real_has_geographs where gridsquare_id=$gridsquare_id");
+				echo "[fixed]";
+			}
+			
+			echo "</li>";
+			flush();
+		}
+		
+		$recordSet->MoveNext();
+	}
+	echo "</ul>";
+	$recordSet->Close(); 
+	echo "<script language=\"javascript\">completed.innerHTML='Completed';</script>\n";
+			
+	
+	
+	$smarty->display('_std_end.tpl');
+	exit;
+}
+
+
+
+$smarty->display('dbcheck.tpl');
+
+	
+?>
---
+++ public_html/templates/basic/dbcheck.tpl
@@ -0,0 +1,17 @@
+{assign var="page_title" value="database check"}
+{include file="_std_begin.tpl"}
+{dynamic}
+
+<h2><a title="Admin home page" href="/admin/index.php">Admin</a> :Database Check</h2>
+<p>This tool will analyse the database tables for corruption at the
+db server level, and also perform some application level sanity checks</p>
+
+<form method="post" action="dbcheck.php">
+<input type="checkbox" checked="checked" name="fix" value="1" id="fix"/>
+<label for="fix">Fix problems if possible</label><br/>
+
+<input type="submit" name="check" value="Perform Database Check"/>
+</form>
+
+{/dynamic}    
+{include file="_std_end.tpl"}
