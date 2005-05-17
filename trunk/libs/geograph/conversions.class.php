d3bcca312008c69b3b54e173e8ab78333797d1a2
barryhunter <barryhunter@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2005-05-17 23:28:24 +0000

    new

---
+++ trunk/libs/geograph/conversions.class.php
@@ -0,0 +1,247 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id$
+ *
+ * GeoGraph geographic photo archive project
+ * http://geograph.sourceforge.net/
+ *
+ * This file copyright (C) 2005 Barry Hunter (geo@barryhunter.co.uk)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/**
+* Perform coordinate conversion for use within Geograph
+*
+* @package Geograph
+* @author Barry Hunter <geo@barryhunter.co.uk>
+* @version $Revision$
+*/
+
+	
+class Conversions
+{		
+//use:	list($x,$y,$reference_index) = wgs84_to_internal($lat,$long);
+		//with reference_index deduced from the location and the approraite conversion used
+
+function wgs84_to_internal($lat,$long) {
+	require_once('geograph/conversionslatlong.class.php');
+	$conv = new ConversionsLatLong;
+
+	//todo
+}
+
+
+//use:	list($e,$n,$reference_index) = wgs84_to_national($lat,$long);
+		//with reference_index deduced from the location and the approraite conversion used
+
+function wgs84_to_national($lat,$long,$usehermert = true) {
+	require_once('geograph/conversionslatlong.class.php');
+	$conv = new ConversionsLatLong;
+	$ire = ($lat > 51.2 && $lat < 55.73 && $long > -12.2 && $long < -4.8);
+	$uk = ($lat > 49 && $lat < 62 && $long > -9.5 && $long < 2.3);
+	if ($ire) {
+		return array_merge($conv->wgs84_to_irish($lat,$long,$usehermert),array(2));
+	} else if ($uk) {
+		return array_merge($conv->wgs84_to_osgb36($lat,$long),array(1));
+	}
+}
+
+
+//use:	list($lat,$long) = internal_to_wgs84($x,$y,$reference_index = 0);
+		//reference_index is optional as we can duduce this (but if known then can pass it in to save having to recaluate)
+			//will probably just call national_to_wgs84 once converted
+
+function internal_to_wgs84($x,$y,$reference_index = 0) {
+	require_once('geograph/conversionslatlong.class.php');
+	$conv = new ConversionsLatLong;
+
+	//todo
+}
+
+
+//use:	list($lat,$long) = national_to_wgs84($e,$n,$reference_index);
+
+function national_to_wgs84($e,$n,$reference_index,$usehermert = true) {
+	require_once('geograph/conversionslatlong.class.php');
+	$conv = new ConversionsLatLong;
+	$latlong = array();
+	if ($reference_index == 1) {
+		$latlong = $conv->osgb36_to_wgs84($e,$n);
+	} else if ($reference_index == 2) {
+		$latlong = $conv->irish_to_wgs84($e,$n,$usehermert);
+	}
+	return $latlong;
+}
+
+
+//use:	list($lat,$long) = gridsquare_to_wgs84(&$gridsquare);
+			//will contain nateastings/natnorthings  or can call getNationalEastings to get them
+
+function gridsquare_to_wgs84(&$gridsquare) {
+	if (!$gridsquare->nateastings)
+		$gridsquare->getNatEastings();
+	return $this->national_to_wgs84($gridsquare->nateastings,$gridsquare->natnorthings,$gridsquare->reference_index);
+}
+
+//--------------------------------------------------------------------------------
+// convenence functions
+
+//use:    $gr = internal_to_gridref($x,$y,$gr_length,$reference_index = 0);
+         //reference_index is optional as we can duduce this
+
+function internal_to_gridref($x,$y,$gr_length,$reference_index = 0) {
+	list($e,$n,$reference_index) = $this->internal_to_national($x,$y,$reference_index);
+
+	return $this->national_to_gridref($e-500,$n-500,$gr_length,$reference_index);
+}
+
+
+//use:    list($gr,$len) = national_to_gridref($e,$n,$gr_length,$reference_index);
+
+function national_to_gridref($e,$n,$gr_length,$reference_index) {
+	list($x,$y) = $this->national_to_internal($e,$n,$reference_index );
+
+	$db = $this->_getDB();
+
+	$sql="select prefix from gridprefix ".
+		"where $x between origin_x and (origin_x+width-1) and ".
+		"$y between origin_y and (origin_y+height-1) and reference_index=$reference_index";
+	$prefix=$db->GetOne($sql);
+
+	$eastings = sprintf("%05d",$e % 100000);
+	$northings = sprintf("%05d",$n % 100000);
+	
+
+	if ($len) {
+		$len = intval($gr_length/2);
+	} else {
+		//try to work out the shortest grid ref length
+		$east = preg_replace("/^(\d+?)0*$/",'$1',$eastings);
+		$north = preg_replace("/^(\d+?)0*$/",'$1',$northings);
+		$len = max(strlen($east),strlen($north),2);
+	}
+	
+	$eastings = substr($eastings,0,$len);
+	$northings = substr($northings,0,$len);
+	return array($prefix.$eastings.$northings,$len);
+}
+
+//use:    list($x,$y) = national_to_internal($e,$n,$reference_index );
+
+function national_to_internal($e,$n,$reference_index ) {
+	global $CONF;
+	$x = intval($e / 1000);
+	$y = intval($n / 1000);
+	
+	//add the internal origin
+	$x += $CONF['origins'][$reference_index][0];
+	$y += $CONF['origins'][$reference_index][1];
+	return array($x,$y);
+}
+
+
+//use:    list($e,$n,$reference_index) = internal_to_national($x,$y,$reference_index = 0);
+// note gridsquare has its own version that takes into account the userspecified easting/northing
+function internal_to_national($x,$y,$reference_index = 0) {
+	global $CONF;
+	if (!$reference_index) {
+		$db = $this->_getDB();
+		
+		$reference_index=$db->GetOne("select reference_index from gridsquare where x=$x and y=$y");
+		
+		//But what to do when the square is not on land??
+		
+		if (!$reference_index) {
+			//when not on land just try any square!
+			// but favour the _smaller_ grid - works better, but still not quite right where the two grids almost overlap
+			$where_crit =  "order by reference_index desc";
+		
+			$sql="select reference_index from gridprefix ".
+				"where $x between origin_x and (origin_x+width-1) and ".
+				"$y between origin_y and (origin_y+height-1) $where_crit";
+			$reference_index=$db->GetOne($sql);
+		}
+	}
+
+	if ($reference_index) {
+		//remove the internal origin
+		$x -= $CONF['origins'][$reference_index][0];
+		$y -= $CONF['origins'][$reference_index][1];
+
+		//lets position the national coords in the center of the square!
+		$e = intval($x * 1000 + 500);
+		$n = intval($y * 1000 + 500);
+		return array($e,$n,$reference_index);
+	} else {
+		return array();
+	}
+}
+
+
+//use:    list($x,$y,$reference_index) = gridref_to_internal($gr);
+
+//use:    list($e,$n,$reference_index) = gridref_to_national($gr);
+
+//use:	list($x,$y) = alignInternalToNationalLines($x,$y,$reference_index = 0);
+	 //reference_index is optional as we can duduce this
+	 // for mosaic->setAlignedOrigin to handle the hardcoded alignments
+
+//use:	list($e,$n) = osgb36_to_irish($e,$n);
+			// this is used when we have a dataset in osgb and need to convert it to irish national (eg loc_placenames etc)
+
+function wgs84_to_friendly($lat,$long) {
+	$el = ($long > 0)?'E':'W';
+	$nl = ($lat > 0)?'N':'S';
+	
+	$xd = intval(abs($long));
+	$xm = intval((abs($long)-$xd)*60);
+	$xs = (abs($long)*3600)-($xm*60)-($xd*3600);
+
+	$yd = intval(abs($lat));
+	$ym = intval((abs($lat)-$yd)*60);
+	$ys = (abs($lat)*3600)-($ym*60)-($yd*3600);
+
+	$ymd = sprintf("%.4f",$ym+($ys/60));
+	$xmd = sprintf("%.4f",$xm+($xs/60));
+	
+	return array("$yd:$ymd$nl","$xd:$xmd$el");
+}
+
+//----------------------------------------------------------------
+
+	/**
+	 * get stored db object, creating if necessary
+	 * @access private
+	 */
+	function &_getDB()
+	{
+		if (!is_object($this->db))
+			$this->db=NewADOConnection($GLOBALS['DSN']);
+		if (!$this->db) die('Database connection failed');
+		return $this->db;
+	}
+
+	/**
+	 * set stored db object
+	 * @access private
+	 */
+	function _setDB(&$db)
+	{
+		$this->db=$db;
+	}
+
+}
