cf9636f8d4acff079ac09d9921be264ea355522d
barryhunter <barryhunter@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2005-05-30 01:33:15 +0000

    split GB & I 10km squares into two tables which now make sence!

--- trunk/public_html/statistics/most_geographed.php
+++ trunk/public_html/statistics/most_geographed.php
@@ -40,8 +40,11 @@ if (!$smarty->is_cached($template, $cacheid))
 	$db=NewADOConnection($GLOBALS['DSN']);
 	if (!$db) die('Database connection failed');  
 	#$db->debug = true;
-	
-	$both = array();
+
+$mosaic=new GeographMapMosaic;
+$mosaic->setScale(40);
+$mosaic->setMosaicFactor(2);
+
 	
 	foreach (array(1,2) as $ri) {
 		$letterlength = 3 - $ri; #should this be auto-realised by selecting a item from gridprefix?
@@ -51,65 +54,41 @@ if (!$smarty->is_cached($template, $cacheid))
 			
 		$most = $db->GetAll("select grid_reference,x,y,concat(substring(grid_reference,1,".($letterlength+1)."),substring(grid_reference,".($letterlength+3).",1)) as tenk_square,count(*) as geograph_count from gridsquare where reference_index = $ri and imagecount>0 group by tenk_square having geograph_count > 1 order by geograph_count desc limit 50");
 		
+		$i = 1;
 		foreach($most as $id=>$entry) 
 		{
 			$most[$id]['x'] = ( intval(($most[$id]['x'] - $origin['origin_x'])/10)*10 ) +  $origin['origin_x'];
 			$most[$id]['y'] = ( intval(($most[$id]['y'] - $origin['origin_y'])/10)*10 ) +  $origin['origin_y'];
-		}
-		
-		$both = array_merge($both,$most);
-	}
-	
-	function cmp_perc($a, $b) {
-			global $both;
-			if ($both[$a]['geograph_count'] == $both[$b]['geograph_count']) {
-					 return 0;
+
+			if ($lastgeographs == $most[$id]['geograph_count'])
+				$most[$id]['ordinal'] = '&quot;&nbsp;&nbsp;&nbsp;';
+			else {
+
+				$units=$i%10;
+				switch($units)
+				{
+					case 1:$end=($i==11)?'th':'st';break;
+					case 2:$end=($i==12)?'th':'nd';break;
+					case 3:$end=($i==13)?'th':'rd';break;
+					default: $end="th";	
+				}
+
+				$most[$id]['ordinal']=$i.$end;
+				$lastgeographs = $most[$id]['geograph_count'];
 			}
-			return ($both[$a]['geograph_count'] > $both[$b]['geograph_count']) ? -1 : 1;
+			$i++;
+
+
+			//get a token to show a suroudding geograph map
+			$mosaic->setOrigin($most[$id]['x'],$most[$id]['y']);
+
+			$most[$id]['map_token'] = $mosaic->getToken();
+		}	
+		$smarty->assign("most$ri", $most);	
 	}
 
-	uksort($both, "cmp_perc"); 
-
-$mosaic=new GeographMapMosaic;
-$mosaic->setScale(40);
-$mosaic->setMosaicFactor(2);
-
-		
-
-
-	$i = 1;
-	foreach($both as $id=>$entry)
-	{
-		if ($lastgeographs == $both[$id]['geograph_count'])
-			$both[$id]['ordinal'] = '&quot;&nbsp;&nbsp;&nbsp;';
-		else {
-
-			$units=$i%10;
-			switch($units)
-			{
-				case 1:$end=($i==11)?'th':'st';break;
-				case 2:$end=($i==12)?'th':'nd';break;
-				case 3:$end=($i==13)?'th':'rd';break;
-				default: $end="th";	
-			}
-
-			$both[$id]['ordinal']=$i.$end;
-			$lastgeographs = $both[$id]['geograph_count'];
-		}
-		$i++;
-		
-		
-		//get a token to show a suroudding geograph map
-		$mosaic->setOrigin($both[$id]['x'],$both[$id]['y']);
-		
-		$both[$id]['map_token'] = $mosaic->getToken();
 	
-		
-	}
-
-	$smarty->assign_by_ref("both", $both);	
-	
-	$onekm = $db->GetAll("select grid_reference,imagecount from gridsquare where imagecount>1 order by imagecount desc limit 75");
+	$onekm = $db->GetAll("select grid_reference,imagecount from gridsquare where imagecount>1 order by imagecount desc limit 50");
 		
 	$i = 1;
 	foreach($onekm as $id=>$entry)
