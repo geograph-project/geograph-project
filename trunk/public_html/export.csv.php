ee5194e298dd731813dd415ca7a2883595d41d98
barryhunter <barryhunter@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2005-06-26 12:30:34 +0000

    new: requires valid api-key to access

--- trunk/public_html/export.csv.php
+++ trunk/public_html/export.csv.php
@@ -23,11 +23,26 @@
 
 require_once('geograph/global.inc.php');
 
+$db=NewADOConnection($GLOBALS['DSN']);
+
+if (!$_GET['key'] || preg_match("/[^\w]/",$_GET['key']))
+	die("ERROR: no api key");
+	
+$sql = "SELECT * FROM `apikeys` WHERE `key` = '{$_GET['key']}' AND (`ip` = INET_ATON('{$_SERVER['REMOTE_ADDR']}') OR `ip` = 0) AND `enabled` = 'Y'";
+
+$profile = $db->GetRow($sql);
+
+if (!$profile['key'])
+	die("ERROR: invalid api key. contact support at geograph dot co dot uk");
+
+
 # let the browser know what's coming
 header("Content-type: application/octet-stream");
 header("Content-Disposition: attachment; filename=\"geograph.csv\"");
 
-$db=NewADOConnection($GLOBALS['DSN']);
+
+
+
 
 echo "Id,Name,Grid Ref,Submitter,Image Class";
 if ($_GET['ll']) {
@@ -42,15 +57,35 @@ if ($_GET['ll']) {
 if ($_GET['thumb']) {
 	require_once('geograph/gridimage.class.php');
 	$gridimage = new GridImage;
+	$sql_from = ',user_id';
 	echo ",Thumb URL";
 }
 echo "\n";
 
-$recordSet = &$db->Execute("select gridimage_id,title,grid_reference,realname,imageclass,nateastings,natnorthings $sql_from ".
+//todo: use upd_timestamp instead of submitted, but as its new will need to allow it to 'stabalise' first
+
+if ($_GET['since'] && preg_match("/^\d+-\d+-\d+$/",$_GET['since']) ) {
+	$sql_crit = " AND submitted > '{$_GET['since']}'";
+} elseif ($_GET['last'] && preg_match("/^\d+ \w+$/",$_GET['last']) ) {
+	$_GET['last'] = preg_replace("/s$/",'',$_GET['last']);
+	$sql_crit = " AND submitted > date_sub(now(), interval {$_GET['last']})";
+} elseif ($_GET['limit'] && is_numeric($_GET['limit'])) {
+	$sql_crit = " ORDER BY submitted DESC LIMIT {$_GET['limit']}";
+}
+
+if ($_GET['ll'] || $_GET['en']) {
+
+	$recordSet = &$db->Execute("select gridimage_id,title,grid_reference,realname,imageclass,nateastings,natnorthings $sql_from ".
 	"from user ".
 	"inner join gridimage using(user_id) ".
 	"inner join gridsquare using(gridsquare_id) ".
-	"where moderation_status in ('accepted','geograph')");
+	"where moderation_status in ('accepted','geograph') $sql_crit");
+
+} else {
+	$recordSet = &$db->Execute("select gridimage_id,title,grid_reference,realname,imageclass $sql_from ".
+	"from gridimage_search ".
+	"where moderation_status in ('accepted','geograph') $sql_crit");
+}
 while (!$recordSet->EOF) 
 {
 	$image = $recordSet->fields;
@@ -82,6 +117,10 @@ while (!$recordSet->EOF)
 	$recordSet->MoveNext();
 	$i++;
 }
-$recordSet->Close(); 
+$recordSet->Close();
 	
+$sql = "UPDATE apikeys SET accesses=accesses+1, records=records+$i,last_use = NOW() WHERE `key` = '{$_GET['key']}'";
+
+$db->Execute($sql);	
+
 ?>
