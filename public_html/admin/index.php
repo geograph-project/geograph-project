d09695be70fcd1e93f53c55b551b890f36dea69d
Paul Dixon <444004+lordelph@users.noreply.github.com>
2005-09-11 13:45:31 +0000

    added some caching

--- public_html/admin/index.php
+++ public_html/admin/index.php
@@ -28,33 +28,38 @@ $USER->mustHavePerm("admin");
 
 $smarty = new GeographPage;
 
+$template='admin_index.tpl';
+$cacheid='';
 
-//lets get some stats
-$db=NewADOConnection($GLOBALS['DSN']);
-if (!$db) die('Database connection failed');  
+if (!$smarty->is_cached($template, $cacheid))
+{
+	//lets get some stats
+	$db=NewADOConnection($GLOBALS['DSN']);
+	if (!$db) die('Database connection failed');  
+	
+	$users_total=$db->GetOne("select count(*) from user where rights>0");
+	$users_thisweek=$db->GetOne("select count(*) from user where rights>0 and ".
+		"(unix_timestamp(now())-unix_timestamp(signup_date))<604800");
+	$users_submitted=$db->GetOne("select count(distinct user_id) from gridimage"); 
+	$users_pending=$db->GetOne("select count(*) from user where rights is null");
+	
+	$images_total=$db->GetOne("select count(*) from gridimage where moderation_status<>'rejected'");
+	$images_thisweek=$db->GetOne("select count(*) from gridimage where moderation_status<>'rejected' and ".
+		"(unix_timestamp(now())-unix_timestamp(submitted))<604800");
+	
+	$images_status=$db->GetAssoc("select moderation_status,count(*) from gridimage group by moderation_status");
+	
+	$smarty->assign('users_total',  $users_total);
+	$smarty->assign('users_submitted',  $users_submitted);
+	$smarty->assign('users_thisweek',  $users_thisweek);
+	$smarty->assign('users_pending',  $users_pending);
+	
+	$smarty->assign('images_total',  $images_total);
+	$smarty->assign('images_thisweek',  $images_thisweek);
+	$smarty->assign_by_ref('images_status',  $images_status);
+}
 
-$users_total=$db->GetOne("select count(*) from user where rights>0");
-$users_thisweek=$db->GetOne("select count(*) from user where rights>0 and ".
-	"(unix_timestamp(now())-unix_timestamp(signup_date))<604800");
-$users_submitted=$db->GetOne("select count(distinct user_id) from gridimage"); 
-$users_pending=$db->GetOne("select count(*) from user where rights is null");
-
-$images_total=$db->GetOne("select count(*) from gridimage where moderation_status<>'rejected'");
-$images_thisweek=$db->GetOne("select count(*) from gridimage where moderation_status<>'rejected' and ".
-	"(unix_timestamp(now())-unix_timestamp(submitted))<604800");
-
-$images_status=$db->GetAssoc("select moderation_status,count(*) from gridimage group by moderation_status");
-
-$smarty->assign('users_total',  $users_total);
-$smarty->assign('users_submitted',  $users_submitted);
-$smarty->assign('users_thisweek',  $users_thisweek);
-$smarty->assign('users_pending',  $users_pending);
-
-$smarty->assign('images_total',  $images_total);
-$smarty->assign('images_thisweek',  $images_thisweek);
-$smarty->assign_by_ref('images_status',  $images_status);
-
-$smarty->display('admin_index.tpl');
+$smarty->display($template,$cacheid);
 
 	
 ?>
