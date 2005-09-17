80f963e334ee7b2dade0da7d3d44d968d767d629
barryhunter <barryhunter@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2005-09-17 10:37:22 +0000

    new - google earth view-based refresh server

---
+++ trunk/public_html/earth.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id$
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 Barry Hunter (geo@barryhunter.co.uk)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once('geograph/global.inc.php');
+	
+if (!isset($_GET['BBOX'])) {
+	die("This page provides a feed for Google Earth"); 
+}
+if (!intval($_GET['i'])) {
+	$_GET['i'] = 1522;
+}
+
+	if (strpos($_ENV["OS"],'Windows') === FALSE) {
+		//check load average, abort if too high
+		$buffer = "0 0 0";
+		$f = fopen("/proc/loadavg","r");
+		if ($f)
+		{
+			if (!feof($f)) {
+				$buffer = fgets($f, 1024);
+			}
+			fclose($f);
+		}
+		$loads = explode(" ",$buffer);
+		$load=(float)$loads[0];
+
+		if ($load>2)
+		{
+			header("Content-type: application/vnd.google-earth.kml+xml");
+			
+			print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?><kml xmlns="http://earth.google.com/kml/2.0">
+<Document>
+<Folder>
+<name>Geograph NetworkLink</name>
+<description>During busy periods we limit the availability of search to ensure the site remains responsive - please try again shortly.</description>
+<open>0</open>
+<visibility>1</visibility>
+</Folder>
+</Document>
+/kml><?
+			exit;
+		}
+	}
+
+require_once('geograph/feedcreator.class.php');
+require_once('geograph/gridimage.class.php');
+require_once('geograph/gridsquare.class.php');
+require_once('geograph/imagelist.class.php');
+
+$format = 'KML';
+
+$rss = new UniversalFeedCreator(); 
+$rss->title = 'Geograph.co.uk'; 
+$rss->link = "http://{$_SERVER['HTTP_HOST']}";
+
+
+require_once('geograph/searchcriteria.class.php');
+require_once('geograph/searchengine.class.php');
+
+	$pg = $_GET['page'];
+	if ($pg == '' or $pg < 1) {$pg = 1;}
+
+$images = new SearchEngine($_GET['i']);
+
+$rss->description = "Images".$images->criteria->searchdesc; 
+$rss->syndicationURL = "http://{$_SERVER['HTTP_HOST']}/syndicator.php?format=$format&amp;i=".$_GET['i'].(($pg>1)?"&amp;page=$pg":'');
+
+$images->Execute($pg);
+
+//create some feed items
+foreach ($images->results as $img)
+{
+	$item = new FeedItem();
+	$item->title = $img->grid_reference." : ".$img->title;
+	$item->link = "http://{$_SERVER['HTTP_HOST']}/photo/{$img->gridimage_id}";
+	if ($img->dist_string || $img->imagetakenString) {
+		$item->description = $img->dist_string.($img->imagetakenString?' Taken: '.$img->imagetakenString:'')."<br/>".$img->comment; 
+		$item->descriptionHtmlSyndicated = true;
+	} else {
+		$item->description = $img->comment;
+	}
+	$item->date = strtotime($img->submitted);
+	$item->source = "http://{$_SERVER['HTTP_HOST']}/";
+	$item->author = $img->realname;
+	     
+	$item->lat = $img->wgs84_lat;
+	$item->long = $img->wgs84_long;
+	$item->thumb = "http://".$_SERVER['HTTP_HOST'].$img->getThumbnail(120,120,true);
+	
+	$rss->addItem($item);
+}
+
+header("Content-type: application/vnd.google-earth.kml+xml");
+header("Content-Disposition: attachment; filename=\"geograph.kml\"");
+
+//we store in var so can be by reference
+$feed =& $rss->createFeed($format); 
+echo $feed;
+
+?>
