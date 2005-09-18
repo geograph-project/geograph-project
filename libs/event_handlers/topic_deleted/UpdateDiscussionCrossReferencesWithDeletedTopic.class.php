7085b5198bc427162b2179d62353475dae404e99
Paul Dixon <444004+lordelph@users.noreply.github.com>
2005-09-18 14:09:30 +0000

    new

---
+++ libs/event_handlers/topic_deleted/UpdateDiscussionCrossReferencesWithDeletedTopic.class.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id$
+ * 
+ * GeoGraph geographic photo archive project
+ * http://geograph.sourceforge.net/
+ *
+ * This file copyright (C) 2005 Paul Dixon (paul@elphin.com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/**
+* handles the new_topic event and performs any necessary
+* updates to gridsquare_topic table
+*
+* @package Geograph
+* @author Paul Dixon <paul@elphin.com>
+* @version $Revision$
+*/
+
+require_once("geograph/eventhandler.class.php");
+require_once("geograph/gridsquare.class.php");
+
+//filename of class file should correspond to class name, e.g.  myhandler.class.php
+class UpdateDiscussionCrossReferencesWithDeletedTopic extends EventHandler
+{
+	function processEvent(&$event)
+	{
+		$db=&$this->_getDB();
+		
+		//very easy  -trash any mappings to this topic
+		$topic_id = $event['event_param'];
+		$db->Execute("delete from gridsquare_topic where topic_id = $topic_id");
+		
+		
+		//return true to signal completed processing
+		//return false to have another attempt later
+		return true;
+	}
+	
+}
+?>
