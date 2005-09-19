b38fce5d36a0968b784c995240a9e9f334ca03db
lordelph <lordelph@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2005-09-19 08:07:44 +0000

    subject and photographer grid reference now available in image object

--- trunk/public_html/view.php
+++ trunk/public_html/view.php
@@ -2,20 +2,20 @@
 /**
  * $Project: GeoGraph $
  * $Id$
- * 
+ *
  * GeoGraph geographic photo archive project
  * This file copyright (C) 2005 Paul Dixon (paul@elphin.com)
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -42,9 +42,9 @@ if (isset($_GET['id']))
 	$image->loadFromId($_GET['id']);
 	$isowner=($image->user_id==$USER->user_id)?1:0;
 	$ismoderator=$USER->hasPerm('moderator')?1:0;
-	
+
 	$cacheid="img{$_GET['id']}|{$isowner}_{$ismoderator}";
-	
+
 	//is the image rejected? - only the owner and administrator should see it
 	if ($image->moderation_status=='rejected')
 	{
@@ -65,7 +65,12 @@ if (isset($_GET['id']))
 if ($image->isValid())
 {
 	$taken=$image->getFormattedTakenDate();
-	
+
+	//get the photographer position
+	$image->getPhotographerGridref();
+	$image->getSubjectGridref();
+
+
 	//what style should we use?
 	$style='white';
 	$valid_style=array('white', 'black');
@@ -73,7 +78,7 @@ if ($image->isValid())
 	{
 		$style=$_GET['style'];
 		$_SESSION['style']=$style;
-	
+
 		//ToDo - if logged in user, save this in profile
 	}
 	elseif (false) //if logged in user
@@ -83,39 +88,36 @@ if ($image->isValid())
 	elseif (isset($_SESSION['style']))
 	{
 		$style=$_SESSION['style'];
-		
+
 	}
 	$cacheid.=$style;
-	
+
 	if (!$smarty->is_cached($template, $cacheid))
 	{
-	
+
 		$smarty->assign('maincontentclass', 'content_photo'.$style);
-	
-	
+
+
 		//remove grid reference from title
 		$untitled="Untitled photograph for {$image->grid_reference}";
 		if ($image->title!=$untitled)
 			$image->title=trim(str_replace($image->grid_reference, '', $image->title));
-	
+
 		$smarty->assign('page_title', $image->title.":: OS grid {$image->grid_reference}");
 		$smarty->assign('meta_description', $image->comment);
 		$smarty->assign('image_taken', $taken);
 		$smarty->assign('ismoderator', $USER->hasPerm('moderator')?1:0);
 		$smarty->assign_by_ref('image', $image);
-		
+
 		//get a token to show a suroudding geograph map
 		$mosaic=new GeographMapMosaic;
 		$smarty->assign('map_token', $mosaic->getGridSquareToken($image->grid_square));
-		
-		//if this image doesnt have an exact position then we need to remove the move to the center of the square
-		//must be before getNatEastings is called
-		$correction = ($image->grid_square->nateastings)?0:500;
-		
-	
+
+
+
 		//find a possible place within 25km
 		$smarty->assign('place', $image->grid_square->findNearestPlace(135000));
-		
+
 		//let's find posts in the gridref discussion forum
 		$db=NewADOConnection($GLOBALS['DSN']);
 		$sql='select poster_id as user_id,poster_name as realname,CONCAT(\'Discussion on \',t.topic_title) as topic_title,p.post_text,t.topic_id,t.topic_time '.
@@ -126,7 +128,7 @@ if ($image->isValid())
 			't.topic_title = \''.mysql_escape_string($image->grid_square->grid_reference).'\' '.
 			'order by t.topic_time desc limit 3';
 		$news=$db->GetAll($sql);
-		if ($news) 
+		if ($news)
 		{
 			foreach($news as $idx=>$item)
 			{
@@ -136,44 +138,30 @@ if ($image->isValid())
 			}
 			$smarty->assign_by_ref('discuss', $news);
 			$smarty->assign('totalcomments', $totalcomments);
-		} 
-		
+		}
+
 		//count the number of photos in this square
 		$smarty->assign('square_count', $db->getOne("select count(*) from gridimage_search where grid_reference = '{$image->grid_reference}'"));
-		
+
 		//lets add an overview map too
 		$overview=new GeographMapMosaic('overview');
 		$overview->assignToSmarty($smarty, 'overview');
 		$smarty->assign('marker', $overview->getSquarePoint($image->grid_square));
-	
-		//this is needed as smarty is unable to call it!
-		//- now called by findNearestPlace
-		//$image->grid_square->getNatEastings();
-		
+
+
 		require_once('geograph/conversions.class.php');
 		$conv = new Conversions;
-	
+
 		list($lat,$long) = $conv->gridsquare_to_wgs84($image->grid_square);
-		
-	
 		$smarty->assign('lat', $lat);
 		$smarty->assign('long', $long);
-		
+
 		list($latdm,$longdm) = $conv->wgs84_to_friendly($lat,$long);
-		
 		$smarty->assign('latdm', $latdm);
 		$smarty->assign('longdm', $longdm);
-		
-		list($smallgr,$len) = $conv->national_to_gridref($image->grid_square->nateastings-$correction,$image->grid_square->natnorthings-$correction,0,$image->grid_square->reference_index);
-		
-		$smarty->assign('smallgr', $smallgr);
-		
-		$smarty->assign('accucacy', pow(10,6-$len)/10);
-		
-		if ($image->viewpoint_northings) {
-			list($posgr,$len) = $conv->national_to_gridref($image->viewpoint_eastings,$image->viewpoint_northings,0,$image->grid_square->reference_index);
-			$smarty->assign('posgr', $posgr);
-		}
+
+
+
 	}
 }
 
@@ -181,5 +169,5 @@ if ($image->isValid())
 
 $smarty->display($template, $cacheid);
 
-	
+
 ?>
