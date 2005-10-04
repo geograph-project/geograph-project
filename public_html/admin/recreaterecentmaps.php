59a973b21518f6174e92b2e465ba5acb728bb658
barryhunter <1413156+barryhunter@users.noreply.github.com>
2005-10-04 19:02:11 +0000

    update: should run on non png maps

--- public_html/admin/recreaterecentmaps.php
+++ public_html/admin/recreaterecentmaps.php
@@ -103,7 +103,7 @@ function recurse_maps($folder) {
 		if (is_dir($root.$folder.$file) && strpos($file,'.') !== 0) {
 			recurse_maps($folder.$file.'/');
 			print "done $folder $file<br/>";
-		} elseif (preg_match("/detail_(\d+)_(\d+)_(\d+)_(\d+)_(\d+)_(\d+)\.png/",$file,$m)) {
+		} elseif (preg_match("/detail_(\d+)_(\d+)_(\d+)_(\d+)_(\d+)_(\d+)\./",$file,$m)) {
 			array_shift($m);
 			if (filemtime($root.$folder.$file) > $cutoff && $m[4] == 80) {
 				$sql = "INSERT DELAYED IGNORE INTO mapcache2 VALUES(".join(',',$m).",0)";
