0bf8b00cd0579f8051a72f74afa94d1c5d189c58
barryhunter <1413156+barryhunter@users.noreply.github.com>
2005-10-07 10:30:12 +0000

    update: which checks to perform is more customisable

--- public_html/admin/dbcheck.php
+++ public_html/admin/dbcheck.php
@@ -44,85 +44,91 @@ if (isset($_POST['check']))
 	$db = NewADOConnection($GLOBALS['DSN']);
 	if (!$db) die('Database connection failed'); 
 	
-	echo "<h3>Database tables</h3><table class=\"report\">";
-	echo "<thead><tr><td>Table</td><td>Status</td></tr></thead><tbody>";
-	$tables=$db->MetaTables();
-	foreach ($tables as $table)
+	if (isset($_POST['dbtables']))
 	{
-		$result=$db->GetRow("check table $table");
-		echo "<tr><td>$table</td><td>{$result[2]}:{$result[3]}</td></tr>\n";
+		echo "<h3>Database tables</h3><table class=\"report\">";
+		echo "<thead><tr><td>Table</td><td>Status</td></tr></thead><tbody>";
+		$tables=$db->MetaTables();
+		foreach ($tables as $table)
+		{
+			$result=$db->GetRow("check table $table");
+			echo "<tr><td>$table</td><td>{$result[2]}:{$result[3]}</td></tr>\n";
+			flush();
+
+		}
+		echo "</tbody></table>\n";
 		flush();
-		
 	}
-	echo "</tbody></table>\n";
-	flush();
 	
-	echo "<h3>Gridsquare Integrity Check</h3>\n";
-	echo "<p>Here we check how many images we have for a gridsquare and see if it tallies ".
-		"with the cached count in gridsquare.imagecount. ".
-		"We also check if the gridsquare.has_geographs value is sane too.</p>";
-	echo "<p>This can take some time....<span id=\"completed\"></span></p>\n";
-	echo "<script language=\"javascript\">completed=document.getElementById('completed');</script>\n";
-	echo "<ul>";
-	
-	flush();
-	
-	
-	$count=0;
-	$recordSet = &$db->Execute("SELECT gridimage.gridsquare_id, grid_reference, 
-	imagecount as gridsquare_imagecount, count( * ) AS gridsimage_imagecount,
-	has_geographs as gridsquare_has_geographs, sum(moderation_status='geograph') AS gridsimage_geographcount
-	FROM gridimage
-	INNER JOIN gridsquare USING ( gridsquare_id ) 
-	WHERE moderation_status in ('accepted','geograph')
-	GROUP BY gridimage.gridsquare_id
-	HAVING (gridsquare_imagecount != gridsimage_imagecount) OR (gridsquare_has_geographs != (gridsimage_geographcount>0))");
-	
-	while (!$recordSet->EOF) 
+	if (isset($_POST['gridsquares']))
 	{
-		$gridsquare_id=$recordSet->fields['gridsquare_id'];
-		$grid_reference=$recordSet->fields['grid_reference'];
-		$cached_count=$recordSet->fields['gridsquare_imagecount'];
-		$has_geographs=$recordSet->fields['gridsquare_has_geographs'];
-		
-		$realcount=$recordSet->fields['gridsimage_imagecount'];
-		$geographcount=$recordSet->fields['gridsimage_geographcount'];
-		$real_has_geographs=($geographcount>0)?1:0;
-		
-		if ($cached_count!=$realcount)
+
+		echo "<h3>Gridsquare Integrity Check</h3>\n";
+		echo "<p>Here we check how many images we have for a gridsquare and see if it tallies ".
+			"with the cached count in gridsquare.imagecount. ".
+			"We also check if the gridsquare.has_geographs value is sane too.</p>";
+		echo "<p>This can take some time....<span id=\"completed\"></span></p>\n";
+		echo "<script language=\"javascript\">completed=document.getElementById('completed');</script>\n";
+		echo "<ul>";
+
+		flush();
+	
+	
+		$count=0;
+		$recordSet = &$db->Execute("SELECT gridimage.gridsquare_id, grid_reference, 
+		imagecount as gridsquare_imagecount, count( * ) AS gridsimage_imagecount,
+		has_geographs as gridsquare_has_geographs, sum(moderation_status='geograph') AS gridsimage_geographcount
+		FROM gridimage
+		INNER JOIN gridsquare USING ( gridsquare_id ) 
+		WHERE moderation_status in ('accepted','geograph')
+		GROUP BY gridimage.gridsquare_id
+		HAVING (gridsquare_imagecount != gridsimage_imagecount) OR (gridsquare_has_geographs != (gridsimage_geographcount>0))");
+
+		while (!$recordSet->EOF) 
 		{
-			echo "<li>gridsquare $gridsquare_id (<a href=\"/gridref/$grid_reference\">{$grid_reference}</a>) ".
-				"has imagecount $cached_count, should be $realcount ";
-			if (isset($_POST['fix']))
+			$gridsquare_id=$recordSet->fields['gridsquare_id'];
+			$grid_reference=$recordSet->fields['grid_reference'];
+			$cached_count=$recordSet->fields['gridsquare_imagecount'];
+			$has_geographs=$recordSet->fields['gridsquare_has_geographs'];
+
+			$realcount=$recordSet->fields['gridsimage_imagecount'];
+			$geographcount=$recordSet->fields['gridsimage_geographcount'];
+			$real_has_geographs=($geographcount>0)?1:0;
+
+			if ($cached_count!=$realcount)
 			{
-				$db->Execute("update gridsquare set imagecount=$realcount where gridsquare_id=$gridsquare_id");
-				echo "[fixed]";
+				echo "<li>gridsquare $gridsquare_id (<a href=\"/gridref/$grid_reference\">{$grid_reference}</a>) ".
+					"has imagecount $cached_count, should be $realcount ";
+				if (isset($_POST['fix']))
+				{
+					$db->Execute("update gridsquare set imagecount=$realcount where gridsquare_id=$gridsquare_id");
+					echo "[fixed]";
+				}
+
+				echo "</li>";
+				flush();
 			}
-			
-			echo "</li>";
-			flush();
-		}
-		
-		if ($real_has_geographs!=$has_geographs)
-		{
-			echo "<li>gridsquare $gridsquare_id (<a href=\"/gridref/$grid_reference\">{$grid_reference}</a>) ".
-				"has has_geographs $has_geographs, should be $real_has_geographs";
-			if (isset($_POST['fix']))
+
+			if ($real_has_geographs!=$has_geographs)
 			{
-				$db->Execute("update gridsquare set has_geographs=$real_has_geographs where gridsquare_id=$gridsquare_id");
-				echo "[fixed]";
+				echo "<li>gridsquare $gridsquare_id (<a href=\"/gridref/$grid_reference\">{$grid_reference}</a>) ".
+					"has has_geographs $has_geographs, should be $real_has_geographs";
+				if (isset($_POST['fix']))
+				{
+					$db->Execute("update gridsquare set has_geographs=$real_has_geographs where gridsquare_id=$gridsquare_id");
+					echo "[fixed]";
+				}
+
+				echo "</li>";
+				flush();
 			}
-			
-			echo "</li>";
-			flush();
+
+			$recordSet->MoveNext();
 		}
-		
-		$recordSet->MoveNext();
-	}
-	echo "</ul>";
-	$recordSet->Close(); 
-	echo "<script language=\"javascript\">completed.innerHTML='Completed';</script>\n";
-			
+		echo "</ul>";
+		$recordSet->Close(); 
+		echo "<script language=\"javascript\">completed.innerHTML='Completed';</script>\n";
+	}		
 	
 	
 	$smarty->display('_std_end.tpl');
