91030ab3759640a6f2d89a6b1f2210601b293e72
barryhunter <1413156+barryhunter@users.noreply.github.com>
2005-10-07 00:45:58 +0000

    bugfix: threw a javascript error when attempting to validate a short gr

--- public_html/geograph.js
+++ public_html/geograph.js
@@ -5,7 +5,10 @@ function popupOSMap(gridref)
         var wLeft = Math.round(0.5 * (screen.availWidth - wWidth));
         var wTop = Math.round(0.5 * (screen.availHeight - wHeight)) - 20;
         if (gridref.length > 0) {
-        var newWin = window.open('http://getamap.ordnancesurvey.co.uk/getamap/frames.htm?mapAction=gaz&gazName=g&gazString='+gridref, 
+        	if (gridref.length < 7) {
+			gridref = gridref.substr(0,gridref.length-2)+'5'+gridref.substr(gridref.length-2,2)+'5';
+		}
+	var newWin = window.open('http://getamap.ordnancesurvey.co.uk/getamap/frames.htm?mapAction=gaz&gazName=g&gazString='+gridref, 
 		'gam',
 		'left='+wLeft+',screenX='+wLeft+',top='+wTop+',screenY='+wTop+',width='+wWidth+',height='+wHeight+',status,scrolling=no');
 	} else {
