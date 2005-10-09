21dcbf9a505cd3b91d4c0b8f9d258bbe3e786f13
barryhunter <1413156+barryhunter@users.noreply.github.com>
2005-10-09 15:46:36 +0000

    update: show the photographer name on thumbnail alt tags

--- public_html/discuss/bb_func_vthread.php
+++ public_html/discuss/bb_func_vthread.php
@@ -148,9 +148,9 @@ if (preg_match_all("/\[\[(\[?)(\w*\d+)(\]?)\]\]/",$posterText,$g_matches)) {
 			$ok = $g_image->loadFromId($g_id);
 			if ($ok) {
 				if ($g_matches[1][$i]) {
-					$g_img = $g_image->getThumbnail(120,120);
-					$g_img = str_replace('alt="','alt="'.$g_image->grid_reference.' : ',$g_img);
-					$g_title=$g_image->grid_reference.' : '.htmlentities($g_image->title);
+					$g_img = $g_image->getThumbnail(120,120,false,true);
+					#$g_img = preg_replace('/alt="(.*?)"/','alt="'.$g_image->grid_reference.' : \1 by '.$g_image->realname.'"',$g_img);
+					$g_title=$g_image->grid_reference.' : '.htmlentities($g_image->title).' by '.$g_image->realname;
 					$posterText = str_replace("[[[$g_id]]]","<a href=\"http://{$_SERVER['HTTP_HOST']}/photo/$g_id\" target=\"_blank\" title=\"$g_title\">$g_img</a>",$posterText);
 				} else {
 					$posterText = str_replace("[[$g_id]]","{<a href=\"http://{$_SERVER['HTTP_HOST']}/photo/$g_id\" target=\"_blank\">{$g_image->grid_reference} : {$g_image->title}</a>}",$posterText);
