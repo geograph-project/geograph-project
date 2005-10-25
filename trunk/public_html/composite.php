6177500fdc2b0b20a207bae25c206e77ee6b2a3b
barryhunter <barryhunter@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2005-10-25 20:28:21 +0000

    new

---
+++ trunk/public_html/composite.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id$
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 Barry Hunter (geo@barryhunter.co.uk)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once('geograph/global.inc.php');
+init_session();
+
+
+
+	$db=NewADOConnection($GLOBALS['DSN']);
+	if (!$db) die('Database connection failed');  
+	#$db->debug = true;
+	
+	$all = $db->cachegetAssoc(3600,"select right(grid_reference,4) as gr,count(*) as c from gridimage_search group by right(grid_reference,4)");
+
+	foreach ($all as $gr => $c) {
+		$m = max($m,$c);
+	}
+$imgw = 400; $imgh=400; $pixels_per_km = 4;
+	$img=imagecreatetruecolor($imgw,$imgh);
+
+	//fill in with sea
+	$blue=imagecolorallocate ($img, 101,117,255);
+	imagefill($img,0,0,$blue);
+	$o = 255;
+	$ratio = 1.28 * $m;
+	for($i = 0;$i < $m; $i++) {
+		$o = 255- ( log($i) * $ratio);
+		$col[$i]=imagecolorallocate ($img, $o,$o,$o);
+	#	$o-=(255/$m);
+	#	print "$i $o<br>";
+	}
+	#exit;
+	foreach ($all as $gr => $c) {
+		preg_match("/(\d{2})(\d{2})/",$gr,$m);
+		$imgx1 = $m[1] * $pixels_per_km;
+		$imgy1 = $imgh - ($m[2] * $pixels_per_km) - $pixels_per_km;
+		
+	
+		$imgx2=$imgx1 + $pixels_per_km;
+		$imgy2=$imgy1 + $pixels_per_km;
+		imagefilledrectangle ($img, $imgx1, $imgy1, $imgx2, $imgy2, $col[$c]);
+		
+
+	}		
+	
+	imagepng($img);
+?>
