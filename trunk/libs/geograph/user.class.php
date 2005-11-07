16e4db974f8828ef80f4bb7b112db5e246b3a84a
barryhunter <barryhunter@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2005-11-07 18:29:36 +0000

    bugfix: broke the squares count

--- trunk/libs/geograph/user.class.php
+++ trunk/libs/geograph/user.class.php
@@ -115,7 +115,7 @@ class GeographUser
 		$this->stats['ftf']=$db->GetOne("select count(*) from gridimage_search where user_id='{$this->user_id}' and ftf=1");
 		$this->stats['total']=$db->GetOne("select count(*) from gridimage where user_id='{$this->user_id}' and moderation_status<>'rejected'");
 		$this->stats['pending']=$db->GetOne("select count(*) from gridimage where user_id='{$this->user_id}' and moderation_status='pending'");
-		$this->stats['squares']=$db->GetOne("select count(distinct gridsquare_id) from gridimage_search where user_id='{$this->user_id}'");
+		$this->stats['squares']=$db->GetOne("select count(distinct grid_reference) from gridimage_search where user_id='{$this->user_id}'");
 		
 
 	}
