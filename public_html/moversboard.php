d6c2304ca333566da5a036d742f637dc0e5c35ed
barryhunter <1413156+barryhunter@users.noreply.github.com>
2006-04-17 16:43:24 +0000

    update: cutdown on the variations linked to

--- public_html/moversboard.php
+++ public_html/moversboard.php
@@ -149,7 +149,8 @@ if (!$smarty->is_cached($template, $cacheid))
 	$smarty->assign_by_ref('topusers', $topusers);
 	$smarty->assign('cutoff_time', time()-86400*7);
 	
-	$smarty->assign('types', array('points','geosquares','geographs','squares','images'));
+	#$smarty->assign('types', array('points','geosquares','geographs','squares','images'));
+	$smarty->assign('types', array('points','geosquares','images'));
 	
 	//lets find some recent photos
 	new RecentImageList($smarty);
