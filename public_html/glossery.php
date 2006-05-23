a973bf6805187ce9c67111b24afbcc33422cf7d9
barryhunter <1413156+barryhunter@users.noreply.github.com>
2006-05-23 09:50:54 +0000

    update: add all the columns

--- public_html/glossery.php
+++ public_html/glossery.php
@@ -28,14 +28,21 @@ init_session();
 
 $smarty = new GeographPage;
 
-$template=((isset($_GET['add']) && $USER->hasPerm("basic"))?'glossery_add':'statistics_table').'.tpl';
+$template=((isset($_GET['add']) && $USER->hasPerm("basic"))?'glossery_add':((isset($_GET['test']))?'glossery_test':'statistics_table')).'.tpl';
 $cacheid='glossery';
-
+function print_rp(&$in,$exit = false) {
+	print "<pre>";
+	print_r($in);
+	print "</pre>";
+	if ($exit)
+		exit;
+}
 if ($_POST['add']) {
 	$db=NewADOConnection($GLOBALS['DSN']);
 	if (!$db) die('Database connection failed');  
 	#$db->debug = true;
 	
+
 	$USER->mustHavePerm("basic");
 	$updates = array();
 	foreach (explode(' ','source_lang source_dialect source_word tran_lang tran_dialect tran_word tran_definition') as $field) {
@@ -43,16 +50,69 @@ if ($_POST['add']) {
 	}
 	$updates['created_by'] = $USER->user_id;
 	
+
 	$sql = "INSERT INTO `glossery` (`".
 	implode('`,`',array_keys($updates)).
 	"`) VALUES (".
 	implode(',',array_values($updates)).
 	");";
 	
+
 	$db->Execute($sql);
 	
 	$smarty->clear_cache($template, $cacheid);
 }
+
+function smarty_modifier_glossery($input) {
+	$plain = strtolower(preg_replace('/[^a-zA-Z0-9]+/',' ',str_replace("'",'',strip_tags($input))));
+	$words = preg_split('/ +/',$plain);
+	
+	$db=NewADOConnection($GLOBALS['DSN']);
+	if (!$db) die('Database connection failed');
+	
+	print "<pre>$plain</pre>";
+	
+	
+	
+	$all = $db->getAssoc("select 
+	source_word,
+	group_concat(tran_word) as tran_word,
+	group_concat(tran_lang) as tran_lang,
+	group_concat(tran_dialect) as tran_dialect,
+	group_concat(tran_definition) as tran_definition,
+	group_concat(source_lang) as source_lang,
+	group_concat(source_dialect) as source_dialect
+	from glossery
+	group by source_word");
+
+	foreach ($all as $word => $row) {
+		$wordregex = str_replace(')(*','\w*)(',str_replace('*)(',')(\w*','(\b)('.$word.')(\b)'));
+		if (strpos($row['tran_word'],',') !== FALSE) {
+			$tran_words = explode(',',$row['tran_word']);
+			$tran_langs = explode(',',$row['tran_lang']);
+			$tran_dialects = explode(',',$row['tran_dialect']);
+			$tran_definitions = explode(',',$row['tran_definition']);
+			$source_langs = explode(',',$row['source_lang']);
+			$source_dialects = explode(',',$row['source_dialect']);
+			
+			$text = '';
+			foreach ($tran_words as $i => $tran_word) {
+				if ($text)
+					$text .="\n-------------------------------\n";
+				$text .= "Meaning: {$tran_word} ({$tran_langs[$i]} {$tran_dialects[$i]})\n{$tran_definitions[$i]}\n Source: {$source_langs[$i]} {$source_dialects[$i]}";
+			}
+		} else {
+			$text = "Meaning: {$row['tran_word']} ({$row['tran_lang']} {$row['tran_dialect']})\n{$row['tran_definition']}\n Source: {$row['source_lang']} {$row['source_dialect']}";
+		}
+	
+		$input = preg_replace('/'.$wordregex.'/i',"$1<acronym title=\"$text\">\$2</acronym>$3",$input);
+	}
+	
+	return $input;
+}
+
+$smarty->register_modifier("glossery", "smarty_modifier_glossery");
+
 
 if (!$smarty->is_cached($template, $cacheid))
 {
@@ -63,14 +123,14 @@ if (!$smarty->is_cached($template, $cacheid))
 	}
 	if (isset($_GET['add']) && $USER->hasPerm("basic")) {
 
-	} else {
+	} elseif (!isset($_GET['test'])) {
 		$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 
 
 		$sql = "SELECT 
 		CONCAT('<b>',`source_word`,'</b>') as 'Word'
-		,`source_lang` as 'Language'
-		,`source_dialect` as 'Dialect'
+		,`source_lang` as 'Word Language'
+		,`source_dialect` as 'Word Dialect'
 		,CONCAT('<b>',`tran_word`,'</b>') as 'General Meaning'
 		,`tran_lang` as 'Language'
 		,`tran_dialect` as 'Dialect'
