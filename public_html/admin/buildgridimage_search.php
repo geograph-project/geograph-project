4d529add29a1e1ae9cda63be68c5c576c8dc87a4
barry <1413156+barryhunter@users.noreply.github.com>
2006-08-28 20:45:45 +0000

    update: add multi-stage rebuild (much quicker in a live enviroment) also slight improvement to the lat/long update

--- public_html/admin/buildgridimage_search.php
+++ public_html/admin/buildgridimage_search.php
@@ -42,6 +42,8 @@ $smarty->display('_std_begin.tpl');
 <form action="buildgridimage_search.php" method="post">
 <input type="checkbox" id="recreate" name="recreate" value="1" checked="checked">
 <label for="recreate">Recreate entire gridimage_search table from gridimage table</label><br>
+&nbsp;<input type="checkbox" id="use_new" name="use_new" value="1" checked="checked">
+<label for="use_new">Use multi-stage copy (recommended on a live site)</label><br>
 
 <input type="checkbox" id="update" name="update" value="1" checked="checked">
 <label for="update">Update lat/long values in gridimage_search</label><br>
@@ -58,18 +60,64 @@ if (isset($_POST['recreate']))
 	echo "<h3>Rebuilding gridimage_search from gridimage</h3>";
 	flush();
 	
-	$db->Execute("TRUNCATE gridimage_search");
+	if ($_POST['use_new']) {
+	
+		echo "<p>Creating gridimage copy...</p>";flush();
+		$db->Execute("create table tmpimg select gridimage_id,moderation_status, title, submitted, imageclass, imagetaken, upd_timestamp,comment,ftf,seq_no,user_id,gridsquare_id from gridimage where moderation_status in ('accepted','geograph');");
+
+		echo "<p>Creating gridsquare copy...</p>";flush();
+		$db->Execute("create table tmpsq select gridsquare_id,grid_reference,x, y,reference_index from gridsquare;");
+
+		echo "<p>Creating user copy...</p>";flush();
+		$db->Execute("create table tmpus select user_id,realname from user;");
+
+
+		echo "<p>Add keys to copies...</p>";flush();
+		$db->Execute("alter table tmpus add primary key(`user_id`);");
+		$db->Execute("alter table tmpsq add primary key(`gridsquare_id`);");
+
+
+		echo "<p>Clear out gridimage_search...</p>";flush();
+		$db->Execute("truncate table gridimage_search;");
+
+		$db->Execute("ALTER TABLE `gridimage_search` DISABLE KEYS;");
+
+		echo "<p>Rebuilding gridimage_search...</p>";flush();
+		$db->Execute("INSERT INTO gridimage_search
+			SELECT gridimage_id, gi.user_id, moderation_status, title, submitted, imageclass, imagetaken, upd_timestamp, x, y, gs.grid_reference, user.realname,reference_index,comment,0,0,ftf,seq_no
+			FROM tmpimg AS gi
+			INNER JOIN tmpsq AS gs
+			USING ( gridsquare_id )
+			INNER JOIN tmpus as user ON ( gi.user_id = user.user_id );");
+
+		echo "<p>Enable keys on gridimage_search...</p>";flush();
+		$db->Execute("ALTER TABLE `gridimage_search` ENABLE KEYS;");
+
+		echo "<p>Remove temp tables...</p>";flush();
+		$db->Execute("drop table tmpimg");
+		$db->Execute("drop table tmpsq");
+		$db->Execute("drop table tmpus");
+		
+	
+	} else {
 	
 	
-	$db->Execute("INSERT INTO gridimage_search
-		SELECT gridimage_id, gi.user_id, moderation_status, title, submitted, imageclass, imagetaken, upd_timestamp, x, y, gs.grid_reference, user.realname,reference_index,comment,0,0,ftf,seq_no
-		FROM gridimage AS gi
-		INNER JOIN gridsquare AS gs
-		USING ( gridsquare_id )
-		INNER JOIN user ON ( gi.user_id = user.user_id )
-		WHERE moderation_status in ('accepted','geograph') ");
-	echo "<p>Rebuild completed</p>";
+		$db->Execute("TRUNCATE gridimage_search");
+
+
+		$db->Execute("INSERT INTO gridimage_search
+			SELECT gridimage_id, gi.user_id, moderation_status, title, submitted, imageclass, imagetaken, upd_timestamp, x, y, gs.grid_reference, user.realname,reference_index,comment,0,0,ftf,seq_no
+			FROM gridimage AS gi
+			INNER JOIN gridsquare AS gs
+			USING ( gridsquare_id )
+			INNER JOIN user ON ( gi.user_id = user.user_id )
+			WHERE moderation_status in ('accepted','geograph') ");
+	}
 	
+	echo "<h3>Rebuild completed</h3>";
+	if (!isset($_POST['update']))
+		print "<h3>It is now recommended to update the Lat/Long</h3>";
+
 }
 
 if (isset($_POST['update']))
@@ -82,7 +130,7 @@ if (isset($_POST['update']))
 	$recordSet = &$db->Execute("select gridimage_id,x,y,reference_index,nateastings,natnorthings
 		from gridimage
 		INNER JOIN gridsquare AS gs USING ( gridsquare_id )
-		where moderation_status != 'rejected'");
+		where moderation_status in ('accepted','geograph')");
 	$count=0;
 	while (!$recordSet->EOF) 
 	{
@@ -94,9 +142,9 @@ if (isset($_POST['update']))
 			list($lat,$long) = $conv->internal_to_wgs84($image['x'],$image['y'],$image['reference_index']);
 		}
 	
-		$db2->Execute("UPDATE gridimage_search SET wgs84_lat = $lat, wgs84_long = $long WHERE gridimage_id = ".$image['gridimage_id']);
+		$db2->Execute("UPDATE LOW_PRIORITY gridimage_search SET wgs84_lat = $lat, wgs84_long = $long WHERE gridimage_id = ".$image['gridimage_id']);
 		
-		if (++$count%100==0) {
+		if (++$count%500==0) {
 				printf("done %d at <b>%d</b> seconds<br/>",$count,time()-$start);
 				flush();
 		}
