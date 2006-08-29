c0bb8bddd525f95c7b777642bf069d09473df141
barry <1413156+barryhunter@users.noreply.github.com>
2006-08-29 21:18:35 +0000

    update: make a bulk upload (dont need cron as events handle it now)

--- scripts/update_wordnet.php
+++ scripts/update_wordnet.php
@@ -21,12 +21,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-//doesnt seem to work???
-	//$days = $argv[1];
-	
-list ($days) = array_keys($_REQUEST);
-if (!$days) 
-	$days = 3;
 
 //needed to allow the config file to load - could be passed in as a argument??
 $_SERVER['HTTP_HOST'] = "geograph.local";
@@ -38,78 +32,21 @@ $_SERVER['DOCUMENT_ROOT'] = "/home/geograph/public_html/";
 // nothing below here should need changing
 
 require_once('geograph/global.inc.php');
+require_once('geograph/wordnet.inc.php');
 
 $db = NewADOConnection($GLOBALS['DSN']);
 
 
-function doaddw2($phrase) {
-	global $w2;
-	$w2[$phrase]++; 
-}
-
-function doaddw3($phrase) {
-	global $w3;
-	$w3[$phrase]++; 
-}
-	
-function docount($text,$field,$id) {
-	global $db,$w1,$w2,$w3;
-	
-	$alltext = strtolower(preg_replace('/[^a-zA-Z0-9]+/',' ',str_replace("'",'',$text)));
-	
-	if (strlen($text)< 1)
-		return;
-		
-	$words = preg_split('/ /',$alltext);
-	
-	$w1 = array();
-	$w2 = array();
-	$w3 = array();
-	
-	//build a list of one word phrases
-	foreach ($words as $word) {
-		$w1[$word]++;
-	}
-	
-	//build a list of two word phrases
-		$text = $alltext;
-	$text = preg_replace('/(\w+) (\w+)/e','doaddw2("$1 $2")',$text);	
-		$text = $alltext;
-		$text = preg_replace('/(\w+)/','',$text,1);
-	$text = preg_replace('/(\w+) (\w+)/e','doaddw2("$1 $2")',$text);
-	
-	//build a list of three word phrases
-		$text = $alltext;
-	$text = preg_replace('/(\w+) (\w+) (\w+)/e','doaddw3("$1 $2 $3")',$text);	
-		$text = $alltext;
-		$text = preg_replace('/(\w+)/','',$text,1);
-	$text = preg_replace('/(\w+) (\w+) (\w+)/e','doaddw3("$1 $2 $3")',$text);	
-		$text = $alltext;
-		$text = preg_replace('/(\w+) (\w+)/','',$text,1);
-	$text = preg_replace('/(\w+) (\w+) (\w+)/e','doaddw3("$1 $2 $3")',$text);
-		
-	
-	
-	foreach ($w1 as $word=>$count) {
-		$db->Execute("insert into wordnet set gid = $id,`len` = 1,words = '$word',$field = $count");// ON DUPLICATE KEY UPDATE $field=$field+$count");
-	}
-	foreach ($w2 as $word=>$count) {
-		$db->Execute("insert into wordnet set gid = $id,`len` = 2,words = '$word',$field = $count");
-	}	
-	foreach ($w3 as $word=>$count) {
-		$db->Execute("insert into wordnet set gid = $id,`len` = 3,words = '$word',$field = $count");
-	}	
-}
 	$tim = time();
 	
 	
-	$recordSet = &$db->Execute("select gridimage_id,title,comment from gridimage where moderation_status != 'rejected' and upd_timestamp > date_sub(now(), interval $days day)");
+	$recordSet = &$db->Execute("select gridimage_id,title from gridimage where moderation_status != 'rejected'");
 	while (!$recordSet->EOF) 
 	{
-		$db->Execute("delete from wordnet where gid = ".$recordSet->fields['gridimage_id']);
-		docount($recordSet->fields['title'],'title',$recordSet->fields['gridimage_id']);
-		docount($recordSet->fields['comment'],'comment',$recordSet->fields['gridimage_id']);
-				
+		updateWordnet($db,$recordSet->fields['title'],'title',$recordSet->fields['gridimage_id']);
+		if ($recordSet->fields['gridimage_id']%5000==0)
+			printf("done %d at %d seconds\n",$recordSet->fields['gridimage_id'],time()-$tim);
+	
 		$recordSet->MoveNext();
 	}
 	$recordSet->Close(); 
