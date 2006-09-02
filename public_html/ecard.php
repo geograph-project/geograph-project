18b9d56f810f815cfffff77bee794d223e71bdcd
barry <1413156+barryhunter@users.noreply.github.com>
2006-09-02 12:41:31 +0000

    nope, lets try plain \n

--- public_html/ecard.php
+++ public_html/ecard.php
@@ -159,9 +159,9 @@ if (!$throttle && isset($_POST['msg']))
 				$headers[] = "Reply-To: $from_name <$from_email>";
 			$headers[] = "X-GeographUserId:{$USER->user_id}";
 
-			$headers[] = "Content-Type: multipart/alternative;\r\n	boundary=\"----=_NextPart_000_00DF_01C5EB66.9313FF40\"";
+			$headers[] = "Content-Type: multipart/alternative;\n	boundary=\"----=_NextPart_000_00DF_01C5EB66.9313FF40\"";
 
-			@mail("$to_name <$to_email>", $subject, $body, implode("\r\n",$headers));
+			@mail("$to_name <$to_email>", $subject, $body, implode("\n",$headers));
 
 			$smarty->assign('sent', 1);
 		}
