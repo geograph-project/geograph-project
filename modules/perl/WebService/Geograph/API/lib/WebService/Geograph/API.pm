84ffcc2d17b6254cd03c9aa28dfb8d4dc353abdd
spiros <spiros@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2006-11-10 10:58:00 +0000

    documentation & function POD

--- modules/perl/WebService/Geograph/API/lib/WebService/Geograph/API.pm
+++ modules/perl/WebService/Geograph/API/lib/WebService/Geograph/API.pm
@@ -25,7 +25,7 @@ sub new {
 	
 	# Please do not change the following parameter. 
 	# It does not provide geograph.co.uk with any personal information
-	# but helps then track usage of this module.
+	# but helps them track usage of this module.
 		
 	my %options = ( 'agent' => 'WebService::Geograph::API' ) ;
 	my $self = new LWP::UserAgent ( %options );
@@ -45,8 +45,11 @@ sub lookup {
 	$args->{key} = $self->{key} ;
 	
 	my $request = new WebService::Geograph::Request (  $mode , $args  ) ;	
-	
-	$self->execute_request($request) ;
+	if (defined $request) {
+		$self->execute_request($request) ;
+		} else {
+			return ;
+		}
 }
 
 sub execute_request {
@@ -90,49 +93,76 @@ sub create_results_node {
 	
 }
 
-#################### main pod documentation begin ###################
-## Below is the stub of documentation for your module. 
-## You better edit it!
-
-
 =head1 NAME
 
-WebService::Geograph::API - Module abstract (<= 44 characters) goes here
+WebService::Geograph::API - Perl interface to the Geograph.co.uk API
 
 =head1 SYNOPSIS
 
   use WebService::Geograph::API;
-  blah blah blah
+  
+  my $api = new WebService::Geograph::API ( { 'key' => 'your_api_key_here'} ) ;
 
+  my $rv = $api->lookup ( 'csv', { 'i'     => 12345,
+                                   'll'    => 1,
+                                   'thumb' => 1,
+                                 }) ;
+
+  my $data = $rd->{results} ;
 
 =head1 DESCRIPTION
 
-Stub documentation for this module was created by ExtUtils::ModuleMaker.
-It looks like the author of the extension was negligent enough
-to leave the stub unedited.
+This module provides a simple interface to using the geograph.co.uk API service.
 
-Blah blah blah.
+The actual core class, C<WebService::Geograph::API> is a subclass of C<LWP::UserAgent> so 
+all the usual parameters apply.
 
+=head2 METHODS
 
-=head1 USAGE
+=over 4
 
+=item C<new>
 
+The following constructing method creates a new C<WebService::Geograph::API> object and returns it. 
+It accepts a single parameter, I<key>, which is the API key for the service. You B<must> obtain
+a valid key otherwise you will not be able to use the API.
 
-=head1 BUGS
+Obtaining a key is free. See : http://www.geograph.org.uk/help/api#api for more information.
 
+	my $api = new WebService::Geograph::API ( { 'key' => 'your_api_key_here'} ) ;
 
+=item C<lookup>
+
+Creates a new C<WebService::Geograph::Request> object and executes it.
+
+	my $rv = $api->lookup ( 'csv', { 'i' => 12345, 'll' => 1, } ) ;
+
+Valid modes at the moment include I<csv> for exporting CSV and I<search> for creating custom searches
+and obtaining their 'i' number. A very good and detailed overview of the various parameters they
+support can be find on the API page located at : http://www.geograph.org.uk/help/api#api
+
+This method creates and returns a new C<WebService::Geograph::Response> object. The object is
+a standard C<HTTP::Response> object with some additional fields. If no errors occur, the
+results of the query will be located inside I<results> ;
+
+	my $data = $rv->{results} ;
+
+=back
 
 =head1 SUPPORT
 
+Please feel free to send any bug reports and suggestions to my email listed below.
 
+For more information and useless facts on my life, you can also check my blog:
+
+  http://idaru.blogspot.com/
 
 =head1 AUTHOR
 
     Spiros Denaxas
     CPAN ID: SDEN
-    Lokku Ltd
+    Lokku Ltd ( http://www.nestoria.co.uk )
     s [dot] denaxas [@] gmail [dot]com
-    http://idaru.blogspot.com
 
 =head1 COPYRIGHT
 
@@ -142,10 +172,9 @@ it and/or modify it under the same terms as Perl itself.
 The full text of the license can be found in the
 LICENSE file included with this module.
 
-
 =head1 SEE ALSO
 
-perl(1).
+L<WebService::Geograph::Request>, L<WebService::Geograph::Response>, L<http://www.geograph.co.uk>, L<http://www.geograph.org.uk/help/api#api>
 
 =cut
 
