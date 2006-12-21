6bc8e1ed573e154a10b8ffd83304e4938d78a675
barry <1413156+barryhunter@users.noreply.github.com>
2006-12-21 22:59:49 +0000

    resturcture to allow a command line interface

--- scripts/build_rastermaps.php
+++ scripts/build_rastermaps.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * $Project: GeoGraph $
- * $Id: imagemap.php 1690 2005-12-22 15:05:42Z barryhunter $
+ * $Id: recreate_maps.php 2578 2006-09-27 20:58:54Z barry $
  * 
  * GeoGraph geographic photo archive project
  * This file copyright (C) 2005 Barry Hunter (geo@barryhunter.co.uk)
@@ -21,6 +21,60 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+
+    
+
+//these are the arguments we expect
+$param=array(
+	'dir'=>'/var/www/geograph_live/',		//base installation dir
+	'config'=>'www.geograph.org.uk', //effective config
+	'help'=>0,		//show script help?
+);
+
+//very simple argument parser
+for($i=1; $i<count($_SERVER['argv']); $i++)
+{
+	$arg=$_SERVER['argv'][$i];
+
+	if (substr($arg,0,2)=='--')
+
+	{
+		$arg=substr($arg,2);
+		$bits=explode('=', $arg,2);
+		
+		//if we have a value, use it, else just flag as true
+		$param[$bits[0]]=isset($bits[1])?$bits[1]:true;
+
+		//fake it into the GET array;
+		$_GET[$bits[0]]=isset($bits[1])?$bits[1]:true;
+	}
+	else die("unexpected argument $arg - try --help\n");
+	
+}
+
+
+if ($param['help'])
+{
+	echo <<<ENDHELP
+---------------------------------------------------------------------
+build_rastermaps.php 
+---------------------------------------------------------------------
+    --dir=<dir>         : base directory (/var/www/geograph_live/)
+    --config=<domain>   : effective domain config (www.geograph.org.uk)
+    --help              : show this message	
+---------------------------------------------------------------------
+	
+ENDHELP;
+exit;
+}
+	
+//set up  suitable environment
+ini_set('include_path', $param['dir'].'/libs/');
+$_SERVER['DOCUMENT_ROOT'] = $param['dir'].'/public_html/'; 
+$_SERVER['HTTP_HOST'] = $param['config'];
+
+#-----------------------------------------------
+
 require_once('geograph/global.inc.php');
 require_once('geograph/gridsquare.class.php');
 require_once('geograph/gridimage.class.php');
@@ -28,7 +82,6 @@ require_once('geograph/map.class.php');
 require_once('geograph/mapmosaic.class.php');
 require_once('geograph/image.inc.php');
 require_once('geograph/rastermapOS.inc.php');
-init_session();
 
 $gr = "SH7042";
 $tile = "SH64";
@@ -38,7 +91,6 @@ $tile = "SH64";
 		define('TIFF_KMW_BY10',TIFF_KMW / 10);
 		define('TIFF_PX_PER_KM',TIFF_W / TIFF_KMW);
 
-$USER->mustHavePerm("admin");
 
 $m = new RasterMapOS();
 
@@ -78,6 +130,6 @@ if ($_GET['combineTiles'])
 	$m->combineTiles($gr);
 
 
+print "\n\ndone\n";
+?>
 
-	
-?>Done
