f91a0069803cf0862ab68b9850707693f5c849d2
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2007-01-07 23:14:59 +0000

    new: excel export

--- trunk/public_html/export.csv.php
+++ trunk/public_html/export.csv.php
@@ -26,150 +26,17 @@ require_once('geograph/global.inc.php');
   $smarty = new GeographPage;
   dieUnderHighLoad();
 
-$db=NewADOConnection($GLOBALS['DSN']);
+#	#	#	#	#	#	#	#	#	#	#	#	#	#	#
 
-if ((empty($_GET['key']) || preg_match("/[^\w\.@]/",$_GET['key'])) && empty($_GET['u']))
-	die("ERROR: no api key or email address");
-	
-$sql = "SELECT * FROM `apikeys` WHERE `apikey` = ".$db->Quote($_GET['key'])." AND (`ip` = INET_ATON('{$_SERVER['REMOTE_ADDR']}') OR `ip` = 0) AND `enabled` = 'Y'";
+include('geograph/export.inc.php');
 
-$profile = $db->GetRow($sql);
-
-if ($profile['apikey']) {
-	$sql_hardlimit = $hardlimit = '';
-} elseif (!empty($_GET['u']) && preg_match("/^\d+$/",$_GET['u']) && (init_session() || true) && $USER->hasPerm('basic')) {
-	$sql_hardlimit = $hardlimit = '';
-} else {
-	#die("ERROR: invalid api key. contact support at geograph dot co dot uk");
-	$hardlimit = 250;
-	$sql_hardlimit = " LIMIT $hardlimit";
-} 
+#	#	#	#	#	#	#	#	#	#	#	#	#	#	#
 
 # let the browser know what's coming
 header("Content-type: application/octet-stream");
 header("Content-Disposition: attachment; filename=\"geograph.csv\"");
 
-
-#	#	#	#	#	#	#	#	#	#	#	#	#	#	#
-
-$sql_crit = '';
-
-echo "Id,Name,Grid Ref,Submitter,Image Class";
-if (!empty($_GET['thumb'])) {
-	require_once('geograph/gridimage.class.php');
-	$gridimage = new GridImage;
-	$sql_from = ',gi.user_id,x,y';
-	echo ",Thumb URL";
-}
-if (!empty($_GET['en'])) {
-	echo ",Easting,Northing";
-	if (!empty($_GET['ppos'])) {
-		echo ",Photographer Eastings,Photographer Northings";
-		$sql_from .= ",viewpoint_eastings,viewpoint_northings";
-	}
-} elseif (!empty($_GET['ll'])) {
-	$sql_from = ',wgs84_lat,wgs84_long';
-	echo ",Lat,Long";
-}
-
-if (!empty($_GET['taken'])) {
-	echo ",Date Taken";
-	$sql_from .= ",imagetaken";
-}
-
-if (!empty($_GET['dir'])) {
-	echo ",View Direction";
-	$sql_from .= ",view_direction";
-}
-echo "\n";
-
-#	#	#	#	#	#	#	#	#	#	#	#	#	#	#
-
-if (!empty($_GET['ri']) && preg_match("/^\d$/",$_GET['ri']) ) {
-	$sql_crit .= " AND reference_index = {$_GET['ri']}";
-}
-$user_crit = 0;
-if (!empty($_GET['u']) && preg_match("/^\d+$/",$_GET['u'])) {
-	$sql_crit .= " AND gi.user_id = {$_GET['u']}";
-	$user_crit = 1;
-}
-
-if (!empty($_GET['since']) && preg_match("/^\d+-\d+-\d+$/",$_GET['since']) ) {
-	$sql_crit .= " AND upd_timestamp >= '{$_GET['since']}' $sql_hardlimit";
-} elseif (!empty($_GET['last']) && preg_match("/^\d+ \w+$/",$_GET['last']) ) {
-	$_GET['last'] = preg_replace("/s$/",'',$_GET['last']);
-	$sql_crit .= " AND upd_timestamp > date_sub(now(), interval {$_GET['last']}) $sql_hardlimit";
-} elseif (!empty($_GET['limit']) && preg_match("/^\d+(,\d+|)?$/",$_GET['limit'])) {
-	if ($hardlimit) {
-		if (preg_match("/^(\d+),(\d+)?$/",$_GET['limit'],$m)) {
-			$_GET['limit'] = "{$m[1]},$hardlimit";
-		} else {
-			$_GET['limit'] = min($_GET['limit'],$hardlimit);
-		}
-	}
-	$sql_crit .= " ORDER BY upd_timestamp DESC LIMIT {$_GET['limit']}";
-} elseif (empty($_GET['i']) && empty($_GET['u'])) {
-	die("ERROR: whole db export disabled. contact support at geograph dot co dot uk");
-	$sql_crit .= " $sql_hardlimit";
-}
-
-if (!empty($_GET['supp']) xor empty($_GET['u'])) {
-	$mod_sql = "moderation_status in ('accepted','geograph')";
-} else {
-	$mod_sql = "moderation_status = 'geograph'";
-}
-
-#	#	#	#	#	#	#	#	#	#	#	#	#	#	#
-
-
-$i=(!empty($_GET['i']))?intval($_GET['i']):'';
-
-if ($i && !$user_crit ) {
-	require_once('geograph/searchcriteria.class.php');
-	require_once('geograph/searchengine.class.php');
-	
-		$pg = (!empty($_GET['page']))?intval($_GET['page']):0;
-		if (empty($pg) || $pg < 1) {$pg = 1;}
-
-	$engine = new SearchEngine($i);
-	
-	if (!empty($_GET['count'])) {
-		if (preg_match("/^\d+$/",$_GET['count'])) {
-			$engine->criteria->resultsperpage = $_GET['count'];
-		} elseif ($_GET['count'] == -1) {
-			$engine->criteria->resultsperpage = 999999999;
-		}
-		if ($hardlimit) {
-			$engine->criteria->resultsperpage = min($engine->criteria->resultsperpage,$hardlimit);
-		}
-	}
-	
-	//return a recordset
-		//if want en then we HAVE to use the non cached version!
-	$recordSet = $engine->ReturnRecordset($pg,isset($_GET['en']));
-
-} elseif (!empty($_GET['en']) || !empty($_GET['ppos'])) {
-	if (!empty($_GET['ftf'])) {
-		$mod_sql .= " and ftf = 1"; 
-	}
-	$recordSet = &$db->Execute("select gridimage_id,title,grid_reference,realname,imageclass,nateastings,natnorthings,gi.user_id $sql_from 
-	from user 
-	inner join gridimage gi using(user_id) 
-	inner join gridsquare using(gridsquare_id) 
-	where $mod_sql $sql_crit");
-} else {
-	if (!empty($_GET['supp'])) {
-		$mod_sql = 1; //no point checking what will always be 1 ;-)
-	}
-	if (!empty($_GET['ftf'])) {
-		$mod_sql .= " and ftf = 1"; 
-	}
-	$recordSet = &$db->Execute("select gridimage_id,title,grid_reference,realname,imageclass,user_id $sql_from 
-	from gridimage_search gi 
-	where $mod_sql $sql_crit");
-}
-
-#	#	#	#	#	#	#	#	#	#	#	#	#	#	#
+echo "$csvhead\n";
 
 $counter = -1;
 while (!$recordSet->EOF) 
@@ -185,8 +52,7 @@ while (!$recordSet->EOF)
 		echo ','.$gridimage->getThumbnail(120,120,true);
 	}
 	if (!empty($_GET['en'])) {
-		if ($image['nateastings'])
-			echo ",{$image['nateastings']},{$image['natnorthings']}";
+		echo ",{$image['nateastings']},{$image['natnorthings']}";
 		if (!empty($_GET['ppos']))
 			echo ",{$image['viewpoint_eastings']},{$image['viewpoint_northings']}";
 	} elseif (!empty($_GET['ll']))
