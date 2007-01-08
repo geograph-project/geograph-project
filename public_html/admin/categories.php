f95014c924709c3e16acb0c374f582f8b625b30a
barry <1413156+barryhunter@users.noreply.github.com>
2007-01-08 16:56:13 +0000

    allow mods access

--- public_html/admin/categories.php
+++ public_html/admin/categories.php
@@ -24,7 +24,7 @@
 require_once('geograph/global.inc.php');
 init_session();
 
-$USER->mustHavePerm("admin");
+$USER->hasPerm("admin") || $USER->mustHavePerm("moderator");
 
 $smarty = new GeographPage;
 
