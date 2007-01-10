3876e09440baed113fadfde625d36934566388a7
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2007-01-10 13:41:50 +0000

    update: clarifying message to submit map update: add gridref and extract to article

--- trunk/libs/geograph/rastermap.class.php
+++ trunk/libs/geograph/rastermap.class.php
@@ -392,7 +392,7 @@ class RasterMap
 
 	function getFootNote() 
 	{
-		return "<span id=\"mapFootNoteOS50k\"".($this->service == 'OS50k'?'':' style="display:none"')."><br/>OS Maps are still in testing, please visit forum for more info.</span>".
+		return "<span id=\"mapFootNoteOS50k\"".(($this->service == 'OS50k' && $this->issubmit)?'':' style="display:none"')."><br/>Centre the circle on the subject and mark the photographer position with the cross below the camera. The red arrow will then show view direction.</span>".
 		"<span id=\"mapFootNoteVoB\"".($this->service == 'VoB'?'':' style="display:none"')."><br/>Historical Map provided by <a href=\"http://www.visionofbritain.org.uk/\" title=\"Vision of Britain\">VisionOfBritain.org.uk</a></span>";
 	}
 
