e45926db1d6d8fd4e3cf7258e8de8a8c7d334577
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2007-01-13 20:01:30 +0000

    arrrg, and b tags!

--- trunk/public_html/article/article.php
+++ trunk/public_html/article/article.php
@@ -102,13 +102,13 @@ function smarty_function_articletext($input) {
 	$replacement[]='';
 
 	//fix a bug where double spacing on a previous match would swallow the newline needed for the next
-	$pattern[]='/\n\n(<h\d>)\#/';
+	$pattern[]='/\n\n(<\w{1,3}>)\#/';
 	$replacement[]="\n\$1#";
 	
 	$pattern[]='/\n\n\#/';
 	$replacement[]="\n\r\n\$1#";
 	
-	$pattern[]='/\n(<h\d>)?\#([\w]{1,2})? ([^\n]+)(<\/h\d>)?(\n{2})?/e';
+	$pattern[]='/\n(<\w{1,3}>)?\#([\w]{1,2})? ([^\n]+)(<\/\w{1,3}>)?(\n{2})?/e';
 	$replacement[]="'<ol style=\"margin-bottom:0px;'.('\$1'?'':'margin-top:0px').'\"'.('\$2'?' start=\"\$2\"':'').'><li>\$1\$3\$4</li></ol>'.('\$5'?'\n':'')";
 	$pattern[]='/<\/ol>\n?<ol style=\"margin-bottom:0px;margin-top:0px\">/';
 	$replacement[]='';
