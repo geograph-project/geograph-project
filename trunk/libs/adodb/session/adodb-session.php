28d57a828c0c952beb42550be6c998351b8301a1
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2007-02-19 23:31:42 +0000

    add hack to record remote IP

--- trunk/libs/adodb/session/adodb-session.php
+++ trunk/libs/adodb/session/adodb-session.php
@@ -695,6 +695,7 @@ class ADODB_Session {
 
 		if (!$clob) {	// no lobs, simply use replace()
 			$arr[$data] = $conn->qstr($val);
+			$$arr['ipaddr'] = $conn->qstr(getRemoteIP());
 			$rs = $conn->Replace($table, $arr, 'sesskey', $autoQuote = true);
 			
 		} else {
