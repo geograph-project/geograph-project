fb655e4db791d678653503d8b8f97348f29fbfde
barry <1413156+barryhunter@users.noreply.github.com>
2007-03-20 21:52:05 +0000

    slight tweaks to the superlayer

--- public_html/_scripts/kml-centisquare.php
+++ public_html/_scripts/kml-centisquare.php
@@ -93,7 +93,7 @@ $folder->setItem('name',"$gr :: Geograph SuperLayer");
 		}
 		$point = new kmlPoint($wgs84_lat,$wgs84_long);			
 
-		$placemark = new kmlPlacemark_photo(null,$square->grid_reference.' :: '.$entry['title'],$point);
+		$placemark = new kmlPlacemark_Photo(null,$square->grid_reference.' :: '.$entry['title'],$point);
 		$placemark->useHoverStyle();
 
 		$image=new GridImage;
