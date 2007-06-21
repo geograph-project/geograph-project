b856c74545a52f9f7cb2ffd7343215c394420a3f
david <david@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2007-06-21 22:36:47 +0000

    Juppy download pages

--- trunk/public_html/juppy.php
+++ trunk/public_html/juppy.php
@@ -1,51 +1,108 @@
-<?php 
-
-header('Content-type: application/x-java-jnlp-file');
-echo '<?xml version="1.0" encoding="utf8"?>' 
-
-
-/********************************************************
-
-This script delivers the jnlp file to start juppy. It's
-coded as a script to remove the need to change it as it moves
-between servers
-
-*********************************************************/
-?>
-
-<jnlp spec="1.0+" href="juppy.php" 
 <?php
-	echo 'codebase="http://' . $_SERVER['SERVER_NAME'] . '">';
+/**
+ * $Project: GeoGraph $
+ * $Id: index.php 3264 2007-04-13 00:46:33Z paul $
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 Paul Dixon (paul@elphin.com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once('geograph/global.inc.php');
+init_session();
+
+
+$smarty = new GeographPage;
+
+
+$template='juppy.tpl';
+$cacheid=rand(1,5); //so we get a selection of homepages
+
+//regenerate?
+if (!$smarty->is_cached($template, $cacheid))
+{
+	require_once('geograph/gridimage.class.php');
+	require_once('geograph/gridsquare.class.php');
+	require_once('geograph/imagelist.class.php');
+
+	require_once('geograph/map.class.php');
+	require_once('geograph/mapmosaic.class.php');
+
+	$preset=($CONF['template']=='charcoal')?'overview_charcoal':'overview';
+	$overview=new GeographMapMosaic($preset);
+	$overview->assignToSmarty($smarty, 'overview');
+	
+	
+	if ($CONF['template']=='charcoal')
+	{
+		require_once('geograph/pictureoftheday.class.php');
+		$potd=new PictureOfTheDay;
+		$potd->assignToSmarty($smarty); 
+	}
+	
+	//lets find some recent photos
+	new RecentImageList($smarty);
+	
+	//let's find recent posts in the announcements forum made by
+	//administrators
+	$db=NewADOConnection($GLOBALS['DSN']);
+	$sql="select u.user_id,u.realname,t.topic_title,p.post_text,t.topic_id,t.topic_time, posts_count - 1 as comments 
+		from geobb_topics as t
+		inner join geobb_posts as p on(t.topic_id=p.topic_id)
+		inner join user as u on (p.poster_id=u.user_id)
+		where find_in_set('admin',u.rights)>0 and
+		abs(unix_timestamp(t.topic_time) - unix_timestamp(p.post_time) ) < 10 and
+		t.forum_id=1
+		order by t.topic_time desc limit 3";
+	$news=$db->GetAll($sql);
+	if ($news) 
+	{
+		foreach($news as $idx=>$item)
+		{
+			$news[$idx]['post_text']=str_replace('<br>', '<br/>', GeographLinks($news[$idx]['post_text'],true));
+		}
+	}
+	
+
+	$smarty->assign_by_ref('news', $news);
+        
+        // juppy page specific stuff
+        
+        if (!$GLOBALS['USER']->registered) {
+          $sadly = "As you're not logged in, we don't know how many images you've submitted.";
+        } else {
+          $icsql = "select count(gridimage_id) from gridimage where user_id = " . $GLOBALS['USER']->user_id;
+          if ($rs = $db->Execute($icsql)) {
+            $userimages = $rs->fields[0];
+            if ($userimages >= $CONF['juppy_minimum_images']) {
+              $sadly = "Happily, you've already submitted $userimages image(s) which is enough to let you loose with JUppy.";
+            } else {
+              $sadly = "Sadly, your current submission count is only $userimages, so you've got a bit further to go.";
+            }
+          }
+        }
+        
+        
+        $smarty->assign_by_ref('imagecount', $CONF['juppy_minimum_images']);
+	$smarty->assign_by_ref('sadly', $sadly);
+
+}
+
+
+$smarty->display($template, $cacheid);
+
+	
 ?>
-
- <information>
-  <title>JUppy Geograph uploader</title>
-  <vendor>Those nice Geograph people</vendor> 
-  <homepage href="http://www.geograph.org.uk" />
-  <description kind="one-line">
-   JUppy - Java batch uploader for geograph project
-  </description>
-  <description kind="short">
-   JUppy is the Geograph project batch uploader. This permits off-line
-   compilation of an image queue which can be sent to the Geograph
-   project when a connection is available.
-  </description>
-  <offline-allowed />
- </information>
-
- <security>
-  <all-permissions />
-  <!-- ...which means the jar has to be signed -->
- </security>
-
- <resources>
-  <j2se version="1.5" />
-  <jar href="JUploader.jar" />
- </resources>
-
- <application-desc>
-  <argument><?php echo $_SERVER['SERVER_NAME'];?></argument>
- </application-desc>
-
-</jnlp>
-
