b6f17a0a933d2754012bd51dc51de29a8a68c2d2
barry <1413156+barryhunter@users.noreply.github.com>
2007-06-24 13:24:31 +0000

    better dropdown of discussion topics

--- public_html/search.php
+++ public_html/search.php
@@ -782,13 +782,27 @@ if (isset($_GET['fav']) && $i) {
 			$recordSet->Close();
 			$smarty->assign_by_ref('countylist', $countylist);
 
-			$topics = $db->GetAssoc("select gp.topic_id,concat(topic_title,' [',count(*),']') from gridimage_post gp
+			$topicsraw = $db->GetAssoc("select gp.topic_id,concat(topic_title,' [',count(*),']') as title,forum_name from gridimage_post gp
 				inner join geobb_topics using (topic_id)
-				group by gp.topic_id
-				having count(*) > 5
-				order by topic_title");
+				inner join geobb_forums using (forum_id)
+				group by gp.topic_id 
+				having count(*) > 4
+				order by geobb_topics.forum_id desc,topic_title");
 
-			$topics=array("1"=>"Any Topic") + $topics;
+			$topics=array("1"=>"Any Topic"); 
+
+			$options = array();
+			foreach ($topicsraw as $topic_id => $row) {
+				if ($last != $row['forum_name'] && $last) {
+					$topics[$last] = $options;
+					$options = array();
+				}
+				$last = $row['forum_name'];
+
+				$options[$topic_id] = $row['title'];
+			}
+			$topics[$last] = $options;
+	
 			$smarty->assign_by_ref('topiclist',$topics);
 
 			require_once('geograph/gridsquare.class.php');
