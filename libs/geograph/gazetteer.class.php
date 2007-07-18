2b2ad167054fd10556373b937d5243fe8cf4cb11
barry <1413156+barryhunter@users.noreply.github.com>
2007-07-18 20:58:21 +0000

    quick fix: try this on DB2

--- libs/geograph/gazetteer.class.php
+++ libs/geograph/gazetteer.class.php
@@ -543,7 +543,7 @@ class Gazetteer
 	function &_getDB()
 	{
 		if (!is_object($this->db))
-			$this->db=NewADOConnection($GLOBALS['DSN']);
+			$this->db=NewADOConnection(!empty($GLOBALS['DSN2'])?$GLOBALS['DSN2']:$GLOBALS['DSN']);
 		if (!$this->db) die('Database connection failed');  
 		return $this->db;
 	}
