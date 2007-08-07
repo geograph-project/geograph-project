8128cfae5959cb0098118114fc59d33f53b130c1
david <david@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2007-08-07 19:18:35 +0000

    Update to 1.7

---
+++ trunk/apps/JUploader/src/net/brassedoff/Debug.java
@@ -0,0 +1,20 @@
+/*
+ * Debug.java
+ *
+ * Created on 24 July 2007, 19:46
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package net.brassedoff;
+
+/**
+ *
+ * @author david
+ */
+public class Debug {
+    
+    public static boolean ON = false;
+    
+}
