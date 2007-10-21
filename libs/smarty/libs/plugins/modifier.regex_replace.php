c4e7bf3a5f6bcbbd4c4ce304d72232b3ce359651
barry <1413156+barryhunter@users.noreply.github.com>
2007-10-21 01:22:50 +0000

    experimental cluster search results (requires mod to smarty)

--- libs/smarty/libs/plugins/modifier.regex_replace.php
+++ libs/smarty/libs/plugins/modifier.regex_replace.php
@@ -22,10 +22,7 @@
  */
 function smarty_modifier_regex_replace($string, $search, $replace)
 {
-    if (preg_match('!([a-zA-Z\s]+)$!s', $search, $match) && (strpos($match[1], 'e') !== false)) {
-        /* remove eval-modifier from $search */
-        $search = substr($search, 0, -strlen($match[1])) . preg_replace('![e\s]+!', '', $match[1]);
-    }
+    
        
     return preg_replace($search, $replace, $string);
 }
