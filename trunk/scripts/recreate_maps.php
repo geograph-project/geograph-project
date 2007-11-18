7e38eec67e711690faaa818054c8b58c554fd8a7
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2007-11-18 21:21:18 +0000

    redraw depth maps

--- trunk/scripts/recreate_maps.php
+++ trunk/scripts/recreate_maps.php
@@ -142,11 +142,11 @@ $map=new GeographMap;
 			
 while (1) {
 
-	$invalid_maps = $db->GetOne("select count(*) from mapcache where age > 0 and type_or_user >= 0");
+	$invalid_maps = $db->GetOne("select count(*) from mapcache where age > 0 and type_or_user >= -1");
 
 	if ($invalid_maps) {
 		//done as many small select statements to allow new maps to be processed 
-		$recordSet = &$db->Execute("select * from mapcache where age > 0 and type_or_user >= 0
+		$recordSet = &$db->Execute("select * from mapcache where age > 0 and type_or_user >= -1
 			order by pixels_per_km desc, age desc limit 50");
 		while (!$recordSet->EOF) 
 		{
