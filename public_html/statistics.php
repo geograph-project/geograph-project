8f767bc7d0b1414a6c142e6c29b7328f377dd29b
barry <1413156+barryhunter@users.noreply.github.com>
2008-04-20 03:36:34 +0000

    More streamlined stats generation (combines queries)

--- public_html/statistics.php
+++ public_html/statistics.php
@@ -76,25 +76,36 @@ if (!$smarty->is_cached($template, $cacheid))
 	foreach (array(1,2) as $ri) {
 		$letterlength = 3 - $ri; #should this be auto-realised by selecting a item from gridprefix?
 
-		$smarty->assign("images_total_$ri",  $db->GetOne("select count(*) from gridimage_search where reference_index = $ri"));
-		$smarty->assign("images_thisweek_$ri",  $db->GetOne("select count(*) from gridimage_search where reference_index = $ri and (unix_timestamp(now())-unix_timestamp(submitted))<604800"));
 
-		$smarty->assign("squares_total_$ri",  $db->CacheGetOne(24*3600,"select count(*) from gridsquare where reference_index = $ri and percent_land > 0"));
-		$smarty->assign("squares_submitted_$ri",  $db->GetOne("select count(*) from gridsquare where reference_index = $ri and imagecount > 0 and percent_land > 0"));
+		$smarty->assign("images_thisweek_$ri",  $db->CacheGetOne(3*3600,"select count(*) from gridimage_search where reference_index = $ri and (unix_timestamp(now())-unix_timestamp(submitted))<604800"));
 
-		$smarty->assign("geographs_submitted_$ri",  $db->GetOne("select count(*) from gridsquare where reference_index = $ri and has_geographs > 0 and percent_land > 0"));
+		$stats= $db->CacheGetOne(3*3600,$sql = "select 
+			count(*) as squares_total,
+			sum(imagecount) as images_total,
+			sum(imagecount > 0) as squares_submitted,
+			sum(has_geographs > 0) geographs_submitted,
+			count(distinct concat(substring(grid_reference,1,".($letterlength+1)."),substring(grid_reference,".($letterlength+3).",1))) as tenk_total
+		from gridsquare 
+		where reference_index = $ri and percent_land > 0");
+
+
+		$stats += $db->CacheGetOne(3*3600,"select 
+			count(distinct substring(grid_reference,1,$letterlength)) as grid_submitted,
+			count(distinct concat(substring(grid_reference,1,".($letterlength+1)."),substring(grid_reference,".($letterlength+3).",1))) as tenk_submitted,
+			avg( x ) as x,
+			avg( y ) as y
+		from gridsquare 
+		where reference_index = $ri and percent_land > 0 and has_geographs > 0");
+
+		foreach ($stats as $key => $value) {
+			$smarty->assign("{$key}_$ri",$value);
+		}
 
 		$smarty->assign("grid_total_$ri",  $db->CacheGetOne(24*3600,"select count(*) from gridprefix where reference_index = $ri and landcount > 0"));
-		$smarty->assign("grid_submitted_$ri",  $db->GetOne("select count(distinct substring(grid_reference,1,$letterlength)) from gridimage_search where reference_index = $ri"));
 
-		$smarty->assign("tenk_total_$ri",  $db->CacheGetOne(24*3600,"select count(distinct concat(substring(grid_reference,1,".($letterlength+1)."),substring(grid_reference,".($letterlength+3).",1))) from gridsquare where reference_index = $ri and percent_land > 0"));
-
-		$smarty->assign("tenk_submitted_$ri",  $db->GetOne("select count(distinct concat(substring(grid_reference,1,".($letterlength+1)."),substring(grid_reference,".($letterlength+3).",1))) from gridsquare where reference_index = $ri and has_geographs > 0 and percent_land > 0"));
-
-		$center = $db->GetRow("SELECT avg( x ) , avg( y ) FROM `gridsquare` WHERE imagecount >0 AND reference_index = $ri");
 
 		$censquare = new GridSquare;
-		$ok = $censquare->loadFromPosition(intval($center[0]),intval($center[1]));
+		$ok = $censquare->loadFromPosition(intval($stats['x']),intval($stats['y']));
 
 		if ($ok) {
 			$smarty->assign("centergr_$ri",$censquare->grid_reference);
