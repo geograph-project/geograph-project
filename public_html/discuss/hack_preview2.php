d5fc12fe380af9452db07933dc4894dd2b8f060e
barry <1413156+barryhunter@users.noreply.github.com>
2008-05-03 19:22:28 +0000

    thumbnail quota on forum and elsewhere

--- public_html/discuss/hack_preview2.php
+++ public_html/discuss/hack_preview2.php
@@ -8,26 +8,33 @@ $topicTitle2=stripslashes(textFilter($_POST['topicTitle'],$topic_max_length,$pos
 $postText2=stripslashes(textFilter($_POST['postText'],$post_text_maxlength,$post_word_maxlength,1,$disbbcode,1,$logged_admin));
 
 if (empty($CONF['disable_discuss_thumbs']) && preg_match_all('/\[\[(\[?)(\w{0,2} ?\d+ ?\d*)(\]?)\]\]/',$postText2,$g_matches)) {
+	$thumb_count = 0;
 	foreach ($g_matches[2] as $i => $g_id) {
 		if (is_numeric($g_id)) {
-			if (!isset($g_image)) {
-				require_once('geograph/gridimage.class.php');
-				require_once('geograph/gridsquare.class.php');
-				$g_image=new GridImage;
-			}
-			$ok = $g_image->loadFromId($g_id);
-			if ($g_image->moderation_status == 'rejected' && !isset($userRanks[$cc]))
-				$ok = false;
-			if ($ok) {
-				if ($g_matches[1][$i]) {
-					$g_img = $g_image->getThumbnail(120,120,false,true);
-					#$g_img = preg_replace('/alt="(.*?)"/','alt="'.$g_image->grid_reference.' : \1 by '.$g_image->realname.'"',$g_img);
-					$g_title=$g_image->grid_reference.' : '.htmlentities($g_image->title).' by '.$g_image->realname;
-					$postText2 = str_replace("[[[$g_id]]]","<a href=\"http://{$_SERVER['HTTP_HOST']}/photo/$g_id\" target=\"_blank\" title=\"$g_title\">$g_img</a>",$postText2);
-				} else {
-					$postText2 = preg_replace("/(?<!\[)\[\[$g_id\]\]/","{<a href=\"http://{$_SERVER['HTTP_HOST']}/photo/$g_id\" target=\"_blank\">{$g_image->grid_reference} : {$g_image->title}</a>}",$postText2);
+			if ($global_thumb_count >= 200 || $thumb_count >= 60) {
+				$posterText = preg_replace("/\[?\[\[$g_id\]\]\]?/","[[<a href=\"http://{$_SERVER['HTTP_HOST']}/photo/$g_id\">$g_id</a>]]",$posterText);
+			} else {
+				if (!isset($g_image)) {
+					require_once('geograph/gridimage.class.php');
+					require_once('geograph/gridsquare.class.php');
+					$g_image=new GridImage;
 				}
-			}			
+				$ok = $g_image->loadFromId($g_id);
+				if (ok && $g_image->moderation_status == 'rejected' && (!isset($userRanks[$cc]) || $userRanks[$cc] == 'Member'))
+					$ok = false;
+				if ($ok) {
+					if ($g_matches[1][$i]) {
+						$g_img = $g_image->getThumbnail(120,120,false,true);
+						#$g_img = preg_replace('/alt="(.*?)"/','alt="'.$g_image->grid_reference.' : \1 by '.$g_image->realname.'"',$g_img);
+						$g_title=$g_image->grid_reference.' : '.htmlentities($g_image->title).' by '.$g_image->realname;
+						$postText2 = str_replace("[[[$g_id]]]","<a href=\"http://{$_SERVER['HTTP_HOST']}/photo/$g_id\" target=\"_blank\" title=\"$g_title\">$g_img</a>",$postText2);
+					} else {
+						$postText2 = preg_replace("/(?<!\[)\[\[$g_id\]\]/","{<a href=\"http://{$_SERVER['HTTP_HOST']}/photo/$g_id\" target=\"_blank\">{$g_image->grid_reference} : {$g_image->title}</a>}",$postText2);
+					}
+				}
+				$global_thumb_count++;
+			}
+			$thumb_count++;
 		} else {
 			$postText2 = str_replace("[[$g_id]]","<a href=\"http://{$_SERVER['HTTP_HOST']}/gridref/".str_replace(' ','+',$g_id)."\" target=\"_blank\">$g_id</a>",$postText2);
 		}
