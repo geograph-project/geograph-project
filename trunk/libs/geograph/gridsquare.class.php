8133face2ea02ca83369c25637d8e833a3c45a7c
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2008-06-21 10:53:14 +0000

    add link to mapfixer

--- trunk/libs/geograph/gridsquare.class.php
+++ trunk/libs/geograph/gridsquare.class.php
@@ -526,7 +526,7 @@ class GridSquare
 			
 			if ($this->percent_land==0 && (!$allowzeropercent || $this->imagecount==0) )
 			{
-				$this->_error("$gridref seems to be all at sea! Please contact us if you think this is in error.");
+				$this->_error("$gridref seems to be all at sea! Please <a href=\"/mapfixer.php?gridref=$gridref\">contact us</a> if you think this is in error.");
 				$ok=false;
 
 			}
