d9f4e61f432c95e976c81e3da9e7dee669a305ab
barry <1413156+barryhunter@users.noreply.github.com>
2008-07-04 22:52:13 +0000

    New finder section

---
+++ public_html/finder/contributors.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id: faq.php 15 2005-02-16 12:23:35Z lordelph $
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 Paul Dixon (paul@elphin.com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once('geograph/global.inc.php');
+init_session();
+
+
+
+
+$smarty = new GeographPage;
+
+if (!empty($_GET['q'])) {
+	$q=trim($_GET['q']);
+
+	$sphinx = new sphinxwrapper($q);
+
+	//gets a cleaned up verion of the query (suitable for filename etc) 
+	$cacheid = $sphinx->q;
+
+	$sphinx->pageSize = $pgsize = 15;
+
+	$sphinx->processQuery();
+
+	$pg = (!empty($_GET['page']))?intval(str_replace('/','',$_GET['page'])):0;
+	if (empty($pg) || $pg < 1) {$pg = 1;}
+
+	#$ids = $sphinx->returnUserIds($pg);	
+	
+	$ids = array(3,4,5);
+	$sphinx->where = "user_id IN(".join(",",$ids).")";
+	
+	if (count($ids)) {
+		$where = $sphinx->where;
+
+		$db=NewADOConnection($GLOBALS['DSN']);
+
+		$limit = 25;
+
+		$prev_fetch_mode = $ADODB_FETCH_MODE;
+		$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+		$rows = $db->getAssoc("
+		select user.user_id,nickname,realname,images
+		from user 
+		left join user_stat using (user_id)
+		where $where
+		limit $limit");
+
+		$results = array();
+		foreach ($ids as $c => $id) {
+			$row = $rows[$id];
+			$row['user_id'] = $id;
+			$results[] = $row;
+		}
+		$smarty->assign_by_ref('results', $results);
+		$cacheid = md5($q);
+	}
+	$ADODB_FETCH_MODE = $prev_fetch_mode;
+	
+	$smarty->assign("q",$sphinx->q);
+	
+}
+
+$smarty->display('finder_contributors.tpl',$cacheid);
+
+	
+?>
---
+++ public_html/templates/basic/finder_contributors.tpl
@@ -0,0 +1,62 @@
+{assign var="page_title" value="Contributor Search"}
+{include file="_std_begin.tpl"}
+{literal}
+<style type="text/css">
+	ul.explore tt {
+		border:1px solid gray;
+		padding:5px;
+	}
+</style>
+
+  <script type="text/javascript">
+  
+  function focusBox() {
+  	el = document.getElementById('fq');
+  	el.focus();
+  }
+  AttachEvent(window,'load',focusBox,false);
+  
+  </script>
+
+{/literal}
+
+<h2>Contributor Search</h2>
+
+<form action="{$script_name}" method="get" target="searchwindow" onsubmit="focusBox()">
+	<p>
+		<label for="fq">Free Text Search</label>: <input type="text" name="q" id="fq" size="40"{dynamic}{if $q} value="{$q|escape:'html'}"{/if}{/dynamic}/>
+		<input type="submit" value="Search"/>
+	</p>
+	<p>
+		<small>&middot; There is no paging of results, to access more images simply add more keywords to refine your search (view <a href="#cheatsheet">Cheatsheet</a>)<br/>
+	</p>
+</form>
+
+
+<ol>
+{foreach from=$results item=item}
+	<li>
+	<b><a href="/profile/{$item.user_id}" target="_top">{$item.realname|escape:'html'}</a></b>
+	{if $item.nickname}<small>Nickname: {$item.realname|escape:'html'}</small>{/if}
+	
+	{if $item.images}
+	<small><small style="color:gray">{$item.images} images submitted</small></small>{/if}
+	</li>
+{foreachelse}
+	{if $q}
+		<li><i>There is no content to display at this time.</i></li>
+	{/if}
+{/foreach}
+
+</ol>
+
+<div class="interestBox" style="margin-top:60px;">
+	<big><a name="cheatsheet"></a>Cheatsheet</big>:
+	<ul class="explore">
+		<li>prefix a keyword with - to <b>exclude</b> that word from the match; example : <tt>smith -john</tt></li>
+		<li>can use OR to match <b>either/or</b> keywords; example: <tt>john OR joan</tt></li>
+	</ul>
+</div>
+
+
+{include file="_std_end.tpl"}
