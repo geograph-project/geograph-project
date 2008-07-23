7fd64b922733869cc78f09356a60cb740fb9c1b5
barry <1413156+barryhunter@users.noreply.github.com>
2008-07-23 23:38:54 +0000

    that 'hack' a bad idea - turns out we are recompiling - becuase we have three compiled sets - and their serials end up different -  guess solution is put compiled folder on nfs - gulp!

--- libs/smarty/libs/internals/core.process_compiled_include.php
+++ libs/smarty/libs/internals/core.process_compiled_include.php
@@ -22,8 +22,7 @@ function smarty_core_process_compiled_include($params, &$smarty)
     $_return = $params['results'];
 
     foreach ($smarty->_cache_info['cache_serials'] as $_include_file_path=>$_cache_serial) {
-        $smarty->_include($_include_file_path, true, array('cache_serial' => $_cache_serial, 'valid_cache' => &$params['valid_cache']));
-        if (!$params['valid_cache']) return;
+        $smarty->_include($_include_file_path, true);
     }
 
     foreach ($smarty->_cache_serials as $_include_file_path=>$_cache_serial) {
