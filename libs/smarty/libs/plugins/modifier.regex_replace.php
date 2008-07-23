6a7d5c94f57cb142e44539df3a7548ec53bfae83
barry <1413156+barryhunter@users.noreply.github.com>
2008-07-23 23:05:41 +0000

    Update to the latest version of smarty (2.6.19) - to hopefully resolve the nocache bug

--- libs/smarty/libs/plugins/modifier.regex_replace.php
+++ libs/smarty/libs/plugins/modifier.regex_replace.php
@@ -22,7 +22,12 @@
  */
 function smarty_modifier_regex_replace($string, $search, $replace)
 {
-    
+    if (($pos = strpos($search,"\0")) !== false)
+      $search = substr($search,0,$pos);
+    #if (preg_match('!([a-zA-Z\s]+)$!s', $search, $match) && (strpos($match[1], 'e') !== false)) {
+    #    /* remove eval-modifier from $search */
+    #    $search = substr($search, 0, -strlen($match[1])) . preg_replace('![e\s]+!', '', $match[1]);
+    #}
        
     return preg_replace($search, $replace, $string);
 }
