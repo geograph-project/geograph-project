46f666165644c84cba7cf2a67013f7f6e802c18b
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2008-07-31 13:24:31 +0000

    allow title:word syntax

--- trunk/public_html/stuff/search-service.php
+++ trunk/public_html/stuff/search-service.php
@@ -36,6 +36,8 @@ $q = isset($_GET['q'])?$_GET['q']:'';
 
 $q = preg_replace('/ OR /',' | ',$q);
 
+$q = preg_replace('/\b([a-z_]+):/','@$1 ',$q);
+
 $q = trim(preg_replace('/[^\w~\|\(\)@"\/-]+/',' ',trim(strtolower($q))));
 
 $q = preg_replace('/^(.*) *near +([a-zA-Z]{1,2} *\d{2,5} *\d{2,5}) *$/','$2 $1',$q);
