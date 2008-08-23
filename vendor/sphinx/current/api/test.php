5cda68ee5ba7e76649b0088cee1ee440f0470001
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2008-08-23 19:13:15 +0000

    merge in 0.9.8

--- vendor/sphinx/current/api/test.php
+++ vendor/sphinx/current/api/test.php
@@ -1,7 +1,7 @@
 <?php
 
 //
-// $Id: test.php,v 1.17 2007/04/01 21:11:14 shodan Exp $
+// $Id: test.php 1343 2008-07-08 21:28:20Z shodan $
 //
 
 require ( "sphinxapi.php" );
@@ -23,14 +23,18 @@ if ( !is_array($_SERVER["argv"]) || empty($_SERVER["argv"]) )
 	print ( "-h, --host <HOST>\tconnect to searchd at host HOST\n" );
 	print ( "-p, --port\t\tconnect to searchd at port PORT\n" );
 	print ( "-i, --index <IDX>\tsearch through index(es) specified by IDX\n" );
-	print ( "-s, --sortby <EXPR>\tsort matches by 'EXPR'\n" );
+	print ( "-s, --sortby <CLAUSE>\tsort matches by 'CLAUSE' in sort_extended mode\n" );
+	print ( "-S, --sortexpr <EXPR>\tsort matches by 'EXPR' DESC in sort_expr mode\n" );
 	print ( "-a, --any\t\tuse 'match any word' matching mode\n" );
 	print ( "-b, --boolean\t\tuse 'boolean query' matching mode\n" );
 	print ( "-e, --extended\t\tuse 'extended query' matching mode\n" );
+	print ( "-ph,--phrase\t\tuse 'exact phrase' matching mode\n" );
 	print ( "-f, --filter <ATTR>\tfilter by attribute 'ATTR' (default is 'group_id')\n" );
 	print ( "-v, --value <VAL>\tadd VAL to allowed 'group_id' values list\n" );
 	print ( "-g, --groupby <EXPR>\tgroup matches by 'EXPR'\n" );
-	print ( "-gs, --groupsort <EXPR>\tsort groups by 'EXPR'\n" );
+	print ( "-gs,--groupsort <EXPR>\tsort groups by 'EXPR'\n" );
+	print ( "-d, --distinct <ATTR>\tcount distinct values of 'ATTR''\n" );
+	print ( "-l, --limit <COUNT>\tretrieve COUNT matches (default: 20)\n" );
 	exit;
 }
 
@@ -47,6 +51,10 @@ $groupby = "";
 $groupsort = "@group desc";
 $filter = "group_id";
 $filtervals = array();
+$distinct = "";
+$sortby = "";
+$limit = 20;
+$ranker = SPH_RANK_PROXIMITY_BM25;
 for ( $i=0; $i<count($args); $i++ )
 {
 	$arg = $args[$i];
@@ -54,14 +62,26 @@ for ( $i=0; $i<count($args); $i++ )
 	if ( $arg=="-h" || $arg=="--host" )				$host = $args[++$i];
 	else if ( $arg=="-p" || $arg=="--port" )		$port = (int)$args[++$i];
 	else if ( $arg=="-i" || $arg=="--index" )		$index = $args[++$i];
-	else if ( $arg=="-s" || $arg=="--sortby" )		$sortby = $args[++$i];
+	else if ( $arg=="-s" || $arg=="--sortby" )		{ $sortby = $args[++$i]; $sortexpr = ""; }
+	else if ( $arg=="-S" || $arg=="--sortexpr" )	{ $sortexpr = $args[++$i]; $sortby = ""; }
 	else if ( $arg=="-a" || $arg=="--any" )			$mode = SPH_MATCH_ANY;
 	else if ( $arg=="-b" || $arg=="--boolean" )		$mode = SPH_MATCH_BOOLEAN;
 	else if ( $arg=="-e" || $arg=="--extended" )	$mode = SPH_MATCH_EXTENDED;
+	else if ( $arg=="-e2" )							$mode = SPH_MATCH_EXTENDED2;
+	else if ( $arg=="-ph"|| $arg=="--phrase" )		$mode = SPH_MATCH_PHRASE;
 	else if ( $arg=="-f" || $arg=="--filter" )		$filter = $args[++$i];
-	else if ( $arg=="-v" || $arg=="--value" )		$filtervals[] = (int)$args[++$i];
+	else if ( $arg=="-v" || $arg=="--value" )		$filtervals[] = $args[++$i];
 	else if ( $arg=="-g" || $arg=="--groupby" )		$groupby = $args[++$i];
 	else if ( $arg=="-gs"|| $arg=="--groupsort" )	$groupsort = $args[++$i];
+	else if ( $arg=="-d" || $arg=="--distinct" )	$distinct = $args[++$i];
+	else if ( $arg=="-l" || $arg=="--limit" )		$limit = (int)$args[++$i];
+	else if ( $arg=="-r" )
+	{
+		$arg = strtolower($args[++$i]);
+		if ( $arg=="bm25" )		$ranker = SPH_RANK_BM25;
+		if ( $arg=="none" )		$ranker = SPH_RANK_NONE;
+		if ( $arg=="wordcount" )$ranker = SPH_RANK_WORDCOUNT;
+	}
 	else
 		$q .= $args[$i] . " ";
 }
@@ -72,14 +92,17 @@ for ( $i=0; $i<count($args); $i++ )
 
 $cl = new SphinxClient ();
 $cl->SetServer ( $host, $port );
+$cl->SetConnectTimeout ( 1 );
 $cl->SetWeights ( array ( 100, 1 ) );
 $cl->SetMatchMode ( $mode );
-if ( count($filtervals) )
-	$cl->SetFilter ( $filter, $filtervals );
-if ( $groupby )
-	$cl->SetGroupBy ( $groupby, SPH_GROUPBY_ATTR, $groupsort );
-if ( $sortby )
-	$cl->SetSortMode ( SPH_SORT_EXTENDED, $sortby );
+if ( count($filtervals) )	$cl->SetFilter ( $filter, $filtervals );
+if ( $groupby )				$cl->SetGroupBy ( $groupby, SPH_GROUPBY_ATTR, $groupsort );
+if ( $sortby )				$cl->SetSortMode ( SPH_SORT_EXTENDED, $sortby );
+if ( $sortexpr )			$cl->SetSortMode ( SPH_SORT_EXPR, $sortexpr );
+if ( $distinct )			$cl->SetGroupDistinct ( $distinct );
+if ( $limit )				$cl->SetLimits ( 0, $limit, ( $limit>1000 ) ? $limit : 1000 );
+$cl->SetRankingMode ( $ranker );
+$cl->SetArrayResult ( true );
 $res = $cl->Query ( $q, $index );
 
 ////////////////
@@ -106,14 +129,20 @@ if ( $res===false )
 	{
 		$n = 1;
 		print "Matches:\n";
-		foreach ( $res["matches"] as $doc => $docinfo )
+		foreach ( $res["matches"] as $docinfo )
 		{
-			print "$n. doc_id=$doc, weight=$docinfo[weight]";
+			print "$n. doc_id=$docinfo[id], weight=$docinfo[weight]";
 			foreach ( $res["attrs"] as $attrname => $attrtype )
 			{
 				$value = $docinfo["attrs"][$attrname];
-				if ( $attrtype==SPH_ATTR_TIMESTAMP )
-					$value = date ( "Y-m-d H:i:s", $value );
+				if ( $attrtype & SPH_ATTR_MULTI )
+				{
+					$value = "(" . join ( ",", $value ) .")";
+				} else
+				{
+					if ( $attrtype==SPH_ATTR_TIMESTAMP )
+						$value = date ( "Y-m-d H:i:s", $value );
+				}
 				print ", $attrname=$value";
 			}
 			print "\n";
@@ -123,7 +152,7 @@ if ( $res===false )
 }
 
 //
-// $Id: test.php,v 1.17 2007/04/01 21:11:14 shodan Exp $
+// $Id: test.php 1343 2008-07-08 21:28:20Z shodan $
 //
 
 ?>
