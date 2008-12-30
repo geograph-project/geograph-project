601bdd7f523008600ad7930c1116506854fc1e83
barry <1413156+barryhunter@users.noreply.github.com>
2008-12-30 17:32:20 +0000

    fix to allow postcodes and gridrefernces in the text search (by specifing a place)

--- libs/geograph/searchenginebuilder.class.php
+++ libs/geograph/searchenginebuilder.class.php
@@ -63,12 +63,13 @@ class SearchEngineBuilder extends SearchEngine
 		
 		$nearstring = $this->getNearString($distance);
 		
+		$has_location = preg_match('/\bnear\b/',$q);
 		
 		$searchclass = '';
 		$limit1 = '';
 		$location = '';
 		$q = trim(strip_tags($q));
-		if (preg_match("/\b([A-Z]{1,2})([0-9]{1,2}[A-Z]?) *([0-9]?)([A-Z]{0,2})\b/i",$q,$pc) 
+		if (!$has_location && preg_match("/\b([A-Z]{1,2})([0-9]{1,2}[A-Z]?) *([0-9]?)([A-Z]{0,2})\b/i",$q,$pc) 
 		&& !in_array($pc[1],array('SV','SX','SZ','TV','SU','TL','TM','SH','SJ','TG','SC','SD','NX','NY','NZ','OV','NS','NT','NU','NL','NM','NO','NF','NH','NJ','NK','NA','NB','NC','ND','HW','HY','HZ','HT','Q','D','C','J','H','F','O','T','R','X','V')) ) {
 			//these prefixs are not postcodes but are valid gridsquares
 			$searchq = strtoupper($pc[1].$pc[2].($pc[3]?" ".$pc[3]:''));
@@ -83,7 +84,7 @@ class SearchEngineBuilder extends SearchEngine
 			} else {
 				$this->errormsg = "Invalid Postcode or a newer Postcode not in our database, please try a different search method.";
 			}
-		} elseif (preg_match("/\b([a-zA-Z]{1,2}) ?(\d{1,5})[ \.]?(\d{1,5})\b/",$q,$gr)) {
+		} elseif (!$has_location && preg_match("/\b([a-zA-Z]{1,2}) ?(\d{1,5})[ \.]?(\d{1,5})\b/",$q,$gr)) {
 			require_once('geograph/gridsquare.class.php');
 			$square=new GridSquare;
 			$grid_ok=$square->setByFullGridRef($gr[1].$gr[2].$gr[3],false,true);
