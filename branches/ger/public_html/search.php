f2ed2f9bddfc43471298309fe0d8504961012b81
hansjorg <hansjorg@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2009-01-15 20:16:24 +0000

    * allow grid prefixes of three characters: 30_gridprefix.diff

--- branches/ger/public_html/search.php
+++ branches/ger/public_html/search.php
@@ -106,7 +106,7 @@ if (isset($_GET['fav']) && $i) {
 	} elseif (!empty($_GET['first'])) {
 		$_GET['first'] = strtoupper(preg_replace('/\s+/','',$_GET['first']));
 
-		if (preg_match('/^[A-Z_%]{0,2}[\d_%]{1,4}$/',$_GET['first']) ) {
+		if (preg_match('/^[A-Z_%]{0,3}[\d_%]{1,4}$/',$_GET['first']) ) {
 
 			//replace for myriads
 			$gr = preg_replace('/^([A-Z]{1,3}|%)(\d|_)(\d|_)$/','$1$2_$3_',$_GET['first']);//FIXME?
