76f5fe12ad1342c4f43c556b12fbc37825857487
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2009-02-24 22:46:31 +0000

    fix issue with rss caching track syndicator queries seperatly.

--- trunk/libs/geograph/searchengine.class.php
+++ trunk/libs/geograph/searchengine.class.php
@@ -185,7 +185,7 @@ class SearchEngine
 			(strlen($this->criteria->sphinx['query']) || !empty($this->criteria->sphinx['d']) || !empty($this->criteria->sphinx['filters']))
 			&& $this->criteria->sphinx['impossible'] == 0) {
 			$this->noCache = 1;
-			return $this->ExecuteSphinxRecordSet($pg,$sql_fields,$sql_order);
+			return $this->ExecuteSphinxRecordSet($pg);
 		} elseif ($this->criteria->sphinx['no_legacy']) {
 			//oh dear, no point even trying :(
 			$this->resultCount = 0;
