d5b112c9e86440cf5356c4f7cfa11132b0ff97cf
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2009-03-28 14:39:08 +0000

    better layout

--- branches/british-isles/public_html/discuss/index.php
+++ branches/british-isles/public_html/discuss/index.php
@@ -438,9 +438,9 @@ if ($viewTopicsIfOnlyOneForum!=1) {
 		require($pathToFiles.'bb_func_ldisc.php');
 		$listTopics=$list_topics;
 		if ($sortByNew==0) {
-			if($list_topics!='') echo ParseTpl(makeUp('main_last_discussions'));
-		} else {
 			if($list_topics!='') echo ParseTpl(makeUp('main_last_discussions_tip'));
+		} else {
+			if($list_topics!='') echo ParseTpl(makeUp('main_last_discussions'));
 		}
 		print "<div style=\"text-align:center\">Show: ";
 		foreach (array(5,10,20,30,40,80,100) as $n) {
