80c4cba2a60875720d4fad1d113e71f11853e87e
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2009-04-05 19:35:58 +0000

    Incorperate palette into invalidation system
    Change recreate_maps.php - to DELETE maps - with time possibly will be more efficient as wont redraw lots of never to be used again tiles.

--- branches/british-isles/scripts/recreate_maps.php
+++ branches/british-isles/scripts/recreate_maps.php
@@ -73,6 +73,8 @@ $param=array(
 	'timeout'=>14, //timeout in minutes
 	'sleep'=>10,	//sleep time in seconds
 	'load'=>100,	//maximum load average
+	'base'=>0,	//delete the basemaps?
+	'dryrun'=>0,	//test only?
 	'help'=>0,		//show script help?
 );
 
@@ -110,6 +112,8 @@ php recreate_maps.php
     --timeout=<minutes> : maximum runtime of script (14)
     --sleep=<seconds>   : seconds to sleep if load average exceeded (10)
     --load=<loadavg>    : maximum load average (100)
+    --base=1/0          : delete the basemap (0)
+    --dryrun=1/0        : dont actully delete (0)
     --help              : show this message	
 ---------------------------------------------------------------------
 	
@@ -131,6 +135,7 @@ require_once('geograph/gridimage.class.php');
 require_once('geograph/gridsquare.class.php');
 require_once('geograph/map.class.php');
 require_once('geograph/image.inc.php');
+require_once('geograph/mapmosaic.class.php');
 
 $db = NewADOConnection($GLOBALS['DSN']);
 
@@ -138,54 +143,38 @@ $start_time = time();
 
 $end_time = $start_time + (60*$param['timeout']);
 
-#$map=new GeographMap;
-			
+
 while (1) {
 
 	$invalid_maps = $db->GetOne("select count(*) from mapcache where age > 0 and type_or_user >= -1");
 
 	if ($invalid_maps) {
-		//done as many small select statements to allow new maps to be processed 
-		$recordSet = &$db->Execute("select * from mapcache where age > 0 and type_or_user >= -1
-			order by pixels_per_km desc, age desc limit 50");
-		while (!$recordSet->EOF) 
-		{
-			//sleep until calm if we've specified a load average
-			if ($param['load']<100)
-			{
-				while (get_loadavg() > $param['load'])
-				{
-					sleep($param['sleep']);
-					if (time()>$end_time) 
-						exit;	
-
-				}
-			}
-
-			$map=new GeographMap;
-			
-			foreach($recordSet->fields as $name=>$value)
-			{
-				if (!is_numeric($name))
-					$map->$name=$value;
-			}
-
-			$ok = $map->_renderMap();
-				
-			echo (($ok?'re-rendered ':'FAILED: ').$map->getImageFilename()."\n");
-			flush();
-			
-			
-			if (time()>$end_time) {
-				$recordSet->Close(); 
-				//well come to the end of the scripts useful life
-				exit;	
-			}
-
-			$recordSet->MoveNext();
-
-		}
 		
+		//sleep until calm if we've specified a load average
+		if ($param['load']<100)
+		{
+			while (get_loadavg() > $param['load'])
+			{
+				sleep($param['sleep']);
+				if (time()>$end_time) 
+					exit;
+			}
+		}
+
+		//mysql might of closed the connection in the meantime if we reuse the same object
+		$mosaic = new GeographMapMosaic;
+		
+		$crit = " age > 0 and type_or_user >= -1
+			order by pixels_per_km desc, age desc limit 50";
+
+		$count = $mosaic->deleteBySql($crit,$param['dryrun'],$param['base']);
+		print "Deleted $count\n";
+
+		if (time()>$end_time) {
+			//well come to the end of the scripts useful life
+			exit;
+		}
+
 	} else {
 		//nothing more to do here
 
@@ -194,14 +183,12 @@ while (1) {
 	
 	//sleep anyway for a bit
 	sleep($param['sleep']);
-					
+	
 	if (time()>$end_time) {
 		//retreat and let the next recruit take the strain
-		exit;	
+		exit;
 	}
-}	
+}
 
- 
 
-	
 ?>
