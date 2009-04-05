e43acf05d1be5292d4d8fa2a10a5f437b8659254
geograph <1413156+barryhunter@users.noreply.github.com>
2009-04-05 19:47:34 +0000

    new stuff

---
+++ system/proxyserver/var/www/sitedown/index_dns.php
@@ -0,0 +1,77 @@
+<?
+header("HTTP/1.1 503 Service Unavailable");
+?><html>
+<head>
+<title>Geograph.org.uk - Temporarily Offline</title>
+
+<style type="text/css">
+
+body
+{
+background:#cccccc;
+}
+
+#msg
+{
+padding:30px;
+margin:30px;
+border: 2px solid black;
+background:white;
+}
+
+h1
+{
+font-family:Georgia,Arial;
+}
+
+p
+{
+font-family:Arial;
+}
+
+</style>
+
+</head>
+<body>
+
+<div id="msg">
+
+<h1>Temporarily Offline</h1>
+
+<p>Geograph is temporarily offline - some technical issues with our
+DNS provider have broken the site. We're working to restore things
+as soon as we possibly can!</p>
+
+<p>Our DNS provider, 123-reg, have written:</p>
+
+<p style="margin-left:30px;font-style:italic">
+We are currently aware of a problem regarding 123-reg nameservers which in turn is affecting the visibility of websites and services such as email and web forwarding.
+<br><br>
+
+At present, we have engineers looking into this as a matter of urgency.
+<br><br>
+
+Please be assured we are working to rectify this issue as a priority and hope to resume normal service as soon as possible.
+<br><br>
+
+Thank you for your patience in the mean time.
+<br><br>
+
+We apologise for any inconvenience caused.
+<br><br>
+</p>
+
+<p>Bear with us folks!</p>
+
+<p>Paul Dixon<br>
+Sat 17th Nov, 10:30</p>
+
+
+<p><b>Update: Sat 13:45.</b> The issue has disappeared from 123-reg's status page, but we will wait until matters stabalize and we can be sure there was no damage before reopening.</p>
+
+<p><b>Update: Sat 16:10. The issue is still ongoing and 123-reg's servers are not reliable. Its again listed as a issue (with the same wording as above) - so will just have to wait and see.</p>
+
+</div>
+
+</body>
+</html>
