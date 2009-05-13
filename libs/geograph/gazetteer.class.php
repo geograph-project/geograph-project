cdbbf5ff0abfbfd463aad688237b6d0c885e7c96
barry <1413156+barryhunter@users.noreply.github.com>
2009-05-13 12:37:40 +0000

    bunch of minor updates

--- libs/geograph/gazetteer.class.php
+++ libs/geograph/gazetteer.class.php
@@ -653,7 +653,7 @@ class Gazetteer
 					}
 			                $places[$id]['full_name'] = _utf8_decode($row['full_name']);
 				}
-				if ($c > 14) {
+				if ($c > 4) {
 					$placename = strtolower($placename);
 					foreach($places as $id => $row) {
 						$p1 = strtolower($row['full_name']);
