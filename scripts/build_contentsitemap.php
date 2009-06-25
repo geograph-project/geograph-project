1e0267c9340a0e46742a3f8b5b0d6443d6fc603c
barry <1413156+barryhunter@users.noreply.github.com>
2009-06-25 19:13:28 +0000

    Tidy up sitemap generation, in particular: * Split out KML files to own sitemaps - mainly so can use geo extensions - maybe depreciate later anyway * Split users out into own sitemap - just for tidyness * Add a XML version of the content sitemap * Rename the wrapper

--- scripts/build_contentsitemap.php
+++ scripts/build_contentsitemap.php
@@ -81,12 +81,12 @@ require_once('geograph/global.inc.php');
 
 $db = NewADOConnection($GLOBALS['DSN']);
 
-//this upper limit is set by google
-$urls_per_sitemap=50000;
+//set this low - to try it out...
+$urls_per_sitemap=1000;
 
 //how many sitemap files must we write?
-printf("Counting images...\r");
-$images=$db->GetOne("select count(*) from gridimage_search");
+printf("Counting users...\r");
+$images=$db->GetOne("select count(*) from content where source != 'themed'");
 $sitemaps=ceil($images / $urls_per_sitemap);
 
 //go through each sitemap file...
@@ -95,56 +95,38 @@ $count=0;
 for ($sitemap=1; $sitemap<=$sitemaps; $sitemap++)
 {
 	//prepare output file and query
-	printf("Preparing sitemap %d of %d, %d%% complete...\r", $sitemap, $sitemaps,$percent);
+	printf("Preparing user sitemap %d of %d, %d%% complete...\r", $sitemap, $sitemaps,$percent);
 		
-	$filename=sprintf('%s/public_html/sitemap/root/sitemap%04d.xml', $param['dir'], $sitemap); 
+	$filename=sprintf('%s/public_html/sitemap/root/sitemap-content%04d.xml', $param['dir'], $sitemap); 
 	$fh=fopen($filename, "w");
 	
 	fprintf($fh, '<?xml version="1.0" encoding="UTF-8"?>'."\n");
 	fprintf($fh, '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'."\n");
 	
 	
-	$filename2=sprintf('%s/public_html/sitemap/root/sitemap-geo%04d.xml', $param['dir'], $sitemap); 
-	$fh2=fopen($filename2, "w");
-
-	fprintf($fh2, '<?xml version="1.0" encoding="UTF-8"?>'."\n");
-	fprintf($fh2, '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:geo="http://www.google.com/geo/schemas/sitemap/1.0>'."\n");
-	
-	
 	$maxdate="";
 	
 	$offset=($sitemap-1)*$urls_per_sitemap;
 	$recordSet = &$db->Execute(
-		"select i.gridimage_id,date(upd_timestamp) as moddate ".
-		"from gridimage_search as i ".
-		"order by i.gridimage_id ".
+		"select url,date(updated) as updated ".
+		"from content where source != 'themed' ".
 		"limit $offset,$urls_per_sitemap");
 	
 	//write one <url> line per result...
 	while (!$recordSet->EOF) 
 	{
 		//figure out most recent update
-		$date=$recordSet->fields['moddate'];
+		$date=$recordSet->fields['updated'];
 		
 		if (strcmp($date,$maxdate)>0)
 			$maxdate=$date;
 		
 		fprintf($fh,"<url>".
-			"<loc>http://{$param['config']}/photo/%d</loc>".
+			"<loc>http://{$param['config']}%s</loc>".
 			"<lastmod>%s</lastmod>".
-			"<changefreq>yearly</changefreq><priority>0.8</priority>".
+			"<changefreq>monthly</changefreq>".
 			"</url>\n",
-			$recordSet->fields['gridimage_id'],
-			$date
-			);
-			
-		fprintf($fh2,"<url>".
-			"<loc>http://{$param['config']}/photo/%d.kml</loc>".
-			"<lastmod>%s</lastmod>".
-			"<changefreq>yearly</changefreq><priority>0.5</priority>".
-			"<geo:geo><geo:format>kml</geo:format></geo:geo>".
-			"</url>\n",
-			$recordSet->fields['gridimage_id'],
+			$recordSet->fields['user_id'],
 			$date
 			);
 			
@@ -153,7 +135,7 @@ for ($sitemap=1; $sitemap<=$sitemaps; $sitemap++)
 		if ($percent!=$last_percent)
 		{
 			$last_percent=$percent;
-			printf("Writing sitemap %d of %d, %d%% complete...\r", $sitemap, $sitemaps,$percent);
+			printf("Writing content sitemap %d of %d, %d%% complete...\r", $sitemap, $sitemaps,$percent);
 		}	
 	
 		
@@ -166,49 +148,16 @@ for ($sitemap=1; $sitemap<=$sitemaps; $sitemap++)
 	fprintf($fh, '</urlset>');
 	fclose($fh); 
 	
-	fprintf($fh2, '</urlset>');
-	fclose($fh2); 
-	
-	
 	//set datestamp on file
-	$unixtime=strtotime("$maxdate 00:00:00");
+	$unixtime=strtotime($maxdate);
 	touch($filename,$unixtime);
-	touch($filename2,$unixtime);
 	
 	//gzip it
 	`gzip $filename`;
-	`gzip $filename2`;
 }
-
-//now we write an index file pointing to our hand edited sitemap sitemap0000.xml)
-//and our generated ones above
-$filename=sprintf('%s/public_html/sitemap/root/sitemap.xml', $param['dir']); 
-$fh=fopen($filename, "w");
-
-fprintf($fh, '<?xml version="1.0" encoding="UTF-8"?>'."\n");
-fprintf($fh, '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'."\n");
-
-for ($s=0; $s<=$sitemaps; $s++)
-{
-	fprintf($fh, "<sitemap>");
-	
-	//first file is not compressed...
-	$fname=($s==0)?"sitemap0000.xml":sprintf("sitemap%04d.xml.gz", $s);
-	
-	$mtime=filemtime($param['dir']."/public_html/sitemap/root/".$fname);
-	$mtimestr=strftime("%Y-%m-%dT%H:%M:%S+00:00", $mtime);
-	
-	fprintf($fh, "<loc>http://{$param['config']}/%s</loc>", $fname);
-	fprintf($fh, "<lastmod>$mtimestr</lastmod>", $fname);
-	fprintf($fh, "</sitemap>\n");
-}
-
-fprintf($fh, '</sitemapindex>');
-fclose($fh); 
-
 
 //now we write an index file pointing to our generated ones above
-$filename=sprintf('%s/public_html/sitemap/root/sitemap-geo.xml', $param['dir']); 
+$filename=sprintf('%s/public_html/sitemap/root/sitemap-content.xml', $param['dir']); 
 $fh=fopen($filename, "w");
 
 fprintf($fh, '<?xml version="1.0" encoding="UTF-8"?>'."\n");
@@ -218,7 +167,7 @@ for ($s=1; $s<=$sitemaps; $s++)
 {
 	fprintf($fh, "<sitemap>");
 	
-	$fname=sprintf("sitemap-geo%04d.xml.gz", $s);
+	$fname=sprintf("sitemap-content%04d.xml.gz", $s);
 	
 	$mtime=filemtime($param['dir']."/public_html/sitemap/root/".$fname);
 	$mtimestr=strftime("%Y-%m-%dT%H:%M:%S+00:00", $mtime);
