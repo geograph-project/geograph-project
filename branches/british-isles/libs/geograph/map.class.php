2ce1788c32b869d3d75fd33f42632ebcf78e4208
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2009-07-05 03:49:51 +0000

    Store the errors in the table!!!

--- branches/british-isles/libs/geograph/map.class.php
+++ branches/british-isles/libs/geograph/map.class.php
@@ -552,13 +552,15 @@ class GeographMap
 		} 
 
 		//save it for rerendering later. 
-		if ($ok) {
+		//if ($ok) {
 			$db=&$this->_getDB();
+			
+			$age = ($ok)?0:-1;
 
-			$sql=sprintf("replace into mapcache set map_x=%d,map_y=%d,image_w=%d,image_h=%d,pixels_per_km=%f,type_or_user=%d,palette=%d",$this->map_x,$this->map_y,$this->image_w,$this->image_h,$this->pixels_per_km,$this->type_or_user,$this->palette);
+			$sql=sprintf("replace into mapcache set map_x=%d,map_y=%d,image_w=%d,image_h=%d,pixels_per_km=%f,type_or_user=%d,palette=%d,age=%d",$this->map_x,$this->map_y,$this->image_w,$this->image_h,$this->pixels_per_km,$this->type_or_user,$this->palette,$age);
 
 			$db->Execute($sql);
-		}
+		//}
 		return $ok;
 	}
 	
