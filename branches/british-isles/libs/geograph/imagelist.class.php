bc3ccdc0468d11ac9bbead3b0432760064f74c5d
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2009-09-12 09:54:40 +0000

    use read only connection

--- branches/british-isles/libs/geograph/imagelist.class.php
+++ branches/british-isles/libs/geograph/imagelist.class.php
@@ -370,9 +370,7 @@ class ImageList
 	 */
 	function &_getDB()
 	{
-		if (!is_object($this->db))
-			$this->db=NewADOConnection($GLOBALS['DSN']);
-		if (!$this->db) die('Database connection failed');  
+		$db = GeographDatabaseConnection(true);
 		return $this->db;
 	}
 
