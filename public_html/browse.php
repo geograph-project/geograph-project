ec85c640e058f6bea611fb20a3a5566a702d2d38
barry <1413156+barryhunter@users.noreply.github.com>
2009-11-05 22:48:49 +0000

    better bugfix

--- public_html/browse.php
+++ public_html/browse.php
@@ -644,8 +644,8 @@ if ($grid_given)
 				}
 			} elseif ($_GET['by'] == 'user') {
 				$breakdown_title = "Contributor";
-				$all = $db->cacheGetAll($cacheseconds,"SELECT user.realname AS user_realname,COUNT(*) AS count,user_id,
-				gridimage_id $columns
+				$all = $db->cacheGetAll($cacheseconds,"SELECT user.realname AS user_realname,COUNT(*) AS count,
+				gridimage_id, user_id, $columns
 				FROM gridimage gi INNER JOIN user USING(user_id)
 				WHERE gridsquare_id = '{$square->gridsquare_id}'
 				AND $user_crit $custom_where
