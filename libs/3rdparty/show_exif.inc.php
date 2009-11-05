f9d9eb581ef55eb83a51bfed5fd4d44298fc0501
barry <1413156+barryhunter@users.noreply.github.com>
2009-11-05 22:33:04 +0000

    new version from rudi

--- libs/3rdparty/show_exif.inc.php
+++ libs/3rdparty/show_exif.inc.php
@@ -30,8 +30,6 @@
 * @version $Revision$
 */
 
-#based on http://users.aber.ac.uk/ruw/geograph/exif_kml.php
-
 /*
 string angle_kml($exif,$ee,$nn,$len,$vdir,$lat,$lon,$filename,$reference_index)
 version: 090711
@@ -55,7 +53,7 @@ function angle_kml($exif,$ee,$nn,$len,$vdir,$lat,$lon,$filename='',$reference_in
   $fract=explode('/',$focalLength);
   $focalLength=$fract[0]/$fract[1];
 
-  $fpxr=($exif['EXIF']['FocalPlaneXResolution']) or die('<p><b>Error: Can\'t determine focal place resolution.</b>');
+  $fpxr=($exif['EXIF']['FocalPlaneXResolution']) or die('<p><b>Error: Can\'t determine focal plane resolution.</b>');
   $fract=explode('/',$fpxr);
   $fpxr=$fract[0]/$fract[1];
 
@@ -115,6 +113,7 @@ function angle_kml($exif,$ee,$nn,$len,$vdir,$lat,$lon,$filename='',$reference_in
     printf("      <tilt>80.</tilt>\n");
     printf("    </Camera>\n");
     printf("    <Placemark>\n");    // view cone
+    printf("      <name>view cone</name>\n");
     printf("      <Style>\n");
     printf("        <LineStyle>\n");
     printf("          <color>aa0000ff</color>\n");
@@ -129,6 +128,7 @@ function angle_kml($exif,$ee,$nn,$len,$vdir,$lat,$lon,$filename='',$reference_in
     printf("      </LineString>\n");
     printf("    </Placemark>\n");
     printf("    <Placemark>\n");    // subject
+    printf("      <name>subject</name>\n");
     printf("      <Point>\n");
     printf("        <coordinates>$lon,$lat,0</coordinates>\n");
     printf("      </Point>\n");
@@ -143,9 +143,6 @@ function angle_kml($exif,$ee,$nn,$len,$vdir,$lat,$lon,$filename='',$reference_in
 }
 
 
-
-#based on http://users.aber.ac.uk/ruw/geograph/show_exif.inc
-
 /*
 integer show_exif(string $exif_array)
 exif_array - Raw EXIF data from exif_read_data
@@ -154,6 +151,7 @@ return value: Number of exif tags successfully processed
 function show_exif($exif_array) {
   // list of exif tags to be processed - show_exif() silently ignores any that aren't present in the file
   $relevant=array(
+    'Make'
     'Model',
     'CCDWidth',
     'ExposureTime',
@@ -171,68 +169,82 @@ function show_exif($exif_array) {
   // find relevant tags
   foreach ($relevant as $tag) {
     // look up non-numerical values, and add units where appropriate
-    if     ($tag=='ExposureTime') $value=$exif[$tag].' s';
-    elseif ($tag=='FNumber') {
-      $fract=explode('/',$exif[$tag]);
-      $value=$fract[0]/$fract[1];
-      $value='f/'.$value;
-    }
-    elseif ($tag=='ExposureProgram') {
-      if     ($exif[$tag]==1) $value='manual';
-      elseif ($exif[$tag]==2) $value='normal';
-      elseif ($exif[$tag]==3) $value='aperure priority';
-      elseif ($exif[$tag]==4) $value='shutter priority';
-      elseif ($exif[$tag]==5) $value='creative (biased towards depth of field)';
-      elseif ($exif[$tag]==6) $value='action (biased towards shutter speed)';
-      elseif ($exif[$tag]==7) $value='portrait (closeup with background out of focus)';
-      elseif ($exif[$tag]==8) $value='landscape (background in focus)';
-      else                    $value=null;
-    }
-    elseif ($tag=='MeteringMode') {
-      if     ($exif[$tag]==1) $value='average';
-      elseif ($exif[$tag]==2) $value='centre-weighted average';
-      elseif ($exif[$tag]==3) $value='spot';
-      elseif ($exif[$tag]==4) $value='multi-spot';
-      elseif ($exif[$tag]==5) $value='pattern';
-      elseif ($exif[$tag]==6) $value='partial';
-      else                    $value=null;
-    }
-    elseif ($tag=='LightSource') {
-      if     ($exif[$tag]== 1) $value='daylight';
-      elseif ($exif[$tag]== 2) $value='fluorescent';
-      elseif ($exif[$tag]== 3) $value='tungsten (incandescent)';
-      elseif ($exif[$tag]== 4) $value='flash';
-      elseif ($exif[$tag]== 9) $value='fine weather';
-      elseif ($exif[$tag]==10) $value='cloudy';
-      elseif ($exif[$tag]==11) $value='shade';
-      elseif ($exif[$tag]==12) $value='daylight fluorescent (7100K)';
-      elseif ($exif[$tag]==13) $value='day white fluorescent (5400K)';
-      elseif ($exif[$tag]==14) $value='cool white fluorescent (4500K)';
-      elseif ($exif[$tag]==15) $value='white fluorescent (3700K)';
-      elseif ($exif[$tag]==17) $value='standard light A';  // whatever that means!
-      elseif ($exif[$tag]==18) $value='standard light B';
-      elseif ($exif[$tag]==19) $value='standard light C';
-      elseif ($exif[$tag]==20) $value='D55';
-      elseif ($exif[$tag]==21) $value='D65';
-      elseif ($exif[$tag]==22) $value='D75';
-      elseif ($exif[$tag]==23) $value='D50';
-      elseif ($exif[$tag]==24) $value='ISO studio tungsten';
-      else                     $value=null;
-    }
-    elseif ($tag=='Flash') $exif[$tag]&1 ? $value='on' : $value='off';  // bit 8 indicates if flash was on; rest ignored here
-    elseif ($tag=='FocalLength') {
-      $fract=explode('/',$exif[$tag]);
-      $value=$fract[0]/$fract[1];
-      $value=$value.'mm (actual, <b>not</b> 35mm equivalent)';
-    }
-    else $value=$exif[$tag];
-    if ($tag=='FNumber') $tag='Aperture (F number)';
-    // show result
-    if ($value) {
-      echo "<dt>$tag</dt>\n<dd>$value</dd>\n";
-      $count++;
+    if ($exif[$tag]) {
+      $found++;
+      if     ($tag=='ExposureTime') {
+        $fract=explode('/',$exif[$tag]);
+        if ($fract[0]==1) $value=$exif[$tag].' s';
+        elseif ($fract[0]==10) {
+          $value=(int)($fract[1]/10);
+          $value='1/'.$value.' s';
+        } else {
+          $value=(int)(1000*$fract[0]/$fract[1]);
+          $value=$value.' ms';
+        }
+      }
+      elseif ($tag=='FNumber') {
+        $fract=explode('/',$exif[$tag]);
+        $value=$fract[0]/$fract[1];
+        $value=sprintf('f/%3.1f',$value);
+      }
+      elseif ($tag=='ExposureProgram') {
+        if     ($exif[$tag]==1) $value='manual';
+        elseif ($exif[$tag]==2) $value='normal';
+        elseif ($exif[$tag]==3) $value='aperure priority';
+        elseif ($exif[$tag]==4) $value='shutter priority';
+        elseif ($exif[$tag]==5) $value='creative (biased towards depth of field)';
+        elseif ($exif[$tag]==6) $value='action (biased towards shutter speed)';
+        elseif ($exif[$tag]==7) $value='portrait (closeup with background out of focus)';
+        elseif ($exif[$tag]==8) $value='landscape (background in focus)';
+        else                    $value=null;
+      }
+      elseif ($tag=='MeteringMode') {
+        if     ($exif[$tag]==1) $value='average';
+        elseif ($exif[$tag]==2) $value='centre-weighted average';
+        elseif ($exif[$tag]==3) $value='spot';
+        elseif ($exif[$tag]==4) $value='multi-spot';
+        elseif ($exif[$tag]==5) $value='pattern';
+        elseif ($exif[$tag]==6) $value='partial';
+        else                    $value=null;
+      }
+      elseif ($tag=='LightSource') {
+        if     ($exif[$tag]== 1) $value='daylight';
+        elseif ($exif[$tag]== 2) $value='fluorescent';
+        elseif ($exif[$tag]== 3) $value='tungsten (incandescent)';
+        elseif ($exif[$tag]== 4) $value='flash';
+        elseif ($exif[$tag]== 9) $value='fine weather';
+        elseif ($exif[$tag]==10) $value='cloudy';
+        elseif ($exif[$tag]==11) $value='shade';
+        elseif ($exif[$tag]==12) $value='daylight fluorescent (7100K)';
+        elseif ($exif[$tag]==13) $value='day white fluorescent (5400K)';
+        elseif ($exif[$tag]==14) $value='cool white fluorescent (4500K)';
+        elseif ($exif[$tag]==15) $value='white fluorescent (3700K)';
+        elseif ($exif[$tag]==17) $value='standard light A';  // whatever that means!
+        elseif ($exif[$tag]==18) $value='standard light B';
+        elseif ($exif[$tag]==19) $value='standard light C';
+        elseif ($exif[$tag]==20) $value='D55';
+        elseif ($exif[$tag]==21) $value='D65';
+        elseif ($exif[$tag]==22) $value='D75';
+        elseif ($exif[$tag]==23) $value='D50';
+        elseif ($exif[$tag]==24) $value='ISO studio tungsten';
+        else                     $value=null;
+      }
+      elseif ($tag=='Flash') $exif[$tag]&1 ? $value='on' : $value='off';  // bit 8 indicates if flash was on; rest ignored here
+      elseif ($tag=='FocalLength') {
+        $fract=explode('/',$exif[$tag]);
+        $value=$fract[0]/$fract[1];
+        $value=sprintf('%3.1fmm (actual, <b>not</b> 35mm equivalent)',$value);
+      }
+      else $value=$exif[$tag];
+      if ($tag=='FNumber') $tag='Aperture (F number)';
+      // show result
+      if ($value) {
+        echo "<dt>$tag</dt>\n<dd>$value</dd>\n";
+        $count++;
+      }
     }
   }
+  if (!$count) echo "<p>Geograph has no EXIF data for this image.</p>\n";
   return $count;
 }
 
