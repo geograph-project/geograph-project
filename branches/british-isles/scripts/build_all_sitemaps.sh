af2fdd261439b222ab1b51f70cfb31ebe33745c6
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2010-04-13 21:15:46 +0000

    mistaken commit

--- branches/british-isles/scripts/build_all_sitemaps.sh
+++ branches/british-isles/scripts/build_all_sitemaps.sh
@@ -3,10 +3,10 @@
 
 ############
 
-sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_sitemap.php --dir=/var/www/geograph_live --secret=images4google
+sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_sitemap.php --dir=/var/www/geograph_live --secret=imagesitemap
 GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap.xml > /dev/null
 GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap-geo.xml > /dev/null
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap-images4google.xml > /dev/null
+GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap-imagesitemap.xml > /dev/null
 
 sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_usersitemap.php --dir=/var/www/geograph_live
 GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap-user.xml > /dev/null
@@ -21,10 +21,10 @@ sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_snippetsitema
 GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap-snippet.xml > /dev/null
 
 
-sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_sitemap.php --dir=/var/www/geograph_live --config=www.geograph.ie --ri=2 --suffix=.ie --secret=images4google
+sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_sitemap.php --dir=/var/www/geograph_live --config=www.geograph.ie --ri=2 --suffix=.ie --secret=imagesitemap
 GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.ie%2Fsitemap.ie.xml > /dev/null
 GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.ie%2Fsitemap-geo.ie.xml > /dev/null
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.ie%2Fsitemap-image4google.ie.xml > /dev/null
+GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.ie%2Fsitemap-imagesitemap.ie.xml > /dev/null
 
 sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_usersitemap.ie.php --dir=/var/www/geograph_live --config=www.geograph.ie
 GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.ie%2Fsitemap-user.ie.xml > /dev/null
