51065e178ae3c5124e39d1e66eafb40910630cc1
barry <1413156+barryhunter@users.noreply.github.com>
2010-04-14 00:35:40 +0000

    Experimental Ranking Testing interface

---
+++ public_html/finder/modes.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id: faq.php 15 2005-02-16 12:23:35Z lordelph $
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 Paul Dixon (paul@elphin.com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once('geograph/global.inc.php');
+init_session();
+
+if (!empty($_GET['rating'])) {
+
+	$db=NewADOConnection($GLOBALS['DSN']);
+	if (!$db) die('Database connection failed');
+
+	$ins = "INSERT INTO search_ranking_log SET
+		mode = ".intval(@$_GET['mode']).",
+		q = ".$db->Quote(@$_GET['q']).",
+		comment = ".$db->Quote(@$_GET['comment']).",
+		rating = ".intval(@$_GET['rating']).",
+		ipaddr = INET_ATON('".getRemoteIP()."'),
+		user_id = ".intval($USER->user_id);
+
+	$db->Execute($ins);
+	
+	die("thanks!");
+}
+
+
+$smarty = new GeographPage;
+if (!empty($_GET['q'])) {
+	$q = isset($_GET['q'])?$_GET['q']:'';
+
+	$q = preg_replace('/[^\w=]+/',' ',trim(strtolower($q)));
+	$smarty->assign("q",$q);
+	
+	$words = preg_split('/\s+/',$q);
+	if (count($words) > 1) {
+		
+		$inners = array();
+		foreach (range(1,12) as $key) {
+			$inners[] = array('url'=>"/finder/search-service.php?feedback&mode=$key&q=".urlencode($q));
+		}
+		shuffle($inners);
+		$smarty->assign_by_ref("inners",$inners);
+		$smarty->assign("count_inners",count($inners));
+	}
+}
+
+$smarty->display('finder_modes.tpl');
+
--- public_html/templates/basic/search_service.tpl
+++ public_html/templates/basic/search_service.tpl
@@ -1,7 +1,7 @@
 {assign var="page_title" value="Search Results"}
 
 {include file="_basic_begin.tpl"}
-{if $inner}
+{if $inner || $feedback}
 	<br/>
 {else}
 <div style="text-align:right">
@@ -37,7 +37,33 @@
 	{if $query_info}
 	<p style="clear:both">{$query_info}</p>
 	{/if}
-{if $inner}
+{if $feedback} 
+	<hr/>
+	<b>Rate this set of results!</b><br/>
+	<form method="get" action="/finder/modes.php?sdf=sdf" target="resultsframe" onsubmit="{literal}that=this;setTimeout(function() {that.innerHTML='Thanks!'},1000);return true;{/literal}">
+		<label for="rating">Rating</label><br/>
+		<select name="rating">
+			<option value="10">10 - Excellent</option>
+			<option value="9">9</option>
+			<option value="8">8 - Reasonable</option>
+			<option value="7">7</option>
+			<option value="6">6</option>
+			<option value="5" selected>5 - So So</option>
+			<option value="4">2</option>
+			<option value="3">3 - Below average</option>
+			<option value="2">2</option>
+			<option value="1">1 - Hmm</option>
+		</select><br/><br/>
+		<input type="hidden" name="mode" value="{$mode|escape:'html'}"/>
+		<input type="hidden" name="q" value="{$q|escape:'html'}"/>
+		<label for="comment"><i>Optional</i> comment</label><br/>
+		<input type="text" name="comment" value="" id="comment" maxlength="160"/><br/><br/>
+		
+		<input type="submit" name="test" value="submit feedback"/>
+	</form>
+	
+	<iframe src="about:blank" width="1" height="1" name="resultsframe"></iframe>
+{elseif $inner}
 	<a href="/search.php?q={$searchq|escape:"url"}" target="_parent">Try this query in the Full Search</a> - Might be non functional
 {/if}
 </div>
