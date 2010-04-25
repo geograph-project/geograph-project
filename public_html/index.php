ef7fc356a4f1f2b153998d5f4fb88836e5154bf3
barry <1413156+barryhunter@users.noreply.github.com>
2010-04-25 21:52:10 +0000

    More tweaks

--- public_html/index.php
+++ public_html/index.php
@@ -49,36 +49,14 @@ if (!$smarty->is_cached($template, $cacheid))
 	require_once('geograph/map.class.php');
 	require_once('geograph/mapmosaic.class.php');
 
-	switch($CONF['template']) {
-		case 'charcoal': $preset = 'overview_charcoal'; break;
-		case 'ireland': $preset = 'overview_ireland'; break;
-		default: $preset = 'overview_large'; break;
-	}
+	$preset = 'overview';
+	
 	$overview=new GeographMapMosaic($preset);
 	$overview->type_or_user = 0;
-	if ($preset == 'overview_large') {
-		$overview->assignToSmarty($smarty, 'overview2');
-	} else {
-		$overview->assignToSmarty($smarty, 'overview');
-	}
+	$overview->assignToSmarty($smarty, 'overview');
 	
 	
-	require_once('geograph/pictureoftheday.class.php');
-	$potd=new PictureOfTheDay;
-	if (isset($_GET['potd'])) {
-		$potd->assignToSmarty($smarty,intval($_GET['potd'])); 
-	} else {
-		$potd->assignToSmarty($smarty); 
-	}
-	
-	
-	//lets find some recent photos
-	if ($CONF['template']=='ireland') {
-		new RecentImageList($smarty,2);
-	} else {
-		$smarty->assign('marker', $overview->getSquarePoint($potd->image->grid_square));
-		new RecentImageList($smarty);
-	}
+	new RecentImageList($smarty);
 	
 	
 	$db=NewADOConnection($GLOBALS['DSN']);
@@ -105,6 +83,8 @@ if (!$smarty->is_cached($template, $cacheid))
 	
 
 		$smarty->assign_by_ref('news', $news);
+	
+		$smarty->assign('rss_url','/discuss/syndicator.php?forum='.$CONF['forum_announce'].'&amp;first=1');
 	}
 	
 	$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
@@ -119,17 +99,6 @@ if (!$smarty->is_cached($template, $cacheid))
 	//$stats['percentage'] = sprintf("%.2f",$stats['squares']/$stats['total']*100);
 	$smarty->assign_by_ref('stats', $stats);
 	
-	$smarty->assign('rss_url','/discuss/syndicator.php?forum='.$CONF['forum_announce'].'&amp;first=1');
-	
-	$smarty->assign('messages', array(
-		0=>'click map to zoom in',
-		1=>'click me and explore!',
-		2=>'I\'m zoomable - click me',
-		4=>'',
-		5=>'click to see bigger map',
-		6=>'click for more detail'));
-		
-	$smarty->assign('m',rand(0,6));	
 }
 
 
--- public_html/templates/basic/admin_team.tpl
+++ public_html/templates/basic/admin_team.tpl
@@ -3,7 +3,7 @@
 
 <h3>the Geograph Team</h3>
 
-This is the Team from Geograph Britain and Ireland, which for the moment also maintain Geograph Channel Islands.
+<p>This is the Team from Geograph Britain and Ireland, which for the moment also maintain Geograph Channel Islands.</p>
 
 {if $team}
 
@@ -23,29 +23,6 @@ This is the Team from Geograph Britain and Ireland, which for the moment also ma
 	<td>{$userrow.realname}</td>
 	<td>{$userrow.nickname}</td>
 	<td>{if $userrow.role}{$userrow.role}{else}{if strpos($userrow.rights,'admin') > 0}Developer{else}Moderator{/if}{/if}</td>
-	<td><a href="/profile/{$userrow.user_id}">Profile</a>/<a href="/usermsg.php?to={$userrow.user_id}">Contact</a></td>
-</tr>
-{/if}
-{/foreach}
-</tbody>
-</table>
-
-<p>The following members have also helped out in various capacities previouslly...</p>
-
-<table class="report">
-<thead><tr>
-	<td>Name</td>
-	<td>Nickname</td>
-	<td>Profile/Contact</td>
-</tr></thead>
-<tbody>
-
-{foreach from=$team item=userrow}
-{if $userrow.role eq 'Member'}
-{cycle values="#f0f0f0,#e9e9e9" assign="bgcolor"}
-<tr bgcolor="{$bgcolor}"{if strpos($userrow.rights,'admin') > 0} style="font-weight:bold"{/if}>
-	<td>{$userrow.realname}</td>
-	<td>{$userrow.nickname}</td>
 	<td><a href="/profile/{$userrow.user_id}">Profile</a>/<a href="/usermsg.php?to={$userrow.user_id}">Contact</a></td>
 </tr>
 {/if}
