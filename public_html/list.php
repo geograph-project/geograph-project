b96b43f707f9ff2105e3b8e71c70de4df313ad2c
barry <1413156+barryhunter@users.noreply.github.com>
2010-05-08 15:30:01 +0000

    Restore this!

--- public_html/list.php
+++ public_html/list.php
@@ -21,21 +21,76 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+require_once('geograph/global.inc.php');
+init_session();
 
-if (!empty($_GET['square'])) {
-	header("HTTP/1.0 301 Moved Permanently");
-	header("Status: 301 Moved Permanently");
-	header("Location: /sitemap/{$_GET['square']}.html");
-	print "<a href=\"http://{$_SERVER['HTTP_HOST']}/sitemap/{$_GET['square']}.html\">View page</a>";
-	exit;
-} else {
-	header("HTTP/1.0 301 Moved Permanently");
-	header("Status: 301 Moved Permanently");
-	header("Location: /sitemap/geograph.html");
-	print "<a href=\"http://{$_SERVER['HTTP_HOST']}/sitemap/geograph.html\">View page</a>";
-	exit;
+
+
+
+$smarty = new GeographPage;
+
+$template='list.tpl';
+$cacheid='list|'.substr($_GET['square'],0,2);
+
+if (isset($_GET['refresh']) && $USER->hasPerm('admin'))
+	$smarty->clear_cache($template, 'list');
+
+
+$smarty->caching = 2; // lifetime is per cache
+$smarty->cache_lifetime = 3600*12; //12hr cache
+
+//regenerate?
+if (!$smarty->is_cached($template, $cacheid))
+{
+	require_once('geograph/gridimage.class.php');
+	require_once('geograph/gridsquare.class.php');
+	require_once('geograph/imagelist.class.php');
+
+	$db=NewADOConnection($GLOBALS['DSN']);
+	if (!$db) die('Database connection failed');  
+
+	$squares=array();
+	$images=array();
+	$i=0;
+
+	$page_title='Photograph Listing';
+
+	$prefixes=$db->CacheGetAll(24*3600,"select 
+	LEFT(grid_reference,LENGTH(grid_reference)-4) as gi_prefix,
+	prefix,origin_x,width,origin_y,height,gridprefix.reference_index,gridprefix.title,
+	count(*) as imagecount
+	from gridimage_search
+	left join gridprefix on (LEFT(grid_reference,LENGTH(grid_reference)-4) = prefix)
+	where landcount>0
+	group by gi_prefix 
+	order by title");
+	foreach ($prefixes as $prefix)
+	{
+		//which method to use?
+		if ($_GET['square']==$prefix['prefix'])
+		{
+			$page_title='Photograph Listing ('.$prefix['title'].')';
+		
+			$images=new ImageList;
+		
+			$count=$images->getImagesByArea($prefix['origin_x'],$prefix['origin_x']+$prefix['width']-1,
+			$prefix['origin_y']+$prefix['height']-1,$prefix['origin_y'], $prefix['reference_index']);
+			if ($count>0)
+			{
+				$prefixes[$i]['images']=$images->images;
+			}
+			else
+			{
+				//forget about it
+				unset($images);
+			}
+		}
+		$i++;
+	}
+
+	$smarty->assign_by_ref('page_title', $page_title);
+	$smarty->assign_by_ref('squares', $prefixes);
 }
 
+$smarty->display($template, $cacheid);
 
-	
-?>
