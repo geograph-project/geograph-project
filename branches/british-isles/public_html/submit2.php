40d53f948c22b4ad1936e18b9ae0f00486e381b2
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2010-06-15 18:28:25 +0000

    Tabs display for v2

--- branches/british-isles/public_html/submit2.php
+++ branches/british-isles/public_html/submit2.php
@@ -2,24 +2,24 @@
 /**
  * $Project: GeoGraph $
  * $Id$
- * 
+ *
  * GeoGraph geographic photo archive project
  * This file copyright (C) 2008 Barry Hunter (geo@barryhunter.co.uk)
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  **/
- 
+
 require_once('geograph/global.inc.php');
 init_session();
 
@@ -29,6 +29,11 @@ $smarty = new GeographPage;
 $USER->mustHavePerm("basic");
 
 $template='submit2.tpl';
+
+if (!empty($_GET['display']) && $_GET['display'] == 'tabs') {
+	$template='submit2_tabs.tpl';
+}
+
 $cacheid='';
 
 if (!empty($_REQUEST['use_autocomplete'])) {
@@ -44,10 +49,10 @@ if (isset($_FILES['jpeg_exif']))
 	switch($_FILES['jpeg_exif']['error'])
 	{
 		case 0:
-			if (!filesize($_FILES['jpeg_exif']['tmp_name'])) 
+			if (!filesize($_FILES['jpeg_exif']['tmp_name']))
 			{
 				$smarty->assign('error', 'Sorry, no file was received - please try again');
-			} 
+			}
 			elseif ($uploadmanager->processUpload($_FILES['jpeg_exif']['tmp_name']))
 			{
 				$smarty->assign('upload_id', $uploadmanager->upload_id);
@@ -56,16 +61,16 @@ if (isset($_FILES['jpeg_exif']))
 					$smarty->assign('original_width', $uploadmanager->original_width);
 					$smarty->assign('original_height', $uploadmanager->original_height);
 				}
-				
+
 				$smarty->assign('preview_url', "/submit.php?preview=".$uploadmanager->upload_id);
 				$smarty->assign('preview_width', $uploadmanager->upload_width);
 				$smarty->assign('preview_height', $uploadmanager->upload_height);
-				
+
 				$exif = $uploadmanager->rawExifData;
-				
+
 				if (!empty($exif['GPS'])) {
 					$conv = new Conversions;
-					
+
 					if (is_array($exif['GPS']['GPSLatitude'])) {
 						$deg = FractionToDecimal($exif['GPS']['GPSLatitude'][0]);
 						$min = FractionToDecimal($exif['GPS']['GPSLatitude'][1]);
@@ -76,7 +81,7 @@ if (isset($_FILES['jpeg_exif']))
 						$lat = $exif['GPS']['GPSLatitude'];
 					}
 
-					if ($exif['GPS']['GPSLatitudeRef'] == 'S') 
+					if ($exif['GPS']['GPSLatitudeRef'] == 'S')
 						$lat *= -1;
 
 					if (is_array($exif['GPS']['GPSLongitude'])) {
@@ -89,45 +94,45 @@ if (isset($_FILES['jpeg_exif']))
 						$long = $exif['GPS']['GPSLongitude'];
 					}
 
-					if ($exif['GPS']['GPSLongitudeRef'] == 'W') 
+					if ($exif['GPS']['GPSLongitudeRef'] == 'W')
 						$long *= -1;
 
 
 					list($e,$n,$reference_index) = $conv->wgs84_to_national($lat,$long);
 
 					list ($grid_reference,$len) = $conv->national_to_gridref(intval($e),intval($n),0,$reference_index);
-					
-					$smarty->assign('photographer_gridref',$grid_reference); 
-					
+
+					$smarty->assign('photographer_gridref',$grid_reference);
+
 					list ($grid_reference,$len) = $conv->national_to_gridref(intval($e),intval($n),4,$reference_index);
-					
+
 					$smarty->assign('grid_reference', $grid_reference);
-				} 
-				
+				}
+
 				if (preg_match("/(_|\b)([B-DF-JL-OQ-TV-X]|[HNST][A-Z]|MC|OV)[ \._-]?(\d{2,5})[ \._-]?(\d{2,5})(\b|[A-Za-z_])/i",$_FILES['jpeg_exif']['name'],$m)) {
 					if (strlen($m[3]) != strlen($m[4])) {
 						if (preg_match("/(_|\b)([B-DF-JL-OQ-TV-X]|[HNST][A-Z]|MC|OV)[ \._-]?(\d{4,10})(\b|[A-Za-z_])/i",$_FILES['jpeg_exif']['name'],$m)) {
 							if (strlen($m[3])%2==0) {
-								$smarty->assign('grid_reference', $grid_reference = $m[2].$m[3]); 
+								$smarty->assign('grid_reference', $grid_reference = $m[2].$m[3]);
 							}
 						}
 					} else {
-						$smarty->assign('grid_reference', $grid_reference = $m[2].$m[3].$m[4]); 
+						$smarty->assign('grid_reference', $grid_reference = $m[2].$m[3].$m[4]);
 					}
-		
+
 				} elseif (!empty($exif['COMMENT']) && preg_match("/\b([B-DF-JL-OQ-TV-X]|[HNST][A-Z]|MC|OV)[ \._-]?(\d{2,5})[ \._-]?(\d{2,5})(\b|[A-Za-z_])/i",implode(' ',$exif['COMMENT']),$m)) {
 					if (strlen($m[2]) == strlen($m[3]) || (strlen($m[2])+strlen($m[3]))%2==0) {
-						$smarty->assign('grid_reference', $grid_reference = $m[1].$m[2].$m[3]); 
+						$smarty->assign('grid_reference', $grid_reference = $m[1].$m[2].$m[3]);
 					}
 				}
-				
+
 				if (isset($uploadmanager->exifdate)) {
 					$smarty->assign('imagetaken', $uploadmanager->exifdate);
 				}
-				
+
 				$smarty->assign('filename',basename(str_replace("\\",'/',$_FILES['jpeg_exif']['name'])));
 
-				
+
 				$smarty->assign('success', 1);
 			} else {
 				$smarty->assign('error', $uploadmanager->errormsg);
@@ -157,13 +162,13 @@ if (isset($_FILES['jpeg_exif']))
 			$smarty->assign('error', 'We were unable to process your upload - please try again');
 			break;
 	}
-	
-	
-	
-} elseif (isset($_POST['finalise'])) {  
+
+
+
+} elseif (isset($_POST['finalise'])) {
 	$status = array();
 	$filenames = array();
-	
+
 	foreach ($_POST['upload_id'] as $key => $upload_id) {
 		$uploadmanager = new UploadManager();
 		$square = new GridSquare();
@@ -184,12 +189,12 @@ if (isset($_FILES['jpeg_exif']))
 				//bug? in Picasa sends the name in the value if blank, useful! (but only seems to apply to textareas)
 				$uploadmanager->setComment($_POST['comment'][$key]);
 			}
-			
+
 			if (($_POST['imageclass'][$key] == 'Other' || empty($_POST['imageclass'][$key])) && !empty($_POST['imageclassother'][$key])) {
 				$imageclass = stripslashes($_POST['imageclassother'][$key]);
 			} else if ($_POST['imageclass'] != 'Other') {
 				$imageclass =  stripslashes($_POST['imageclass'][$key]);
-			}			
+			}
 			$uploadmanager->setClass($imageclass);
 
 			if ($_POST['pattrib'] == 'other') {
@@ -204,7 +209,7 @@ if (isset($_FILES['jpeg_exif']))
 			if ($ok) {
 				$err = $uploadmanager->commit(isset($_GET['nofrills'])?'nofrills':'submit2');
 
-				if (empty($err)) { 
+				if (empty($err)) {
 					$status[$key] = "ok:".$uploadmanager->gridimage_id;
 				} else {
 					$status[$key] = $err;
@@ -224,9 +229,9 @@ if (isset($_FILES['jpeg_exif']))
 		if (!empty($_POST['photographer_gridref'])) {
 			$_SESSION['last_photographer_gridref'] = $_POST['photographer_gridref'];
 		}
-						
-				
-		
+
+
+
 		$clear_cache[$square->gridsquare_id] = 1;
 	}
 
@@ -238,28 +243,28 @@ if (isset($_FILES['jpeg_exif']))
 	$smarty->assign('filenames', $filenames);
 	$smarty->assign('grid_reference', $grid_reference);
 
-} 
+}
 
 if (isset($_REQUEST['inner'])) {
 	$template='submit2_inner.tpl';
 	$step = 1;
-	
-	if (!empty($_REQUEST['grid_reference'])) 
+
+	if (!empty($_REQUEST['grid_reference']))
 	{
-		$step = 2; 
+		$step = 2;
 		$square=new GridSquare;
-		
+
 		$ok= $square->setByFullGridRef($_REQUEST['grid_reference']);
 
 		if ($ok) {
 			$smarty->assign('grid_reference', $grid_reference = $_REQUEST['grid_reference']);
-			
+
 			$smarty->assign('success', 1);
 		} else {
-			$smarty->assign('errormsg', $square->errormsg);	
+			$smarty->assign('errormsg', $square->errormsg);
 		}
-	} 
-	
+	}
+
 	$smarty->assign('step', $step);
 }
 
@@ -271,14 +276,14 @@ $smarty->display($template, $cacheid);
 
 flush();
 
-//things that can be done 'lazy' ie after given confirmation to the user... 
+//things that can be done 'lazy' ie after given confirmation to the user...
 
 if (!empty($_POST['pattrib_default'])) {
 	$USER->setCreditDefault(($_POST['pattrib'] == 'other')?stripslashes($_POST['pattrib_name']):'');
 }
 
 if (!empty($clear_cache) && count($clear_cache)) {
-	
+
 	foreach ($clear_cache as $gridsquare_id => $dummy) {
 		if ($memcache->valid) {
 			//the submit list
@@ -289,12 +294,12 @@ if (!empty($clear_cache) && count($clear_cache)) {
 			$memcache->name_delete('gi',$mkey);
 		}
 	}
-	
+
 	//clear user profile
 	$ab=floor($USER->user_id/10000);
 	$smarty->clear_cache(null, "user$ab|{$USER->user_id}");
 
-	
+
 }
 
 
