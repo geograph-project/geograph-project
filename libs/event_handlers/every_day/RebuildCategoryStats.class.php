c9de96c651d8f94fd57113cd8b66cb20c1c7932b
geograph <1413156+barryhunter@users.noreply.github.com>
2010-08-22 21:20:50 +0000

    use a consistant id

--- libs/event_handlers/every_day/RebuildCategoryStats.class.php
+++ libs/event_handlers/every_day/RebuildCategoryStats.class.php
@@ -45,14 +45,14 @@ class RebuildCategoryStats extends EventHandler
 		
 		
 		$db->Execute("CREATE TABLE category_stat_tmp (
-					`category_id` int(11) NOT NULL AUTO_INCREMENT,
+					`category_id` int(11) UNSIGNED NOT NULL,
 					`imageclass` varchar(32) NOT NULL DEFAULT '',
 					`c` int(11) NOT NULL DEFAULT '0',
 					`gridimage_id` int(11) NOT NULL ,
 					PRIMARY KEY (`category_id`),
 					INDEX (c))
 				ENGINE=MyISAM
-				SELECT imageclass,count(*) as c,gridimage_id
+				SELECT CRC32(imageclass) AS category_id,imageclass,count(*) AS c,gridimage_id
 				FROM gridimage_search
 				GROUP BY imageclass"); //the autoincrement column doesnt need 'null' for some reason. 
 		
