85a8c992d82ea1c2d91599425e7671f3dfc3d5be
barry <1413156+barryhunter@users.noreply.github.com>
2010-10-22 19:25:29 +0000

    new

---
+++ public_html/stuff/canonical.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id: conversion.php 5502 2009-05-13 14:18:23Z barry $
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 BArry Hunter (geo@barryhunter.co.uk)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once('geograph/global.inc.php');
+init_session();
+
+$USER->mustHavePerm("basic");
+
+
+$smarty = new GeographPage;
+
+$template='stuff_canonical.tpl';
+$cacheid='';
+
+if (!empty($_GET['mode'])) {
+	
+	if (!empty($_POST) && $_POST['submit'] && !empty($_POST['imageclass']) && !empty($_POST['canonical'])) {
+		$db = GeographDatabaseConnection(false);
+	
+		switch ($_POST['canonical']) {
+		
+			case 'asis':
+				$canonical = $_POST['imageclass'];
+				break;
+			case 'other': 
+				$canonical = $_POST['other'];
+				break;
+			case 'new': 
+				$canonical = $_POST['new'];
+				break;
+			case 'bad': 
+				$canonical = '-bad-';
+				break;
+		}
+		if (!empty($canonical)) {
+			$updates = array();
+			$updates['imageclass'] = $_POST['imageclass'];
+			$updates['canonical'] = $canonical;
+			$updates['user_id'] = $USER->user_id;
+			
+			$db->Execute('REPLACE INTO category_map SET `'.implode('` = ?,`',array_keys($updates)).'` = ?',array_values($updates));
+	
+		} else {
+			//try again!
+			$imageclass = $_POST['imageclass'];
+		}
+	} else {
+		$db = GeographDatabaseConnection(true);
+	}
+	
+	if (!empty($imageclass)) {
+		$row = array('imageclass'=>$imageclass);
+	} else {
+		switch ($_GET['mode']) {
+
+			case 'alpha':
+				$row = $db->GetRow("
+					SELECT * 
+					FROM category_stat cs 
+					LEFT JOIN category_map cm 
+						ON (cs.imageclass=cm.imageclass AND user_id = {$USER->user_id})
+					WHERE cm.category_map_id IS NULL
+					ORDER BY cs.imageclass
+					LIMIT 1");
+
+				break;
+			case 'random':
+				//TODO add some more randomness?
+				$row = $db->GetRow("
+					SELECT * 
+					FROM category_stat cs 
+					LEFT JOIN category_map cm 
+						ON (cs.imageclass=cm.imageclass AND user_id = {$USER->user_id})
+					WHERE cm.category_map_id IS NULL
+					ORDER BY category_id
+					LIMIT 1");
+
+				break;
+			case 'unmapped':
+				//TODO - this should use the final 'approved' list.
+				$row = $db->GetRow("
+					SELECT * 
+					FROM category_stat cs 
+					LEFT JOIN category_map cm 
+						ON (cs.imageclass=cm.imageclass)
+					WHERE cm.category_map_id IS NULL
+					ORDER BY cs.category_id
+					LIMIT 1");
+
+				break;
+			default:
+				$q=trim($_GET['mode']);
+				
+				$sphinx = new sphinxwrapper($q);
+			
+				$sphinx->pageSize = $pgsize = 100; 
+			
+				$pg = 1;
+					
+				$offset = (($pg -1)* $sphinx->pageSize)+1;
+	print "TEST";		
+				if ($offset < (1000-$pgsize) ) { 
+					$sphinx->processQuery();
+			
+					$sphinx->q = "\"^{$sphinx->q}$\" | ($sphinx->q)";
+			
+					$ids = $sphinx->returnIds($pg,'category');
+					
+					if (!empty($ids) && count($ids)) {
+			
+						$where = "category_id IN(".join(",",$ids).")";
+						$row = $db->GetRow("
+							SELECT * 
+							FROM category_stat cs 
+							LEFT JOIN category_map cm 
+								ON (cs.imageclass=cm.imageclass AND user_id = {$USER->user_id})
+							WHERE cm.category_map_id IS NULL
+							AND $where
+							ORDER BY cs.imageclass
+							LIMIT 1");
+					}
+				}
+				
+				break;
+		}
+	}
+	
+	if ($row) {
+		if (empty($row['imageclass']) && !empty($row[1])) {
+			//work around adodb bug. if label consists solely of hyphens its ignored
+			$row['imageclass'] = $row[1];
+		}
+		
+		
+		$smarty->assign($row);
+		$smarty->assign('mode',$_GET['mode']);
+		
+		//todo - use this from the confirmed one?
+		$list = $db->getCol("SELECT DISTINCT canonical FROM category_map");
+		$smarty->assign('list',$list);
+		
+	} else {
+	
+		$smarty->assign('done',1);
+	}
+}
+
+
+$smarty->display($template, $cacheid);
---
+++ public_html/templates/basic/stuff_canonical.tpl
@@ -0,0 +1,102 @@
+{assign var="page_title" value="Canonical Categories"}
+{include file="_std_begin.tpl"}
+{dynamic}
+<h2>Canonical Category Mapping</h2>
+
+{if $done}
+	<p>Congratulations. Nothing more to do right now.</p>
+{elseif $mode}
+	
+	<h3>Original Category</h3>
+	<div class="interestBox" style="padding-left:20px;">
+		<h4>{$imageclass|escape:'html'}</h4>
+	</div>
+	
+	<h3>Canonical Category</h3>
+	
+	<form method="post" action="{$script_name}?mode={$mode}" name="theForm">
+		<input type="hidden" name="imageclass" value="{$imageclass|escape:'html'}"/>
+		
+		<div class="interestBox">
+			Choose one:
+			
+			<p><input type="radio" name="canonical" value="asis" id="iasis" onclick="checkform()"/> <label for="iasis"><b>Use as is (<tt>{$imageclass|escape:'html'}</tt>)</b> - creating the canonical category if required</label></p>
+
+			<p><input type="radio" name="canonical" value="other" id="iother" onclick="checkform()"/> <label for="iother"><b>Use this one:</b> - <select name="other" onchange="checkform()"><option value=""></option>
+			{foreach from=$list item=i}<option>{$i|escape:'html'}</option>{/foreach}
+			</select> (current canonical list)</label></p>
+
+			<p><input type="radio" name="canonical" value="new" id="inew" onclick="checkform()"/> <label for="inew"><b>Create new:</b> - <input type="text" name="new" value="" maxlength="32" onkeyup="checkform()"></label> (<a href="javascript:void(copyit());">copy</a> current, then edit)</p>
+
+			<p><input type="radio" name="canonical" value="bad" id="ibad" onclick="checkform()"/> <label for="ibad"><b>Nonsence category</b> - this should be changed. A category such as 'Geograph' is clearly non-sensical.</label></p>
+		
+		</div>
+		<br/><br/>
+		<input type="submit" name="submit" value="submit suggestion" disabled id="submitButton"/>	
+	</form>	
+	<script type="text/javascript">{literal}
+	function copyit() {
+		var f=document.theForm;
+		f.elements['new'].value = f.elements['imageclass'].value;
+	}
+	function checkform() {
+		var f=document.theForm;
+		var good=false;
+		if (f.elements['canonical'][0].checked) {
+			good=true;
+		} else if (f.elements['canonical'][1].checked) {
+			if (f.elements['other'].selectedIndex>0) {
+				good=true;
+			}
+		} else if (f.elements['canonical'][2].checked) {
+			if (f.elements['new'].value.length>3 && f.elements['new'].value != f.elements['imageclass'].value) {
+				good=true;
+			}
+		} else if (f.elements['canonical'][3].checked) {
+			good=true;
+		}
+		
+		f.elements['submit'].disabled = !good;
+	}
+	
+	{/literal}</script>
+	<br/><br/>
+	<a href="?">Start over</a>
+{else}
+
+	<p>Use this section to suggest {external href="http://en.wikipedia.org/wiki/Canonical" text="canonical" title='Basic, canonic, canonical: reduced to the simplest and most significant form possible without loss of generality, e.g., "a basic story line"; "a canonical syllable pattern."'} categories for the full category list. The idea is to create a simplified short category list. By maintaining a mapping of full categories to canonical form, we can offer searching by a greatly simplified category dropdown. The full category would still be attached to the image to maintain the more specific information.</p>
+
+	Please choose: -
+
+	<form method="get">
+		<input type="hidden" name="mode" value="random"/>
+		<input type="submit" value="Randomly Selected Categories"/>
+	</form>
+
+	- or -
+
+	<form method="get">
+		<input type="hidden" name="mode" value="alpha"/>
+		<input type="submit" value="In Alphabetical Order"/>
+	</form>
+
+	- or -
+
+	<form method="get">
+		Keyword Search: <input type="text" name="mode" value=""/>
+		<input type="submit" value="Matching Categories"/> - works with at most 100 matching categories<br/>
+		(uses the same format as the 'Auto-complete' category selector)
+	</form>
+
+	- or -
+
+	<form method="get">
+		<input type="hidden" name="mode" value="unmapped"/>
+		<input type="submit" value="Unmapped categories"/>
+	</form>
+
+{/if}
+	<br/><br/>
+
+{/dynamic}
+{include file="_std_end.tpl"}
