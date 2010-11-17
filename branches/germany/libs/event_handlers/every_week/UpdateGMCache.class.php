206b0ac5dcf99753f5bbeb15e02159698fe77969
hansjorg <hansjorg@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2010-11-17 23:44:15 +0000

    * mercator map: forgot to add event handler: 9301_gmmap.diff

---
+++ branches/germany/libs/event_handlers/every_week/UpdateGMCache.class.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id: RebuildUserStats.class.php 3288 2007-04-20 11:32:27Z barry $
+ * 
+ * GeoGraph geographic photo archive project
+ * http://geograph.sourceforge.net/
+ *
+ * This file copyright (C) 2008  Barry Hunter (geo@barryhunter.co.uk)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/**
+*
+* @package Geograph
+* @author Barry Hunter <geo@barryhunter.co.uk>
+* @version $Revision: 3288 $
+*/
+
+require_once("geograph/eventhandler.class.php");
+require_once("geograph/map.class.php");
+
+//filename of class file should correspond to class name, e.g.  myhandler.class.php
+class UpdateGMCache extends EventHandler
+{
+	function processEvent(&$event)
+	{
+		//perform actions
+		$map = new GeographMap;
+		$success = $map->updateGMcache(50, 100);
+
+		//return true to signal completed processing
+		//return false to have another attempt later
+		return $success;
+	}
+	
+}
