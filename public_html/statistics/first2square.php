d7db3c65d141478114e37fac7bc244097df6e526
hansjorg <mail@hlipp.de>
2011-02-06 22:10:57 +0000

    uasort(): Array was modified by the user comparison function

--- public_html/statistics/first2square.php
+++ public_html/statistics/first2square.php
@@ -62,7 +62,7 @@ if (!$smarty->is_cached($template, $cacheid))
 	
 	function cmp($a, $b) 
 	{
-	   global $topusers;
+	   #global $topusers;
 	   if ($a['imgcount'] == $b['imgcount']) {
 	       return 0;
 	   }
