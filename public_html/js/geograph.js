5da23b35161b7ea2e34ce44d8e8a84301db16bc0
geograph <1413156+barryhunter@users.noreply.github.com>
2011-08-07 16:28:01 +0000

    removing getamap

--- public_html/js/geograph.js
+++ public_html/js/geograph.js
@@ -7,15 +7,15 @@ function popupOSMap(gridref,gridref2)
 {
 	if (!gridref && gridref2.length)
 		gridref = gridref2;
-        var wWidth = 740;
-        var wHeight = 520;
+        var wWidth = 400;
+        var wHeight = 400;
         var wLeft = Math.round(0.5 * (screen.availWidth - wWidth));
         var wTop = Math.round(0.5 * (screen.availHeight - wHeight)) - 20;
         if (gridref.length > 0) {
         	if (gridref.length < 7) {
 			gridref = gridref.substr(0,gridref.length-2)+'5'+gridref.substr(gridref.length-2,2)+'5';
 		}
-	var newWin = window.open('http://getamap.ordnancesurvey.co.uk/getamap/frames.htm?mapAction=gaz&gazName=g&gazString='+gridref, 
+	var newWin = window.open('/showmap.php?gridref='+gridref, 
 		'gam',
 		'left='+wLeft+',screenX='+wLeft+',top='+wTop+',screenY='+wTop+',width='+wWidth+',height='+wHeight+',status,scrolling=no');
 	} else {
