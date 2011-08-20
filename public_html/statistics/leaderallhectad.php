35ad8d9a57893649c4e966f88f95db3c9d35e57c
barry <1413156+barryhunter@users.noreply.github.com>
2011-08-20 13:35:44 +0000

    Fix for PHP 5.3

--- public_html/statistics/leaderallhectad.php
+++ public_html/statistics/leaderallhectad.php
@@ -119,7 +119,6 @@ if (!$smarty->is_cached($template, $cacheid))
 	
 	function cmp(&$a, &$b) 
 	{
-		global $topusers;
 		if ($a['imgcount'] == $b['imgcount']) {
 			if ($a['bigesthectad'] == $b['bigesthectad']) {
 				return 0;
