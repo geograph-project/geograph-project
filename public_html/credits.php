c386473099596c85a5942e249c720a3ce50c16ad
barry <1413156+barryhunter@users.noreply.github.com>
2011-09-25 22:21:38 +0000

    refined listing

--- public_html/credits.php
+++ public_html/credits.php
@@ -40,9 +40,11 @@ $when = (isset($_GET['when']) && preg_match('/^\d{4}(-\d{2}|)(-\d{2}|)$/',$_GET[
 
 $where = (isset($_GET['where']) && preg_match('/^[\w]{1,}$/',$_GET['where']))?$_GET['where']:'';
 
+$filter =  (isset($_GET['filter']) && preg_match('/^[\w-]$/i',$_GET['filter']))?$_GET['filter']:'';
+
 customGZipHandlerStart();
 
-$cacheid=$when.'|'.$where;
+$cacheid=$when.'|'.$where.'|'.$filter;
 
 //regenerate?
 if (!$smarty->is_cached($template,$cacheid))
@@ -50,7 +52,7 @@ if (!$smarty->is_cached($template,$cacheid))
 	if (!$db) {
 		$db = GeographDatabaseConnection(true);
 	}
-	$andwhere = '';
+	$andwhere = $andfilter = '';
 	
 	if ($when) {
 		if (strlen($when) == 7) {
@@ -69,77 +71,122 @@ if (!$smarty->is_cached($template,$cacheid))
 		$smarty->assign_by_ref('where',$where);
 	}
 	
+	if ($filter) {
+		if ($filter == '-') {
+			$andfilter = " and user.realname regexp '^[^A-Z]+'";
+		} else {
+			$andfilter = " and user.realname like '$filter%'";
+		}
 
+		$smarty->assign_by_ref('filter',$filter);
+	}
+	
 	$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 	
-	if (isset($_GET['cloud'])) {
-		if ($andwhere) {
-			$users = $db->CacheGetAssoc(3600*6,"select
-				nickname,user.user_id,user.realname,user.user_id,count(*) as images
-				from user
-					inner join gridimage_search gi using (user_id)
-				where nickname != '' and rights IS NOT NULL $andwhere
-				group by gi.user_id
-				order by images desc");
-			$smarty->assign('what','Contributors');
+	if ($filter || $when || $where || isset($_GET['cloud'])) {
+		
+
+		if (isset($_GET['cloud'])) {
+			if ($andwhere) {
+				$users = $db->CacheGetAssoc(3600*6,"select
+					nickname,user.user_id,user.realname,user.user_id,count(*) as images
+					from user
+						inner join gridimage_search gi using (user_id)
+					where nickname != '' and rights IS NOT NULL $andwhere $andfilter
+					group by gi.user_id
+					order by images desc");
+				$smarty->assign('what','Contributors');
+			} else {
+				$users = $db->CacheGetAssoc(3600*6,"select
+					nickname,user.user_id,user.realname,user.user_id,`us`.images
+					from user
+						inner join user_stat `us` using (user_id)
+					where nickname != '' and rights IS NOT NULL  $andfilter
+					order by images desc");
+				$smarty->assign('what','Contributors');
+			}
+
+			$size = $startsize = 30;
+				$sizedelta = 0.05;
+
+			foreach($users as $nick=>$obj) {
+
+				$users[$nick]['size'] = round($size);
+
+				$size -= $sizedelta;
+
+				if ($size < 10)
+					$sizedelta = 0;
+
+			}
+
+			function cmp($a, $b) {
+				return strnatcasecmp ($a, $b);
+			}
+			uksort($users, "cmp");
 		} else {
-			$users = $db->CacheGetAssoc(3600*6,"select
-				nickname,user.user_id,user.realname,user.user_id,`us`.images
-				from user
-					inner join user_stat `us` using (user_id)
-				where nickname != '' and rights IS NOT NULL 
-				order by images desc");
-			$smarty->assign('what','Contributors');
+			if ($andwhere) {
+				$users = $db->CacheGetAssoc(3600*6,"select
+					user.user_id,nickname,gi.realname as realname,user.realname as user_realname,user.user_id,count(*) as images
+					from user
+						inner join gridimage_search gi using (user_id)
+					where rights IS NOT NULL $andwhere $andfilter
+					group by gi.user_id,gi.realname
+					order by realname");
+				$smarty->assign('what','Photographers'); //because we *can*
+			} else {
+				$users = $db->CacheGetAssoc(3600*6,"select
+					user.user_id,nickname,user.realname,user.user_id,`us`.images
+					from user
+						inner join user_stat `us` using (user_id)
+					where rights IS NOT NULL $andfilter
+					order by realname");
+				$smarty->assign('what','Contributors');
+			}
 		}
 
-		$size = $startsize = 30;
-			$sizedelta = 0.05;
-
-		foreach($users as $nick=>$obj) {
-
-			$users[$nick]['size'] = round($size);
-
-			$size -= $sizedelta;
-
-			if ($size < 10)
-				$sizedelta = 0;
-
-		}
-
-		function cmp($a, $b) {
-			return strnatcasecmp ($a, $b);
-		}
-		uksort($users, "cmp");
+		$smarty->assign_by_ref('users',$users);
+		$smarty->assign('user_count',count($users));
 	} else {
-		if ($andwhere) {
-			$users = $db->CacheGetAssoc(3600*6,"select
-				user.user_id,nickname,gi.realname as realname,user.realname as user_realname,user.user_id,count(*) as images
-				from user
-					inner join gridimage_search gi using (user_id)
-				where rights IS NOT NULL $andwhere
-				group by gi.user_id,gi.realname
-				order by realname");
-			$smarty->assign('what','Photographers'); //because we *can*
-		} else {
-			$users = $db->CacheGetAssoc(3600*6,"select
-				user.user_id,nickname,user.realname,user.user_id,`us`.images
-				from user
-					inner join user_stat `us` using (user_id)
-				where rights IS NOT NULL
-				order by realname");
-			$smarty->assign('what','Contributors');
+		$smarty->assign('alphas',$db->getAll("SELECT SUBSTRING(TRIM(realname),1,1) AS alpha,COUNT(*) AS count FROM user INNER JOIN user_stat USING (user_id) WHERE realname REGEXP '^[A-Z]+' GROUP BY SUBSTRING(realname,1,1)"));
+	
+		$smarty->assign('user_count',$db->getOne("SELECT COUNT(*) FROM user INNER JOIN user_stat USING (user_id)"));
+		
+		$r = rand(1,9); $r = rand(1,9); $r = rand(1,9);
+		switch($r) {
+			case 8:
+			case 1: $sample = $db->getAssoc("SELECT user_id,realname,images FROM user INNER JOIN user_stat USING (user_id) ORDER BY user_id DESC LIMIT 64"); 
+				$samplename = "Recent Contributors"; break;
+				
+			case 9:
+			case 2: $sample = $db->getAssoc("SELECT user_id,realname,images FROM user INNER JOIN user_stat USING (user_id) ORDER BY last DESC LIMIT 64"); 
+				$samplename = "With Recent Contributions"; break;
+				
+			case 3: $sample = $db->getAssoc("SELECT * FROM (SELECT user_id,realname,images FROM user INNER JOIN user_stat USING (user_id) ORDER BY images DESC LIMIT 64) t2 ORDER BY realname"); 
+				$samplename = "Prolific Contributors"; break;
+				
+			case 4: $sample = $db->getAssoc("SELECT user_id,realname,images FROM user INNER JOIN user_stat USING (user_id) ORDER BY RAND() DESC LIMIT 64"); 
+				$samplename = "Random"; break;
+				
+			case 5: $alpha = chr(rand(65,90));
+				$sample = $db->getAssoc("SELECT user_id,realname,images FROM user INNER JOIN user_stat USING (user_id) WHERE realname LIKE '$alpha%' ORDER BY RAND() DESC LIMIT 64"); 
+				$samplename = "Random beginning with $alpha"; break;
+				
+			case 6: $alpha = chr(rand(65,90));
+				$sample = $db->getAssoc("SELECT user_id,realname,points AS images FROM user INNER JOIN user_stat USING (user_id) WHERE realname LIKE '$alpha%' ORDER BY points DESC LIMIT 64"); 
+				$samplename = "beginning with $alpha by Points"; break;
+				
+			case 7: $i = rand(0,22);
+				$sample = $db->getAssoc("SELECT user_id,realname,images FROM user INNER JOIN user_stat USING (user_id) WHERE images MOD 23 = $i GROUP BY images ORDER BY images LIMIT 64"); 
+				$samplename = "Random Spread"; break;
+		}
+		
+		if (!empty($sample) && $samplename) {
+			$smarty->assign_by_ref('sample',$sample);
+			$smarty->assign_by_ref('samplename',$samplename);
 		}
 	}
-
-	$smarty->assign_by_ref('users',$users);
-	$smarty->assign('user_count',count($users));
 }
 
 
-
-
-
 $smarty->display($template,$cacheid);
-
-
-?>
--- public_html/templates/basic/userlist.tpl
+++ public_html/templates/basic/userlist.tpl
@@ -4,22 +4,79 @@
 
 {if $where}
 <div style="float:right">Switch to <a href="/credits.php?cloud&amp;where={$where}{if $whenname}&amp;when={$when}/{/if}">cloud version</a></div>
-{else}
+{elseif $whenname}
 <div style="float:right">Switch to <a href="/credits.php?cloud{if $whenname}&amp;when={$when}{/if}">cloud version</a> or <a href="/statistics/breakdown.php?by=user{if $whenname}&amp;when={$when}{/if}">statistics version</a>.</div>
 {/if}
 
-<h2>Geograph Britain and Ireland {$what} <small>[{$user_count}]</small></h2>
+<h2>Geograph Britain and Ireland {$what} <small>[{$user_count|thousends} contributors]</small></h2>
 {if $whenname}
 	<h3>Submitting images March 2005 though {$whenname}</h3>
 {/if}
 {if $where}
 	<h3>Submitting in {$where} Myriad</h3>
 {/if}
+{if $filter}
+	<h3>Beginning with {$filter}</h3>
+{/if}
 
-<p class="wordnet" style="font-size:0.8em;line-height:1.4em" align="center"> 
+{if $users}
+<p class="wordnet" style="font-size:0.8em;line-height:1.4em" align="center">
 {foreach from=$users key=user_id item=obj}
 &nbsp;<a title="{$obj.nickname|escape:'html'}, {$obj.images} images" {if $obj.images > 100} style="font-weight:bold"{/if} href="/profile/{$user_id}{if $obg.user_realname && $obj.realname ne $obj.user_realname}?a={$obj.realname|escape:'url'}{/if}">{$obj.realname|escape:'html'|replace:' ':'&middot;'}</a><small>&nbsp;[{$obj.images}]</small> &nbsp;
-{/foreach} 
+{/foreach}
 </p>
- 		
+{else}
+	{if $sample}
+		<div class="interestBox" style="font-size:0.8em;width:200px;float:right;margin-left:10px;">
+		<b>{$samplename}</b><br/>&nbsp; sample listing<br/><br/>
+		{foreach from=$sample key=user_id item=obj}
+		<a title="{$obj.nickname|escape:'html'}, {$obj.images} images" {if $obj.images > 100} style="font-weight:bold"{/if} href="/profile/{$user_id}{if $obg.user_realname && $obj.realname ne $obj.user_realname}?a={$obj.realname|escape:'url'}{/if}">{$obj.realname|escape:'html'|replace:' ':'&middot;'}</a><small>&nbsp;[{$obj.images}]</small><br/>
+		{/foreach}
+		</div>
+	{/if}
+
+	We now have so many contributors, that listing everyone on one page makes for a very long page. One that takes a lot of resources.
+
+	<p>So we have provided a few ways to browse the contributor lists easier...</p>
+
+	<ul>
+		<li><b>Contributor Search</b>:<br/>
+			<form method="get" action="/finder/contributors.php">
+				<label for="fcq">Keywords</label> <input type="text" name="q" id="fcq" size="40"{dynamic}{if $q} value="{$q|escape:'html'}"{/if}{/dynamic}/>
+				<input type="submit" value="Search"/>
+			</form><br/></li>
+
+		<li><b>For a Hectad/Myriad</b>:<br/>
+			<form method="get" action="/browse.php">
+				<label for="gridref">Grid Ref</label> <input type="text" name="gridref" id="gridref" size="10"/>
+				<input type="submit" value="Go"/><br/>
+				Enter a Hectad (eg <tt>TQ75</tt> or <tt>H45</tt>) or a Myriad (eg <tt>J</tt> or <tt>NT</tt> - see myriads on <a href="/map/">map</a>)
+			</form><br/></li>
+
+		<li><b>For a Gridsquare</b>:<br/>
+			<form method="get" action="/statistics/groupby.php">
+				<input type="hidden" name="groupby" value="auser_id"/>
+				<label for="gridsquare">Grid Ref</label> <input type="text" name="filter[agridsquare]" id="gridsquare" size="10"/>
+				<input type="submit" value="Go"/><br/>
+				Enter a 4figure Grid Rweference (eg <tt>TQ7351</tt>)
+			</form><br/></li>
+
+		<li><b>By First Letter</b>:<br/>
+			<ol>
+				{foreach from=$alphas item=alpha}
+					<li value="{$alpha.count}"><a href="/credits/?filter={$alpha.alpha}" title="{$alpha.count} contributors">{$alpha.alpha}</a></li>
+				{/foreach}
+			</ol>
+			[<a href="/credits/?filter=-">non letters</a>]
+			<br/><br/></li>
+
+		<li>We do still have a <a href="/sitemap/credits.html">single page listing everyone</a> (updated once a day)<br/>
+			- but its big. About 1.1 Megabytes, so may take a while depending on your connection. <br/><br/></li>
+	</ul>
+
+	(Note: All these listings are combined listings of Contributors to <a href="/explore/places/1/">Great Britain</a> and <a href="/explore/places/2/">Ireland</a>.)
+
+	<br style="clear:both"/>
+{/if}
+
 {include file="_std_end.tpl"}
