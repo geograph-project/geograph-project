f971b5d0dbe2fe7d53465b22b93fe7e8c52f8c37
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2011-12-30 00:30:45 +0000

    Fix, stolen from r7538  :)

--- branches/british-isles/libs/geograph/searchcriteria.class.php
+++ branches/british-isles/libs/geograph/searchcriteria.class.php
@@ -616,7 +616,7 @@ class SearchCriteria
 							$sql_where .= "imagetaken LIKE '$y%' ";
 							$this->sphinx['filters']['takenyear'] = $y;
 						} elseif ($d == 0) {
-							$sql_where .= "imagetaken = '".sprintf('%04d-%02d',$y,$m)."%' ";
+							$sql_where .= "imagetaken LIKE '".sprintf('%04d-%02d',$y,$m)."%' ";
 							$this->sphinx['filters']['takenmonth'] = sprintf('%04d%02d',$y,$m);
 						} else {
 							$sql_where .= "imagetaken = '".$dates[0]."' ";
