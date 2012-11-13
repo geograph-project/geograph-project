8088da1dd9b93674b094aa994fda66d92a5b20e8
hansjorg <hansjorg@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2012-11-13 20:34:23 +0000

    * some forum translation

--- branches/germany/public_html/discuss/lang/eng.php
+++ branches/germany/public_html/discuss/lang/eng.php
@@ -362,6 +362,12 @@ $l_show='Show';
 $l_next='Next';
 $l_prev='Prev';
 $l_showrec='Show in Recent Topics?';
-$l_vpost='View or post in';
+$l_reload='Reload';
+$l_watch='Watch';
+$l_shorthead1='Show ';
+$l_shorthead_list='Forum List';
+$l_shorthead2=' | View ';
+$l_shorthead_gsd='Recent Grid Square Discussions';
+$l_update='Update';
 
 ?>
