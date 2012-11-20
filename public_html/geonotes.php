685a87512c1bd54a456413879339713c01e9f7f6
hansjorg <mail@hlipp.de>
2012-11-20 22:42:33 +0000

    * image annotation update

--- public_html/geonotes.php
+++ public_html/geonotes.php
@@ -44,7 +44,6 @@ function check_params($no)
 		$suffix = '_'.$no;
 	}
 	if (  !isset($_REQUEST["id$suffix"])
-	    ||!isset($_REQUEST["imageid$suffix"])
 	    ||!isset($_REQUEST["x1$suffix"])
 	    ||!isset($_REQUEST["x2$suffix"])
 	    ||!isset($_REQUEST["y1$suffix"])
@@ -56,9 +55,7 @@ function check_params($no)
 	    ||!isset($_REQUEST["comment$suffix"])) {
 		return '-4:0:missing parameters';
 	}
-	if (  !preg_match('/^[\x09\x0a\x0d\x20-\xff]*[\x21-\xff][\x09\x0a\x0d\x20-\xff]*$/', $_REQUEST["comment$suffix"])
-	    ||!preg_match('/^\s*-?[0-9]+\s*$/', $_REQUEST["id$suffix"])
-	    ||!preg_match('/^\s*-?[0-9]+\s*$/', $_REQUEST["imageid$suffix"])
+	if (  !preg_match('/^\s*-?[0-9]+\s*$/', $_REQUEST["id$suffix"])
 	    ||!preg_match('/^\s*-?[0-9]+\s*$/', $_REQUEST["x1$suffix"])
 	    ||!preg_match('/^\s*-?[0-9]+\s*$/', $_REQUEST["y1$suffix"])
 	    ||!preg_match('/^\s*-?[0-9]+\s*$/', $_REQUEST["x2$suffix"])
@@ -68,7 +65,7 @@ function check_params($no)
 	    ||!preg_match('/^\s*-?[0-9]+\s*$/', $_REQUEST["imgheight$suffix"])
 	    ||$_REQUEST["status$suffix"] != 'visible' && $_REQUEST["status$suffix"] != 'deleted' && $_REQUEST["status$suffix"] != 'pending'
            ) {
-		$a = "{$_REQUEST["comment$suffix"]},{$_REQUEST["id$suffix"]},{$_REQUEST["imageid$suffix"]},{$_REQUEST["x1$suffix"]},{$_REQUEST["y1$suffix"]},{$_REQUEST["x2$suffix"]},{$_REQUEST["y2$suffix"]},{$_REQUEST["z$suffix"]},{$_REQUEST["imgwidth$suffix"]},{$_REQUEST["imgwidth$suffix"]},{$_REQUEST["imgheight$suffix"]},{$_REQUEST["status$suffix"]}";
+		$a = "{$_REQUEST["comment$suffix"]},{$_REQUEST["id$suffix"]},{$_REQUEST["x1$suffix"]},{$_REQUEST["y1$suffix"]},{$_REQUEST["x2$suffix"]},{$_REQUEST["y2$suffix"]},{$_REQUEST["z$suffix"]},{$_REQUEST["imgwidth$suffix"]},{$_REQUEST["imgwidth$suffix"]},{$_REQUEST["imgheight$suffix"]},{$_REQUEST["status$suffix"]}";
 		trigger_error("inv val: $a", E_USER_WARNING);
 		return '-3:0:invalid parameters';
 	}
@@ -84,7 +81,7 @@ function check_params($no)
  * Note upload: Evaluate input parameters and try to change or create a note.
  * Returns: status message
  */
-function commit_note($no)
+function commit_note($no, $gridimage_id, $imagewidth, $imageheight, $ismoderator, $isowner)
 {
 	global $USER;
 	if ($no == 1) {
@@ -94,12 +91,16 @@ function commit_note($no)
 	}
 	$note_id = intval($_REQUEST["id$suffix"]);
 	$comment = $_REQUEST["comment$suffix"];
+	if (!preg_match('/^[\x09\x0a\x0d\x20-\xff]*[\x21-\xff][\x09\x0a\x0d\x20-\xff]*$/', $comment)) {
+		trigger_error("inv comment", E_USER_WARNING);
+		return "-3:$note_id:invalid parameters";
+	}
 	$oldsu = mb_substitute_character();
 	//if (!mb_substitute_character(0)) { // php only allows 0x0001...0xffff
 	if (!mb_substitute_character(1)) {
 		trigger_error("could not change substitute character", E_USER_WARNING);
 	}
-	$comment = mb_convert_encoding($comment, 'Windows-1252', 'UTF-8'); // store as Windows-1252 despite declaring latin1, see http://en.wikipedia.org/wiki/Windows-1252
+	$comment = mb_convert_encoding($comment, 'Windows-1252', 'UTF-8'); // store as Windows-1252 despite declaring latin1, see http://www.w3.org/TR/2009/WD-html5-20090423/infrastructure.html#character-encodings-0 or http://dev.w3.org/html5/spec/parsing.html#character-encodings-0
 	mb_substitute_character($oldsu);
 	//if (strpos($comment, chr(0)) !== false) {
 	if (strpos($comment, chr(1)) !== false) {
@@ -115,17 +116,7 @@ function commit_note($no)
 	$iw = intval($_REQUEST["imgwidth$suffix"]);
 	$ih = intval($_REQUEST["imgheight$suffix"]);
 	$z  = intval($_REQUEST["z$suffix"]);
-	$gridimage_id = intval($_REQUEST["imageid$suffix"]);
-	$image=new GridImage;
-	$image->loadFromId($gridimage_id);
-	if (!$image->isValid()) {
-		return "-5:$note_id:invalid image id";
-	}
-	$imagesize = $image->_getFullSize();
-	$imagewidth = $imagesize[0];
-	$imageheight = $imagesize[1];
-	$isowner=($image->user_id==$USER->user_id)?1:0;
-	$ismoderator=$USER->hasPerm('moderator')?1:0;
+
 	if (   $x1 < 0 || $x2 >= $iw
 	    || $y1 < 0 || $y2 >= $ih
 	    || $iw < $imagewidth || $ih < $imageheight
@@ -155,17 +146,26 @@ function commit_note($no)
 		if (!$note->isValid() || $note->gridimage_id != $gridimage_id) {
 			return "-6:$note_id:invalid note id";
 		}
+		$note->applyTickets($USER->user_id);
 		if ($status == 'pending' && $note->status != 'pending') {
 			return "-3:$note_id:setting status to pending is not allowed";
 		}
 		$ticket->setNotes("Changed image annotation $note_id.");
-		$ticket->updateField('x1', $note->x1, $x1, $mod, $note_id);
-		$ticket->updateField('x2', $note->x2, $x2, $mod, $note_id);
-		$ticket->updateField('y1', $note->y1, $y1, $mod, $note_id);
-		$ticket->updateField('y2', $note->y2, $y2, $mod, $note_id);
+		if (   $note->x1 != $x1
+		    || $note->x2 != $x2
+		    || $note->y1 != $y1
+		    || $note->y2 != $y2
+		    || $note->imgwidth != $iw
+		    || $note->imgheight != $ih
+		   ) { // this way, we can display old and new position even if only one coordinate changes
+			$ticket->updateField('x1', $note->x1, $x1, $mod, $note_id, true);
+			$ticket->updateField('x2', $note->x2, $x2, $mod, $note_id, true);
+			$ticket->updateField('y1', $note->y1, $y1, $mod, $note_id, true);
+			$ticket->updateField('y2', $note->y2, $y2, $mod, $note_id, true);
+			$ticket->updateField('imgwidth', $note->imgwidth, $iw, $mod, $note_id, true);
+			$ticket->updateField('imgheight', $note->imgheight, $ih, $mod, $note_id, true);
+		}
 		$ticket->updateField('z', $note->z, $z, $mod, $note_id);
-		$ticket->updateField('imgwidth', $note->imgwidth, $iw, $mod, $note_id);
-		$ticket->updateField('imgheight', $note->imgheight, $ih, $mod, $note_id);
 		$ticket->updateField('comment', $note->comment, $comment, $mod, $note_id);
 		$ticket->updateField('status', $note->status, $status, $mod, $note_id);
 		// FIXME updateField should report errors and we should pass that to the client
@@ -207,7 +207,10 @@ if (isset($_POST['commit'])) {
 	//    0:  applied changes
 	//    1:  pending (awaiting moderation)
 	//    2:  old values = new values, no changes made
-	if (!preg_match('/^\s*[1-9][0-9]*\s*$/', $_POST['commit'])) {
+	if (  !preg_match('/^\s*[1-9][0-9]*\s*$/', $_POST['commit'])
+	    ||!isset($_REQUEST['imageid'])
+	    ||!preg_match('/^\s*[1-9][0-9]*\s*$/', $_REQUEST['imageid'])
+	   ) {
 		trigger_error("inv commit: {$_POST['commit']}", E_USER_WARNING);
 		print "-3:0:invalid parameters";
 		exit;
@@ -226,15 +229,35 @@ if (isset($_POST['commit'])) {
 			exit;
 		}
 	}
-	$ret = commit_note(1);
+	$gridimage_id = intval($_REQUEST["imageid"]);
+	$image=new GridImage($gridimage_id);
+	if (!$image->isValid()) {
+		print "-5:0:invalid image id";
+		exit;
+	}
+	$imagesize = $image->_getFullSize();
+	$imagewidth = $imagesize[0];
+	$imageheight = $imagesize[1];
+	$isowner=($image->user_id==$USER->user_id)?1:0;
+	$ismoderator=$USER->hasPerm('moderator')?1:0;
+
+	$ret = commit_note(1, $gridimage_id, $imagewidth, $imageheight, $ismoderator, $isowner);
 	for ($i = 2; $i <= $commitnotes; ++$i) {
-		$ret .= '#' . commit_note($i);
+		$ret .= '#' . commit_note($i, $gridimage_id, $imagewidth, $imageheight, $ismoderator, $isowner);
 	}
 	print $ret;
 	$smarty = new GeographPage;
-	$ab=floor($image->gridimage_id/10000);
-	$smarty->clear_cache(null, "img$ab|{$image->gridimage_id}");
+	$ab=floor($gridimage_id/10000);
+	$smarty->clear_cache(null, "img$ab|$gridimage_id");
 	exit;
+}
+
+$uid = 0;
+$ticketid = 0;
+if (isset($_GET['ticket'])) {
+	$ticketid = intval($_GET['ticket']);
+} elseif (isset($_GET['u'])) { /* ignored when there's a ticket parameter */
+	$uid = intval($_GET['u']);
 }
 
 $smarty = new GeographPage;
@@ -245,17 +268,11 @@ $cacheid=0;
 
 $image=new GridImage;
 
-if (isset($_GET['id']))
-{
+if (isset($_GET['id'])) {
 	$id = intval($_GET['id']);
 	$image->loadFromId($id);
 	$isowner=($image->user_id==$USER->user_id)?1:0;
-	$ismoderator=$USER->hasPerm('moderator')?1:0;
-
-	$ab=floor($id/10000);
-
-	// cache id must depend on user as we also display pending changes made by the user...
-	$cacheid="img$ab|{$id}|notes|{$USER->user_id}_{$isowner}_{$ismoderator}"; # FIXME is caching still sensible?
+	$ismoderator=$USER->hasPerm('moderator')?1:0; // FIXME ticketmod?
 
 	//is the image accepted? - otherwise, only the owner and administrator should see it
 	if (!$isowner&&!$ismoderator) {
@@ -274,13 +291,30 @@ if (isset($_GET['id']))
 }
 
 //do we have a valid image?
-if ($image->isValid())
-{
+if ($image->isValid()) {
+	if ($ticketid) {
+		$ticket=new GridImageTroubleTicket($ticketid);
+		if (!$ticket->isValid())
+			die("invalid ticket id");
+		if ($ticket->gridimage_id != $image->gridimage_id)
+			die("ticket/image mismatch");
+		$uid = $ticket->user_id;
+	}
+	if (!$uid) {
+		$uid = $USER->user_id;
+	}
+	if ($uid != $USER->user_id && !$isowner) {
+		$USER->mustHavePerm("moderator"); // FIXME ticketmod?
+	}
 	#if (isset($_GET['note_id'])) {
+	#   TODO
 	#}
-	# FIXME We need to think about a way to show moderators (or image owners) specific changes, editimage.php would link to here, then.
-	# Should we (also?) use a ticket_id parameter? Could display the new (and the old?) values,
-	# probably as coloured boxes. Maybe a button "show/hide other notes" would be helpful, too?
+
+	$ab=floor($id/10000);
+
+	// cache id must depend on user as we also display pending changes made by the user...
+	$cacheid="img$ab|{$id}|notes|{$USER->user_id}_{$isowner}_{$ismoderator}_{$ticketid}_{$uid}"; # FIXME is caching still sensible?
+
 
 	#//what style should we use?
 	$style = 'white';
@@ -297,14 +331,34 @@ if ($image->isValid())
 	customCacheControl($mtime,$hash,($USER->user_id == 0));
 
 
-	if (!$smarty->is_cached($template, $cacheid))
-	{
-		// FIXME possibly add a column creator to the geonotes object?
-		if (!$isowner&&!$ismoderator) {
-			$notes = $image->getNotes(array('visible', 'pending'), $USER->user_id);
+	if (!$smarty->is_cached($template, $cacheid)) {
+		if ($ticketid) {
+			$affectednotes = $ticket->getAffectedNotes();
+			if ($affectednotes === false) {
+				die("database error");
+			}
+			# test length of array? currently only 1 possible...
+			# FIXME assure that all of x1,x2,y1,y2,width,height go into the ticket if at least on of them changes! save all old/new pairs even if unchanged? (probably "immediate" or new "unchanged" then)
+			# FIXME show 'deleted' notes
+			# FIXME style old/new forms / table
+			# FIXME disable editing/saving
+			# FIXME implement toggle events
+			# FIXME get rid of status lines
+			#$notes = $image->getNotes(array('visible', 'pending'), $uid, $ticketid-1, null, false, null, $affectednotes);
+			$notes = $image->getNotes(array('visible', 'pending'), 0, null, null, true, null, $affectednotes);
+			#$oldnotes = $image->getNotes(array('visible', 'pending', 'deleted'), $uid, $ticketid, null, false, $affectednotes, null, true);
+			$oldnotes = $image->getNotes(array('visible', 'pending', 'deleted'), $uid, $ticketid, $ticketid, false, $affectednotes, null, true);
+			#$newnotes = $image->getNotes(array('visible', 'pending', 'deleted'), $uid, $ticketid, null, false, $affectednotes);
+			$newnotes = $image->getNotes(array('visible', 'pending', 'deleted'), $uid, $ticketid, $ticketid, false, $affectednotes);
+			$smarty->assign_by_ref("oldnotes",$oldnotes);
+			$smarty->assign_by_ref("newnotes",$newnotes);
+			$smarty->assign_by_ref("ticket",$ticket);
+		} elseif (!$isowner && !$ismoderator) {
+			$notes = $image->getNotes(array('visible', 'pending'), $uid);
 		} else {
-			$notes = $image->getNotes(array('visible', 'pending', 'deleted'), $USER->user_id);
+			$notes = $image->getNotes(array('visible', 'pending', 'deleted'), $uid);
 		}
+		$smarty->assign_by_ref("notes",$notes);
 
 		$imagesize = $image->_getFullSize();
 
@@ -312,6 +366,7 @@ if ($image->isValid())
 		if ($image->original_width) {
 			$smarty->assign('original_width', $image->original_width);
 			$smarty->assign('original_height', $image->original_height);
+			$smarty->assign('orig_url', $image->_getOriginalpath());
 			// check if original size == std size
 			$uploadmanager=new UploadManager;
 			list($destwidth, $destheight, $destdim, $changedim) = $uploadmanager->_new_size($image->original_width, $image->original_height);
@@ -321,11 +376,9 @@ if ($image->isValid())
 		}
 		$smarty->assign('std_width', $imagesize[0]);
 		$smarty->assign('std_height', $imagesize[1]);
-		$smarty->assign('showorig', $showorig);
-		$smarty->assign('orig_url', $image->_getOriginalpath());
 		$smarty->assign('img_url', $image->_getFullpath());
+		$smarty->assign('showorig', $showorig);
 		$smarty->assign('maincontentclass', 'content_photo'.$style);
-		$smarty->assign_by_ref("notes",$notes);
 
 		//remove grid reference from title
 		$image->bigtitle=trim(preg_replace("/^{$image->grid_reference}/", '', $image->title));
