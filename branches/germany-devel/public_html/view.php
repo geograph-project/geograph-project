f0e85e05abbf3cc446b7556802b03cd90b296868
hansjorg <hansjorg@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2012-11-25 02:53:45 +0000

    * geonotes+IE: another approach

--- branches/germany-devel/public_html/view.php
+++ branches/germany-devel/public_html/view.php
@@ -123,14 +123,14 @@ if (isset($_GET['id']))
 //do we have a valid image?
 if ($image->isValid())
 {
-	if (strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== false
-	    ||    strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6') === false
-	       && strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 7') === false) {
-		// FIXME still sensible to find out how to get it working on IE7?
+	#if (strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== false
+	##    ||    strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6') === false
+	#       && strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 7') === false) {
+	#	// FIXME still sensible to find out how to get it working on IE7?
 		$notes = $image->getNotes(array('visible'));
-	} else {
-		$notes = array();
-	}
+	#} else {
+	#	$notes = array();
+	#}# would have to be included in cache id. trying ie specific comments in templates
 
 	//what style should we use?
 	$style = $USER->getStyle();
