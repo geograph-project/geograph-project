32e3652f3d652c0b9dad201a9bc2bd125d1945a4
tmwdr <tmwdr@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2013-03-11 10:07:29 +0000

    User registration currently disactivaed

--- branches/swiss/public_html/register.php
+++ branches/swiss/public_html/register.php
@@ -21,6 +21,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+die("Registration is not currently available");
+
+
 require_once('geograph/global.inc.php');
 init_session();
 
