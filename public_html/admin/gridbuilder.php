3c5db121df058441c8beaa04aea29f9b74096f21
hansjorg <mail@hlipp.de>
2013-04-22 03:20:02 +0000

    * backport gridbuilder update to trunk

--- public_html/admin/gridbuilder.php
+++ public_html/admin/gridbuilder.php
@@ -25,16 +25,45 @@ require_once('geograph/global.inc.php');
 require_once('geograph/gridshader.class.php');
 init_session();
 
-$USER->mustHavePerm("admin");
+$USER->hasPerm("mapmod") || $USER->mustHavePerm("admin");
 
 $smarty = new GeographPage;
 
+if (isset($_POST['newfilename']) && isset($_POST['shader_image_new'])) {
+	$_POST['shader_image'] = $_POST['shader_image_new'];
+}
+
+if (!empty($_FILES['uploadpng'])) {
+	if (isset($_POST['upload']) && $_FILES['uploadpng']['error'] === 0 && filesize($_FILES['uploadpng']['tmp_name'])) {
+		$name = basename($_FILES['uploadpng']['name']);
+		$name = preg_replace('/[^-_A-Za-z0-9.]/', '', $name);
+		$name = preg_replace('/\.[pP][nN][gG]$/', '', $name);
+		$name .= '.png';
+		$prefix = 'u'.$USER->user_id.strftime('_%Y-%m-%d_%H.%M.%S_');
+		$ok = false;
+		for ($i = 0; $i < 10; $i++) {
+			$filename = $prefix.$i.'_'.$name;
+			$fullname = $_SERVER['DOCUMENT_ROOT'].'/admin/gridshade/'.$filename;
+			if (!file_exists($fullname)) {
+				$ok = true;
+				break;
+			}
+		}
+		if ($ok && copy($_FILES['uploadpng']['tmp_name'], $fullname)) {
+			$_POST['shader_image']=$filename;
+		}
+	}
+	@unlink($_FILES['uploadpng']['tmp_name']);
+}
 
 //gather inputs
-$shader_image=isset($_POST['shader_image'])?$_POST['shader_image']:'admin/gb.png';
-$shader_x=isset($_POST['shader_x'])?$_POST['shader_x']:(54 + 206);
-$shader_y=isset($_POST['shader_y'])?$_POST['shader_y']:7;
-$reference_index=isset($_POST['reference_index'])?$_POST['reference_index']:1;
+$shader_image='';
+if (isset($_POST['shader_image']) && preg_match('/^[-_A-Za-z0-9][-_A-Za-z0-9.]*\.[pP][nN][gG]$/', $_POST['shader_image'])) {
+	$shader_image=$_POST['shader_image'];
+}
+$shader_x=isset($_POST['shader_x'])?$_POST['shader_x']:(54 + 206);                 #FIXME default?
+$shader_y=isset($_POST['shader_y'])?$_POST['shader_y']:7;                          #FIXME default?
+$reference_index=isset($_POST['reference_index'])?$_POST['reference_index']:1;     #FIXME default?
 
 
 $clearexisting=isset($_POST['clearexisting'])?true:false;
@@ -42,6 +71,10 @@ $skipupdategridprefix=isset($_POST['skipupdategridprefix'])?true:false;
 $redrawmaps=isset($_POST['redrawmaps'])?true:false;
 $ignore100=isset($_POST['ignore100'])?true:false;
 $dryrun=isset($_POST['dryrun'])?true:false;
+$minx=isset($_POST['minx'])?intval($_POST['minx']):0;
+$maxx=isset($_POST['maxx'])?intval($_POST['maxx']):100000;
+$miny=isset($_POST['miny'])?intval($_POST['miny']):0;
+$maxy=isset($_POST['maxy'])?intval($_POST['maxy']):100000;
 
 /*
 ireland image is 
@@ -59,6 +92,36 @@ So this bitmap should be 17,178
 This doesn't look right! Try 27,168 (Irish origin of 10,149)
 */
 
+$smarty->assign('shader_image', $shader_image);
+$smarty->assign('shader_x', $shader_x);
+$smarty->assign('shader_y', $shader_y);
+$smarty->assign('clearexisting', $clearexisting);
+$smarty->assign('skipupdategridprefix', $skipupdategridprefix);
+$smarty->assign('redrawmaps', $redrawmaps);
+$smarty->assign('ignore100', $ignore100);
+$smarty->assign('reference_index', $reference_index);
+$smarty->assign('dryrun', $dryrun);
+$smarty->assign('minx', $minx);
+$smarty->assign('maxx', $maxx);
+$smarty->assign('miny', $miny);
+$smarty->assign('maxy', $maxy);
+
+if (isset($_POST['listfiles'])) {
+	$filelist = array();
+	$files = glob( $_SERVER['DOCUMENT_ROOT'].'/admin/gridshade/*.[pP][nN][gG]', GLOB_NOESCAPE);
+	foreach($files as $file) {
+		$filelist[] = basename($file);
+	}
+	$smarty->assign('filelist', $filelist);
+	$smarty->display('gridbuilder_files.tpl');
+	exit;
+}
+
+if (isset($_POST['uploadfile'])) {
+	$smarty->display('gridbuilder_upload.tpl');
+	exit;
+}
+
 //do some processing?
 if (isset($_POST['shader']))
 {
@@ -69,30 +132,20 @@ if (isset($_POST['shader']))
 	set_time_limit(3600*24);
 	
 	//create shader and set it going!
-	$imgfile=$_SERVER['DOCUMENT_ROOT'].'/'.$shader_image;
+	$imgfile=$_SERVER['DOCUMENT_ROOT'].'/admin/gridshade/'.$shader_image;
 	$shader=new GridShader;
+	if ($dryrun)
+		$smarty->display('gridbuilder_back.tpl');
 
-	$shader->process($imgfile, $shader_x, $shader_y, $reference_index, $clearexisting, !$skipupdategridprefix,$redrawmaps,$ignore100,$dryrun);
+	$shader->process($imgfile, $shader_x, $shader_y, $reference_index, $clearexisting, !$skipupdategridprefix,$redrawmaps,$ignore100,$dryrun,$minx,$maxx,$miny,$maxy);
 	
-
 	//close output and exit (we don't want to output a page twice)
-
+	if (!$dryrun)
+		$smarty->display('gridbuilder_back.tpl');
 	$smarty->display('_std_end.tpl');
 	exit;
 }
 
-
-$smarty->assign('shader_image', $shader_image);
-$smarty->assign('shader_x', $shader_x);
-$smarty->assign('shader_y', $shader_y);
-$smarty->assign('clearexisting', $clearexisting);
-$smarty->assign('skipupdategridprefix', $skipupdategridprefix);
-$smarty->assign('redrawmaps', $redrawmaps);
-$smarty->assign('ignore100', $ignore100);
-$smarty->assign('reference_index', $reference_index);
-$smarty->assign('dryrun', $dryrun);
-
 $smarty->display('gridbuilder.tpl');
 
-	
 ?>
