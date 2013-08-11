d224e6c038abf9a12b8c34d2e824ba5022075613
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2013-08-11 01:38:39 +0000

    new tables to speed up queries, mostly in effect materialized views

--- apps/trunk/geogridfs/database.sql
+++ apps/trunk/geogridfs/database.sql
@@ -1,4 +1,37 @@
--- MySQL dump 10.13  Distrib 5.1.70, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.24, for Linux (x86_64)
+-- Server version	5.5.24-55
+
+--
+-- Table structure for table `api_log`
+--
+
+CREATE TABLE `api_log` (
+  `log_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `remote_id` tinyint(4) NOT NULL,
+  `command` varchar(30) NOT NULL,
+  `folder` varchar(255) NOT NULL,
+  `mode` varchar(20) NOT NULL,
+  `ids` mediumint(8) unsigned NOT NULL,
+  `ipaddr` int(10) unsigned NOT NULL,
+  `useragent` varchar(128) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `notes` text NOT NULL,
+  PRIMARY KEY (`log_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `backup_task`
+--
+
+CREATE TABLE `backup_task` (
+  `task_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `start` int(10) unsigned DEFAULT NULL,
+  `end` int(10) unsigned DEFAULT NULL,
+  `files` bigint(21) NOT NULL DEFAULT '0',
+  `mode` varchar(4) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `identity` varchar(32) NOT NULL,
+  PRIMARY KEY (`task_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `file`
@@ -8,7 +41,7 @@ CREATE TABLE `file` (
   `file_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `filename` varchar(255) NOT NULL,
   `folder_id` int(10) unsigned NOT NULL,
-  `size` mediumint(8) unsigned DEFAULT NULL,
+  `size` mediumint(8) unsigned NOT NULL,
   `file_created` datetime NOT NULL,
   `file_modified` datetime NOT NULL,
   `file_accessed` datetime NOT NULL,
@@ -16,16 +49,17 @@ CREATE TABLE `file` (
   `replicas` set('cream','jam','milk','scone','toast') NOT NULL,
   `replica_count` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `replica_target` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `backups` set('amz','dwo') NOT NULL,
+  `backups` set('amz','dwo') NOT NULL DEFAULT '',
   `backup_count` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `backup_target` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `meta_created` datetime NOT NULL,
   `meta_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `class` varchar(32) NOT NULL, --todo: change this into a enum!
+  `class` enum('','full.jpg','original.jpg','thumb.jpg','detail.png','detail.jpg','base.png','thumb.gd','preview.jpg','pending.jpg','tile.png','tile.tif','kml','sitemap.gz','sitemap.html','torrents','templates','rss','.others') NOT NULL DEFAULT '',
+  `replica_active` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`file_id`),
   UNIQUE KEY `filename` (`filename`),
   KEY `folder_id` (`folder_id`),
-  KEY `class` (`class`)
+  KEY `replica_active` (`replica_active`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -35,17 +69,42 @@ CREATE TABLE `file` (
 CREATE TABLE `folder` (
   `folder_id` int(11) NOT NULL AUTO_INCREMENT,
   `folder` varchar(255) NOT NULL,
-  `folder_created` datetime NOT NULL,
-  `folder_modified` datetime NOT NULL,
-  `folder_accessed` datetime NOT NULL,
   `meta_created` datetime NOT NULL,
   `meta_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`folder_id`),
   UNIQUE KEY `folder` (`folder`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Table structure for table `remote`
+--
 
-alter table file add replica_active tinyint unsigned not null default 0, add index(`replica_active`);
+CREATE TABLE `remote` (
+  `remote_id` int(11) NOT NULL AUTO_INCREMENT,
+  `identity` varchar(10) NOT NULL,
+  `secret` varchar(32) NOT NULL,
+  `active` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `created` datetime NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`remote_id`),
+  UNIQUE KEY `identity` (`identity`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-alter table file add backup_active tinyint unsigned not null default 0, add index(`backup_active`);
+--
+-- Table structure for table `stat`
+--
 
+CREATE TABLE `stat` (
+  `id` varchar(64) NOT NULL,
+  `value` varchar(64) NOT NULL,
+  `files` int(10) unsigned NOT NULL,
+  `total_bytes` bigint(20) unsigned NOT NULL,
+  `folders` mediumint(8) unsigned NOT NULL,
+  `oldest` datetime NOT NULL,
+  `newest` datetime NOT NULL,
+  `example` varchar(255) NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Dump completed on 2013-08-11  2:33:40
