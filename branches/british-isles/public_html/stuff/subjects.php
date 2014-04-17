456b4cf8d5922dfa8c66e32995693de42d95df03
geograph <geograph@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2014-04-17 11:39:43 +0000

    basic system for viewing/modding the subjects and category->subject mapping

---
+++ branches/british-isles/public_html/stuff/subjects.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id: xmas.php 6235 2009-12-24 12:33:07Z barry $
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 Barry Hunter (geo@barryhunter.co.uk)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once('geograph/global.inc.php');
+init_session();
+
+$smarty = new GeographPage;
+
+$USER->mustHavePerm("basic");
+
+
+
+if (!empty($_POST) && !empty($_POST['submitone'])) {
+	$db = GeographDatabaseConnection(false);
+
+}
+
+$action = (isset($_GET['action']) && ctype_alnum($_GET['action']))?$_GET['action']:'reuse';
+$smarty->assign('action',$action);
+
+$template = 'stuff_subjects.tpl';
+$cacheid = '';
+
+if (!empty($_GET['admin'])) {
+	$smarty->assign('admin',1);
+	$cacheid=1;
+}
+
+if (!$smarty->is_cached($template, $cacheid)) {
+	if (empty($db))
+		$db = GeographDatabaseConnection(true);
+
+
+
+
+
+	$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+
+
+
+$sql = " (
+	select subject,t.tag,count(gridimage_id) images from subjects s left join tag t on (t.tag = s.subject and t.prefix = 'subject' and gt.status = 2) left join gridimage_tag gt using (tag_id) group by subject order by null
+) union (
+	select subject,t.tag,count(gridimage_id) images from tag t inner join gridimage_tag gt using (tag_id) left join subjects s on (t.tag = s.subject) where t.prefix = 'subject' and gt.status = 2 and s.subject is null group by tag_id order by null
+) order by coalesce(subject,tag)";
+
+//union (
+//      select canonical as subject,null as tag, from gridimage_search inner join category_mapping using (imageclass) group by canonical
+
+	$list = $db->getAll($sql);
+
+
+	$historic = $db->getAssoc("select canonical as subject,count(*) images,count(distinct imageclass) as cats from gridimage_search inner join category_mapping using (imageclass) group by canonical order by null");
+	foreach ($list as $idx => $row) {
+		if (!empty($historic[$row['subject']])) {
+			$list[$idx]['historic'] = $historic[$row['subject']]['images'];
+			$list[$idx]['cats'] = $historic[$row['subject']]['cats'];
+			unset($historic[$row['subject']]);
+		}
+	}
+	if (!empty($historic)) {
+		foreach ($historic as $subject => $row) {
+			//store in tag, because not offical subject
+			$list[] = array('tag'=>$subject,'historic'=>$row['images'],'cats'=>$row['cats']);
+		}
+		unset($historic);
+	}
+
+
+	$smarty->assign_by_ref('list',$list);
+
+}
+
+$smarty->display($template, $cacheid);
+
+
---
+++ branches/british-isles/public_html/templates/basic/stuff_subjects.tpl
@@ -0,0 +1,51 @@
+{assign var="page_title" value="Subjects"}
+{include file="_std_begin.tpl"}
+<script src="{"/sorttable.js"|revision}"></script>
+
+<h2>Subjects</h2>
+
+{if $admin}
+<p>Where a subject is not 'Offical' there are a couple of ways this can be rectified:</p>
+<ol>
+	<li>Add it to the offical subject list, by clicking Approve</li>
+	<li>Change/correct the category->subject mapping, so only uses a offical subject</li>
+	<li>Report the tag, so the images can be moved to an offical subject</li>
+	<li>Report the tag, so the subject prefix can be removed (to make a standard unprifixed tag)</li>
+</ol>
+<p>Also a subject that is used on very few images, is a sign it could removed from the offical list (using the Disapprove), then cleared up as above</p>
+{/if}
+
+<table class="report sortable" id="catlist" style="font-size:8pt;" cellpadding=4>
+<thead>
+<tr>
+	<td>Subject</td>
+	<td>Offical</td>
+	<td>Images (using tag)</td>
+	<td>Images (via Category)</td>
+	<td>Original Categories</td>
+	{if $admin}
+		<td>Actions</td>
+	{/if}
+</tr>
+<thead>
+<tbody>
+{foreach from=$list item=row}
+	{cycle values="#f0f0f0,#e9e9e9" assign="bgcolor"}
+	<tr bgcolor="{$bgcolor}">
+		<td>{$row.subject|default:$row.tag|escape:'html'}</td>
+              	<td>{if $row.subject}Y{/if}</td>
+              	<td align="right" sortvalue="{$row.images}">{if $row.images}<a href="/search.php?text=[subject:{$row.subject|default:$row.tag|escape:'url'}]">{$row.images}</a>{else}0{/if}</td>
+              	<td align="right" sortvalue="{$row.historic}">{if $row.historic}<a href="/search.php?canonical=.{$row.subject|default:$row.tag|escape:'url'}&amp;do=1">{$row.historic}</a>{else}0{/if}</td>
+		<td align="right">{$row.cats}</td>
+		{if $admin}
+			<td>{if $row.subject}Disapprove{else}Approve{/if}
+			{if $row.historic}&middot; <a href="/stuff/category_mapping.php?subject={$row.subject|default:$row.tag|escape:'url'}">View Mapping</a>{/if}
+			{if $row.images && $row.tag}&middot; <a href="/tags/report.php?tag=subject:{$row.tag|escape:'url'}">Report Tag</a>{/if}</td>
+		{/if}
+	</tr>
+{/foreach}
+</tbody>
+</table>
+
+
+{include file="_std_end.tpl"}
