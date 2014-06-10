accfbe1a5a11f65d75a01dedbb0ce4054e25ef87
geograph <1413156+barryhunter@users.noreply.github.com>
2014-06-10 21:06:17 +0000

    these files where never commited for some reason

---
+++ public_html/finder/recent.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id: contributors.php 6407 2010-03-03 20:44:37Z barry $
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2008 Barry Hunter (geo@barryhunter.co.uk)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once('geograph/global.inc.php');
+init_session();
+
+
+
+
+$smarty = new GeographPage;
+$template = 'finder_recent.tpl';
+
+$cacheid = intval($_GET['page']);
+$extra = array();
+
+
+if (true) {
+	if (!empty($_GET['q'])) {
+		$q=trim($_GET['q']);
+	} else {
+		$q = '';
+	}
+
+	$sphinx = new sphinxwrapper($q);
+	if (!empty($sphinx->q))
+		$extra[] = "q=".urlencode($sphinx->q);
+
+	//gets a cleaned up verion of the query (suitable for filename etc)
+	$cacheid = $sphinx->q;
+
+	$sphinx->pageSize = $pgsize = 100;
+
+	$pg = (!empty($_GET['page']))?intval(str_replace('/','',$_GET['page'])):0;
+	if (empty($pg) || $pg < 1) {$pg = 1;}
+
+	$cacheid .=".".$pg;
+
+	if (isset($_REQUEST['inner'])) {
+		$cacheid .= '.iframe';
+		$smarty->assign('inner',1);
+		$extra[] = "inner";
+	}
+
+        if (!empty($_REQUEST['status']) && preg_match('/^\w+$/',$_REQUEST['status'])) {
+                $cacheid .= '.'.$_REQUEST['status'];
+                $smarty->assign('status',$_REQUEST['status']);
+                $extra[] = "status=".$_REQUEST['status'];
+
+		$sphinx->q .= " @status {$_REQUEST['status']}";
+        }
+
+
+	if (!$smarty->is_cached($template, $cacheid)) {
+
+		$sphinx->processQuery();
+
+		$client = $sphinx->_getClient();
+
+		$db = GeographDatabaseConnection(true);
+
+		$max = $db->getOne("SELECT MAX(gridimage_id) FROM gridimage_search");
+		$client->SetIDRange($max-1000,$max+10);
+
+			$bits = array();
+			$bits[] = "uniqueserial(atakenyear)";
+			$bits[] = "uniqueserial(takendays)";
+			$bits[] = "uniqueserial(ahectad)";
+			$bits[] = "uniqueserial(classcrc)";
+			$bits[] = "uniqueserial(scenti)";
+			$bits[] = "uniqueserial(uint(agridsquare))";
+			if (!preg_match('/user_id/',$q)) {
+				$bits[] = "uniqueserial(auser_id)";
+			}
+			$client->setSelect(implode('+',$bits)." as myint");
+			$sphinx->sort = "myint ASC,sequence ASC";
+
+		$ids = $sphinx->returnIds($pg,'_images');
+
+		if (count($ids)) {
+			$where = "gridimage_id IN(".join(",",$ids).")";
+
+
+			$limit = $pgsize;
+
+			$prev_fetch_mode = $ADODB_FETCH_MODE;
+			$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+			$rows = $db->getAssoc("
+			select gridimage_id,realname,user_id,title,grid_reference,imagetaken
+			from gridimage_search
+			where $where
+			limit $limit");
+
+			$results = array();
+			foreach ($ids as $c => $id) {
+				$row = $rows[$id];
+				$row['gridimage_id'] = $id;
+				$gridimage = new GridImage;
+                                $gridimage->fastInit($row);
+				$results[] = $gridimage;
+			}
+
+			$smarty->assign_by_ref('results', $results);
+			$smarty->assign("query_info",$sphinx->query_info);
+
+			if ($sphinx->numberOfPages > 1) {
+				$smarty->assign('pagesString', pagesString($pg,$sphinx->numberOfPages,$_SERVER['PHP_SELF']."?".implode('&amp;',$extra)."&amp;page=") );
+				$smarty->assign("offset",(($pg -1)* $sphinx->pageSize)+1);
+			}
+			$ADODB_FETCH_MODE = $prev_fetch_mode;
+		}
+	}
+
+	$smarty->assign("q",$sphinx->qclean);
+}
+
+
+$smarty->display($template,$cacheid);
+
---
+++ public_html/templates/basic/finder_recent.tpl
@@ -0,0 +1,121 @@
+{assign var="page_title" value="Recent Images"}
+{if $inner}
+{include file="_basic_begin.tpl"}
+{else}
+{include file="_std_begin.tpl"}
+{/if}
+
+<div style="width:200px;float:right;position:relative;text-align:center">
+	<a href="/results/1522">View as search result</a>
+</div>
+
+<h2>Recently Submitted Images</h2>
+
+<div style="width:200px;float:right;position:relative">
+	<div class="interestBox">
+	<b>Popular Tags</b><br/>
+	<div id="results" style="border-bottom:1px solid silver">
+		Loading tags...
+	</div><br/>
+	Tags are still a work in progress on the site, and many images still don't have tags, so the results here are <b>only approximate</b>.
+	</div>
+	<br/><br/>
+	&middot; <a href="/browser/">Browse images more fully</a> &middot;
+</div>
+
+<div style="margin-right:220px;">
+
+{foreach from=$results item=image}
+
+	  <div style="float:left;position:relative; width:130px; height:130px">
+	  <div align="center">
+	  <a title="{$image->grid_reference} : {$image->title|escape:'html'} by {$image->realname} {$image->dist_string} - click to view full size image" href="/photo/{$image->gridimage_id}" target="_parent">{$image->getThumbnail(120,120,false,true)|replace:'src=':'src="/img/blank.gif" data-src='}</a></div>
+	  </div>
+
+{foreachelse}
+	{if $q}
+		<p><i>There is no content to display at this time.</i></p>
+	{/if}
+{/foreach}
+
+</div>
+
+<div style="margin-top:0px;clear:both"> 
+{if $pagesString}
+	( Page {$pagesString})
+{/if}
+</div>	
+
+{if $query_info}
+	<p>{$query_info}</p>
+{/if}
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js" type="text/javascript"></script>
+<script src="/js/lazy.v1.js" type="text/javascript"></script>
+<script src="/js/preview2.v5.js" type="text/javascript"></script>
+<script>
+var query = '';
+var iii = 1522;
+{literal}
+
+$(function() {
+	startIt(query);
+});
+
+function redo(fragment) {
+	if (fragment.indexOf('-') == 0) {
+		fragment = fragment.replace(/^\-/,'-"').replace(/:/g,' ')+'"';
+	} if (fragment.indexOf(':') > 0) {
+		fragment = 'tags:"'+fragment.replace(/:/g,' ')+'"';
+	}
+
+	var url = "/search.php?text="+encodeURIComponent(query+" "+fragment)+"&i="+iii+"&redo=1";
+	window.location.href = url;
+}
+
+function startIt(query) {
+
+	var url = "/finder/bytag.json.php?q="+encodeURIComponent(query)+"&callback=?&recent=1";
+
+	$.ajax({
+		url: url,
+		dataType: 'jsonp',
+		jsonpCallback: 'serveCallback',
+		cache: true,
+		success: function(data) {
+			if (data && data.length > 0) {
+				str = "";
+				for(var tag_id in data) {
+					text = data[tag_id].tag;
+					if (data[tag_id].prefix)
+						text = data[tag_id].prefix+':'+text;
+
+					str = str + '<div style="position:relative;padding-left:2px;padding-top:2px;border-top:1px solid silver;cursor:pointer" onclick="redo(\''+text+'\')"><span>'+text+"</span>"+((data[tag_id].count && data[tag_id].count > 1)?(" ["+data[tag_id].count+"]"):'')+"</div>";
+				}
+				str = str + "</ol>";
+				$('#results').html(str);
+			} else {
+				$('#results').html("No Tags Found");
+			}
+		}
+	});
+}
+
+function showBtn(tag_id) {
+	$('#div'+tag_id).show().parent().css('backgroundColor','white');
+}
+function hideBtn(tag_id) {
+	$('#div'+tag_id).hide().parent().css('backgroundColor','inherit');
+}
+
+{/literal}
+</script>
+
+
+{if $inner}
+</body>
+</html>
+{else}
+{include file="_std_end.tpl"}
+{/if}
