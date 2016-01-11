e70c67ef3c27d66d41011aa2f9497d13525291f6
hansjorg <mail@hlipp.de>
2016-01-11 22:00:15 +0000

    * rendering tiles seems to need more memory using php 5.6; further investigation needed

--- public_html/_scripts/runscript.php
+++ public_html/_scripts/runscript.php
@@ -14,8 +14,14 @@ if (empty($CONF['phpbin'])) {
 	$phpbin=$CONF['phpbin'];
 }
 
+if (!empty($CONF['mem_maptiles'])) {
+	$memopt = '-d memory_limit='.$CONF['mem_maptiles'];
+} else {
+	$memopt = '';
+}
+
 $scripts=array(
-	'maps'=> "{$phpdir}/{$phpbin} {$_SERVER['DOCUMENT_ROOT']}/../scripts/recreate_maps.php --load=1.5 --dir={$_SERVER['DOCUMENT_ROOT']}/../ --config={$_SERVER['HTTP_HOST']}",
+	'maps'=> "{$phpdir}/{$phpbin} {$memopt} {$_SERVER['DOCUMENT_ROOT']}/../scripts/recreate_maps.php --load=1.5 --dir={$_SERVER['DOCUMENT_ROOT']}/../ --config={$_SERVER['HTTP_HOST']}",
 	'kml' => "{$phpdir}/{$phpbin} {$_SERVER['DOCUMENT_ROOT']}/../scripts/recreate_kml.php --load=1.5 --dir={$_SERVER['DOCUMENT_ROOT']}/../ --config={$_SERVER['HTTP_HOST']}",
 	'tmp' => "{$phpdir}/{$phpbin} {$_SERVER['DOCUMENT_ROOT']}/../scripts/cleanup_tmp.php --load=1.5 --dir={$_SERVER['DOCUMENT_ROOT']}/../ --config={$_SERVER['HTTP_HOST']}"
 );
@@ -33,12 +39,20 @@ if (($_SERVER['REMOTE_ADDR'] != $_SERVER['SERVER_ADDR']) && $_SERVER['REMOTE_ADD
 	header("HTTP/1.0 404 Not Found"); //FIXME
 	exit(1);
 }
+if (!empty($CONF['time_runscript'])) {
+	set_time_limit($CONF['time_runscript']);
+}
+
+#file_put_contents($logfile, "start $job ".date("Y-m-d H:i:s")."\n", FILE_APPEND);
 
 $logcmd=" >> {$logfile} 2>&1";
 chdir($_SERVER['DOCUMENT_ROOT']);
 header('content-type: text/plain');
 $return_var = 0;
 passthru($scripts[$job].$logcmd, $return_var);
+
+#file_put_contents($logfile, "end $job ".date("Y-m-d H:i:s")."\n", FILE_APPEND);
+
 echo $return_var;
 #exit($return_var);
 
