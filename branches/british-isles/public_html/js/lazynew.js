8d197f7c0831906eaa783b5432060d7ffcb19781
geograph <geograph@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2016-08-16 12:50:44 +0000

    new image display, based on concept of 'Story Map', syncing view with scrolling thumbs. used new lazyload system

---
+++ branches/british-isles/public_html/js/lazynew.js
@@ -0,0 +1,32 @@
+//enabled lazy loading of images
+//uses jQuery, which it will load if NOT already loaded.  
+//however does need jQl loaded (provided by geograph.js) 
+
+
+//we have to be extra careful checking if a real jquery, as jQl creates a fake jQuery object. 
+if (typeof jQuery === "undefined" || jQuery === null || typeof jQuery.fn === "undefined" || typeof jQuery.fn.load === "undefined") {
+	jQl.loadjQ('http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js');
+}
+
+
+$(function() {
+	$.ajaxSetup({
+	 	cache: true
+	});
+	$.getScript('http://s1.geograph.org.uk/js/jquery.lazyload.js',function() {
+                initLazy();
+		if (typeof initLazy2 == 'function') { 
+			initLazy2(); 
+		}
+	});
+});
+
+
+function initLazy() {
+
+	$("img[data-src]").lazyload({
+		data_attribute: 'src',
+		effect: 'fadeIn', 
+		container: ($('#scroller').length == 1)?$('#scroller'):window
+	});
+}
