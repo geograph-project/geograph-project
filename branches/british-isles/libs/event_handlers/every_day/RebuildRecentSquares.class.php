39e958168dbf034f6bbbcb658cebffcd4d2ce8b9
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2017-01-09 15:50:44 +0000

    was not resetting max_ftf (in case of no longer any geos)

--- branches/british-isles/libs/event_handlers/every_day/RebuildRecentSquares.class.php
+++ branches/british-isles/libs/event_handlers/every_day/RebuildRecentSquares.class.php
@@ -38,28 +38,27 @@ class RebuildRecentSquares extends EventHandler
 	function processEvent(&$event)
 	{
 		//perform actions
-		
+
 		$db=&$this->_getDB();
-		
+
 		$db->Execute("DROP TABLE IF EXISTS recent_only_tmp");
-		
-		
+
 		$db->Execute("CREATE TABLE recent_only_tmp (
 				  `gridsquare_id` int(11) NOT NULL default '0',
 				  `images` smallint NOT NULL default '0',
 				  PRIMARY KEY  (`gridsquare_id`)
-				) 
-				SELECT gridsquare_id,COUNT(*) AS images 
-				FROM gridimage gi 
-				WHERE imagetaken > DATE(DATE_SUB(NOW(), INTERVAL 5 YEAR)) 
+				)
+				SELECT gridsquare_id,COUNT(*) AS images
+				FROM gridimage gi
+				WHERE imagetaken > DATE(DATE_SUB(NOW(), INTERVAL 5 YEAR))
 				AND moderation_status = 'geograph'
 				GROUP BY gi.gridsquare_id
-				ORDER BY NULL"); 
-		
+				ORDER BY NULL");
+
 		$db->Execute("DROP TABLE IF EXISTS recent_only");
 		$db->Execute("RENAME TABLE recent_only_tmp TO recent_only");
-		
-		$db->Execute("UPDATE gridsquare SET has_recent = 0");
+
+		$db->Execute("UPDATE gridsquare SET has_recent = 0, max_ftf = 0");
 		$db->Execute("UPDATE gridsquare INNER JOIN recent_only USING (gridsquare_id) SET gridsquare.has_recent = 1");
 
                 $db->Execute("create temporary table gridsquare_updater (gridsquare_id INT UNSIGNED primary key) select gridsquare_id,max(ftf) as max_ftf from gridimage where moderation_status = 'geograph' group by gridsquare_id order by null");
@@ -69,5 +68,5 @@ class RebuildRecentSquares extends EventHandler
 		//return false to have another attempt later
 		return true;
 	}
-	
+
 }
