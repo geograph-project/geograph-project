570e7d22f86c0c5a21759069dfa469a1aa348a5a
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2017-02-25 20:57:01 +0000

    move the EXIF data to flat files (tends to bloat the mysql database), inlucding a batch exporter. But its only alpha quality, not battle tested

--- branches/british-isles/public_html/resubmit.php
+++ branches/british-isles/public_html/resubmit.php
@@ -32,27 +32,34 @@ if (isset($_GET['preview'])) {
 }
 require_once('geograph/uploadmanager.class.php');
 
+dieIfReadOnly();
 
 init_session();
 
 $uploadmanager=new UploadManager;
 //display preview image?
-if (isset($_GET['preview']))
+if (isset($_GET['preview']) && $uploadmanager->validUploadId($_GET['preview']))
 {
+	header("Content-Type: image/jpeg");
 	$uploadmanager->outputPreviewImage($_GET['preview']);
 	exit;
-}			
+}
 
 $smarty = new GeographPage;
+
 
 //you must be logged in to request changes
 $USER->mustHavePerm("basic");
 
+if (isset($_SERVER['HTTP_X_PSS_LOOP']) && $_SERVER['HTTP_X_PSS_LOOP'] == 'pagespeed_proxy') {
+	$smarty->assign("status_message",'<div class="interestBox" style="background-color:yellow;border:6px solid red;padding:20px;margin:20px;font-size:1.1em;">geograph.org.uk is currently in reduced functionality mode - to deal with traffic levels. <b>The maximum filesize that can be uploaded is now 5Mb.</b> To upload a larger image, please use <a href="http://www.geograph.ie/submit2.php">www.geograph.ie</a> or <a href="http://schools.geograph.org.uk/submit2.php" onclick="location.host = \'schools.geograph.org.uk\'; return false">schools.geograph.org.uk</a> <small>(they upload to the same database)</small></div>');
+	$smarty->assign("small_upload",1);
+}
 
 $template='resubmit.tpl';
 $cacheid='';
 
-$image=new GridImage;	
+$image=new GridImage;
 
 if (isset($_REQUEST['id']))
 {
@@ -71,39 +78,38 @@ if (isset($_REQUEST['id']))
 			header("Location: /photo/{$_REQUEST['id']}");
 			exit;
 		}
-		
+
 		$smarty->assign_by_ref('image',$image);
-		
+
 		if (isset($_POST['finalise']))
 		{
 			if($uploadmanager->setUploadId($_POST['upload_id']))
 			{
 				$uploadmanager->setLargestSize($_POST['largestsize']);
-				
+
 				$uploadmanager->addOriginal($image);
-			
+
 				$smarty->assign('step',4);
-			
+
 			} else {
 				$smarty->assign('step',-1);
 			}
-			
+
 		} elseif (isset($_POST['abandon'])) {
-		
+
 			if($uploadmanager->setUploadId($_POST['upload_id']))
 			{
 				$uploadmanager->cleanUp();
 			}
-			
+
 			$smarty->assign('step',-1);
-			
+
 		} elseif (isset($_POST['next'])) {
-			
-			
-			if (!filesize($_FILES['jpeg']['tmp_name'])) 
+
+			if (!filesize($_FILES['jpeg']['tmp_name']))
 			{
 				$smarty->assign('error', 'Sorry, no file was received - please try again');
-			} 
+			}
 			elseif ($uploadmanager->processUpload($_FILES['jpeg']['tmp_name']))
 			{
 				$smarty->assign('upload_id', $uploadmanager->upload_id);
@@ -113,37 +119,33 @@ if (isset($_REQUEST['id']))
 					$smarty->assign('original_height', $uploadmanager->original_height);
 				}
 
+				$fullpath=$image->_getFullpath();
+	                        if ($fullpath == '/photos/error.jpg' || !file_exists($_SERVER['DOCUMENT_ROOT'].$fullpath)) {
+					 $smarty->assign('allow_same', 1);
+				}
+
 				$smarty->assign('preview_url', "/resubmit.php?preview=".$uploadmanager->upload_id);
 				$smarty->assign('preview_width', $uploadmanager->upload_width);
 				$smarty->assign('preview_height', $uploadmanager->upload_height);
-				
 			} else {
 				$smarty->assign('error', $uploadmanager->errormsg);
-			
 			}
-			
-			
+
 			$smarty->assign('step',2);
-			
 		} else {
-			
-			$db=NewADOConnection($GLOBALS['DSN']);
-					
+			$db=GeographDatabaseConnection(false);
+
 			$exif = $db->getOne("SELECT exif FROM gridimage_exif WHERE gridimage_id = ".$image->gridimage_id);
-			if (empty($exif)) {
-				$exif = $db->getOne("SELECT exif FROM gridimage_exif3 WHERE gridimage_id = ".$image->gridimage_id);
-			}
-			if (empty($exif)) {
-				$exif = $db->getOne("SELECT exif FROM gridimage_exif2 WHERE gridimage_id = ".$image->gridimage_id);
-			}
-			if (empty($exif)) {
-				$exif = $db->getOne("SELECT exif FROM gridimage_exif1 WHERE gridimage_id = ".$image->gridimage_id);
+			if (!empty($exif)) {
+				$exif = unserialize($exif);
+			} else {
+				$exif = read_exif($image->gridimage_id);
+				//returns already unserialized
 			}
 
 			if (!empty($exif)) {
-				$exif = unserialize($exif);
 				$exif2 = array();
-				
+
 				if (preg_match('/(\w+\.jpg)/i',$exif['EXIF']['MakerNote'],$m)) {
 					$exif2['filename'] = $m[1];
 				}
@@ -153,13 +155,13 @@ if (isset($_REQUEST['id']))
 				if (($date = $exif['EXIF']['DateTimeOriginal']) ||
 				    ($date = $exif['EXIF']['DateTimeDigitized']) ||
 				    ($date = $exif['IFD0']['DateTime']) ) {
-				
+
 					$exif2['datetime'] = $date;
 				}
 				if (!empty($exif['IFD0']['Model'])) {
 					$exif2['model'] = $exif['IFD0']['Model'];
 				}
-				if ((is_integer($size = $exif['IFD0']['UndefinedTag:0x1001'])) || 
+				if ((is_integer($size = $exif['IFD0']['UndefinedTag:0x1001'])) ||
 				    ($size = $exif['EXIF']['ExifImageWidth']) ||
 				    ($size = $exif['COMPUTED']['Width']) ) {
 
@@ -175,14 +177,11 @@ if (isset($_REQUEST['id']))
 
 					$exif2['filesize'] = $size;
 				}
-				
+
 				$smarty->assign_by_ref('exif',$exif2);
 			}
-;			$smarty->assign('step',1);
+			$smarty->assign('step',1);
 		}
-
-
-
 
 	}
 	else
@@ -196,7 +195,39 @@ else
 	$smarty->assign('error', 'No image id specified');
 }
 
+if (!empty($_GET['repair']))
+	$smarty->assign('repair',1);
+
 $smarty->display($template, $cacheid);
 
 
-?>
+//this function should be in a central lib - maybe even gridimage.class.php - but for now this is the only page that uses it!
+function read_exif($id) {
+        $folder = "/mnt/combined/geograph_live/exif";
+
+        $start = intval($id/1000)*1000;
+        $dir1 = sprintf("%02d", floor($start/1000000)%100);
+        $dir2 = sprintf("%02d", floor($start/10000)%100);
+        $filename = "$folder/$dir1/$dir2/$start.exif";
+
+        if (file_exists("$filename.gz"))
+                $h = gzopen($opened = "$filename.gz",'rb');
+        elseif (file_exists("$filename"))
+                $h = gzopen($opened = $filename,'rb'); //still use gzopen as it reads uncompressed anyway, and needed for gzgets/gzclose
+        else
+                return false;
+
+        $prefix = "$id\t";
+        $prefixlen = strlen($prefix);
+        while ($h && !feof($h)) {
+                $string = gzgets($h);
+
+                //use prefix compare, rather than split then compare, to avoid splitting all strings.
+                if (strncmp($string, $prefix, $prefixlen) === 0) {
+                        list($id2,$encoded) = explode("\t",$string,2);
+                        return unserialize(base64_decode($encoded));
+                }
+        }
+        gzclose($h);
+}
+
