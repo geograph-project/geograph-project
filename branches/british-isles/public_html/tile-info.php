33bec33cb9fbfe5cfbb9c88a0f04cf7d051ddca1
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2017-10-20 09:21:25 +0000

    fix some cross domain iframes

--- branches/british-isles/public_html/tile-info.php
+++ branches/british-isles/public_html/tile-info.php
@@ -21,6 +21,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+define('ALLOW_FRAMED',1); //HAVE to be CAREFUL to taint all input!
+
 require_once('geograph/global.inc.php');
 
 
