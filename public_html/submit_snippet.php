b058e45c857260412584dba8976918f9e83afc5a
barry <1413156+barryhunter@users.noreply.github.com>
2018-01-05 16:35:08 +0000

    various fixes; hide deleted snippets more, can search by ID better, and filter location searches to grid

--- public_html/submit_snippet.php
+++ public_html/submit_snippet.php
@@ -2,20 +2,20 @@
 /**
  * $Project: GeoGraph $
  * $Id$
- * 
+ *
  * GeoGraph geographic photo archive project
  * This file copyright (C) 2009 Barry Hunter (geo@barryhunter.co.uk)
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -26,7 +26,7 @@ require_once('geograph/global.inc.php');
 init_session();
 
 $smarty = new GeographPage;
-$template='submit_snippet.tpl';	
+$template='submit_snippet.tpl';
 
 $USER->mustHavePerm("basic");
 
@@ -40,19 +40,20 @@ if (!empty($_GET['upload_id'])) {
 
 	$smarty->assign('upload_id',$_GET['upload_id']);
 	$smarty->assign('gridimage_id',$gid);
+
 } elseif (!empty($_REQUEST['gridimage_id'])) {
 
 	$gid = intval($_REQUEST['gridimage_id']);
-	
+
 	$image=new GridImage();
 	$ok = $image->loadFromId($gid);
-		
+
 	if (!$ok) {
 		die("invalid image");
 	} elseif ($image->user_id != $USER->user_id && !$USER->hasPerm('moderator')) {
 		die("unable to access this image");
 	}
-	
+
 	$smarty->assign('gridimage_id',$gid);
 }
 
@@ -64,22 +65,20 @@ if (empty($_REQUEST['tab']) && !empty($_POST['find'])) {
 
 if (!empty($_POST['create']) && (!empty($_POST['title']) || !empty($_POST['comment'])) ) {
 
-
-split_timer('snippet'); //starts the timer
-
+	split_timer('snippet'); //starts the timer
 
 	$updates = array();
 	$updates['user_id'] = $USER->user_id;
 	$updates['title'] = $_POST['title'];
 	$updates['comment'] =  $_POST['comment'];
-	
+
 	$square=new GridSquare;
 	if (!empty($_POST['nogr'])) {
 		$updates['nateastings'] = 0;
 		$point = "'POINT(0 0)'";
-		
+
 	} elseif ((!empty($_POST['grid_reference']) && $square->setByFullGridRef($_POST['grid_reference'],true)) || $square->setByFullGridRef($_GET['gr'],true) ) {
-		
+
 		require_once('geograph/conversions.class.php');
 		$conv = new Conversions;
 		list($lat,$long) = $conv->gridsquare_to_wgs84($square);
@@ -91,21 +90,21 @@ split_timer('snippet'); //starts the timer
 			$updates['natgrlen'] = "".$square->natgrlen;//have to be careful its a enum but holding numberic - so needs string
 		}
 		$updates['reference_index'] = $square->reference_index;
-		
+
 		//for the sphinx index
 		$updates['grid_reference'] = $square->grid_reference;
 
 		$updates['wgs84_lat'] = $lat;
 		$updates['wgs84_long'] = $long;
-		
-		//for mysql indexing (where sphinx not available) 
+
+		//for mysql indexing (where sphinx not available)
 		$point = "'POINT({$square->nateastings} {$square->natnorthings})'";
 	} else {
 		$point = "'POINT(0 0)'";
 	}
-	
+
 	$db->Execute('INSERT INTO snippet SET created=NOW(),point_en=GeomFromText('.$point.'),`'.implode('` = ?,`',array_keys($updates)).'` = ?',array_values($updates));
-	
+
 	if ($gid) {
 		$updates = array();
 		$updates['user_id'] = $USER->user_id;
@@ -113,30 +112,32 @@ split_timer('snippet'); //starts the timer
 		$updates['gridimage_id'] = $gid;
 
 		$db->Execute('INSERT INTO gridimage_snippet SET `'.implode('` = ?,`',array_keys($updates)).'` = ?',array_values($updates));
+
+		$_SESSION['last_snippet'] = intval($updates['snippet_id']);
 	}
 	if ($gid < 4294967296) {
 		//clear any caches involving this photo
 		$ab=floor($gid/10000);
 		$smarty->clear_cache(null, "img$ab|{$gid}");
-		
+
 		$smarty->clear_cache("snippet.tpl", $updates['snippet_id']);
-		
+
 		$memcache->name_delete('sd', $gid);
 	}
 
-split_timer('snippet','create',$gid); //logs the wall time
-	
+	split_timer('snippet','create',$gid); //logs the wall time
+
 } elseif ($gid && !empty($_POST['remove'])) {
-	
-split_timer('snippet'); //starts the timer
+
+	split_timer('snippet'); //starts the timer
 
 	$criteria = array();
 	$criteria['gridimage_id'] = $gid;
-	
+
 	foreach ($_POST['remove'] as $id => $text) {
-		
+
 		$criteria['snippet_id'] = $id;
-		
+
 		$db->Execute('DELETE FROM gridimage_snippet WHERE `'.implode('` = ? AND `',array_keys($criteria)).'` = ?',array_values($criteria));
 	}
 
@@ -144,45 +145,44 @@ split_timer('snippet'); //starts the timer
 		//clear any caches involving this photo
 		$ab=floor($gid/10000);
 		$smarty->clear_cache(null, "img$ab|{$gid}");
-		
+
 		$smarty->clear_cache("snippet.tpl", $criteria['snippet_id']);
-		
+
 		$memcache->name_delete('sd', $gid);
 	}
 
-split_timer('snippet','remove',$gid); //logs the wall time
-
+	split_timer('snippet','remove',$gid); //logs the wall time
 
 } elseif ($gid && !empty($_POST['add'])) {
-	
-split_timer('add'); //starts the timer
+
+	split_timer('add'); //starts the timer
 
 	$updates = array();
 	$updates['gridimage_id'] = $gid;
 	$updates['user_id'] = $USER->user_id;
-	
+
 	foreach ($_POST['add'] as $id => $text) {
-		
+
 		$updates['snippet_id'] = $id;
-		
+
 		$db->Execute('INSERT IGNORE INTO gridimage_snippet SET `'.implode('` = ?, `',array_keys($updates)).'` = ?',array_values($updates));
+
+		$_SESSION['last_snippet'] = intval($id);
+
 	}
-	
+
 	if ($gid < 4294967296) {
 		//clear any caches involving this photo
 		$ab=floor($gid/10000);
 		$smarty->clear_cache(null, "img$ab|{$gid}");
-		
+
 		$smarty->clear_cache("snippet.tpl", $updates['snippet_id']);
-		
+
 		$memcache->name_delete('sd', $gid);
 	}
 
-split_timer('snippet','remove',$gid); //logs the wall time
-
-	
+	split_timer('snippet','remove',$gid); //logs the wall time
 }
-
 
 
 if ($gid) {
@@ -191,7 +191,7 @@ if ($gid) {
 	$used = $db->getAll("SELECT * FROM gridimage_snippet INNER JOIN snippet USING (snippet_id) WHERE gridimage_id = $gid ORDER BY gridimage_snippet.created");
 
 	$smarty->assign_by_ref('used',$used);
-	
+
 	split_timer('snippet','single',$gid); //logs the wall time
 
 }
@@ -201,7 +201,7 @@ if (empty($_GET['gr']) && !empty($_GET['gr2'])) {
 
 if (!empty($_REQUEST['gr']) || !empty($_REQUEST['q']) || !empty($_REQUEST['tab'])) {
 	$square=new GridSquare;
-	
+
 	$grid_given=false;
 	if (!empty($_REQUEST['gr'])) {
 		if ($_REQUEST['gr'] == '-' || $_REQUEST['gr'] == 'none') {
@@ -209,13 +209,12 @@ if (!empty($_REQUEST['gr']) || !empty($_REQUEST['q']) || !empty($_REQUEST['tab']
 			$smarty->assign('gr',$_REQUEST['gr']);
 		} elseif ($grid_ok=$square->setByFullGridRef($_REQUEST['gr'],true)) {
 			$grid_given = true;
-		
+
 			$smarty->assign('gr',$_REQUEST['gr']);
 
 			if ($square->natgrlen > 4) {
 				$smarty->assign('centisquare',1);
 			}
-
 		} else {
 			$grid_given = true;
 			print "invalid GR!";
@@ -224,44 +223,49 @@ if (!empty($_REQUEST['gr']) || !empty($_REQUEST['q']) || !empty($_REQUEST['tab']
 	$fields = '';
 
 	if ($grid_given && $grid_ok) {
-		$fields = ",if(natnorthings > 0,(nateastings-{$square->nateastings})*(nateastings-{$square->nateastings})+(natnorthings-{$square->natnorthings})*(natnorthings-{$square->natnorthings}),0) as distance";
+		$fields = ",if(natnorthings > 0,pow(cast(nateastings as signed)-{$square->nateastings},2)+pow(cast(natnorthings as signed)-{$square->natnorthings},2),0) as distance";
 	}
 
-	if (!empty($_REQUEST['tab']) && $_REQUEST['tab'] == 'recent') {  
-		
+	####################################################
+	# Recent Tab
+
+	if (!empty($_REQUEST['tab']) && $_REQUEST['tab'] == 'recent') {
+
 		split_timer('snippet'); //starts the timer
 
-		$results = $db->getAll($sql="SELECT s.*,MAX(gs.created) AS last_used $fields FROM snippet s INNER JOIN gridimage_snippet gs USING (snippet_id) WHERE gs.user_id = {$USER->user_id} AND gridimage_id != $gid GROUP BY s.snippet_id ORDER BY last_used DESC LIMIT 50"); 
+		$results = $db->getAll($sql="SELECT s.*,MAX(gs.created) AS last_used $fields FROM snippet s INNER JOIN gridimage_snippet gs USING (snippet_id) WHERE gs.user_id = {$USER->user_id} AND gridimage_id != $gid AND enabled = 1 GROUP BY s.snippet_id ORDER BY last_used DESC LIMIT 50"); 
 
-		
 		$smarty->assign('tab',$_REQUEST['tab']);
-		
+
 		split_timer('snippet','recent',$USER->user_id); //logs the wall time
 
-		
 	} else {
 		$where = array();
 		$orderby = "ORDER BY s.snippet_id";
-	
-		if (!empty($_REQUEST['tab']) && $_REQUEST['tab'] == 'suggestions') {  
-		
+
+
+	####################################################
+	# Suggestions Tab
+
+		if (!empty($_REQUEST['tab']) && $_REQUEST['tab'] == 'suggestions') {
+
 			split_timer('snippet'); //starts the timer
-			
+
 			$pg = 1;
-			
+
 			$q=preg_replace("/[^\w ]+/",' ',$_REQUEST['corpus']);
 			$q=trim(preg_replace('/\b(or|and|the|geograph|amp|quot|pound|a|about|above|according|across|actually|adj|after|afterwards|again|against|all|almost|alone|along|already|also|although|always|among|amongst|an|another|any|anyhow|anyone|anything|anywhere|are|arent|around|as|at|b|be|became|because|become|becomes|becoming|been|before|beforehand|begin|beginning|behind|being|below|beside|besides|between|beyond|billion|both|but|by|c|can|cant|cannot|caption|co|co.|could|couldnt|d|did|didnt|do|does|doesnt|dont|down|during|e|each|eg|e.g.|eight|eighty|either|else|elsewhere|end|ending|enough|etc|etc.|even|ever|every|everyone|everything|everywhere|except|f|few|fifty|first|five|for|former|formerly|forty|found|four|from|further|g|h|had|has|hasnt|have|havent|he|hed|hell|hes|hence|her|here|heres|hereafter|hereby|herein|hereupon|hers|herself|him|himself|his|how|however|hundred|i|id|ill|im|ive|ie|if|in|inc|inc.|indeed|instead|into|is|isnt|it|its|its|itself|j|k|l|last|later|latter|latterly|least|less|let|lets|like|likely|ltd|m|made|make|makes|many|maybe|me|meantime|meanwhile|might|million|miss|more|moreover|most|mostly|mr|mrs|much|must|my|myself|n|namely|neither|never|nevertheless|next|nine|ninety|no|nobody|none|nonetheless|noone|nor|not|nothing|now|nowhere|o|of|off|often|on|once|one|ones|only|onto|other|others|otherwise|our|ours|ourselves|out|over|overall|own|p|per|perhaps|q|r|rather|recent|recently|s|same|seem|seemed|seeming|seems|seven|seventy|several|she|shed|shell|shes|should|shouldnt|since|six|sixty|so|some|somehow|someone|something|sometime|sometimes|somewhere|still|stop|such|t|taking|ten|than|that|thatll|thats|thatve|their|them|themselves|then|thence|there|thered|therell|therere|theres|thereve|thereafter|thereby|therefore|therein|thereupon|these|they|theyd|theyll|theyre|theyve|thirty|this|those|though|thousand|three|through|throughout|thru|thus|to|together|too|toward|towards|trillion|twenty|two|u|under|unless|unlike|unlikely|until|up|upon|us|used|using|v|very|via|w|was|wasnt|we|wed|well|were|weve|well|were|werent|what|whatll|whats|whatve|whatever|when|whence|whenever|where|wheres|whereafter|whereas|whereby|wherein|whereupon|wherever|whether|which|while|whither|who|whod|wholl|whos|whoever|whole|whom|whomever|whose|why|will|with|within|without|wont|would|wouldnt|x|y|yes|yet|you|youd|youll|youre|youve|your|yours|yourself|yourselves|z)\b/','',$q));
-			
+
 			if ($grid_given && $grid_ok) {
 				$q .= ' '.$square->grid_reference.' '.$square->gridsquare;
 			}
-			
+
 			if (strlen($q) > 40) {
 				$q = '"'.$q.'"/3';
 			} else {
 				$q = '~'.$q;
 			}
-			
+
 			$sphinx = new sphinxwrapper($q);
 			$sphinx->pageSize = $pgsize = 40;
 
@@ -285,30 +289,37 @@ if (!empty($_REQUEST['gr']) || !empty($_REQUEST['q']) || !empty($_REQUEST['tab']
 				$orderby = "ORDER BY FIELD(s.snippet_id,$id_list)";
 			} else {
 				$where[] = '0';
-			}			
+			}
 			$smarty->assign('tab',$_REQUEST['tab']);
-			
+
 			split_timer('snippet','suggestions',$USER->user_id); //logs the wall time
-			
-		} elseif (!empty($_REQUEST['q']) && is_numeric($_REQUEST['q'])) {  
+
+	####################################################
+	# Numeric single id search
+	//now done, by sphinx (image ids are put in a field, and the snippet Id is prepended)
+
+		} elseif (false && !empty($_REQUEST['q']) && is_numeric($_REQUEST['q'])) {
 
 			split_timer('snippet'); //starts the timer
-			
+
 			$ids = $db->getCol("SELECT snippet_id FROM gridimage_snippet WHERE gridimage_id = ".intval($_REQUEST['q']));
 
 			$ids[] = intval($_REQUEST['q']); //incase it's a snippet ID
 
 			$where[] = "s.snippet_id IN (".implode(',',$ids).")";
+			$where[] = "enabled = 1";
 
-			$_POST['radius'] = 1000; //it ignored anyway. 
-			
+			$_POST['radius'] = 1000; //it ignored anyway.
+
 			split_timer('snippet','nemeric',intval($_REQUEST['q'])); //logs the wall time
 
-			
+	####################################################
+	# Standard Sphinx Search
+
 		} elseif ($CONF['sphinx_host'] && !empty($_REQUEST['q'])) {  //todo - for the moment we only use sphinx for full text searches- because of the indexing delay 
-			
+
 			split_timer('snippet'); //starts the timer
-			
+
 			require_once('geograph/conversions.class.php');
 			$conv = new Conversions;
 
@@ -376,22 +387,35 @@ if (!empty($_REQUEST['gr']) || !empty($_REQUEST['q']) || !empty($_REQUEST['tab']
 				$ids = $sphinx->returnIds($pg,'snippet');
 			}
 
+			if (is_numeric($_REQUEST['q'])) {
+				array_unshift($ids,intval($_REQUEST['q']));
+				$where[] = "enabled = 1"; //just in case its a disabled id. Doesnt normally need this as sphinx only contains enabled ones!
+			}
+			if (!empty($_SESSION['last_snippet'])) {
+				array_unshift($ids,intval($_SESSION['last_snippet']));
+				$where[] = "enabled = 1";
+			}
+
 			if (!empty($ids) && count($ids)) {
 				$id_list = implode(',',$ids);
 				$where[] = "s.snippet_id IN($id_list)";
 				$orderby = "ORDER BY FIELD(s.snippet_id,$id_list)";
-				
+
 				split_timer('snippet','q',$sphinx->qo); //logs the wall time
-				
+
 			} else {
 				$where[] = '0';
-				
+
 				split_timer('snippet','q-zero',$sphinx->qo); //logs the wall time
 			}
+
+	####################################################
+	# Fallback Search (if no sphinx available) - but also used for some non query lists (eg nearby)
+
 		} else {
 			split_timer('snippet'); //starts the timer
-			
-			if (!empty($_REQUEST['gr']) && (empty($_REQUEST['radius']) || $_REQUEST['radius'] <= 20) ) {
+
+			if ($CONF['sphinx_host'] && !empty($_REQUEST['gr']) && (empty($_REQUEST['radius']) || $_REQUEST['radius'] <= 20) ) {
 				$radius = !empty($_REQUEST['radius'])?intval($_REQUEST['radius']*1000):1000;
 
 				$left=$square->nateastings-$radius;
@@ -406,7 +430,7 @@ if (!empty($_REQUEST['gr']) || !empty($_REQUEST['q']) || !empty($_REQUEST['tab']
 
 				$ids = $sphinx->returnIds($pg,'snippet');
 
-				$where[] = "CONTAINS(
+				$where[] = "reference_index = {$square->reference_index} AND CONTAINS(
 						GeomFromText($rectangle),
 						point_en)".(empty($ids)?'':(' OR s.snippet_id IN ('.implode(',',$ids).')'));
 			}
@@ -419,7 +443,7 @@ if (!empty($_REQUEST['gr']) || !empty($_REQUEST['q']) || !empty($_REQUEST['tab']
 			if (!empty($_REQUEST['q'])) {
 				$q=mysql_real_escape_string(trim($_REQUEST['q']));
 
-				$where[] = "(title LIKE '%$q%' OR comment LIKE '%$q%')";
+				$where[] = "(title LIKE '%$q%' OR comment LIKE '%$q%' OR s.snippet_id = '$q')";
 				$smarty->assign('q',trim($_POST['q']));
 			}
 
@@ -445,7 +469,9 @@ if (!empty($_REQUEST['gr']) || !empty($_REQUEST['q']) || !empty($_REQUEST['tab']
 		}
 
 		if (empty($results) && empty($_REQUEST['tab'])) {
-			$results = $db->getAll($sql="SELECT s.*,MAX(gs.created) AS last_used $fields FROM snippet s INNER JOIN gridimage_snippet gs USING (snippet_id) WHERE gs.user_id = {$USER->user_id} AND gridimage_id != $gid GROUP BY s.snippet_id ORDER BY last_used DESC LIMIT 50"); 
+			//if no results, fallback and just show recent snippets anyway...
+
+			$results = $db->getAll($sql="SELECT s.*,MAX(gs.created) AS last_used $fields FROM snippet s INNER JOIN gridimage_snippet gs USING (snippet_id) WHERE gs.user_id = {$USER->user_id} AND gridimage_id != $gid AND enabled = 1 GROUP BY s.snippet_id ORDER BY last_used DESC LIMIT 50"); 
 
 			if (count($results) > 1)
 				$smarty->assign('tab','recent');
@@ -453,6 +479,8 @@ if (!empty($_REQUEST['gr']) || !empty($_REQUEST['q']) || !empty($_REQUEST['tab']
 
 		split_timer('snippet','query',$where); //logs the wall time
 	}
+
+	####################################################
 
 	if ($fields) {
 		foreach ($results as $id => $row) {
