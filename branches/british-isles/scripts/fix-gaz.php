36c4a09d02b99d22b2cb355dd9ca61230067665b
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2018-02-24 16:55:01 +0000

    turns out a few rows in loc_placenames needed fixing too

--- branches/british-isles/scripts/fix-gaz.php
+++ branches/british-isles/scripts/fix-gaz.php
@@ -37,11 +37,10 @@ but its actully an import of utf8 data, so doesnt work as latin1.
 
 alter table os_gaz add `def_nam_utf` varchar(60) NOT NULL DEFAULT '' after  `def_nam`;
 update os_gaz set def_nam_utf = def_nam;
-*/
 
-//runit('os_gaz','def_nam','seq');
+runit('os_gaz','def_nam','seq');
 
-/*
+#####################################################
 
 select placename_id,Place,def_nam from sphinx_placenames inner join os_gaz on (seq = placename_id - 1000000 and Place = BINARY def_nam_utf )
 where reference_index = 1 and Place  not rlike binary '^[ -~\\n\\r]*$' limit 100
@@ -50,6 +49,7 @@ update sphinx_placenames inner join os_gaz on (seq = placename_id - 1000000 and 
 set Place = IF(os_gaz.has_dup,CONCAT(def_nam,'/',os_gaz.km_ref),def_nam)
 where reference_index = 1 and Place  not rlike binary '^[ -~\\n\\r]*$';
 
+#####################################################
 
 alter table os_open_names2
  add `name1_utf` varchar(255) NOT NULL after `name1`,
@@ -58,16 +58,36 @@ alter table os_open_names2
 
 update os_open_names2 set name1_utf=name1, name2_utf=name2, populated_place_utf=populated_place;
 
-runit('os_open_names2','name1','id');
-runit('os_open_names2','name2','id');
-runit('os_open_names2','populated_place','id');
-
-*/
+runit('os_open_names','name1','id');
+runit('os_open_names','name2','id');
+runit('os_open_names','populated_place','id');
 
 runit('os_open_names2','name1','seq');
 runit('os_open_names2','name2','seq');
 runit('os_open_names2','populated_place','seq');
 
+#####################################################
+
+alter table loc_placenames add `full_name_utf` varchar(65) NOT NULL DEFAULT '' after  `full_name`;
+update loc_placenames set full_name_utf = full_name;
+
+runit('loc_placenames','full_name','id');
+
+select placename_id,sphinx_placenames.reference_index,Place,full_name
+from sphinx_placenames inner join loc_placenames on (id = placename_id AND Place = BINARY full_name_utf )
+where Place  not rlike binary '^[ -~\\n\\r]*$' AND sphinx_placenames.has_dup = 0 limit 100;
+
+-- ONLY safe to run on has_dup=0 rows!?!
+
+update sphinx_placenames inner join loc_placenames on (id = placename_id)
+set Place = full_name
+where Place  not rlike binary '^[ -~\\n\\r]*$' AND sphinx_placenames.has_dup=0;
+
+update placename_index inner join loc_placenames using (id) set name = full_name where name not rlike binary '^[ -~\\n\\r]*$';
+
+*/
+
+#####################################################
 
 function runit($table,$column,$primary) {
 	global $db;
@@ -83,10 +103,21 @@ function runit($table,$column,$primary) {
 
 		//convert data that is really UTF8 encoded, back to ISO-8859-15 as that is what Geograph expects!
 
+		if ($table == 'loc_placenames')
+			//dont really understand how this happened, by this table seems to have quotes as this byte sequence. Possibyl a smart quote in original data, but just utf_decoding it doesnt wor!
+			//$row[$column.'_utf'] = str_replace(urldecode("%27%E2%82%AC%E2%84%A2"),"'",$row[$column.'_utf']);
+			$row[$column.'_utf'] = str_replace(urldecode("%27%80%99"),"'",$row[$column.'_utf']);
+
 		$row[$column] = mb_convert_encoding($row[$column.'_utf'], 'ISO-8859-15', 'UTF-8');
 
 		$q = $db->Quote($row[$column]);
 		$sql = "UPDATE $table SET $column = $q WHERE $primary = ".$db->Quote($row[$primary]);
+
+//print_r("$sql\n\n");
+//print str_replace('+',' ',urlencode($sql))."\n\n";
+//exit;
+//$recordSet->MoveNext();
+//continue;
 
 		$db->Execute($sql);
 		$m += mysql_affected_rows();
