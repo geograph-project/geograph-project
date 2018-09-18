62b7da9ddee15232a0b2aaaa41c4e8b550e90aca
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2018-09-18 10:45:05 +0000

    allow slashes in title

--- branches/british-isles/public_html/blog/edit.php
+++ branches/british-isles/public_html/blog/edit.php
@@ -2,20 +2,20 @@
 /**
  * $Project: GeoGraph $
  * $Id: faq.php 15 2005-02-16 12:23:35Z lordelph $
- * 
+ *
  * GeoGraph geographic photo archive project
  * This file copyright (C) 2006 Barry Hunter (geo@barryhunter.co.uk)
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -47,7 +47,7 @@ $cacheid = '';
 		$smarty->assign('user_id', $USER->user_id);
 		$page = array();
 
-		$USER->getStats();	
+		$USER->getStats();
 	        if ($USER->stats['images'] < 5) {
 			die("Due to spam - this feature is only available to photo contributors. If you would still like to contribute, please <a href=\"/contact.php\">Contact Us</a>, otherwise <a href=\"javascript:history.go(-1)\">Go back</a>");
 		}
@@ -55,18 +55,16 @@ $cacheid = '';
 
 	} else {
 		$sql_where = " blog_id = ".$db->Quote($_REQUEST['id']);
-		 
-		$prev_fetch_mode = $ADODB_FETCH_MODE;
-		$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;	
+
+		$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 		$page = $db->getRow($sql = "
 		select blog.*,realname,gs.grid_reference
-		from blog 
+		from blog
 			left join user using (user_id)
 			left join gridsquare gs on (blog.gridsquare_id = gs.gridsquare_id)
 		where $sql_where
 		limit 1");
-		$ADODB_FETCH_MODE = $prev_fetch_mode;
-		
+
 		if (count($page) && ($page['user_id'] == $USER->user_id || $USER->hasPerm('moderator'))) {
 			$smarty->assign($page);
 			$smarty->assign('id', $page['blog_id']);
@@ -80,11 +78,10 @@ $cacheid = '';
 
 if ($template != 'static_404.tpl' && isset($_POST) && isset($_POST['submit'])) {
 	$errors = array();
-	
 
 	$_POST['published']=sprintf("%04d-%02d-%02d %02d:%02d:%02d",$_POST['publishedYear'],$_POST['publishedMonth'],$_POST['publishedDay'],$_POST['publishedHour'],$_POST['publishedMinute'],$_POST['publishedSecond']);
-	$_POST['title'] = preg_replace('/[^\w-\.,\' ]+/','',trim($_POST['title']));
-	
+	$_POST['title'] = preg_replace('/[^\w\/\.,\'" -]+/','',trim($_POST['title']));
+
 	if ($_POST['title'] == "New Entry")
 		$errors['title'] = "Please give a meaningful title";
 
@@ -93,19 +90,19 @@ if ($template != 'static_404.tpl' && isset($_POST) && isset($_POST['submit'])) {
 		if ($gs->setByFullGridRef($_POST['grid_reference'])) {
 			$_POST['gridsquare_id'] = $gs->gridsquare_id;
 			$smarty->assign('grid_reference', $_POST['grid_reference']);
-		} else 
+		} else
 			$errors['grid_reference'] = $gs->errormsg;
 	}
-	
+
 	$_POST['content'] = strip_tags($_POST['content']);
-	
+
 	$updates = array();
 	foreach (array('title','content','tags','published','gridsquare_id','gridimage_id','template') as $key) {
 		if ($page[$key] != $_POST[$key]) {
-			$updates[] = "`$key` = ".$db->Quote($_POST[$key]); 
+			$updates[] = "`$key` = ".$db->Quote($_POST[$key]);
 			$smarty->assign($key, $_POST[$key]);
-		} elseif (empty($_POST[$key]) && $key != 'tags' && $key != 'gridsquare_id' && $key != 'gridimage_id' ) 
-			$errors[$key] = "missing required info";		
+		} elseif (empty($_POST[$key]) && $key != 'tags' && $key != 'gridsquare_id' && $key != 'gridimage_id' )
+			$errors[$key] = "missing required info";
 	}
 
 	if (isset($_POST['initial'])) {
@@ -116,22 +113,20 @@ if ($template != 'static_404.tpl' && isset($_POST) && isset($_POST['submit'])) {
 		$errors[1] =1;
 	}
 	if ($_REQUEST['id'] == 'new') {
-	
+
 		$updates[] = "`user_id` = {$USER->user_id}";
 		$updates[] = "`created` = NOW()";
 		$sql = "INSERT INTO blog SET ".implode(',',$updates);
 	} else {
-		
 		$sql = "UPDATE blog SET ".implode(',',$updates)." WHERE blog_id = ".$db->Quote($_REQUEST['id']);
 	}
 	if (!count($errors) && count($updates)) {
-		
+
 		$db->Execute($sql);
 		if ($_REQUEST['id'] == 'new') {
 			$_REQUEST['id'] = $db->Insert_ID();
 		}
-	;
-		
+
 		$smarty->clear_cache('blogs.tpl');
 		$smarty->clear_cache('blogs.tpl',$USER->user_id);
 		$smarty->clear_cache('blog_entry.tpl',$_REQUEST['id']);
@@ -140,7 +135,7 @@ if ($template != 'static_404.tpl' && isset($_POST) && isset($_POST['submit'])) {
 			$smarty->clear_cache('blog_entry'.$i.'.tpl',$_REQUEST['id']);
 			$smarty->clear_cache('blog_entry'.$i.'.tpl',$_REQUEST['id']."|".$USER->user_id);
 		}
-		
+
 		header("Location: /blog/entry.php?id=".intval($_REQUEST['id']));
 		exit;
 	} else {
@@ -148,7 +143,7 @@ if ($template != 'static_404.tpl' && isset($_POST) && isset($_POST['submit'])) {
 			$smarty->assign('error', "Please see messages below...");
 		$smarty->assign_by_ref('errors',$errors);
 	}
-} 
+}
 
 $templates = array(
  1 => 'Directly Inline (Original format)',
