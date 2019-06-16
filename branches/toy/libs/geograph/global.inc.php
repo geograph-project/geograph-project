c7e3e3c55d66f76f518f24bb26c29a64230692da
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2019-06-16 13:56:49 +0000

    initial version - still lots of parts to add, just smarty and filesystem so far

---
+++ branches/toy/libs/geograph/global.inc.php
@@ -0,0 +1,60 @@
+<?
+
+require "conf/example.conf";
+
+#####################################################################
+// smarty configuration
+
+//smarty needed everywhere too
+require_once('smarty/libs/Smarty.class.php');
+
+
+class GeographPage extends Smarty
+{
+        /**
+        * Constructor - sets up smarty appropriately
+        */
+        function GeographPage()
+        {
+                global $CONF;
+
+               //base constructor
+                $this->Smarty();
+
+                //set up paths
+                $this->template_dir=$_SERVER['DOCUMENT_ROOT'].'/templates/'.$CONF['template'];
+                $this->compile_dir=$this->template_dir."/compiled";
+                $this->cache_dir=$this->template_dir."/cache";
+
+                //setup optimisations
+                $this->compile_check = $CONF['smarty_compile_check'];
+                $this->debugging = $CONF['smarty_debugging'];
+
+	}
+
+}
+
+#####################################################################
+// folder config
+
+class FileSystem {
+	function exists($path) {
+		return file_exists($path);
+	}
+}
+
+#####################################################################
+//database configuration
+
+
+#####################################################################
+// Sphinx/Manticore configuration
+
+
+#####################################################################
+// Redis Host
+
+#####################################################################
+// memcache, may be implemented with redis (to avoid needing BOTH redis and memcache backends!
+
+#####################################################################
---
+++ branches/toy/libs/smarty/libs/internals/core.process_compiled_include.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * Replace nocache-tags by results of the corresponding non-cacheable
+ * functions and return it
+ *
+ * @param string $compiled_tpl
+ * @param string $cached_source
+ * @return string
+ */
+
+function smarty_core_process_compiled_include($params, &$smarty)
+{
+    $_cache_including = $smarty->_cache_including;
+    $smarty->_cache_including = true;
+
+    $_return = $params['results'];
+
+    foreach ($smarty->_cache_info['cache_serials'] as $_include_file_path=>$_cache_serial) {
+        $smarty->_include($_include_file_path, true);
+    }
+
+    foreach ($smarty->_cache_serials as $_include_file_path=>$_cache_serial) {
+        $_return = preg_replace_callback('!(\{nocache\:('.$_cache_serial.')#(\d+)\})!s',
+                                         array(&$smarty, '_process_compiled_include_callback'),
+                                         $_return);
+    }
+    
+    
+    ###############
+    # catch any still left and hide them. but notify the developer of the problem
+    $count = 0;
+    $_return = preg_replace('!(\{nocache\:(\w+)#(\d+)\})!sU','',$_return,-1,$count);
+
+    if ($count > 0 && function_exists('apc_store') && !apc_fetch('nocache_warning'.$_include_file_path)) {
+    
+	ob_start();
+	print_r($_SERVER);
+	debug_print_backtrace();
+	print "\n\nHost: ".`hostname`."\n\n";
+	print_r($GLOBALS);
+	print_r(get_included_files());
+	$con = ob_get_clean();
+	mail('geograph@barryhunter.co.uk','[Geograph Error] nocache: '.$_SERVER['SCRIPT_NAME'],$con);
+	
+    
+    
+    	apc_store('nocache_warning'.$_include_file_path,1,500);
+    }
+    ###############
+    
+    
+    $smarty->_cache_including = $_cache_including;
+    return $_return;
+}
+
+?>
