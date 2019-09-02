92ce82c1bc9572b8ba87211cc44bc9e4837abbf4
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2019-09-02 17:12:04 +0000

    next version, more tidyups

---
+++ branches/toy/public_html/index.php
@@ -0,0 +1,21 @@
+<h2>Geograph Test / Referenece Implemenation</h2>
+
+This is a basic implementation of Geograph Website code. It's not a proper user-facing website,
+just enough code to test the backend services. We use this internaly to test our servers are capable of running the <a href="https://www.geograph.org.uk/">real Geograph website</a>.
+
+<hr>
+
+<ul>
+
+        <li><a href="https://svn.geograph.org.uk/viewsvn/?do=view&project=geograph&path=/branches/toy/README.txt">View README.txt</a></li>
+
+	<li><a href="test.php">Run Live Application Test</a></li>
+
+	<li><a href="source.php">View listing of files</a></li>
+
+	<li><a href="https://svn.geograph.org.uk/viewsvn/?do=browse&project=geograph&path=/branches/toy/">View Code in SVN</a></li>
+
+</ul>
+
+<hr>
+Created by barry [at] geograph [dot org [dot] uk - contact me if any questions.
---
+++ branches/toy/public_html/source.php
@@ -0,0 +1,54 @@
+<table>
+<tr>
+	<th>Filename (click to view in SVN)</th>
+	<th>View Live</th>
+	<th></th>
+<?
+
+$files = `find /var/www/geograph_toy/ -type f | grep -v /.svn/ |  grep -v /smarty/libs/internals/ | grep -v .back | grep -v _attic`;
+
+$svn = 'https://svn.geograph.org.uk/viewsvn/?do=view&project=geograph&path=/branches/toy$dir2/$file2';
+
+foreach (explode("\n",$files) as $file) {
+	if (empty($file))
+		continue;
+	if (strpos($file,'/smarty') !== FALSE && strpos($file,'/Smarty.class.php') === FALSE) {
+		continue;
+	}
+	print "<tr>";
+	$dir2 = str_replace('/var/www/geograph_toy','',dirname($file));
+	$file2 = basename($file);
+	print "<td style=\"font-family:monospace\">$dir2/";
+
+	if (strpos($dir2,'/compiled') !== FALSE) { //todo, maybe use SVN info?
+		print "$file2</td>";
+	} else {
+		$url = str_replace('$dir2',$dir2,$svn);
+		$url = str_replace('$file2',$file2,$url);
+		print "<b><a href=\"$url\">$file2</a></b></td>";
+	}
+
+	if (strpos($dir2,'/public_html') === 0 && preg_match('/\.(php|jpg|txt)$/',$file2) && strpos($dir2,'/compiled') === FALSE) {
+		$url = str_replace('/var/www/geograph_toy/public_html/','https://toy.geograph.org.uk/',$file);
+		print "<td><a href=\"$url\">view</a></td>";
+	} else {
+		print "<td></td>";
+	}
+
+	if ( preg_match('/\.(php|jpg|conf|mysql|txt)$/',$file2)) {
+		print "<td align=right>".filesize($file)." bytes";
+	}
+	if ( preg_match('/\.(php|conf|mysql)$/',$file2)) {
+		print "<td align=right>".intval(`wc -l $file`)." lines";
+	}
+
+	print "</tr>\n";
+
+	if (strpos($file,'/Smarty.class.php') !== FALSE) {
+		$url = "https://svn.geograph.org.uk/viewsvn/?do=browse&project=geograph&path=/branches/toy/libs/smarty/libs/";
+		print "<tr><td colspan=3>... <i>rest of Smarty internals hidden for brevity, to see rest: <a href=\"$url\">view in SVN</a>";
+	}
+
+}
+
+
