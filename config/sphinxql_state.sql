11f1925d05bb098f52d759fd944e057e7a15b6e1
barry <1413156+barryhunter@users.noreply.github.com>
2019-09-06 18:24:31 +0000

    bulk out the manticore config. Also more notes for other services into README

---
+++ config/sphinxql_state.sql
@@ -0,0 +1,5 @@
+CREATE FUNCTION uniqueserial RETURNS INT SONAME 'uniqueserial.so';
+CREATE FUNCTION withinfirstx RETURNS INT SONAME 'withinfirstx.so';
+CREATE FUNCTION withinfirstxmva32 RETURNS INT SONAME 'withinfirstx.so';
+CREATE FUNCTION withinfirstxstring RETURNS INT SONAME 'withinfirstx.so';
+CREATE FUNCTION uniqueserialstring RETURNS INT SONAME 'uniqueserial.so';
