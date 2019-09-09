0ebdebc53d90e2178e1ddaada4aca2098ffb7854
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2019-09-09 14:29:04 +0000

    tweaks

--- branches/toy/public_html/index.php
+++ branches/toy/public_html/index.php
@@ -4,20 +4,29 @@ This is a basic implementation of Geograph Website code. It's not a proper user-
 just enough code to test the backend services. We use this internaly to test our servers are capable of running the <a href="https://www.geograph.org.uk/">real Geograph website</a>.
 
 <hr>
-
+<style>ul li {padding:8px} </style>
 <ul>
 
         <li><a href="https://svn.geograph.org.uk/viewsvn/?do=view&project=geograph&path=/branches/toy/README.txt">View README.txt</a></li>
 
-	<li><a href="test.php">Run Live Application Test</a></li>
+	<li><a href="test.php">Run Live Application Test</a><ul>
 
 	<li>There is <a href="heartbeat.php">Automated Test</a> - its output doesn't show much - designed for powering a uptime/proxy check</li>
 
+	</ul></li>
+
 	<li><a href="source.php">View listing of files</a></li>
 
-	<li><a href="https://svn.geograph.org.uk/viewsvn/?do=browse&project=geograph&path=/branches/toy/">View Code in SVN</a></li>
+	<li>Subversion Repository: <a href="https://svn.geograph.org.uk/svn/branches/toy/">https://svn.geograph.org.uk/svn/branches/toy/</a><ul>
 
-	<li><a href="https://svn.geograph.org.uk/viewsvn/?do=log&project=geograph&path=/branches/toy/">View SVN Log</a></li>
+		<li><a href="https://svn.geograph.org.uk/viewsvn/?do=browse&project=geograph&path=/branches/toy/">View Code in SVN viewer</a></li>
+
+		<li>Live Repository checkout:<br>
+		<tt>svn checkout https://svn.geograph.org.uk/svn/branches/toy/ geograph_toy/</tt> </li>
+
+		<li><a href="https://svn.geograph.org.uk/viewsvn/?do=log&project=geograph&path=/branches/toy/">View SVN Log</a></li>
+
+        </ul></li>
 
 </ul>
 
