1051c5d1c9bb85f1352ab044b71d9c7c2a2c5985
barry <1413156+barryhunter@users.noreply.github.com>
2019-09-10 11:59:18 +0000

    upgrade to smarty 2.6.31 to get php7 compatility

--- libs/smarty/libs/internals/core.process_compiled_include.php
+++ libs/smarty/libs/internals/core.process_compiled_include.php
@@ -25,36 +25,11 @@ function smarty_core_process_compiled_include($params, &$smarty)
         $smarty->_include($_include_file_path, true);
     }
 
-    foreach ($smarty->_cache_serials as $_include_file_path=>$_cache_serial) {
+    foreach ($smarty->_cache_info['cache_serials'] as $_include_file_path=>$_cache_serial) {
         $_return = preg_replace_callback('!(\{nocache\:('.$_cache_serial.')#(\d+)\})!s',
                                          array(&$smarty, '_process_compiled_include_callback'),
                                          $_return);
     }
-    
-    
-    ###############
-    # catch any still left and hide them. but notify the developer of the problem
-    $count = 0;
-    $_return = preg_replace('!(\{nocache\:(\w+)#(\d+)\})!sU','',$_return,-1,$count);
-
-    if ($count > 0 && function_exists('apc_store') && !apc_fetch('nocache_warning'.$_include_file_path)) {
-    
-	ob_start();
-	print_r($_SERVER);
-	debug_print_backtrace();
-	print "\n\nHost: ".`hostname`."\n\n";
-	print_r($GLOBALS);
-	print_r(get_included_files());
-	$con = ob_get_clean();
-	mail('geograph@barryhunter.co.uk','[Geograph Error] nocache: '.$_SERVER['SCRIPT_NAME'],$con);
-	
-    
-    
-    	apc_store('nocache_warning'.$_include_file_path,1,500);
-    }
-    ###############
-    
-    
     $smarty->_cache_including = $_cache_including;
     return $_return;
 }
