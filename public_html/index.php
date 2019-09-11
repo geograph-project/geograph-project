901b412f52d3b49ca78d4795aa66464b180633a7
barry <1413156+barryhunter@users.noreply.github.com>
2019-09-11 23:33:58 +0000

    tidy up the homepage

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,16 @@
-<h2>Geograph Test / Referenece Implemenation</h2>
+<h2>Geograph Test / Reference Implementation</h2>
 
-This is a basic implementation of Geograph Website code. It's not a proper user-facing website,
-just enough code to test the backend services. We use this internaly to test our servers are capable of running the <a href="https://www.geograph.org.uk/">real Geograph website</a>.
+This is a basic implementation of Geograph Website code. It's <b>not</b> a proper user-facing website,
+just enough code to test the backend services. We use this internally to to document, and test our
+servers are capable of running the <a href="https://www.geograph.org.uk/">real Geograph website</a>.
 
 <hr>
 <style>ul li {padding:8px} </style>
 <ul>
 
-        <li><a href="https://svn.geograph.org.uk/viewsvn/?do=view&project=geograph&path=/branches/toy/README.txt">View README.txt</a></li>
+        <li><b><a href="https://svn.geograph.org.uk/viewsvn/?do=view&project=geograph&path=/branches/toy/README.txt">View README.txt</a></b></li>
+
+	<li><a href="capabilities.txt">List of Capabilities of this implementation</a></li>
 
 	<li><a href="test.php">Run Live Application Test</a><ul>
 
@@ -15,9 +18,12 @@ just enough code to test the backend services. We use this internaly to test our
 
 	</ul></li>
 
-	<li><a href="capabilities.txt">capabilities.txt</a></li>
-
-	<li><a href="source.php">View listing of files</a></li>
+	<li><b><a href="source.php">View listing of files in this implementation</a></b><ul>
+		<li>Key files:
+			<a href="https://svn.geograph.org.uk/viewsvn/?do=view&project=geograph&path=/branches/toy/libs/conf/example.conf.php">Application Config</a>,
+			<a href="https://svn.geograph.org.uk/viewsvn/?do=view&project=geograph&path=/branches/toy/public_html/test.php">Test Script</a>,
+			<a href="https://svn.geograph.org.uk/viewsvn/?do=view&project=geograph&path=/branches/toy/config/apache.vhost.conf">Apache Config</a>
+	</ul></li>
 
 	<li>Subversion Repository: <a href="https://svn.geograph.org.uk/svn/branches/toy/">https://svn.geograph.org.uk/svn/branches/toy/</a><ul>
 
@@ -30,7 +36,7 @@ just enough code to test the backend services. We use this internaly to test our
 
         </ul></li>
 
-	<li><a href="todo.php">todo list</a></li>
+	<li><a href="todo.php">Todo/Task List Progress</a></li>
 
 </ul>
 
