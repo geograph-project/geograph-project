76f572b74d90e56a4e07093b7adc73972de11ff1
barry <barry@eb6bd97b-b719-0410-a3e2-ef71b2e770e9>
2019-10-09 14:16:52 +0000

    limit scales

--- branches/british-isles/public_html/ol/geograph-openlayers.js
+++ branches/british-isles/public_html/ol/geograph-openlayers.js
@@ -48,9 +48,10 @@ function loadMapInner(mapdiv,skippermalink) { //NOTE: does not center the map, t
         OSAPIKey: "A493C3EB96133019E0405F0ACA6056E3", // Geograph's key key
         OSKeysUrl: 'http://'+window.location.host+'/', // Geograph's URL (escaped) registered against the key
         layerName: "Ordnance Survey GB",
-        resolutions: [1000, 500, 200, 100, 50, 25, 10, 5], // metres per pixel
-        products: ["OV1", "OV2", "MSR", "MS", "250KR", "250K", "50KR", "50K"] //product names
     });
+    olmap.layers['os'].resolutions = [1000, 500, 200, 100, 50, 25, 10, 5]; // metres per pixel
+    olmap.layers['os'].products = ["OV1", "OV2", "MSR", "MS", "250KR", "250K", "50KR", "50K"]; //product names
+    olmap.layers['os'].minResolution = 5;
 
 
     if (tileserver_default) { // this is set by nls-api.js
