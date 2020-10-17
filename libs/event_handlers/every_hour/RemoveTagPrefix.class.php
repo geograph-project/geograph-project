1dc313170420743d90ea81cf17f91279af3532b7
barry <1413156+barryhunter@users.noreply.github.com>
2020-10-17 22:57:10 +0100

    jsut avoid some notices

--- libs/event_handlers/every_hour/RemoveTagPrefix.class.php
+++ libs/event_handlers/every_hour/RemoveTagPrefix.class.php
@@ -1,57 +1,48 @@
 <?php
 /**
  * $Project: GeoGraph $
- * $Id: MaintainRecentlyModeratedList.class.php 3753 2007-09-05 19:34:21Z barry $
- *
+ * $Id: RebuildUserStats.class.php 3288 2007-04-20 11:32:27Z barry $
+ * 
  * GeoGraph geographic photo archive project
  * http://geograph.sourceforge.net/
  *
- * This file copyright (C) 2008 Barry Hunter (geo@barryhunter.co.uk)
- *
+ * This file copyright (C) 2005  Barry Hunter (geo@barryhunter.co.uk)
+ * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+/**
+*
+* @package Geograph
+* @author Barry Hunter <geo@barryhunter.co.uk>
+* @version $Revision: 3288 $
+*/
 
 require_once("geograph/eventhandler.class.php");
-require_once("geograph/gridsquare.class.php");
-
 
 //filename of class file should correspond to class name, e.g.  myhandler.class.php
-class ClearPostCacheDueToUpdate extends EventHandler
+class RemoveTagPrefix extends EventHandler
 {
 	function processEvent(&$event)
 	{
-		global $memcache;
+		$db=&$this->_getDB();
 
-		if ($memcache->valid) {
-			$db=&$this->_getDB();
-
-			$bits = explode(',',$event['event_param']);
-			$gridimage_id = $bits[0];
-
-			$posts = $db->getCol("select distinct post_id from gridimage_post where gridimage_id = $gridimage_id");
-
-			foreach ($posts as $post_id) {
-				//clear any caches involving this post
-				$memcache->name_delete('fp',$post_id);
-			}
-		}
+		$db->Execute("UPDATE IGNORE tag SET prefix='other' WHERE prefix IN ('type','bucket') AND canonical IS NULL");
 
 		//return true to signal completed processing
 		//return false to have another attempt later
 		return true;
 	}
 }
-
