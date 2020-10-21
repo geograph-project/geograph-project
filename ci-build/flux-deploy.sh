841e85cfdbd90e97e3428c91781c07b0225ed683
Chris Boot <crb@tiger-computing.co.uk>
2020-10-21 16:52:21 +0100

    Wait for images to be available before deploying them

--- ci-build/flux-deploy.sh
+++ ci-build/flux-deploy.sh
@@ -7,16 +7,35 @@
 set -eu
 
 COMPONENT="${CI_JOB_NAME#${CI_JOB_STAGE}/*}"
+WORKLOAD="${KUBE_NAMESPACE}:${FLUX_KIND}/${COMPONENT}"
 IMAGE_NAME="${CI_REGISTRY_IMAGE}/${GEO_REGION}/${FLUX_IMAGE:="${COMPONENT}"}"
 IMAGE_TAG=$CI_COMMIT_SHORT_SHA
 
-fluxctl list-workloads -n "${KUBE_NAMESPACE}"
+image_exists() {
+  fluxctl list-images --workload "${WORKLOAD}" 2>/dev/null | \
+    grep -q "\\<${IMAGE_TAG}\\>"
+}
 
-fluxctl list-images \
-  --workload "${KUBE_NAMESPACE}:${FLUX_KIND}/${COMPONENT}"
+echo "Deploying image ${IMAGE_NAME}:${IMAGE_TAG} ..."
+echo "  ... to ${WORKLOAD}."
+
+for i in $(seq 30 -1 1); do
+  # If the image exists, fall out of the loop
+  image_exists && break
+
+  if [ $i -gt 1 ]; then
+    echo "[ ... waiting for image to become available ... ]"
+    sleep 10
+  else
+    echo "The image hasn't appeared, giving up."
+    exit 1
+  fi
+done
+
+set -x
 
 fluxctl release \
-      --workload "${KUBE_NAMESPACE}:${FLUX_KIND}/${COMPONENT}" \
+      --workload "${WORKLOAD}" \
       --update-image "${IMAGE_NAME}:${IMAGE_TAG}" \
       --user "${GITLAB_USER_NAME} <${GITLAB_USER_EMAIL}>" \
       --message "Deployed by CI pipeline ${CI_PIPELINE_URL}" \
