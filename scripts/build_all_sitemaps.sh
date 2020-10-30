315b8260cdfa50e4bf225f30f93966008e70b9e8
barry <1413156+barryhunter@users.noreply.github.com>
2020-10-30 12:53:54 +0000

    sort out file to run cleanly in container

--- scripts/build_all_sitemaps.sh
+++ scripts/build_all_sitemaps.sh
@@ -1,55 +1,43 @@
 #!/bin/sh
-#the following is based on one used on geograph.org.uk - ideally could do with making general. 
 
 ############
 
-sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_sitemap.php --dir=/var/www/geograph_live --config=www.geograph.org.uk --images=0
-sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_sitemap.php --dir=/var/www/geograph_live --config=www.geograph.org.uk --normal=0 --geo=0 --secret=imagesitemap --per=10000
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap.xml > /dev/null
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap-geo.xml > /dev/null
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap-imagesitemap.xml > /dev/null
+/usr/bin/php $BASE_DIR/scripts/build_sitemap.php
+###/usr/bin/php $BASE_DIR/scripts/build_sitemap.php --normal=0 --images=1 --secret=$CONF_SITEMAP_SECRET --per=10000
 
-sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_usersitemap.php --dir=/var/www/geograph_live
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap-user.xml > /dev/null
+/usr/bin/php $BASE_DIR/scripts/build_usersitemap.php
 
-sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_contentsitemap.php --dir=/var/www/geograph_live
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap-content.xml > /dev/null
+/usr/bin/php $BASE_DIR/scripts/build_contentsitemap.php
 
-sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_user2sitemap.php --dir=/var/www/geograph_live
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap-usermap.xml > /dev/null
+/usr/bin/php $BASE_DIR/scripts/build_snippetsitemap.php
 
-sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_snippetsitemap.php --dir=/var/www/geograph_live
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap-snippet.xml > /dev/null
+if [ "$CLI_HTTP_HOST" == "www.geograph.org.uk" ]; then
+	/usr/bin/php $BASE_DIR/scripts/build_sitemap.php  --config=www.geograph.ie --ri=2 --suffix=.ie
+	###/usr/bin/php $BASE_DIR/scripts/build_sitemap.php  --config=www.geograph.ie --ri=2 --suffix=.ie --normal=0 --images=1 --secret=$CONF_SITEMAP_SECRET --per=10000
 
+	/usr/bin/php $BASE_DIR/scripts/build_usersitemap.ie.php  --config=www.geograph.ie
+fi
 
-sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_sitemap.php --dir=/var/www/geograph_live --config=www.geograph.ie --ri=2 --suffix=.ie --images=0
-sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_sitemap.php --dir=/var/www/geograph_live --config=www.geograph.ie --ri=2 --suffix=.ie --normal=0 --geo=0 --secret=imagesitemap --per=10000
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.ie%2Fsitemap.ie.xml > /dev/null
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.ie%2Fsitemap-geo.ie.xml > /dev/null
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.ie%2Fsitemap-imagesitemap.ie.xml > /dev/null
+#this pings ALL the sitemaps rebuilt above
+/usr/bin/php $BASE_DIR/scripts/ping-sitemaps.php execute
 
-sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_usersitemap.ie.php --dir=/var/www/geograph_live --config=www.geograph.ie
-GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.ie%2Fsitemap-user.ie.xml > /dev/null
+############
 
+/usr/bin/php $BASE_DIR/scripts/build_userhtml.php
 
 ############
 
 #these dont update that often
 
-#sudo -u www-data rm -f /var/www/geograph_live/public_html/kml/sitemap*.xml.gz
-
-#sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_kmlsitemap.php --dir=/var/www/geograph_live
-#GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fkml.geograph.org.uk%2Fkml%2Fsitemap.xml > /dev/null
+#rm -f $BASE_DIR/public_html/kml/sitemap*.xml.gz
+#/usr/bin/php $BASE_DIR/scripts/build_kmlsitemap.php
 
 ############
 
-#sudo -u www-data rm -f /var/www/geograph_live/public_html/sitemap/sitemap*.xml.gz
-
-#sudo -u www-data /usr/bin/php /var/www/geograph_live/scripts/build_htmlsitemap.php --dir=/var/www/geograph_live
-#GET http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fwww.geograph.org.uk%2Fsitemap%2Fsitemap.xml > /dev/null
+#rm -f $BASE_DIR/public_html/sitemap/sitemap*.xml.gz
+#/usr/bin/php $BASE_DIR/scripts/build_htmlsitemap.php
 
 ############
 
-echo ""
 echo ""
 echo ""
