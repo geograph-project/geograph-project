abfeaf99e2775831668648fe2fcad04a5e0746d2
barry <1413156+barryhunter@users.noreply.github.com>
2020-11-02 11:54:44 +0000

    Start of a new class for processing filesystem requests. based on the concept from toy

---
+++ libs/geograph/filesystem.class.php
@@ -0,0 +1,134 @@
+<?php
+
+/**
+ * $Project: GeoGraph $
+ * $Id: filesystem.class.php $
+ *
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2020 Barry Hunter (geo@barryhunter.co.uk)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/**
+ * Abstraction over the local filesystem, ultimately so can write files directly to S3 avoid posix/s3fs etc.
+ * Allows calling relatively 'standard' filesystem commands, but redirects them to use S3 API directly (in PHP)
+ * ... avoids need to use s3fs etc....
+ * which has several drawbacks, mainly that because uses posix standards, can result in double put on new files
+ * but also reading folders can be very slow (S3 doesnt have real folders, and s3fs tries to be compatible with idfferent standards, so uses lots of GET's lookinf for possible folders)
+ * plus of course we only implement functions we NEED. and avoid loading dedicated binary.
+ * finally, we can use memcache/redis for perofrmance, avoiding the fs based cahcing that s3fs does.
+ */
+
+require_once "3rdparty/S3.php";
+
+class FileSystem extends S3 {
+
+	var $SecurityToken = ''; //comes from meta-data
+	var $defaultStorage = self::STORAGE_CLASS_IT;
+
+	var $buckets = array( //no slash at start! at start of bucket paths. so put slash on folder to match
+		"/var/www/geograph_svn/public_html/" => 'uk-org-geograph-staging-photos/',
+		"/var/www/geograph_live/public_html/" => 'photos.geograph.org.uk/',
+		"/var/www/geograph_svn/rastermaps/" => 'attic.geograph.org.uk/rastermaps/',
+		"/var/www/geograph_live/rastermaps/" => 'attic.geograph.org.uk/rastermaps/',
+	);
+
+	function __construct() {
+		global $CONF;
+
+		if (empty($CONF['aws_role']))
+			return; //no more construction needed, the class can run without S3.
+
+		//todo, maybe fetch folder first to find the only creds available?
+			//$iam-role = file_get_contents("http://169.254.169.254/latest/meta-data/iam/security-credentials/");
+		$json = file_get_contents("http://169.254.169.254/latest/meta-data/iam/security-credentials/".$CONF['aws_role']);
+
+		// assume it never fails for now! tofix (use memcache/apc??)
+		$decode = json_decode($json,true);
+
+//todo, get buckets from $CONF ?
+
+		parent::__construct($decode['AccessKeyId'], $decode['SecretAccessKey'], false);
+
+			//curl http://169.254.169.254/latest/meta-data/placement/region --although we want the region of the bucket(s), not the current instance!
+		$this->setRegion('eu-west-1'); //can also pass this in construct, todo, shouoldnt be harcdoed. 
+
+		if (!empty($decode["Token"])) {
+			$this->setSignatureVersion('v4');
+			$this->SecurityToken = $decode["Token"];
+		}
+	}
+
+	function getBucketPath($filename) {
+		foreach ($this->buckets as $prefix => $bucket) {
+			if (strpos($filename,$prefix) === 0) {
+				$bits = explode("/",$bucket,2);
+				return array($bits[0],str_replace($prefix,$bits[1],$filename));
+			}
+		}
+		return array(null, $filename);
+	}
+
+	function copy($local, $destination, $acl = self::ACL_FULL_CONTROL, $storage = null) {
+
+		//normal filesystems will do this, but we need to do it. In case of S3, the dir wont exist, so can't actully use is_dir($dest) type logic!
+		if (substr($destination,-1) == '/')
+			$destination .= basename($local);
+
+
+		list($bucket, $destination) = $this->getBucketPath($destination);
+
+		if ($bucket) {
+			$headers = array();
+			if (!empty($this->SecurityToken)) {
+				//todo, maybe can put setting X-Amz-Security-Token on EVERY request in the parent class.
+				$headers['X-Amz-Security-Token'] = $this->SecurityToken;
+			}
+
+			//set the storage class
+			$storageClass = empty($storage)?$this->defaultStorage:$storage;
+			//small files, store as STANDARD rather than STANDARD_IA, as there is a minumum of 120kb. 50k is used, because IA is still 40% cost of STD
+			if ($storageClass == 'STANDARD_IA' && filesize($local) < 50000)
+				$storageClass = 'STANDARD';
+
+			//set the mtime, for compatiblity with s3fs etc
+			$headers['x-amz-meta-mtime'] = filemtime($local);
+
+			/* we cant use putObjectFile as it doesnt have storage class param!*/
+
+			//  putObject($input, $bucket, $uri, $acl = self::ACL_PRIVATE, $metaHeaders = array(), $requestHeaders = arr
+			return parent::putObject(self::inputFile($local), $bucket, $destination, $acl, array(), $headers, $storageClass);
+		} else {
+			//fall back basic filesystem operation
+			return copy($local, $destination);
+			//todo, do touch?
+		}
+	}
+
+	//executes command, appending the temporally fielname to the end. Its assumes the comand will write to the file, which is then copied/uploaded
+	function execute($cmd, $destination, $acl = self::ACL_FULL_CONTROL, $storage = null) {
+		$tmpfname = tempnam("/tmp", "FOO");
+		passthru($cmd.$tmpfname); //todo, maybe passthur not right version
+		if (filesize($tmpfname)) //dont bother copying empyu files - probably a command failure 
+			copy($tmpfname, $destination);
+
+		//always, delete, even if failed
+		unlink($tmpfname);
+	}
+
+}
+
+
---
+++ scripts/test-s3-class.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id: recreate_maps.php 2996 2007-01-20 21:39:07Z barry $
+ *
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 Barry Hunter (geo@barryhunter.co.uk)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+############################################
+
+$param = array('execute'=>0);
+
+chdir(__DIR__);
+require "./_scripts.inc.php";
+
+############################################
+
+$local = "/tmp/197537_f6908b09_213x160.jpg";
+$destination = "photos/19/75/197537_f6908b09_213x160.jpg"; //no slash at start!
+
+$destination .= ".test.jpg";
+
+############################################
+
+if (empty($filesystem)) //eventually gloabl will do this!
+	$filesystem = new FileSystem(); //sets up configuation automagically
+
+############################################
+
+$destination = $_SERVER['DOCUMENT_ROOT']."/".$destination;
+
+$r = $filesystem->getBucketPath($destination);
+
+print_r($r);
+
+if (empty($param['execute']))
+	exit;
+
+############################################
+
+$r = $filesystem->copy($local,$destination);
+
+print_r($r);
+
