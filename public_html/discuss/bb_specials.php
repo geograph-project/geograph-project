f6f0ff8adb07988e09c8e315342094017d72c958
barry <1413156+barryhunter@users.noreply.github.com>
2021-01-17 19:30:10 +0000

    just fix a notice

--- public_html/discuss/bb_specials.php
+++ public_html/discuss/bb_specials.php
@@ -12,11 +12,11 @@ $regUsrForums=array();
 
 
 $mkey = "userRanks2";
-$userRanks =& $memcache->get($mkey);
+$userRanks = $memcache->get($mkey);
 
 if(empty($userRanks) && $cols=db_simpleSelect(0,'user','user_id,rights,role','role != \'\' OR rights LIKE \'%admin%\' OR rights',' LIKE ','%moderator%')) {
 	$userRanks=array();
-	
+
 	do {
 		if ($cols[2])
 			$userRanks[$cols[0]]=$cols[2];
@@ -34,4 +34,4 @@ $mods=array();
 
 $themeDesc = array(3798);
 
-?>
+
