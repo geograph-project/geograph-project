cfca7a4a08567f21b63b170eb6269e879d927e4f
barry <1413156+barryhunter@users.noreply.github.com>
2021-02-21 13:39:06 +0000

    Better updates, rather than scanning whole archive

--- libs/event_handlers/every_day/RebuildGridimageDuplicate.class.php
+++ libs/event_handlers/every_day/RebuildGridimageDuplicate.class.php
@@ -45,22 +45,67 @@ class RebuildGridimageDuplicate extends EventHandler
 //Note this script, fines 'exact' duplicates using mysql GROUP BY, there is a seperate `scripts/duplicate_titles.php` that finds sequences
 //... that script creates the titles with # at end, so this script doesn't delete them!
 
-		$prefixes = $db->GetCol("select prefix from gridprefix where landcount > 0");
+
+		$status = $db->getRow("SHOW TABLE STATUS LIKE 'gridimage_duplicate'");
+
+                if (!empty($status['Update_time']) && strtotime($status['Update_time']) > (time() - 60*60*12) && $status['Comment'] != 'rebuild') {
+
+                        $seconds = time() - strtotime($status['Update_time']);
+                        $hours = ceil($seconds/60/60);
+                        $hours++; //just to be safe
+
+			if (true) { //experimental version doing it for single squares;
+
+				//insert into a temporally table, so can delete the squares that are updated
+				$db->Execute("CREATE TEMPORARY TABLE gridimage_duplicate_tmp LIKE gridimage_duplicate");
+
+				$max = $db->getOne("SELECT MAX(gridsquare_id) FROM gridsquare");
+				for($start=1;$start<=$max;$start+=10000) {
+					$end = $start + 9999;
+
+					$this->Execute("INSERT INTO gridimage_duplicate_tmp
+					SELECT grid_reference,title,COUNT(*) AS images,COUNT(DISTINCT user_id) AS users
+					 FROM gridsquare FORCE INDEX(PRIMARY)
+						INNER JOIN gridimage_search USING (grid_reference)
+				                 WHERE last_timestamp > date_sub(now(),interval $hours hour)
+					 AND gridsquare_id BETWEEN $start and $end
+					 GROUP BY grid_reference,title HAVING images > 1 ORDER BY NULL");
+				}
+
+				$this->Execute("DELETE gridimage_duplicate.* FROM gridimage_duplicate INNER JOIN gridimage_duplicate_tmp USING (grid_reference)
+					WHERE gridimage_duplicate.title NOT like '% #'");
+
+				$this->Execute("INSERT INTO gridimage_duplicate SELECT * FROM gridimage_duplicate_tmp");
+
+				return true;
+			}
+
+			//... otherwise fall back to doing it by myriad
+
+			//but we can at least only do the myriad that have updated!
+			//$prefixes = $db->GetCol("select substring(grid_reference,1,3 - reference_index) as prefix,max(last_timestamp) as last
+			//	 from gridsquare group by prefix having last > date_sub(now(),interval $hours hour)");
+
+			$prefixes = $db->GetCol("select prefix from gridprefix where landcount > 0 and last_submitted > date_sub(now(),interval $hours hour)");
+		} else {
+			$prefixes = $db->GetCol("select prefix from gridprefix where landcount > 0");
+		}
+
 
 		foreach ($prefixes as $prefix) {
+			//note, a prefix query on gridimage_search is not very efficent, should change this to use point_xy
 			$crit = $db->Quote("{$prefix}____");
 
-//print "$crit ... ";
 			$db->Execute("DELETE FROM gridimage_duplicate WHERE grid_reference LIKE $crit AND title NOT like '% #'");
-//print " deleted ...";
+
 			$db->Execute("INSERT INTO gridimage_duplicate
 				SELECT grid_reference,title,COUNT(*) AS images,COUNT(DISTINCT user_id) AS users FROM gridimage_search
 				WHERE grid_reference LIKE $crit	GROUP BY grid_reference,title HAVING images > 1 ORDER BY NULL");
-//print " inserted.\n";
+
 			//give the server a breather...
 			//sleep(10);
 		}
-//print "done\n";
+
 		//return true to signal completed processing
 		//return false to have another attempt later
 		return true;
