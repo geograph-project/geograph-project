4ba8acb634294fb1165dac9714381f6bf938552a
barry <1413156+barryhunter@users.noreply.github.com>
2021-03-03 15:54:13 +0000

    lets hobble

--- public_html/statistics/leaderthread_forum.php
+++ public_html/statistics/leaderthread_forum.php
@@ -52,10 +52,10 @@ $cacheid='statistics|leaderthread_forum'.$u.$type.'.'.$f;
 if (!$smarty->is_cached($template, $cacheid))
 {
         //lets hobble this!
-#        header("HTTP/1.1 503 Service Unavailable");
- #       $smarty->assign('searchq',stripslashes($_GET['q']));
-  #      $smarty->display('function_disabled.tpl');
-   #     exit;
+        header("HTTP/1.1 503 Service Unavailable");
+        $smarty->assign('searchq',stripslashes($_GET['q']));
+        $smarty->display('function_disabled.tpl');
+        exit;
 
         $db = GeographDatabaseConnection(true);
 
