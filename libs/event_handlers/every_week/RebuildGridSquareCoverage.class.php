c65926acfead9f53e17ba34c643f48a0eab8b388
barry <1413156+barryhunter@users.noreply.github.com>
2021-03-06 09:57:13 +0000

    actully need to update has_recent on gridsquare

--- libs/event_handlers/every_week/RebuildGridSquareCoverage.class.php
+++ libs/event_handlers/every_week/RebuildGridSquareCoverage.class.php
@@ -37,8 +37,6 @@ class RebuildGridSquareCoverage extends EventHandler
 {
 	function processEvent(&$event)
 	{
-		//perform actions
-		
 		$db=&$this->_getDB();
 		
 		$db->Execute("DROP TABLE IF EXISTS gridsquare_coverage_tmp");
@@ -51,7 +49,7 @@ class RebuildGridSquareCoverage extends EventHandler
 				count(*) as images,
 				(count(distinct nateastings DIV 100, natnorthings DIV 100) - (sum(nateastings = 0) > 0) ) as centis,
 				(count(distinct nateastings DIV 500, natnorthings DIV 500) - (sum(nateastings = 0) > 0) ) as quads,
-				has_recent,
+				IF(SUM(imagetaken > DATE(DATE_SUB(NOW(), INTERVAL 5 YEAR)) AND moderation_status='geograph')>0,1,0) AS has_recent,
 				has_geographs,
 				sum(ftf between 1 and 4) as points,
 				sum(nateastings = 0) as nocenti,
@@ -62,8 +60,16 @@ class RebuildGridSquareCoverage extends EventHandler
 			where moderation_status in ('accepted','geograph') 
 			group by gridsquare_id"); 
 		
-		$db->Execute("ALTER TABLE gridsquare_coverage_tmp ADD KEY (`hectad`)");
-		
+		$db->Execute("ALTER TABLE gridsquare_coverage_tmp ADD KEY (`hectad`),ADD KEY (`grid_reference`)");
+
+		//havent now computed a complete 'has_recent' we can update the gridsquare table
+		//as the main table is updated when new images added to square etc, it wont 'expire' has_recent, if there are no new submissions!		
+		$db->Execute("
+		update gridsquare inner join gridsquare_coverage_tmp using (grid_reference)
+		set gridsquare.has_recent = 0
+		 where gridsquare_coverage_tmp.has_recent = 0 and gridsquare.has_recent =1");
+
+
 		$db->Execute("DROP TABLE IF EXISTS gridsquare_coverage");
 		$db->Execute("RENAME TABLE gridsquare_coverage_tmp TO gridsquare_coverage");
 		
@@ -73,3 +79,4 @@ class RebuildGridSquareCoverage extends EventHandler
 	}
 	
 }
+
