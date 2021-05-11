dec7c6d1b02b603d4ba03407a3679def4e1880a8
barry <1413156+barryhunter@users.noreply.github.com>
2021-05-11 13:51:55 +0000

    Tidy a bit to put in Cron

--- scripts/square-cluster.php
+++ scripts/square-cluster.php
@@ -1,7 +1,7 @@
 <?
 
 //these are the arguments we expect
-$param=array('execute'=>0,'count'=>0,'square'=>'NS5965', 'limit'=>10000, 'query'=>'', 'debug'=>false,'sleep'=>0);
+$param=array('execute'=>0,'count'=>0,'square'=>'NS5965', 'limit'=>1000, 'query'=>'', 'debug'=>false,'sleep'=>0);
 
 chdir(__DIR__);
 require "./_scripts.inc.php";
@@ -13,6 +13,9 @@ $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 
 	require '3rdparty/Carrot2.class.php';
 	$carrot = Carrot2::createDefault();
+
+if (posix_isatty(STDOUT) && !$param['debug'])
+	$param['debug']=1;
 
 ############################################
 
@@ -53,7 +56,12 @@ foreach ($squares as $square => $gridsquare_id) {
 	}
 	$recordSet->Close();
 
-	$c = $carrot->clusterQuery($param['query'],$param['debug']);
+	$c = $carrot->clusterQuery($param['query'],$param['debug']==='2');
+	if (empty($c)) {
+		die("no results for $square (dieing without processing any more squares)\n");
+	}
+
+
 	usort($c, "cmp");
 
 	if (!$param['execute']) {
@@ -65,16 +73,20 @@ foreach ($squares as $square => $gridsquare_id) {
 		exit;
 	}
 
-	print "found ".count($c)." clusters for $square\n";
+	if ($param['debug'])
+		print "found ".count($c)." clusters for $square\n";
 
 	######################
 
 	$db->Execute("delete gridimage_group.* from gridimage inner join gridimage_group using (gridimage_id) where gridsquare_id = $gridsquare_id and source='carrot2'");
-	print "clear1\n";
+	if ($param['debug'])
+		print "clear1\n";
 
 	foreach ($c as $cluster) {
-		$count = count($cluster->document_ids);
-		printf("%5d. %s ",$count,$cluster->label);
+		if ($param['debug']) {
+			$count = count($cluster->document_ids);
+			printf("%5d. %s ",$count,$cluster->label);
+		}
 
 		$values = array();
 		foreach ($cluster->document_ids as $sort_order => $document_id) {
@@ -93,18 +105,16 @@ foreach ($squares as $square => $gridsquare_id) {
 			$values[] = "(".implode(',',$updates).")";
 		}
 		$sql = "INSERT INTO gridimage_group (`".implode('`,`',array_keys($updates))."`) VALUES ".implode(',',$values);
-
-		//print "$sql\n";exit;
 		$db->Execute($sql);
-		print ".. ".$db->Affected_Rows()." affected";
-
-		print "\n";
+		if ($param['debug'])
+			print ".. ".$db->Affected_Rows()." affected\n";
 	}
 
 	######################
 
 	$db->Execute("delete from gridimage_group_stat where grid_reference = ".$db->Quote($param['square']));
-	print "clear2";
+	if ($param['debug'])
+		print "clear2";
 
 	//copied almost as is from RebuildGridimageGroupStat.class.php (just changed the where clause!)
         $sql = "
@@ -120,7 +130,8 @@ foreach ($squares as $square => $gridsquare_id) {
 		//TODO, mariadb, supports LIMIT in group_concat! so GROUP_CONCAT(submitted ORDER BY submitted LIMIT 1,1) AS `second` should work!!
 
 	$db->Execute("INSERT INTO gridimage_group_stat $sql");
-	print " grouped\n";
+	if ($param['debug'])
+		print " grouped\n";
 
 	######################
 
