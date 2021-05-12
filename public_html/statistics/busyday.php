947aee683407bce308e21096efea49bfc1b5b9aa
barry <1413156+barryhunter@users.noreply.github.com>
2021-05-12 10:04:06 +0000

    convert to use new material view (fixing notices at the same time)

--- public_html/statistics/busyday.php
+++ public_html/statistics/busyday.php
@@ -2,20 +2,20 @@
 /**
  * $Project: GeoGraph $
  * $Id: busyday.php 6824 2010-09-15 19:50:33Z geograph $
- * 
+ *
  * GeoGraph geographic photo archive project
  * This file copyright (C) 2005 Barry Hunter (geo@barryhunter.co.uk)
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -23,8 +23,6 @@
 
 require_once('geograph/global.inc.php');
 init_session();
-
-
 
 
 $smarty = new GeographPage;
@@ -45,30 +43,29 @@ $ri = (isset($_GET['ri']) && is_numeric($_GET['ri']))?intval($_GET['ri']):0;
 
 $u = (isset($_GET['u']) && is_numeric($_GET['u']))?intval($_GET['u']):0;
 
-$date = (isset($_GET['date']) && ctype_lower($_GET['date']))?intval($_GET['date']):'submited';
+$date = (isset($_GET['date']) && ctype_lower($_GET['date']))?intval($_GET['date']):'imagetaken';
 
 
 $cacheid='statistics|busyday'.isset($_GET['users']).$date.'.'.$ri.'.'.$u;
 
 if (!$smarty->is_cached($template, $cacheid))
 {
-        //lets hobble this!
-        header("HTTP/1.1 503 Service Unavailable");
-        $smarty->assign('searchq',stripslashes($_GET['q']));
-        $smarty->display('function_disabled.tpl');
-        exit;
-
+	if ($date == 'submitted' && empty($u)) {
+	        //lets hobble this! (cant use a material-view)
+        	header("HTTP/1.1 503 Service Unavailable");
+	        $smarty->display('function_disabled.tpl');
+        	exit;
+	}
 
         $db = GeographDatabaseConnection(true);
 
-	$column = ($date == 'submitted')?'substring(submitted, 1, 10 )':'imagetaken';  
-	
-	
-	$title = ($date == 'submitted')?'Submitted':'Taken'; 
+	$column = ($date == 'submitted')?'substring(submitted, 1, 10 )':'imagetaken';
+
+	$title = ($date == 'submitted')?'Submitted':'Taken';
 	$title = "Busiest Day by $title Date";
 
 	$where = array();
-	
+
 	if (!empty($u)) {
 		$where[] = "user_id=".$u;
 		$smarty->assign('u', $u);
@@ -77,40 +74,55 @@ if (!$smarty->is_cached($template, $cacheid))
 		$smarty->assign_by_ref('profile', $profile);
 		$title .= " for ".($profile->realname);
 	} elseif (isset($_GET['users'])) {
+
+		header("Location: /statistics/groupby.php?distinct=takendays&groupby=auser_id&ri=$ri");
+		exit;
+
 		$group_sql = 'user_id,';
 		$column_sql = "CONCAT('<a href=\"/profile/',user_id,'\">',realname,'</a>') as User,";
 	}
-	
+
 	if ($ri) {
+		if (empty($u)) {
+	        	header("HTTP/1.1 503 Service Unavailable");
+		        $smarty->display('function_disabled.tpl');
+        		exit;
+		}
 		$where[] = "reference_index = $ri";
 		$smarty->assign('ri',$ri);
-		
+
 		$title .= " in ".$CONF['references_all'][$ri];
-	} 
-	
-	if (count($where))
+	}
+
+	if (count($where)) {
 		$where_sql = " AND ".join(' AND ',$where);
-		
-		
-	 $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
-	$table=$db->GetAll("
-	select 
-	$column_sql
-	DATE_FORMAT($column,'%d/%m/%Y') as Date,
-	count(*) as Images 
-	from gridimage_search 
-	where $column not like '%-00%' $where_sql
-	group by $group_sql$column 
-	order by Images desc limit 50;" );
-	
+
+		$table=$db->GetAll("
+		select
+		$column_sql
+		DATE_FORMAT($column,'%d/%m/%Y') as Date,
+		count(*) as Images
+		from gridimage_search
+		where $column not like '%-00%' $where_sql
+		group by $group_sql$column
+		order by Images desc limit 50" );
+	} else {
+		$table = $db->getAll("
+		select
+		DATE_FORMAT(imagetaken,'%d/%m/%Y') as Date,
+		images as Images
+		FROM imagetaken_stat
+		ORDER BY images desc limit 50" );
+	}
+
 	$title = count($table)." $title";
-	
+
 	$smarty->assign_by_ref('table', $table);
-	
+
 	$smarty->assign("h2title",$title);
 	$smarty->assign("total",count($table));
-	$smarty->assign_by_ref('references',$CONF['references_all']);	
-	
+	$smarty->assign_by_ref('references',$CONF['references_all']);
+
 	$extra = array();
 
 	foreach (array('users','date') as $key) {
@@ -118,7 +130,7 @@ if (!$smarty->is_cached($template, $cacheid))
 			$extra[$key] = $_GET[$key];
 		}
 	}
-	$smarty->assign_by_ref('extra',$extra);	
+	$smarty->assign_by_ref('extra',$extra);
 } else {
 	if ($u) {
 		$profile=new GeographUser($u);
@@ -130,5 +142,3 @@ $smarty->assign("filter",2);
 $smarty->assign("nosort",1);
 $smarty->display($template, $cacheid);
 
-	
-?>
