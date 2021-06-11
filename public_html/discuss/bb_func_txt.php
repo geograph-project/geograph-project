697f2b5a7626dd46b027181c12edebb3276d3be4
barry <1413156+barryhunter@users.noreply.github.com>
2021-06-11 16:37:47 +0000

    remove all the extra slashes, _smartQuote attempts to deal with them, but doesnt cope if there are other unescaped quotes (eg from bbcode) in the text

--- public_html/discuss/bb_func_txt.php
+++ public_html/discuss/bb_func_txt.php
@@ -78,13 +78,13 @@ $words[$i]=preg_replace_callback("/^(\!?)([STNH]?[A-Z]{1}|[A-Z]{3})(\d{2,10})([^
 
 
 if ($word!=$words[$i]) {} //we already made a conversion
-elseif(strtolower(substr($words[$i],0,strlen($_SERVER['HTTP_HOST'])+24))=='http://'.$_SERVER['HTTP_HOST'].'/mapbrowse.php?t=') {$c=1;$word='<a href=\"'.trim($words[$i]).'\" target=\"_new\" rel=\"nofollow\">Geograph Map</a>';}
-elseif(strtolower(substr($words[$i],0,7))=='http://') {$c=1;$word='<a href=\"'.trim($words[$i]).'\" target=\"_new\" rel=\"nofollow\">'.trim($word).'</a>';}
-elseif(strtolower(substr($words[$i],0,8))=='https://') {$c=1;$word='<a href=\"'.trim($words[$i]).'\" target=\"_new\" rel=\"nofollow\">'.trim($word).'</a>';}
-elseif(strtolower(substr($words[$i],0,6))=='ftp://') {$c=1;$word='<a href=\"'.trim($words[$i]).'\" target=\"_new\" rel=\"nofollow\">'.trim($word).'</a>';}
-elseif(strtolower(substr($words[$i],0,4))=='ftp.') {$c=1;$word='<a href=\"ftp://'.trim($words[$i]).'\" target=\"_new\" rel=\"nofollow\">'.trim($word).'</a>';}
-elseif(strtolower(substr($words[$i],0,4))=='www.') {$c=1;$word='<a href="http://'.trim($words[$i]).'\" target=\"_new\" rel=\"nofollow\">'.trim($word).'</a>';}
-elseif(strtolower(substr($words[$i],0,7))=='mailto:') {$c=1;$word='<a href=\"'.trim($words[$i]).'\" rel=\"nofollow\">'.trim($word).'</a>';}
+elseif(strtolower(substr($words[$i],0,strlen($_SERVER['HTTP_HOST'])+24))=='http://'.$_SERVER['HTTP_HOST'].'/mapbrowse.php?t=') {$c=1;$word='<a href="'.trim($words[$i]).'" target="_new" rel="nofollow">Geograph Map</a>';}
+elseif(strtolower(substr($words[$i],0,7))=='http://') {$c=1;$word='<a href="'.trim($words[$i]).'" target="_new" rel="nofollow">'.trim($word).'</a>';}
+elseif(strtolower(substr($words[$i],0,8))=='https://') {$c=1;$word='<a href="'.trim($words[$i]).'" target="_new" rel="nofollow">'.trim($word).'</a>';}
+elseif(strtolower(substr($words[$i],0,6))=='ftp://') {$c=1;$word='<a href="'.trim($words[$i]).'" target="_new" rel="nofollow">'.trim($word).'</a>';}
+elseif(strtolower(substr($words[$i],0,4))=='ftp.') {$c=1;$word='<a href="ftp://'.trim($words[$i]).'" target="_new" rel="nofollow">'.trim($word).'</a>';}
+elseif(strtolower(substr($words[$i],0,4))=='www.') {$c=1;$word='<a href="http://'.trim($words[$i]).'" target="_new" rel="nofollow">'.trim($word).'</a>';}
+elseif(strtolower(substr($words[$i],0,7))=='mailto:') {$c=1;$word='<a href="'.trim($words[$i]).'" rel="nofollow">'.trim($word).'</a>';}
 if ($c==1) $words[$i]=$word;
 //$words[$i]=str_replace ("\n ", "\n", $words[$i]);
 }
