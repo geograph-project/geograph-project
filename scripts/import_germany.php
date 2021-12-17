cc6f8fcb78045aba37f56db3c6b93fa063237332
barry <1413156+barryhunter@users.noreply.github.com>
2021-12-17 10:10:13 +0000

    tidy this up a bit

--- scripts/import_germany.php
+++ scripts/import_germany.php
@@ -10,7 +10,20 @@ $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 
 #####################################################
 
-$h = gzopen("http://geo.hlipp.de/dumps/images_max.json.gz",'r');
+$prefix = 'de:';
+$key = "geographkey_".trim($prefix,':');
+
+$table = "geograph_germany.gridimage_search";
+$urlprefix = "http://geo.hlipp.de/dumps/";
+
+#####################################################
+
+$url = "{$urlprefix}images_max.json.gz";
+
+if (!empty($CONF[$key]))
+     	$url .= "?key=".$CONF[$key];
+
+$h = gzopen($url,'r');
 while($h && !feof($h)) {
         $str = fgets($h);
 $str = str_replace("'",'"',$str);
@@ -18,24 +31,28 @@ $str = str_replace("'",'"',$str);
 	$max_id = intval($r['gridimage_id']);
 }
 
+if (empty($max_id))
+	die("Unable to fetch/decode images_max\n");
+
+#####################################################
 
 $db->Execute("SET NAMES utf8"); //dont know if this really enough!
 $c=0;
 for($start = 1; $start < $max_id; $start+=20000) {
-	$url = sprintf("http://geo.hlipp.de/dumps/images_combined.%08d.json.gz",$start);
+	$url = sprintf("{$urlprefix}images_combined.%08d.json.gz",$start);
 	print "$url\n";
 
+	if (!empty($CONF[$key]))
+        	$url .= "?key=".$CONF[$key];
+
 	$h = gzopen($url,'r');
-	//$h = gzopen("/tmp/images_combined.00140001.json.gz",'r');
-	//$h = gzopen("http://geo.hlipp.de/dumps/images_combined.json.gz",'r'); $max_id = 1;
-	//$h = gzopen("/tmp/images_combined.json.gz",'r'); $max_id = 1;
 
 	fseek($h,1); //skip over the opening [
 	while($h && !feof($h)) {
 		$str = fgets($h);
 		$r = json_decode(rtrim($str,"\n,]"),TRUE);
 
-		if (!$db->Execute($sql = 'REPLACE INTO geograph_germany.gridimage_search SET `'.implode('` = ?,`',array_keys($r)).'` = ?',array_values($r))) {
+		if (!$db->Execute($sql = 'REPLACE INTO '.$table.' SET `'.implode('` = ?,`',array_keys($r)).'` = ?',array_values($r))) {
 			print "$c. $str";
 			print_r($r);
 			print "$sql;\n\n";
@@ -52,12 +69,19 @@ for($start = 1; $start < $max_id; $start+=20000) {
 	print "$c.\n";
 }
 
-$h = gzopen("http://geo.hlipp.de/dumps/images_rejected.json.gz",'r');
+#####################################################
+
+$url = "{$urlprefix}images_rejected.json.gz";
+
+if (!empty($CONF[$key]))
+       	$url .= "?key=".$CONF[$key];
+
+$h = gzopen($url,'r');
 while($h && !feof($h)) {
         $str = fgets($h);
 	$r = json_decode($str,TRUE);
 
-        if (!$db->Execute($sql = 'DELETE FROM geograph_germany.gridimage_search WHERE gridimage_id in ('.implode(',',$r).')') ) {
+        if (!$db->Execute($sql = 'DELETE FROM '.$table.' WHERE gridimage_id in ('.implode(',',$r).')') ) {
                 print_r($r);
                 print "$sql;\n\n";
 
@@ -67,10 +91,16 @@ while($h && !feof($h)) {
 	print "deleted = ".$db->Affected_Rows()."\n";
 }
 
-$cmd = "sudo -u manticore indexer germany --rotate";
+#####################################################
 
-print "now run...\n";
-print "$cmd\n";
+//we can now mark them as 'needing' building in the database, which automatically triggers indexer!
+$db->Execute("update sph_server_index set last_indexed = '2000-01-01 00:00:00' where index_name = 'germany'");
+
+
+//$cmd = "sudo -u manticore indexer germany --rotate";
+
+//print "now run...\n";
+//print "$cmd\n";
 
 //todo run automatically?
 //if (hostname = 'tea') passthru($cmd); ??
