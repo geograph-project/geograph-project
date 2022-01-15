ac09978d875725a4143fb964187c5a8e672b99a6
barry <1413156+barryhunter@users.noreply.github.com>
2022-01-15 17:03:34 +0000

    fix

--- public_html/view.php
+++ public_html/view.php
@@ -302,7 +302,7 @@ if ($image->isValid())
 		}
 	}
 
-	if ($_SERVER['HTTP_HOST'] == 'development.geograph.org.uk' || $_SERVER['HTTP_HOST'] == 'www.geograph.org.uk' || $_SERVER['HTTP_HOST'] == 'www.geograph.ie' || $_SERVER['HTTP_HOST'] == 'staging.geograph.org.uk') { //this is mainly to exclude schools!
+	if ($_SERVER['HTTP_HOST'] == 'www.geograph.org.uk' || $_SERVER['HTTP_HOST'] == 'www.geograph.ie' || $_SERVER['HTTP_HOST'] == 'staging.geograph.org.uk') { //this is mainly to exclude schools!
 		//temporally patch, so that we can put set the margin, just like related.js WILL!
 		$smarty->assign('maincontentclass', 'content_photo'.$style.' photopage');
 	} else {
