03b4aeaec27c057b3af03b1cde8f26415632eb77
barry <1413156+barryhunter@users.noreply.github.com>
2022-04-25 16:37:01 +0100

    shouldnt show on all domains

--- libs/geograph/global.inc.php
+++ libs/geograph/global.inc.php
@@ -841,7 +841,7 @@ $str[] = "
 		$_GET['ads'] = 1;
 	}
 
-	if (!empty($_GET['ads'])) {
+	if (!empty($_GET['ads']) && $_SERVER['HTTP_HOST'] == 'www.geograph.org.uk') {
 		$str[] = '<script type="text/javascript"> var infolinks_pid = 3361577; var infolinks_wsid = 0; </script> <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>';
 	}
 
