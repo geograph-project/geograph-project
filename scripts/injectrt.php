76fe76e26e94a2b84493a06c070626fcd0d90fab
Barry <1413156+barryhunter@users.noreply.github.com>
2022-08-21 18:00:55 +0100

    Experiment with creating the RT definition from the plain index config

--- scripts/injectrt.php
+++ scripts/injectrt.php
@@ -30,6 +30,9 @@ $param=array(
 		'drop' => false,
 	'data'=>true,
 
+	'file'=>false, //read a sphinx.conf file and output similar index (overrides index, select, and $options)
+	'table'=>false, //read a table, and automatically create a select statement
+
 	'cluster' => 'manticore',
 	'index' => 'gridimage', // : as part of clusrter
 	'select' => "SELECT * FROM sphinx_view",
@@ -38,14 +41,18 @@ $param=array(
 	'limit' => 1, //if sepcified, myst be under 1000!
 );
 
+$multis = array();
+$options = array();
+
 #########################################################
+
+$cwdir = getcwd();
 
 chdir(__DIR__);
 require "./_scripts.inc.php";
 
-//bodge for now!
-$multis = array();
-if (strpos($param['select'], 'sphinx_view')) {
+//bodge for now! - could be rad from sample6.conf
+if (strpos($param['select'], 'sphinx_view') && empty($param['file']) && empty($param['table'])) {
 	//sql_attr_multi          = uint my_square from ranged-main-query; \
 	//        select gridimage_id, u.user_id from gridimage_search inner join user_gridsquare u using (grid_reference) WHERE  WHERE gridimage_id>=$start AND gridimage_id<=$end order by gridimage_id
 	$multis['my_square'] = 'select u.user_id from gridimage_search inner join user_gridsquare u using (grid_reference) WHERE gridimage_id = $id';
@@ -73,6 +80,89 @@ $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 
 #########################################################
 
+if (!empty($param['file'])) {
+	chdir($cwdir);
+	$text = file_get_contents($param['file']);
+
+	//the config file allow 'escaped new lines' - remove them for easier parsing
+	$text = str_replace("\\\n",' ',str_replace("\r",'',$text));
+
+	//this is a very rudimentry file parser, it wont cope with everything!
+	//and only one index per file for now!
+
+	if (preg_match_all('/sql_query\s*=(.+?)$/m',$text,$m)) {
+		foreach ($m[1] as $query) {
+			if (strpos($query,'sleep(')) //this is to skip the master query!
+				continue;
+			$param['select'] = trim(preg_replace('/\s+/',' ',$query));
+			break;
+		}
+	}
+
+	//todo, if sql_query_range then may need to correct the dynamic variables. 
+
+	//we DONT read the sql_attr* etc, just let them autodtect after running the query. 
+
+	if (preg_match('/^\s*index\s+(\w+)/m',$text,$m) && $param['index'] == 'gridimage') { //just looking if still the 'default!
+		$param['index'] = $m[1];
+	}
+
+	if (preg_match('/index\s+'.$param['index'].'\b(\s*:\s*\w+)?.*?\{(.+?)\}/s',$text,$m)) {
+		foreach(explode("\n", $m[2]) as $line) {
+			if (preg_match('/^\s*(\w+)\s*=\s*(.+)/',$line,$m) && !in_array($m[1],array('type','source','path','dict')))
+				$options[$m[1]] = trim($m[2]);
+		}
+        }
+}
+
+#########################################################
+
+if (!empty($param['table'])) {
+	$columns = $db->getAssoc("DESCRIBE {$param['table']}");
+	$cols = $sheets = array();
+	foreach ($columns as $name => $row) {
+		if ($row['Type'] == 'point') //its just a index for other cols
+			continue;
+		if (strpos($name,'_soundex') !== FALSE) //these are just 'index' cols as well
+			continue;
+		if (preg_match('/(lat|long)_(deg|min)/',$name) || $name == 'gmt') //the os gaz has lat/long in weird split format
+			continue;
+		if ($name == 'e_date' || $name == 'update_co') //no point indexing the date!
+			continue;
+fwrite(STDERR,"$name ".substr($row['Type'],0,40)."\n");
+
+		if (preg_match('/sheet_\d/',$name)) {
+			$sheets[] = $name;
+		} elseif (strpos($row['Type'],'int(') !== FALSE) {
+			if ($row['Key'] == 'PRI')
+				array_unshift($cols,"{$name} AS id"); //make sure it first in index, even if not first in table
+			else
+				$cols[] = $name; //probably want it as attribute!
+		} elseif (preg_match('/(\w+)_utf/',$name,$m)) {
+			if (($idx = array_search($m[1],$cols)) !== FALSE)
+				$cols[$idx] = "$name as {$m[1]}"; //replace it!
+			else
+				$cols[] = "$name as {$m[1]}";
+		} elseif (preg_match('/char\((\d+)/',$row['Type'],$m) || $row['Type'] == 'text' || preg_match('/enum\(/',$row['Type'],$m)) {
+			$cols[] = $name;
+		} else
+			fwrite(STDERR,"$name {$row['Type']} unknown\n");
+	}
+	if (!empty($sheets)) {
+		$cols[] = "concat_ws(',',nullif(".implode(",0),nullif(",$sheets).",0)) as sheet_ids";
+	}
+
+	$cols = implode(", ",$cols);
+	$param['select'] = "SELECT $cols FROM {$param['table']}";
+
+	fwrite(STDERR,"Generated: {$param['select']};\n");
+
+	if ($param['index'] == 'gridimage') //just looking if still the 'default!
+		$param['index'] = $param['table']; //create index of same name
+}
+
+#########################################################
+
 if (!empty($param['schema'])) {
 
 	$sql = $param['select'];
@@ -82,7 +172,7 @@ if (!empty($param['schema'])) {
 	if (!empty($param['drop'])) {
 		if (!empty($param['cluster']))
 			print "ALTER CLUSTER manticore DROP {$param['index']};\n";
-		print "DROP TABLE {$param['index']};\n";
+		print "DROP TABLE IF EXISTS {$param['index']};\n";
 	}
 
 	print "CREATE TABLE {$param['index']} ("; $sep = "\n";
@@ -94,8 +184,13 @@ if (!empty($param['schema'])) {
 		$name  = $r->name;
 			//the numberic is mysqli - todo, maybe swithc to MetaType?
 
-if ($name == 'snippets')
-	print_r($r);
+//if ($name == 'snippets')
+//	print_r($r);
+
+		$enc = mb_detect_encoding($row[$name], 'UTF-8, ISO-8859-15, ASCII');
+		if ($enc == 'ISO-8859-15')
+			fwrite(STDERR,"\n$name encoding = $enc\n");
+
 
 		$type = "text"; //defaults to 'indexed stored'
 		switch ($r->type) {
@@ -103,7 +198,7 @@ if ($name == 'snippets')
 			case 'blob':   case 252:
 			case 'mediumblob': case 250:
 			case 'longblob': case 251:
-				if ($name == 'comment') {
+				if ($name == 'comment' || $name == 'words' || $name == 'url' || $name=='tags') {
 					//leave a simple field - dont want attribute
 					$type = 'text'; //defaults to 'indexed stored'
 				} elseif (preg_match('/s$/',$name) && array_key_exists(preg_replace('/s$/','_ids',$name),$row)) { //isset doesnt find columns will null in them!
@@ -127,6 +222,11 @@ if ($name == 'snippets')
 				if (preg_match('/grlen$/',$name)) {
                         	        $type = 'bit(3)'; //stored as enum, so come out as strings, but really an interger
 					//todo - They are saved in 32-bit chunks, so in order to save space they should be grouped at the end of attributes definitions
+				} elseif ($name == 'source' && array_key_exists('asource',$row)) { //this is from the content table
+					$type = 'text indexed'; //no point storing, we have proper atttibute to get the value
+				} elseif (empty($r->binary)) {
+					//appears to be enum, comes as binary type, but not actully binary!
+					$type = 'string attribute indexed';
 				} else {
 					$type = "TODO-CHECK";
 				}
@@ -154,7 +254,7 @@ if ($name == 'snippets')
 				$type = 'integer'; //todo, bigint if unsigned!
 				break;
 			case 'bigint': case 8:
-				if ($name == 'viewsquare' || $name == 'submitted')
+				if (in_array($name,array('viewsquare','submitted','asource','updated','created')) || $r->unsigned || strpos($name,'timestamp') !== FALSE)
 					$type = 'integer'; //actully fine in this case
 				else
 					$type = 'bigint';
@@ -178,7 +278,13 @@ if ($name == 'snippets')
 	foreach($multis as $name => $query)
 		print "$sep\t`$name` multi";
 
-	print ");\n";
+	print ")";
+	if (!empty($options)) {
+		foreach ($options as $key => $value) {
+			print " $key=".$db->Quote($value);
+		}
+	}
+	print ";\n";
 	if (!empty($param['cluster']))
 		print "ALTER CLUSTER {$param['cluster']} ADD {$param['index']};\n";
 }
@@ -198,8 +304,8 @@ if (!empty($param['data'])) {
 			$limit = max(1000,$param['limit']);
 		} else {
 			$id = "id";
-			if (preg_match('/SELECT (\w+) AS id/i',$param['select'],$m))
-				$id = $m[1];
+			if (preg_match('/SELECT( SQL_NO_CACHE)? (\w+\.)?(\w+) AS id/i',$param['select'],$m))
+				$id = $m[3];
 
 			//otherwise loop though 1000 rows at a time...
 			if (preg_match('/\bWHERE\b/i',$param['select'])) {
@@ -216,7 +322,7 @@ if (!empty($param['data'])) {
 	        if (!mysqli_num_rows($result)) //todo, use show meta instead?
         	        break;
 
-		fwrite(STDERR,"-- dumping ".mysqli_num_rows($result)." rows\n\n");
+		fwrite(STDERR,"-- dumping ".mysqli_num_rows($result)." rows from $lastid\n\n");
 
 		$names=array();
 		$types=array();
@@ -251,13 +357,22 @@ if (!empty($param['data'])) {
 		while($row = mysqli_fetch_row($result)) {
 			print "$line(";
 			$sep = '';
+
+			//todo, would be to transform the data to utf8! (as done in latin1plus_encoded index)
 			foreach($row as $idx => $value) {
 				if ($types[$idx] == 'mva') //mva's need special treatment if importing into index
 					$value = "(".mysqli_real_escape_string($db->_connectionID,$value).")";
 				elseif (is_null($value))
 					$value = "''"; //doesnt support null!
-				elseif ($types[$idx] != 'int' && $types[$idx] != 'real') //Don't just use 'is_numeric', as inserting a number into string attribute, silently fails!
+				elseif ($types[$idx] != 'int' && $types[$idx] != 'real') { //Don't just use 'is_numeric', as inserting a number into string attribute, silently fails!
+					$enc = mb_detect_encoding($value, 'UTF-8, ISO-8859-15, ASCII');
+					if ($enc == 'ISO-8859-15') {//dont just blindly convert, as while MOST columns in database are latin1, not quite all!
+						$value = latin1_to_utf8($value);
+						fwrite(STDERR,"converted $name\n");
+					}
+
 					$value = "'".mysqli_real_escape_string($db->_connectionID,$value)."'";
+				}
 				print "$sep$value";
 				$sep = ',';
 			}
