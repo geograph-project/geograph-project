c5a2ba8993f89148ef6ebae14c76f6605e7530d7
Barry <1413156+barryhunter@users.noreply.github.com>
2022-10-25 22:13:24 +0100

    trival example to show whitelisting the page as responsive!

--- public_html/stuff/unicode.php
+++ public_html/stuff/unicode.php
@@ -4,6 +4,7 @@ require_once('geograph/global.inc.php');
 $db = GeographDatabaseConnection(true);
 
 $smarty = new GeographPage;
+$smarty->assign('responsive', true);
 $smarty->display('_std_begin.tpl');
 
 $recordSet = $db->Execute("select gridimage_id,title from gridimage_funny where title is not null order by  gridimage_id IN (1339706,320042,47189,495051,519472,97714,1049262,1036631) desc,reverse(gridimage_id) limit 500");
