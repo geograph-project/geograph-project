f544d7cc6be6f5a4c3852af96ba727aec42b21a6
Barry <1413156+barryhunter@users.noreply.github.com>
2022-11-22 11:07:21 +0000

    correct link

--- public_html/discuss/bb_func_ldisc.php
+++ public_html/discuss/bb_func_ldisc.php
@@ -57,7 +57,7 @@ if($cols=db_simpleSelect(0, "$Tt Tt left join geobb_lastviewed Tl on (Tt.topic_i
 		$dot = ($cols[11])?'topic_muted.gif?v2':'topic_updated.gif';
         	$topic_reverse = "<a href=\"{$indexphp}action=vpost&amp;forum={$forum}&amp;topic={$topic}&amp;post={$cols[10]}\"><img src=\"{$static_url}/img/$dot\" align=middle border=0 alt=\"Updated Since Last Visit\" height=\"10\" width=\"10\"></a>&nbsp;";
         } elseif (is_null($cols[9])) {
-        	$topic_reverse = "<a href=\"{$indexphp}action=vpost&amp;forum={$forum}&amp;topic={$topic}\"><img src=\"{$static_url}/img/topic_new.gif\" align=middle border=0 alt=\"New Since Last Visit\" height=\"6\" width=\"10\"></a>&nbsp;";
+        	$topic_reverse = "<a href=\"{$indexphp}action=vthread&amp;forum={$forum}&amp;topic={$topic}\"><img src=\"{$static_url}/img/topic_new.gif\" align=middle border=0 alt=\"New Since Last Visit\" height=\"6\" width=\"10\"></a>&nbsp;";
         }
         
         $topic_title=$cols[1];
