f1efc80d19d7f07d6d287164e2898f51a530dcce
Barry <1413156+barryhunter@users.noreply.github.com>
2022-11-30 16:46:00 +0000

    better to use user_gridsquare, rather than gridimage_search

--- public_html/statistics/first2square.php
+++ public_html/statistics/first2square.php
@@ -2,20 +2,20 @@
 /**
  * $Project: GeoGraph $
  * $Id: first2square.php 7375 2011-08-13 23:13:01Z barry $
- * 
+ *
  * GeoGraph geographic photo archive project
  * This file copyright (C) 2005 Barry Hunter (geo@barryhunter.co.uk)
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -23,9 +23,6 @@
 
 require_once('geograph/global.inc.php');
 init_session();
-
-
-
 
 $smarty = new GeographPage;
 
@@ -41,34 +38,22 @@ if (!$smarty->is_cached($template, $cacheid))
 	require_once('geograph/gridsquare.class.php');
 	require_once('geograph/imagelist.class.php');
 
-	$db = GeographDatabaseConnection(true); 
-	
-	$allsqaures=$db->GetAll("select user_id,realname ".
-	"from gridimage_search ".
-	"group by right(grid_reference,4) order by null");
-	
-	$topusers = array();
-	foreach($allsqaures as $idx=>$entry)
-	{
-		if (isset($topusers[$entry['user_id']])) {
-			$topusers[$entry['user_id']]['imgcount']++;
-		} else {
-			$topusers[$entry['user_id']] = $entry;
-			$topusers[$entry['user_id']]['imgcount'] = 1;
-		}
-	}
-	
-	function cmp($a, $b) 
+	$db = GeographDatabaseConnection(true);
+
+	$topusers = $db->GetAll("
+		select user_id,realname,count(*) as imgcount
+		from (select user_id from user_gridsquare group by right(grid_reference,4) order by null) t1
+		inner join user using (user_id) group by user_id");
+
+	function cmp($a, $b)
 	{
 	   if ($a['imgcount'] == $b['imgcount']) {
 	       return 0;
 	   }
 	   return ($a['imgcount'] > $b['imgcount']) ? -1 : 1;
 	}
-	
 	uasort($topusers, "cmp");
-	
-	
+
 	$i = 1; $lastimgcount = '?';
 	foreach($topusers as $idx=>$entry)
 	{
@@ -80,14 +65,12 @@ if (!$smarty->is_cached($template, $cacheid))
 		}
 		$i++;
 	}
-	
+
 	$smarty->assign_by_ref('topusers', $topusers);
-	
+
 	//lets find some recent photos
 	new RecentImageList($smarty);
 }
 
 $smarty->display($template, $cacheid);
 
-	
-?>
