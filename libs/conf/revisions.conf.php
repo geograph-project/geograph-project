3c743e51fa2c8e62d39af000572f3e81fe8c1057
Barry <1413156+barryhunter@users.noreply.github.com>
2022-12-08 11:48:39 +0000

    update revisions for new resource(s)

--- libs/conf/revisions.conf.php
+++ libs/conf/revisions.conf.php
@@ -98,5 +98,5 @@ $REVISIONS['/templates/new2/css/content.css']=24878839;
 $REVISIONS['/templates/r/css/content.css']=24878839;
 $REVISIONS['/js/jquery.storage.js']=111;
 $REVISIONS['/templates/resp/css/responsive.css']=53190789;
-$REVISIONS['/templates/resp/css/modification.css']=53190789;
+$REVISIONS['/templates/resp/css/modification.css']=77229245;
 ?>
