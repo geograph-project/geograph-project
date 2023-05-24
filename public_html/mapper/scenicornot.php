aca1c0c119dcc067440414f378e351ad84264b17
Barry <1413156+barryhunter@users.noreply.github.com>
2023-05-24 16:30:12 +0100

    New map rendering of ScenicOrNot Data

---
+++ public_html/mapper/scenicornot.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id: xmas.php 6235 2009-12-24 12:33:07Z barry $
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2005 Barry Hunter (geo@barryhunter.co.uk)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once('geograph/global.inc.php');
+init_session();
+
+$smarty = new GeographPage;
+
+
+
+$smarty->display('_std_begin.tpl');
+
+
+?>
+
+<h3>ScenicOrNot</h3>
+<p style=max-width:900px>Data from <a href="https://scenicornot.datasciencelab.co.uk/">ScenicOrNot</a> website.
+
+<ul>
+	<li>Plotting rated scenicness, Darker red is more scenic. Yellow less so.
+	<li> is based on one photo per 1km square, and is an effectively random image from the square.</li>
+	<li>Is a snapshot of votes from about 2015, the images where provided to ScenicOrNot early 2009, so squares without images at that time wont have images
+</ul>
+
+<div id="map" style="width:800px; height:800px; max-height:90vh; max-width:80vw;"></div>
+<div id="results"></div>
+
+        <link rel="stylesheet" type="text/css" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" />
+        <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" type="text/javascript"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.5.0/proj4.js"></script>
+        <script type="text/javascript" src="<? echo smarty_modifier_revision("/js/Leaflet.MetricGrid.js"); ?>"></script>
+        <script type="text/javascript" src="<? echo smarty_modifier_revision("/js/mappingLeaflet.js"); ?>"></script>
+
+        <script type="text/javascript">
+        var map = null ;
+        var issubmit = false;
+	var static_host = '<? echo $CONF['STATIC_HOST']; ?>';
+
+                                        function loadmap() {
+
+		//stolen from Leaflet.base-layers.js - alas that file no compatible with mappingLeaflet.js at the moment :(
+
+		var bounds = L.latLngBounds(L.latLng(49.863788, -13.688451), L.latLng(60.860395, 1.795260));
+		var layerAttrib='&copy; Geograph Project';
+                var layerUrl='https://t0.geograph.org.uk/tile/tile-coverage.php?z={z}&x={x}&y={y}';
+                var coverageCoarse = new L.TileLayer(layerUrl, {user_id: 0, minZoom: 5, maxZoom: 12, attribution: layerAttrib, bounds: bounds, opacity:0.6});
+                overlayMaps["Geograph Coverage"] = coverageCoarse;
+
+		layerAttrib='&copy; Geograph Project &amp; Scenic Or Not';
+                layerUrl='https://t0.geograph.org.uk/tile/tile-scenicornot.php?z={z}&x={x}&y={y}';
+		var sceniclayer =  new L.TileLayer(layerUrl, {user_id: 0, minZoom: 7, maxZoom: 10, attribution: layerAttrib, bounds: bounds, opacity:0.9});
+                overlayMaps["ScenicOrNot"] = sceniclayer;
+
+						setupBaseMap(); //creates the map, but does not initialize a view
+
+						//overlayMaps["Geograph Coverage"].addTo(map);
+						overlayMaps["ScenicOrNot"].addTo(map);
+
+						map.setView([53.593,-2.285],7);
+                                        }
+                                        AttachEvent(window,'load',loadmap,false);
+        </script>
+
+
+	<?
+
+
+$smarty->display('_std_end.tpl');
+
+
---
+++ public_html/tile/tile-scenicornot.php
@@ -0,0 +1,179 @@
+<?
+
+if ($_GET['z'] < 7) {
+//         require __DIR__."/tile-coverage-hectad.php";
+         exit;
+}
+
+if ($_GET['z'] > 11) {
+//         require __DIR__."/tile-coverage-large.php";
+         exit;
+}
+
+
+require_once('geograph/global.inc.php');
+ini_set('memory_limit', '128M');
+
+$maxspan = 10;
+
+//customExpiresHeader(empty($_GET['user_id'])?3600*24*3:3600*6,true);
+
+//https://github.com/LaurensRietveld/HeatMap/blob/master/googleMapUtility.php
+require_once ('3rdparty/googleMapUtilityClass.php');
+
+$g = new googleMapUtilityClass($_GET['x'], $_GET['y'], $_GET['z']);
+
+$b = $g->getTileRect();
+
+##long,lat,long,lat
+
+$xd = $b->width/8;
+$yd = $b->height/8;
+$bounds = array();
+$bounds[] = $b->x-$xd;
+$bounds[] = $b->y-$yd;
+$bounds[] = $b->x+$b->width+$xd;
+$bounds[] = $b->y+$b->height+$yd;
+
+########################################################################
+
+	//from public_html/stuff/squares.json.php
+
+
+$sql = array();
+$sql['wheres'] = array();
+
+        #### example: -10.559026590196122,46.59604915850878,7.514135843906623,54.84589681367314
+
+        $span = max($bounds[2] - $bounds[0],$bounds[3] - $bounds[1]);
+
+        $conv = new Conversions;
+
+        if ($span > $maxspan) {
+                $error = "Zoom in closer to the British Isles to see coverage details";
+
+        } elseif (true) {
+		//todo, could also do a wgs84 bbox filter, so could use a index in scenic_votes directly
+
+		//$sql['wheres'][] = sprintf('s.wgs84_lat  BETWEEN %.6f AND %.6f', $bounds[1], $bounds[3]);
+		//$sql['wheres'][] = sprintf('s.wgs84_long BETWEEN %.6f AND %.6f', $bounds[0], $bounds[2]);
+			//lat/long are DECIMAL which doesnt index well!
+
+		//worth creating a new spatial index on scenic_votes as it more selective than gi
+// ALTER TABLE scenic_votes ADD point_ll POINT NOT NULL;
+// UPDATE scenic_votes SET point_ll = GeomFromText(CONCAT('POINT(',wgs84_long,' ',wgs84_lat,')'));
+// ALTER TABLE scenic_votes ADD SPATIAL INDEX(point_ll);
+
+		list($west,$south,$east,$north) = $bounds;
+		$rectangle = "'POLYGON(($west $south,$east $south,$east $north,$west $north,$west $south))'";
+		$sql['wheres'][] = "CONTAINS(GeomFromText($rectangle),s.point_ll)";
+
+        } else {
+                $conv = new Conversions;
+
+                list($x1,$y1) = $conv->wgs84_to_internal(floatval($bounds[1]),floatval($bounds[0])); //bottom-left
+                list($x2,$y2) = $conv->wgs84_to_internal(floatval($bounds[3]),floatval($bounds[2])); //top-rigth
+
+//BODGE!
+	if ($x2 == 0 && $bounds[2] > 2.3 && $bounds[2] < 4) { //wgs84_to_national as a small boundary for GB!
+		$conv2 =  new ConversionsLatLong;
+
+		list($e,$n) = $conv2->wgs84_to_osgb36(floatval($bounds[3]),floatval($bounds[2]));
+		list($x2,$y2) = $conv2->national_to_internal($e,$n,1);
+	}
+
+		if ($x1 > -100 && $x1 < 1000) {
+	                $rectangle = "'POLYGON(($x1 $y1,$x2 $y1,$x2 $y2,$x1 $y2,$x1 $y1))'";
+        	        $sql['wheres'][] = "CONTAINS( GeomFromText($rectangle), point_xy)";
+
+		} else {
+			$error = "Zoom in closer to the British Isles to see coverage details";
+		}
+        }
+
+
+        $db = GeographDatabaseConnection(true);
+
+        $sql['tables'] = array();
+
+                $sql['tables']['gs'] = 'gridimage_search';
+                $sql['tables']['s'] = 'inner join scenic_votes s using (gridimage_id)';
+
+                $sql['columns'] = "x,y,reference_index as ri, average";
+
+        $query = sqlBitsToSelect($sql);
+
+        $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+
+if (!empty($_GET['dd'])) {
+	print_r($bounds);
+	print_r($query);
+	print_r($sql);
+	exit;
+}
+
+	if (empty($error))
+	        $rows = $db->getAll($query);
+
+
+########################################################################
+
+	header('Access-Control-Allow-Origin: *'); //needed for Google Earth, in general Leafet etc use std <img> that ignore CORS
+
+	$im = imagecreate(googleMapUtilityClass::TILE_SIZE,googleMapUtilityClass::TILE_SIZE);
+
+	$bg = imagecolorallocate($im, 255, 255, 255);
+	imagecolortransparent($im,$bg);
+	$fg = imagecolorallocate($im, 255, 0, 0); //marker/red
+	$land = imagecolorallocate($im, 117,255,101); //land/green!
+
+        $colours = getStaticColorKey($im);
+
+	if (!empty($error)) { //todo!
+
+		imagestring($im, 5, 0, 0, $error, $fg);
+
+		header('Content-type: image/png');
+		imagepng($im);
+		exit;
+	}
+
+	if (!empty($rows))
+        foreach ($rows as $idx => $row) {
+		$color = $colours[intval($row['average'])]; //$land;
+
+                $p1 = getPixCoord($row['x'],$row['y'],$row['ri']); //getPixCoord gives us location of bottom left corner.
+                $p2 = getPixCoord($row['x']+1,$row['y']+1,$row['ri']);
+
+		imagefilledrectangle($im, $p1->x,$p1->y, $p2->x,$p2->y, $color);
+	}
+
+	imagesavealpha($im, true);
+	header('Content-type: image/png');
+	imagepng($im);
+
+
+########################################################################
+
+
+function getStaticColorKey(&$img) {
+        $colour=array();
+
+        for ($p=1; $p<=10; $p++) {
+                switch (true) {
+                        case $p == 1: $r=255; $g=255; $b=0; break;
+                        case $p == 2: $r=255; $g=196; $b=0; break;
+                        case $p == 3: $r=255; $g=132; $b=0; break;
+                        case $p == 4: $r=255; $g=64; $b=0; break;
+                        case $p == 5: $r=225; $g=0; $b=0; break;
+                        case $p == 6: $r=200; $g=0; $b=0; break;
+                        case $p == 7: $r=168; $g=0; $b=0; break;
+                        case $p == 8: $r=136; $g=0; $b=0; break;
+                        case $p == 9: $r=112; $g=0; $b=0; break;
+                        case $p ==10: $r=80; $g=0; $b=0; break;
+                }
+                $colour[$p]=imagecolorallocate($img, $r,$g,$b);
+        }
+        return $colour;
+}
+
