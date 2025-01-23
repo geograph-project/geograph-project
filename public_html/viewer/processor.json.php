a4174cc2e65ec5178fabb24bc4d03191249c186c
Barry <1413156+barryhunter@users.noreply.github.com>
2025-01-23 15:31:49 +0000

    new processor

---
+++ public_html/viewer/processor.json.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id: record_vote.php 6944 2010-12-03 21:44:38Z barry $
+ * 
+ * GeoGraph geographic photo archive project
+ * This file copyright (C) 2008 Barry Hunter (geo@barryhunter.co.uk)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once('geograph/global.inc.php');
+
+init_session();
+
+if (isset($_GET['html'])) {
+        print "thanks - you may close this window";
+} else {
+	header("HTTP/1.0 204 No Content");
+	header("Status: 204 No Content");
+	header("Content-Length: 0");
+}
+
+flush();
+
+
+$db = GeographDatabaseConnection(false);
+
+$decode = json_decode(file_get_contents("php://input"),true);
+
+foreach($decode as $row) {
+
+	//todo, replace with REPLACE INTO - but only if create a UNIQUE(gridiamge,source)
+	$ins = "INSERT INTO gridimage_hash SET gridimage_id = ".intval($row['gridimage_id']);
+	foreach(array('source','ahash','dhash','phash','whash','chash') as $key)
+		if (!empty($row[$key]))
+			$ins .= ", `$key` = ".$db->Quote($row[$key]);
+
+	$db->Execute($ins);
+}
+
+
---
+++ public_html/viewer/processor.php
@@ -0,0 +1,157 @@
+<?
+
+require_once('geograph/global.inc.php');
+init_session();
+
+$smarty = new GeographPage;
+
+$USER->mustHavePerm("basic");
+
+$db = GeographDatabaseConnection(true);
+$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+
+
+//cant use a template, as the fake jquery geograph.js creates, as conflicts with imagehas
+//$smarty->display('_std_begin.tpl');
+
+$images = array();
+
+//include user_id just to make fastInit easy
+$sql = "select s.*,user_id from gridimage_search inner join gridimage_size s using (gridimage_id) left join gridimage_hash using (gridimage_id)
+ where user_id = {$USER->user_id} and auto_id is null limit 50";
+
+$data = $db->getAll($sql);
+
+if (empty($data))
+	die('No images to process - yay!');
+
+foreach($data as $row) {
+	$image = new GridImage();
+        $image->fastInit($row);
+//| gridimage_id | width | height | original_width | original_height | original_diff | user_id |
+
+	$path = $image->_getFullpath(false, true);
+
+	$images[] = array('gridimage_id'=>$row['gridimage_id'], 'source'=>'full', 'path'=> $path);
+
+	//we only bother with larger if 'diff' (otherwise the larger would just be same image - full CAME from original!
+	if ($row['original_width'] && $row['original_diff'] == 'yes') {
+		//... this is when we KNOW it different
+
+		$path = $image->getImageFromOriginal(640,640, true);
+		$images[] = array('gridimage_id'=>$row['gridimage_id'], 'source'=>'640px', 'path'=> $path);
+
+	//this is where DONT know, so the hashing we about to do will help know!
+	} elseif ($row['original_width'] && $row['original_diff'] == 'unknown') {
+		// complication is DONT want to go ahead and CREATE a 640px thumb, as its presence is a tell
+		$great = max($row['original_width'],$row['original_height']);
+		if ($great > 800) {
+			$path = $image->getImageFromOriginal(800,800, true);
+			$images[] = array('gridimage_id'=>$row['gridimage_id'], 'source'=>'800px', 'path'=> $path);
+		} else {
+			$path = $image->_getOriginalpath(false, true);
+			$images[] = array('gridimage_id'=>$row['gridimage_id'], 'source'=>'original', 'path'=> $path);
+		}
+	}
+}
+
+print "<p id=msg>Found ".count($images)." image(s) to process...</p><hr>";
+
+	print "<script>\n";
+	print "var images = ".json_encode($images, JSON_PARTIAL_OUTPUT_ON_ERROR).";\n";
+	print "delete window.$;\n"; //need to delete the fake jquery geograph.js creates, as conflicts with imagehash
+	print "window['$'] = undefined;\n";
+	print "</script>";
+
+?>
+
+<img id="img" crossorigin onerror="retryCross(this)"/>
+
+<script src="https://unpkg.com/imagehash-web/dist/imagehash-web.min.js"></script>
+<script>
+var img = null;
+var current = [];
+var results = [];
+
+
+function retryCross(that) {
+	//this function allows retry of tags with crossorigin. Note the query string doesnt do anything on the server, its just to bust the local browser cache (that might have the non-cors image cached)
+        if (that.src.indexOf('crossorigin') == -1 && that.hasAttribute('crossorigin')) {
+                that.src = that.src + '?crossorigin';
+		if (that.hasAttribute('srcset'))
+			that.srcset = that.srcset.replace(/\.jpg/g,'.jpg?crossorigin');
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	img = document.getElementById('img');
+
+	img.onload = function (event) {
+		let result = {};
+		result['gridimage_id'] = current['gridimage_id'];
+		result['source'] = current['source'];
+
+		ahash(img, 8).then(hash => {
+			result['ahash'] = hash.toHexString();
+		});
+		dhash(img, 8).then(hash => {
+			result['dhash'] = hash.toHexString();
+		});
+		phash(img, 8).then(hash => {
+			result['phash'] = hash.toHexString();
+		});
+		whash(img, 8).then(hash => {
+			result['whash'] = hash.toHexString();
+		});
+
+		/* cropResistantHash doesnt sem to work, doesnt get converted to canvas?
+		Uncaught (in promise) TypeError: Cannot assign to read only property 'Symbol(Symbol.toStringTag)' of object '#<HTMLCanvasElement>'
+		cropResistantHash(img).then(hash => {
+			result['chash'] = hash.toJSON();
+		}); */
+
+		results.push(result);
+
+		if (images.length) {
+			document.getElementById('msg').innerHTML = images.length+' remain to process';
+
+			current = images.shift();
+			img.src = current.path;
+		} else {
+			//NOTE, we CANT use jqyery here, conflicts with imagehash (both use $)
+			if ('sendBeacon' in navigator) {
+				document.getElementById('msg').innerHTML = 'Done all in current batch. Submitting...';
+				setTimeout(function() {
+					done = navigator.sendBeacon("/viewer/processor.json.php", JSON.stringify(results));
+					if (done) {
+						//with beacon, dont have to wait - should be queued, even if not sent immidately. 
+						//.. but we should wait, so it has been processed, otherwise will get the same images again!
+						setTimeout(function() {
+							window.location.reload();
+						}, 5000);
+					} else {
+						alert('unable to save results. please contact us!');
+					}
+				}, 1000); //should use await, but for now just to make sure results come back for last limage. 
+
+			} else {
+				alert('unable to save results. please contact us!');
+			}
+		}
+	}
+
+	if (images.length) {
+		current = images.shift();
+		img.src = current.path;
+	} else {
+		alert('no images to process??');
+	}
+});
+
+</script>
+
+<?
+
+
+//$smarty->display('_std_end.tpl',md($_SERVER['PHP_SELF']));
+
