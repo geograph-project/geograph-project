43ce17d158dcf7bc32a8ebc40e067c8547f53fa4
Barry <1413156+barryhunter@users.noreply.github.com>
2025-01-29 20:58:57 +0000

    need Access-Control-Allow-Origin to work from api subdomain

--- public_html/viewer/hashes.json.php
+++ public_html/viewer/hashes.json.php
@@ -5,7 +5,6 @@ require_once('geograph/global.inc.php');
 $db = GeographDatabaseConnection(true);
 $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 
-//$id = intval($_GET['id']);
 $token=new Token;
 if ($token->parse(@$_GET['t'])) {
 	$id = intval($token->getValue("id"));
@@ -25,11 +24,11 @@ $sql = array();
 	$sql['tables'][] = 'inner join gridimage_search gi using (gridimage_id)';
 
 	$sql['wheres'] = array();
-	$sql['wheres'][] = "gi.user_id = $id";
+	$sql['wheres'][] = "gi.user_id = $id"; //currently only returning live images - todo, would be to include the temp area images (plus pending?)
 	$sql['wheres'][] = "$hash != ''";
 
         $sql['columns'] = 'gridimage_id,substr(source,1,1),phash,grid_reference,imagetaken,title';
-	$sql['group'] = $hash;
+	$sql['group'] = $hash; //removes the duplicate hashes - particully multi size for same image
         $sql['order'] = 'NULL';
 
 ##############################################
@@ -41,6 +40,7 @@ $sql = array();
 customGZipHandlerStart();
 customExpiresHeader(3600*6,true);
 
+header('Access-Control-Allow-Origin: https://www.geograph.org.uk');
 header("Content-Type:application/json");
 print "[";
 
@@ -55,11 +55,6 @@ if ($count = $recordSet->RecordCount()) {
         while (!$recordSet->EOF) {
                 $r =& $recordSet->fields;
 		$r['gridimage_id'] = intval($r['gridimage_id']);
-/* $str = '';
-for($i=0;$i<strlen($r[$hash]);$i+=2) {
-	$str.=hex2bin(substr($r[$hash],$i,2));
-}
-print "b=".trim(base64_encode($str),'=')."\n"; */
 
                 print $sep.json_encode(array_values($recordSet->fields),JSON_PARTIAL_OUTPUT_ON_ERROR);
                 $recordSet->MoveNext();
