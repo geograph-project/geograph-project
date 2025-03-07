d96cfc5fbb64f0fabbab22d5ef1b9cdc41dcaf83
Barry <1413156+barryhunter@users.noreply.github.com>
2025-03-07 14:54:55 +0000

    add date

--- public_html/stuff/submissions.json.php
+++ public_html/stuff/submissions.json.php
@@ -40,7 +40,7 @@ if (!empty($_GET['since'])) {
 $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 
 //needs to use gridimage/gridsquare because may be pending images. but join in gridimage_search, as may already be moderated, which case have the lat/long ready to use!
-$sql = "select gridimage_id,g.submitted,gs.grid_reference,g.title,nateastings,natnorthings,natgrlen,gs.reference_index,wgs84_lat,wgs84_long
+$sql = "select gridimage_id,g.submitted,gs.grid_reference,g.title,nateastings,natnorthings,natgrlen,gs.reference_index,wgs84_lat,wgs84_long,g.imagetaken
 	from gridimage g
 		inner join gridsquare gs using (gridsquare_id)
 		left join gridimage_search gi using (gridimage_id)
@@ -68,7 +68,7 @@ if ($count = $recordSet->RecordCount()) {
 
 		$r['title'] = latin1_to_utf8($r['title']);
 
-		if (empty($r['wgs84_lat']) || $r['wgs84_lat'] < 1) {
+		if (empty($r['wgs84_lat']) || $r['wgs84_lat'] < 1) { //todo && !empty($r['nateastings']), because if 4fig subject, doesnt have nateastings!
 		        list($r['wgs84_lat'],$r['wgs84_long']) = $conv->national_to_wgs84($r['nateastings'],$r['natnorthings'],$r['reference_index']);
 		}
 
