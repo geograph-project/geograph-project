a5cd2fcf4dce83aff8e39bc39166e3ed1343e525
Barry <1413156+barryhunter@users.noreply.github.com>
2025-03-08 18:24:45 +0000

    update revisions for new resource(s)

--- libs/conf/revisions.conf.php
+++ libs/conf/revisions.conf.php
@@ -94,6 +94,6 @@ $REVISIONS['/viewer/files.js']=47585305;
 $REVISIONS['/viewer/viewer.js']=58500288;
 $REVISIONS['/viewer/style.css']=43561893;
 $REVISIONS['/viewer/exif.js']=48373910;
-$REVISIONS['/js/submission_utils.js']=25178959;
+$REVISIONS['/js/submission_utils.js']=79721039;
 $REVISIONS['/js/resizeWorker.js']=25178959;
 ?>
