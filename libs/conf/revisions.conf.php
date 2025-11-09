4f285ec1b551a5c34566a2d74329a9ff16d2a6b8
Barry <1413156+barryhunter@users.noreply.github.com>
2025-11-09 12:44:33 +0000

    update revisions for new resource(s)

--- libs/conf/revisions.conf.php
+++ libs/conf/revisions.conf.php
@@ -108,4 +108,5 @@ $REVISIONS['/ai/finder-demo.js']=85093851;
 $REVISIONS['/viewer/zoomer.js']=61635280;
 $REVISIONS['/js/Leaflet.ExpandControl.js']=90513379;
 $REVISIONS['/js/tags-selector.js']=25257510;
+$REVISIONS['/js/survey.js']=46351503;
 ?>
