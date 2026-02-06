314a3af397001d31447598242f64a52f522da97d
Barry <1413156+barryhunter@users.noreply.github.com>
2026-02-06 01:53:06 +0000

    allow cloudflare to be able to cache it

--- public_html/kml-superlayer.php
+++ public_html/kml-superlayer.php
@@ -1,4 +1,5 @@
 <?
   header("HTTP/1.0 404 Not Found");
   header("Status: 404 Not Found");
+  header("Cache-Control: max-age=360000"); //for cloudflare to be able to cache it!
 ?>The SuperLayer is now discontinued. Please contact us if have concerns.
