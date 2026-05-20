56b7468c7a789b5bee147d8fe1b3b00e9960d754
Barry <1413156+barryhunter@users.noreply.github.com>
2026-05-20 19:23:50 +0100

    update revisions for new resource(s)

--- libs/conf/revisions.conf.php
+++ libs/conf/revisions.conf.php
@@ -154,6 +154,6 @@ $REVISIONS['/curated/automated.css']=76102783;
 $REVISIONS['/curated/automated.js']=27367734;
 $REVISIONS['/js/vector.class.js']=67855777;
 $REVISIONS['/js/company-message.js']=63039136;
-$REVISIONS['/app/js/suggestions.libs.js']=77056735;
-$REVISIONS['/js/submit-suggestions.js']=77056735;
+$REVISIONS['/app/js/suggestions.libs.js']=59650308;
+$REVISIONS['/js/submit-suggestions.js']=59650308;
 ?>
