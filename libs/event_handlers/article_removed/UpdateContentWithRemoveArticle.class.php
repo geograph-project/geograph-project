18a9453f586ad14bf096608e32ddad6a5db2dfa8
Barry <1413156+barryhunter@users.noreply.github.com>
2026-06-06 22:15:44 +0100

    need to asyncronsoully delete the content reference

---
+++ libs/event_handlers/article_removed/UpdateContentWithRemoveArticle.class.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * $Project: GeoGraph $
+ * $Id: UpdateDiscussionCrossReferencesWithNewTopic.class.php 4204 2008-03-04 23:33:06Z barry $
+ *
+ * GeoGraph geographic photo archive project
+ * http://geograph.sourceforge.net/
+ *
+ * This file copyright (C) 2005 Paul Dixon (paul@elphin.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+require_once("geograph/eventhandler.class.php");
+require_once("geograph/gridsquare.class.php");
+
+//filename of class file should correspond to class name, e.g.  myhandler.class.php
+class UpdateContentWithRemoveArticle extends EventHandler
+{
+	function processEvent(&$event)
+	{
+		$article_id = $event['event_param'];
+
+		$db=&$this->_getDB();
+
+		$updates = array();
+		$updates[] = "`foreign_id` = {$article_id}";
+		$updates[] = "`source` = 'article'";
+
+		$db->Execute("DELETE FROM content WHERE ".implode(' AND ',$updates));
+
+		//return true to signal completed processing
+		//return false to have another attempt later
+		return true;
+	}
+}
+
