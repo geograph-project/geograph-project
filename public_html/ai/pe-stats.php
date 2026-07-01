7070ac3b31aaf97bcb5c82a33ece6c349b9bdf36
Barry <1413156+barryhunter@users.noreply.github.com>
2026-07-01 15:39:29 +0100

    silly typo

--- public_html/ai/pe-stats.php
+++ public_html/ai/pe-stats.php
@@ -37,7 +37,7 @@ $db = GeographDatabaseConnection(false);
 $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 
 	if (empty($db->readonly)) { //by running this, we can actully get accurate stat!
-		$db->Execute("SELECT @max_done := COALESCE(MAX(max_id), 0) FROM embedding_progress_clip");
+		$db->Execute("SELECT @max_done := COALESCE(MAX(max_id), 0) FROM embedding_progress_pe");
 		$db->Execute("INSERT INTO embedding_progress_pe (day, count, min_id, max_id, done) SELECT     substring(updated, 1, 10) AS day,     COUNT(*) AS new_count,     MIN(seq_id) AS new_min_id,     MAX(seq_id) AS new_max_id,     NULL AS done FROM     gridimage_embedding_1024 WHERE     type = 'image' AND model ='pe'    AND seq_id > @max_done  GROUP BY  day ON DUPLICATE KEY UPDATE     count = embedding_progress_pe.count + VALUES(count),      max_id = VALUES(max_id), `done`=NULL");
 	}
 
