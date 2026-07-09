7dbcb9980efdc048c4f5b4c5f5f9cad754ba3856
Barry <1413156+barryhunter@users.noreply.github.com>
2026-07-09 17:00:50 +0100

    move to new tinyurl API. free quota for now

--- public_html/stuff/getlink.php
+++ public_html/stuff/getlink.php
@@ -1,42 +1,53 @@
-<?
+<?php
+
+require_once('geograph/global.inc.php');
 
 if (!empty($_GET['url'])) {
-	if (!preg_match('/^https?:\/\/(m|www|schools)\.geograph\.(org\.uk|ie)\.?\/.+/',$_GET['url'])) {
-		die();
-	}
+        if (!preg_match('/^https?:\/\/(m|www|schools)\.geograph\.(org\.uk|ie)\.?\/.+/', $_GET['url'])) {
+                die();
+        }
 
-	##http://tinyurl.com/api-create.php?url=http://json-tinyurl.appspot.com/
+        // Setup the TinyURL API request
+        $apiUrl = 'https://api.tinyurl.com/create';
+        $authToken = $CONF['tinyurl_key'];
 
-	$short = file_get_contents("http://tinyurl.com/api-create.php?url=".urlencode($_GET['url']));
+        $postData = json_encode([
+                'url' => $_GET['url'],
+                'domain' => 'tinyurl.com' // You can change this to 'tiny.one' or your custom branded domain
+        ]);
 
-	if ($short) {
-		$res = array('ok'=>TRUE,'tinyurl'=>$short);
-	} else {
-		$res = array('ok'=>FALSE,'error'=>'unknown error');
-	}
+        $ch = curl_init($apiUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Authorization: Bearer ' . $authToken,
+                'Content-Type: application/json',
+                'Accept: application/json'
+        ]);
+
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        // Process response if HTTP status is 200 OK
+        if ($httpCode === 200 && $response) {
+                $responseData = json_decode($response, true);
+                if (isset($responseData['data']['tiny_url'])) {
+                        $res = array('ok' => true, 'tinyurl' => $responseData['data']['tiny_url']);
+                } else {
+                        $res = array('ok' => false, 'error' => 'unexpected API response payload');
+                }
+        } else {
+                // Read error payload from TinyURL if available, otherwise generic
+                $errorData = json_decode($response, true);
+                $errorMsg = isset($errorData['errors'][0]) ? $errorData['errors'][0] : 'API request failed';
+                $res = array('ok' => false, 'error' => $errorMsg . " (HTTP $httpCode)");
+        }
 
 } else {
-	$res = array('ok'=>FALSE,'error'=>'unknown url');
+        $res = array('ok' => false, 'error' => 'unknown url');
 }
 
-
-
-	if (isset($_GET['callback'])) {
-		$callback=preg_replace('/[^\w\.$]+/','',$_GET['callback']);
-		if (empty($callback)) {
-			$callback = "geograph_callback";
-		}
-
-		header('Content-type: application/x-javascript');
-
-		print "/**/{$callback}(";
-	} else {
-		header('Content-type: application/json');
-	}
-
-	print str_replace('-INF','0',json_encode($res));
-
-	if (!empty($callback))
-		print ");";
-
+outputJSON($res);
 
